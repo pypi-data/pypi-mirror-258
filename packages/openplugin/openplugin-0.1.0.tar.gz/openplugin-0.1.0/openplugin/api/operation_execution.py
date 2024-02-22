362d88274c890739f84531f836b1e2f0d0f86e1b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-22 03:03:48 +0000

    Add package openplugin/0.1.0/openplugin-0.1.0.tar.gz

---
+++ packages/openplugin/openplugin-0.1.0.tar.gz/openplugin-0.1.0/openplugin/api/operation_execution.py
@@ -0,0 +1,43 @@
+from fastapi import APIRouter, Depends
+from fastapi.responses import JSONResponse
+from fastapi.security.api_key import APIKey
+
+from openplugin.api import auth
+from openplugin.plugins.execution.operation_execution_impl import (
+    ExecutionException,
+    OperationExecutionImpl,
+)
+from openplugin.plugins.models import OperationExecutionParams
+
+# Create a FastAPI router instance
+router = APIRouter(
+    dependencies=[],
+    responses={404: {"description": "Not found"}},
+)
+
+
+@router.post("/operation-execution")
+def operation_execution(
+    params: OperationExecutionParams, api_key: APIKey = Depends(auth.get_api_key)
+):
+    try:
+        ex = OperationExecutionImpl(params)
+        response = ex.run()
+        return response
+    except ExecutionException as e:
+        return JSONResponse(
+            status_code=500,
+            content={
+                "message": "{}".format(e),
+                "metadata": e.metadata,
+            },
+        )
+    except Exception as e:
+        print(e)
+        return JSONResponse(
+            status_code=500,
+            content={
+                "message": "{}".format(e),
+                "metadata": {},
+            },
+        )
