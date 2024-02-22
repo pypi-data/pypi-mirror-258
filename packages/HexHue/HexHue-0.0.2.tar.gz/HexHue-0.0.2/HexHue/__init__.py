babeb9b3fe5f9d34b79197b5c94d8127821a685e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-22 05:03:10 +0000

    Add package HexHue/0.0.2/HexHue-0.0.2.tar.gz

---
+++ packages/HexHue/HexHue-0.0.2.tar.gz/HexHue-0.0.2/HexHue/__init__.py
@@ -0,0 +1,5 @@
+from .HexHue import Text, Back, Type, Hex
+
+__version__ = '0.0.2'
+
+__all__ = ['Text', 'Back', 'Type', 'Hex']
---
+++ packages/HexHue/HexHue-0.0.2.tar.gz/HexHue-0.0.2/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+import pathlib
+
+here = pathlib.Path(__file__).parent.resolve()
+
+long_description = (here / "README.md").read_text(encoding="utf-8")
+setup(
+    name="HexHue",
+    version="0.0.2",
+    description="A simple Windows Terminal ANSI Escape Code Package",
+    author="aftxrlifx",
+    packages=find_packages(),
+    python_requires=">=3.7",
+    long_description=long_description,
+    long_description_content_type="text/markdown"
+)
