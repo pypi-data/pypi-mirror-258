d3ce227bd8841f06a632e7c7c3a7986de531a3f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-22 17:03:51 +0000

    Add package postmd/0.0.3/postmd-0.0.3.tar.gz

---
+++ packages/postmd/postmd-0.0.3.tar.gz/postmd-0.0.3/README.md
@@ -0,0 +1,16 @@
+This is a simple package for post-processing of data from LAMMPS.
+
+Now it contains the following parts:
+
+- AveTime.py
+- AveChunk.py
+- GreenKubo.py
+- FileOperation.py
+- MDData.py
+- utils.py
+
+## Install
+You can install this package using pip.
+```shell
+pip install PostMD
+```
---
+++ packages/postmd/postmd-0.0.3.tar.gz/postmd-0.0.3/tests/test_greenkubo.py
@@ -0,0 +1,86 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+'''
+@Author  :   Shusong Zhang
+@Email   :   sszhang@mail.nwpu.edu.cn, zhangshusong789@gmail.com
+@File    :   test_greenkubo.py
+@Time    :   2023/04/27 15:34:34
+'''
+
+import matplotlib.pyplot as plt
+import os
+import scipy.constants as C
+os.chdir(os.path.dirname(__file__))
+
+from PostMDtest.GreenKubo import *
+
+e=C.e
+
+# the acf data, included two forms: 
+# one is the raw data of friction force at each step of CNT, in file "friction.txt"
+# another is the autocorrelated data of friction force by 'fix ave/correlate', in file "friction_acf_overwriting.txt"
+raw_data_path=r"./data/friction.txt"
+acf_data_path=r"./data/friction_acf_overwriting.txt"
+
+
+
+
+## ================ For raw data ===================
+unit_trans = e/1e-10  # friction force的单位是eV/Ang
+mywork=GreenKubo()
+mywork.read_file(path=raw_data_path, name_line=2)
+mywork.cal_acf(data_type="raw",col=3,nlag=2000,unit_trans=unit_trans)
+plt.figure()
+plt.plot(mywork.nlag,mywork.acf)
+plt.title("computed from raw data")
+plt.xlabel("nlag")
+plt.ylabel("acf")
+plt.legend()
+
+
+A = 2*np.pi*2.335e-10*204.48e-10
+kB = C.Boltzmann
+T = 298
+plt.figure()
+mywork.integrate_acf(method="simpson")
+plt.plot(mywork.nlag, mywork.int_acf/(A*kB*T), label="simpson method")
+mywork.integrate_acf(method="trap")
+plt.plot(mywork.nlag, mywork.int_acf/(A*kB*T), label="trap method")
+plt.xlabel("nlag")
+plt.ylabel("integration of acf")
+plt.title("computed from raw data")
+plt.legend()
+
+
+
+
+## ================ For autocorrelation data ==================
+unit_trans = e/1e-10  # friction force的单位是eV/Ang
+mywork=GreenKubo()
+mywork.read_file(path=acf_data_path, name_line=3, skiprows=4)
+mywork.cal_acf(data_type="acf",col=5,nlag_col=1,unit_trans=unit_trans)
+plt.figure()
+plt.plot(mywork.nlag,mywork.acf)
+plt.title("computed from acf data")
+plt.xlabel("nlag")
+plt.ylabel("acf")
+plt.legend()
+
+
+A = 2*np.pi*2.335e-10*204.48e-10
+kB = C.Boltzmann
+T = 298
+plt.figure()
+mywork.integrate_acf(method="simpson")
+plt.plot(mywork.nlag, mywork.int_acf/(A*kB*T), label="simpson method")
+mywork.integrate_acf(method="trap")
+plt.plot(mywork.nlag, mywork.int_acf/(A*kB*T), label="trap method")
+plt.xlabel("nlag")
+plt.ylabel("integration of acf")
+plt.title("computed from acf data")
+plt.legend()
+plt.show()
+
+
+# You will find the output is same for both acf and raw data, because these two files are generated from the same MD simulation.
+# This means you can use "fix ave/correlate" to process acf on the fly, or calculate acf by post-processing the raw data.
