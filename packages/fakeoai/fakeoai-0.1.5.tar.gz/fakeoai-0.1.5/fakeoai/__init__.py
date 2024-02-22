62d7c3d244265c3ec272a010415e137620b66b5f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-22 16:04:51 +0000

    Add package fakeoai/0.1.5/fakeoai-0.1.5.tar.gz

---
+++ packages/fakeoai/fakeoai-0.1.5.tar.gz/fakeoai-0.1.5/fakeoai/__init__.py
@@ -0,0 +1,3 @@
+__version__ = '0.1.5'
+
+from .server import FakeOAI as FakeOAI
---
+++ packages/fakeoai/fakeoai-0.1.5.tar.gz/fakeoai-0.1.5/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup
+
+from fakeoai import __version__
+
+with open('README.md', 'r', encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='fakeoai',
+    version=__version__,
+    python_requires='>=3.7',
+    author='baobao',
+    author_email='yuanbao@fakeopenai.cn',
+    keywords='OpenAI ChatGPT',
+    description='A Package for local deployment of ChatGPT.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/FakeOpenAI/FakeOpenAI',
+    packages=['fakeoai'],
+    package_dir={'fakeoai': 'fakeoai'},
+    package_data={"fakeoai": ["flask/**"]},
+    include_package_data=True,
+    install_requires=[
+        "flask",
+        "python-dotenv",
+        "requests"
+    ]
+)
