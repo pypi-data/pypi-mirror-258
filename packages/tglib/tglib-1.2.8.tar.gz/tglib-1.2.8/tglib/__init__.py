72114ab91e9b60e6b345e9933dc8c28f4cc0ff42
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-22 15:04:40 +0000

    Add package tglib/1.2.8/tglib-1.2.8.tar.gz

---
+++ packages/tglib/tglib-1.2.8.tar.gz/tglib-1.2.8/tglib/__init__.py
@@ -0,0 +1,21 @@
+#!/bin/python3
+
+__version__ = '1.2.8'
+VERSION = __version__
+
+__all__ = [
+    '__version__',
+    'VERSION',
+    'Client',
+    'NextManager',
+    'TelegramError',
+    'MARKDOWN_ESCAPES'
+]
+
+from .client import (
+    Client,
+    NextManager,
+    TelegramError
+)
+
+from .utils import MARKDOWN_ESCAPES
