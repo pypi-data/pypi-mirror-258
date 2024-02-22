77eeb6090147ee2e7c01f77b84a507a6c5f781e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-22 03:04:11 +0000

    Add package sentry-kafka-schemas/0.1.55/sentry_kafka_schemas-0.1.55-py2.py3-none-any.whl

---
+++ packages/sentry-kafka-schemas/sentry_kafka_schemas-0.1.55-py2.py3-none-any.whl/sentry_kafka_schemas/schema_types/ingest_performance_metrics_v1.py
@@ -0,0 +1,78 @@
+from typing import List, Literal, Dict, Union, TypedDict, Any
+from typing_extensions import Required
+
+
+class IngestMetric(TypedDict, total=False):
+    """ ingest_metric. """
+
+    org_id: Required[int]
+    """
+    The organization for which this metric is being sent.
+
+    minimum: 0
+    maximum: 18446744073709551615
+
+    Required property
+    """
+
+    project_id: Required[int]
+    """
+    The project for which this metric is being sent.
+
+    minimum: 0
+    maximum: 18446744073709551615
+
+    Required property
+    """
+
+    name: Required[str]
+    """
+    The metric name. Relay sometimes calls this an MRI and makes assumptions about its string shape, and those assumptions also exist in certain queries. The rest of the ingestion pipeline treats it as an opaque string.
+
+    Required property
+    """
+
+    type: Required["_IngestMetricType"]
+    """
+    The metric type. [c]ounter, [d]istribution, [s]et. Relay additionally defines Gauge, but that metric type is completely unsupported downstream.
+
+    Required property
+    """
+
+    timestamp: Required[int]
+    """
+    The timestamp at which this metric was being sent. Relay will round this down to the next 10-second interval.
+
+    minimum: 0
+    maximum: 18446744073709551615
+
+    Required property
+    """
+
+    tags: Required[Dict[str, str]]
+    """ Required property """
+
+    value: Required[Union[str, Union[int, float], Dict[str, Any], List[Any], bool, None]]
+    """ Required property """
+
+    retention_days: Required[int]
+    """
+    minimum: 0
+    maximum: 65535
+
+    Required property
+    """
+
+
+
+_IngestMetricType = Union[Literal['c'], Literal['d'], Literal['s'], Literal['g']]
+""" The metric type. [c]ounter, [d]istribution, [s]et. Relay additionally defines Gauge, but that metric type is completely unsupported downstream. """
+_INGESTMETRICTYPE_C: Literal['c'] = "c"
+"""The values for the 'The metric type. [c]ounter, [d]istribution, [s]et. Relay additionally defines Gauge, but that metric type is completely unsupported downstream' enum"""
+_INGESTMETRICTYPE_D: Literal['d'] = "d"
+"""The values for the 'The metric type. [c]ounter, [d]istribution, [s]et. Relay additionally defines Gauge, but that metric type is completely unsupported downstream' enum"""
+_INGESTMETRICTYPE_S: Literal['s'] = "s"
+"""The values for the 'The metric type. [c]ounter, [d]istribution, [s]et. Relay additionally defines Gauge, but that metric type is completely unsupported downstream' enum"""
+_INGESTMETRICTYPE_G: Literal['g'] = "g"
+"""The values for the 'The metric type. [c]ounter, [d]istribution, [s]et. Relay additionally defines Gauge, but that metric type is completely unsupported downstream' enum"""
+
