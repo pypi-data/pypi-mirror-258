eefdd676f7be2db5b8356ce8f997f9da05de59ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-22 14:02:55 +0000

    Add package cybertask/2.1.42/cybertask-2.1.42-py3-none-any.whl

---
+++ packages/cybertask/cybertask-2.1.42-py3-none-any.whl/cybertask/plugins/open web browser.py
@@ -0,0 +1,37 @@
+"""
+LetMeDoIt AI Plugin - open web browser
+
+open a url with default web browser
+
+[FUNCTION_CALL]
+"""
+
+from cybertask import config
+from cybertask.utils.shared_utils import SharedUtil
+
+# Function method
+def open_browser(function_args):
+    url = function_args.get("url") # required
+    if url:
+        SharedUtil.openURL(url)
+    return ""
+
+# Function Signature
+functionSignature = {
+    "name": "open_browser",
+    "description": f'''Open an url with default web browser''',
+    "parameters": {
+        "type": "object",
+        "properties": {
+            "url": {
+                "type": "string",
+                "description": "The url",
+            },
+        },
+        "required": ["url"],
+    },
+}
+
+# Integrate the signature and method into LetMeDoIt AI
+config.addFunctionCall(name="open_browser", signature=functionSignature, method=open_browser)
+config.inputSuggestions.append("Open url: ")
