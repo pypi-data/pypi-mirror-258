3d09be939870403aa02decba2a90402743ac69a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-22 15:04:23 +0000

    Add package qcanvas/0.0.4.2a0/qcanvas-0.0.4.2a0.tar.gz

---
+++ packages/qcanvas/qcanvas-0.0.4.2a0.tar.gz/qcanvas-0.0.4.2a0/qcanvas/util/self_updater.py
@@ -0,0 +1,35 @@
+import asyncio
+import json
+import os
+from importlib.metadata import version
+
+import httpx
+from packaging.version import Version
+
+from qcanvas.util.constants import package_name
+
+restart_flag = False
+
+
+async def do_update():
+    global restart_flag
+    await asyncio.to_thread(os.system, f"pip install --upgrade {package_name}")
+    restart_flag = True
+
+
+async def get_newer_version() -> tuple[Version | None, Version | None] | None:
+    latest_version = await get_versions()
+    installed_version = Version(version(package_name))
+
+    print(f"latest = {latest_version}, installed = {installed_version}")
+
+    if installed_version < latest_version:
+        return latest_version, installed_version
+    else:
+        return None, None
+
+
+async def get_versions() -> Version:
+    async with httpx.AsyncClient() as client:
+        data = json.loads((await client.get(f"https://pypi.org/pypi/{package_name}/json")).text)
+        return Version(data["info"]["version"])
