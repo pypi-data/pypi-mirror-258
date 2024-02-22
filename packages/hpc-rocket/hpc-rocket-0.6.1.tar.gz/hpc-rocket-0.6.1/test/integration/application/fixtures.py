bc690149b2b7398dad22b7314c47aae16108390e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-22 16:03:47 +0000

    Add package hpc-rocket/0.6.1/hpc-rocket-0.6.1.tar.gz

---
+++ packages/hpc-rocket/hpc-rocket-0.6.1.tar.gz/hpc-rocket-0.6.1/test/integration/application/fixtures.py
@@ -0,0 +1,63 @@
+import os
+from test.testdoubles.pyfilesystem import ArbitraryArgsMemoryFS, OnlySubFSMemoryFS
+from unittest.mock import Mock, patch
+
+import pytest
+from fs.memoryfs import MemoryFS
+
+HOME_DIR = "/home/myuser"
+
+INPUT_AND_EXPECTED_KEYFILE_PATHS = [
+    ("my_private_keyfile", "my_private_keyfile"),
+    ("~/.ssh/private_keyfile", f"{HOME_DIR}/.ssh/private_keyfile"),
+    ("~/~folder~/private_keyfile", f"{HOME_DIR}/~folder~/private_keyfile"),
+    ("~folder~/private_keyfile", f"~folder~/private_keyfile"),
+]
+
+
+@pytest.fixture
+def sshclient_type_mock():
+    patcher = patch("paramiko.SSHClient")
+    type_mock = patcher.start()
+
+    yield type_mock
+
+    patcher.stop()
+
+
+@pytest.fixture(autouse=True)
+def osfs_type_mock():
+    patcher = patch("fs.osfs.OSFS")
+    osfs_type_mock = patcher.start()
+    osfs_type_mock.return_value = Mock(spec=MemoryFS, wraps=ArbitraryArgsMemoryFS())
+    yield osfs_type_mock
+
+    patcher.stop()
+
+
+@pytest.fixture(autouse=True)
+def sshfs_type_mock():
+    patcher = patch("hpcrocket.ssh.chmodsshfs.PermissionChangingSSHFSDecorator")
+
+    sshfs_type_mock = patcher.start()
+    mem_fs = OnlySubFSMemoryFS()
+    mem_fs.makedirs(HOME_DIR)
+    sshfs_type_mock.return_value = Mock(spec=MemoryFS, wraps=mem_fs)
+    sshfs_type_mock.return_value.homedir = lambda: HOME_DIR
+
+    yield sshfs_type_mock
+
+    patcher.stop()
+
+
+@pytest.fixture
+def fs_copy_file_mock():
+    patcher = patch("fs.copy.copy_file")
+    yield patcher.start()
+
+    patcher.stop()
+
+
+@pytest.fixture(autouse=True)
+def home_dir():
+    os.environ["HOME"] = HOME_DIR
---
+++ packages/hpc-rocket/hpc-rocket-0.6.1.tar.gz/hpc-rocket-0.6.1/test/testdoubles/pyfilesystem.py
@@ -0,0 +1,24 @@
+from typing import Callable, Optional, Text
+
+import fs.base
+import fs.subfs
+from fs.memoryfs import MemoryFS
+
+
+class ArbitraryArgsMemoryFS(MemoryFS):
+    def __init__(self, *args, **kwargs) -> None:
+        super().__init__()
+
+
+class OnlySubFSMemoryFS(ArbitraryArgsMemoryFS):
+    def __init__(self, *args, **kwargs) -> None:
+        super().__init__(*args, **kwargs)
+
+    def opendir(
+        self,
+        path: Text,
+        factory: Optional[
+            Callable[["OnlySubFSMemoryFS", Text], fs.subfs.SubFS["OnlySubFSMemoryFS"]]
+        ] = None,
+    ) -> fs.subfs.SubFS["OnlySubFSMemoryFS"]:
+        return super().opendir(path, factory=fs.subfs.SubFS)
