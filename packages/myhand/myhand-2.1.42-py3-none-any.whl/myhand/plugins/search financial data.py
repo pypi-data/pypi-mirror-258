13045ca5dc70c5ab57ac98cccc877cbfaa1038d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-22 14:02:54 +0000

    Add package myhand/2.1.42/myhand-2.1.42-py3-none-any.whl

---
+++ packages/myhand/myhand-2.1.42-py3-none-any.whl/myhand/plugins/search financial data.py
@@ -0,0 +1,38 @@
+"""
+LetMeDoIt AI Plugin - search financial data
+
+search financial data with yfinance
+
+[FUNCTION_CALL]
+"""
+
+from myhand import config
+from myhand.utils.shared_utils import SharedUtil
+import json
+
+def search_finance(function_args):
+    code = function_args.get("code") # required
+    information = SharedUtil.showAndExecutePythonCode(code)
+    if information:
+        info = {
+            "information": information,
+        }
+        return json.dumps(info)
+    return ""
+
+functionSignature = {
+    "name": "search_finance",
+    "description": f'''Search or analyze financial data. Use this function ONLY WHEN package yfinance is useful to resolve my request''',
+    "parameters": {
+        "type": "object",
+        "properties": {
+            "code": {
+                "type": "string",
+                "description": "Python code that integrates package yfinance to resolve my request. Integrate package matplotlib to visualize data, if applicable.",
+            },
+        },
+        "required": ["code"],
+    },
+}
+
+config.addFunctionCall(name="search_finance", signature=functionSignature, method=search_finance)
