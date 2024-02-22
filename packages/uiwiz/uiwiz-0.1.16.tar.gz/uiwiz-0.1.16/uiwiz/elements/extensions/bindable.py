d4e70298bd73b776b19837cd0b3c6cd2076d865a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-22 04:03:00 +0000

    Add package uiwiz/0.1.16/uiwiz-0.1.16.tar.gz

---
+++ packages/uiwiz/uiwiz-0.1.16.tar.gz/uiwiz-0.1.16/uiwiz/elements/extensions/bindable.py
@@ -0,0 +1,22 @@
+from fastapi import Request
+from uiwiz.element import Element
+
+
+class Bindable(Element):
+    def bind_text_from(self, element: Element, trigger: str = "input delay:20ms", swap: str = "outerHTML"):
+        """
+        Remove attribute as it is not used but breaks bind for reasons
+        """
+        assert element.attributes.get("name") is not None
+
+        async def bind_Value(request: Request):
+            data = await request.json()
+            self.set_frame_and_root()
+
+            input_data = data[element.name]
+
+            self.content = str(input_data)
+
+        element.event = {"target": f"#{self.id}", "func": bind_Value, "trigger": trigger, "swap": swap}
+
+        return self
