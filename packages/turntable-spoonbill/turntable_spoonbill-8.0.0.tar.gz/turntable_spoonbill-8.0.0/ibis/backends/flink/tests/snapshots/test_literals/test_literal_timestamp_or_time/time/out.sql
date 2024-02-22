b6302f36fd9673862a9e34713cbdbc4b2bc7dcc2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-22 05:02:56 +0000

    Add package turntable-spoonbill/8.0.0/turntable_spoonbill-8.0.0.tar.gz

---
+++ packages/turntable-spoonbill/turntable_spoonbill-8.0.0.tar.gz/turntable_spoonbill-8.0.0/ibis/backends/flink/tests/snapshots/test_literals/test_literal_timestamp_or_time/time/out.sql
@@ -0,0 +1,2 @@
+SELECT
+  CAST('04:55:59' AS TIMESTAMP) AS `datetime.time(4, 55, 59)`
---
+++ packages/turntable-spoonbill/turntable_spoonbill-8.0.0.tar.gz/turntable_spoonbill-8.0.0/ibis/backends/tests/test_uuid.py
@@ -0,0 +1,40 @@
+from __future__ import annotations
+
+import contextlib
+import uuid
+
+import pytest
+
+import ibis
+import ibis.common.exceptions as com
+import ibis.expr.datatypes as dt
+
+RAW_TEST_UUID = "08f48812-7948-4718-96c7-27fa6a398db6"
+TEST_UUID = uuid.UUID(RAW_TEST_UUID)
+
+UUID_BACKEND_TYPE = {
+    "bigquery": "STRING",
+    "clickhouse": "Nullable(UUID)",
+    "duckdb": "UUID",
+    "exasol": "UUID",
+    "flink": "CHAR(36) NOT NULL",
+    "impala": "STRING",
+    "postgres": "uuid",
+    "risingwave": "character varying",
+    "snowflake": "VARCHAR",
+    "sqlite": "text",
+    "trino": "uuid",
+}
+
+
+@pytest.mark.notimpl(["polars"], raises=NotImplementedError)
+def test_uuid_literal(con, backend):
+    backend_name = backend.name()
+
+    expr = ibis.literal(RAW_TEST_UUID, type=dt.uuid)
+    result = con.execute(expr)
+
+    assert result == TEST_UUID
+
+    with contextlib.suppress(com.OperationNotDefinedError):
+        assert con.execute(expr.typeof()) == UUID_BACKEND_TYPE[backend_name]
