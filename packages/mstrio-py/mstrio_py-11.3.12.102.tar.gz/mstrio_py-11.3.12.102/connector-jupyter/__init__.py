72aaee02f11f0ad20e2db2d590a9de2565a49001
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-22 17:03:58 +0000

    Add package mstrio-py/11.3.12.102/mstrio_py-11.3.12.102.tar.gz

---
+++ packages/mstrio-py/mstrio_py-11.3.12.102.tar.gz/mstrio_py-11.3.12.102/connector-jupyter/__init__.py
@@ -0,0 +1,9 @@
+def _jupyter_nbextension_paths():
+    return [
+        dict(
+            section="notebook",
+            src="production/mstr_jupyter/static",
+            dest="mstr_jupyter",
+            require="mstr_jupyter/main",
+        )
+    ]
