48b665dd495a4f9d9998909388c047ea633fc6c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-22 16:05:39 +0000

    Add package compiler-DMCS/1.1.8/compiler_DMCS-1.1.8.tar.gz

---
+++ packages/compiler-DMCS/compiler_DMCS-1.1.8.tar.gz/compiler_DMCS-1.1.8/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='compiler_DMCS',
+    version='1.1.8',
+    author='DMCS',
+    author_email='rioverrain22@gmail.com',
+    description='A short description of your package',
+    packages=['compiler_DMCS'],
+    install_requires=[],
+)
