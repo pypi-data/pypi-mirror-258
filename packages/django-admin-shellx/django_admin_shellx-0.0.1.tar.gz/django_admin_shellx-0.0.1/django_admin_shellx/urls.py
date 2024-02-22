fa7c16186bf40848324d892f144f910dac609b89
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-22 04:03:41 +0000

    Add package django-admin-shellx/0.0.1/django_admin_shellx-0.0.1.tar.gz

---
+++ packages/django-admin-shellx/django_admin_shellx-0.0.1.tar.gz/django_admin_shellx-0.0.1/django_admin_shellx/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path, re_path
+
+from . import consumers
+
+app_name = "django_admin_shellx"
+
+urlpatterns = []
+
+websocket_urlpatterns = [
+    re_path(r"ws/terminal/(?P<room_name>\w+)/$", consumers.TerminalConsumer.as_asgi()),
+]
