78cf6da7391787a6422b48080ed46b268f04e74f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-22 16:04:53 +0000

    Add package relic-pytorch/0.4.1/relic-pytorch-0.4.1.tar.gz

---
+++ packages/relic-pytorch/relic-pytorch-0.4.1.tar.gz/relic-pytorch-0.4.1/README.md
@@ -0,0 +1,114 @@
+![image](https://github.com/filipbasara0/relic/assets/29043871/130c3459-08fc-49c1-a922-43576f2a255c)
+
+# ReLIC
+
+A PyTorch implementation of a computer vision self-supervised learning method based on [Representation Learning via Invariant Causal Mechanisms (ReLIC)](https://arxiv.org/abs/2010.07922).
+
+This simple approach is very similar to [BYOL](https://arxiv.org/abs/2006.07733) and [SimCLR](https://arxiv.org/abs/2002.05709). The training technique uses a online and target encoder (EMA) with a simple critic MLP projector, while the instance discrimination loss function resembles the contrastive loss used in SimCLR. The other half of the loss function acts as a regularizer - it includes an invariance penalty, which forces the representations to stay invariant under data augmentations and amplifies intra-class distances.
+
+![image](https://github.com/filipbasara0/relic/assets/29043871/70ccdb40-3343-4ea7-946b-80bdc1e7b85d)
+
+Repo includes the multi-crop augmentation, which is used in the follow-up [Pushing the limits of self-supervised ResNets: Can we outperform supervised learning without labels on ImageNet? (ReLICv2)](https://arxiv.org/pdf/2201.05119.pdf) paper. Loss function is extended to support an arbitrary number of small (local) and large (global) views. Using this technique generally results in more robust and higher quality representations.
+
+Also has an experimental support for the sigmoid pairwise loss, from the [SigLIP](https://arxiv.org/abs/2303.15343) paper. This loss is generally less stable and gives slightly worse metrics, but still yields very good representations.
+
+
+# Results
+
+Models are pretrained on training subsets - for `CIFAR10` 50,000 and for `STL10` 100,000 images. For evaluation, I trained and tested LogisticRegression on frozen features from:
+1. `CIFAR10` - 50,000 train images on ReLIC
+2. `STL10` - features were learned on 100k unlabeled images. LogReg was trained on 5k train images and evaluated on 8k test images.
+
+Linear probing was used for evaluating on features extracted from encoders using the scikit LogisticRegression model.
+
+More detailed evaluation steps and results for [CIFAR10](https://github.com/filipbasara0/relic/blob/main/notebooks/linear-probing-cifar.ipynb) and [STL10](https://github.com/filipbasara0/relic/blob/main/notebooks/linear-probing-stl.ipynb) can be found in the notebooks directory. 
+
+| Evaulation model    | Dataset | Feature Extractor| Encoder   | Feature dim | Projection Head dim | Epochs | Top1 % |
+|---------------------|---------|------------------|-----------|-------------|---------------------|--------|--------|
+| LogisticRegression  | CIFAR10 | ReLIC            | ResNet-18 | 512         | 64                  | 100    | 82.53  |
+| LogisticRegression  | STL10   | ReLIC            | ResNet-18 | 512         | 64                  | 100    | 77.12  |
+| LogisticRegression  | STL10   | ReLIC            | ResNet-50 | 2048        | 64                  | 100    | 81.95  |
+
+[Here](https://drive.google.com/file/d/1XaZBdvPGPh2nQzzHAJ_oL41c1f8Lc_FN/view?usp=sharing) is a link to a resnet18 encoder trained on the ImageNet-1k subset. This models pefroms better on both CIFAR10 and STL10.
+
+# Usage
+
+### Instalation
+
+```bash
+$ pip install relic-pytorch
+```
+
+Code currently supports ResNet18, ResNet50 and an experimental version of the EfficientNet model. Supported datasets are STL10 and CIFAR10.
+
+All training is done from scratch.
+
+### Examples
+`CIFAR10` ResNet-18 model was trained with this command:
+
+`relic_train --dataset_name "cifar10" --encoder_model_name resnet18 --fp16_precision --gamma 0.99 --alpha 1.0`
+
+`STL10` ResNet-50 model was trained with this command:
+
+`relic_train --dataset_name "stl10" --encoder_model_name resnet50 --fp16_precision`
+
+### Detailed options
+Once the code is setup, run the following command with optinos listed below:
+`relic_train [args...]⬇️`
+
+```
+ReLIC
+
+options:
+  -h, --help            show this help message and exit
+  --dataset_path DATASET_PATH
+                        Path where datasets will be saved
+  --dataset_name {stl10,cifar10}
+                        Dataset name
+  -m {resnet18,resnet50,efficientnet}, --encoder_model_name {resnet18,resnet50,efficientnet}
+                        model architecture: resnet18, resnet50 or efficientnet (default: resnet18)
+  -save_model_dir SAVE_MODEL_DIR
+                        Path where models
+  --num_epochs NUM_EPOCHS
+                        Number of epochs for training
+  -b BATCH_SIZE, --batch_size BATCH_SIZE
+                        Batch size
+  -lr LEARNING_RATE, --learning_rate LEARNING_RATE
+  -wd WEIGHT_DECAY, --weight_decay WEIGHT_DECAY
+  --fp16_precision      Whether to use 16-bit precision GPU training.
+  --proj_out_dim PROJ_OUT_DIM
+                        Projector MLP out dimension
+  --proj_hidden_dim PROJ_HIDDEN_DIM
+                        Projector MLP hidden dimension
+  --log_every_n_steps LOG_EVERY_N_STEPS
+                        Log every n steps
+  --gamma GAMMA         Initial EMA coefficient
+  --use_siglip          Whether to use siglip loss.
+  --alpha ALPHA         Regularization loss factor
+  --update_gamma_after_step UPDATE_GAMMA_AFTER_STEP
+                        Update EMA gamma after this step
+  --update_gamma_every_n_steps UPDATE_GAMMA_EVERY_N_STEPS
+                        Update EMA gamma after this many steps
+```
+
+# Citation
+
+```
+@misc{mitrovic2020representation,
+      title={Representation Learning via Invariant Causal Mechanisms}, 
+      author={Jovana Mitrovic and Brian McWilliams and Jacob Walker and Lars Buesing and Charles Blundell},
+      year={2020},
+      eprint={2010.07922},
+      archivePrefix={arXiv},
+      primaryClass={cs.LG}
+}
+
+@misc{zhai2023sigmoid,
+      title={Sigmoid Loss for Language Image Pre-Training}, 
+      author={Xiaohua Zhai and Basil Mustafa and Alexander Kolesnikov and Lucas Beyer},
+      year={2023},
+      eprint={2303.15343},
+      archivePrefix={arXiv},
+      primaryClass={cs.CV}
+}
+```
---
+++ packages/relic-pytorch/relic-pytorch-0.4.1.tar.gz/relic-pytorch-0.4.1/train.py
@@ -0,0 +1,165 @@
+import torch
+from torch.utils.data import DataLoader
+from torch.cuda.amp import GradScaler, autocast
+import torch.nn.functional as F
+import multiprocessing
+import random
+import numpy as np
+from tqdm.auto import tqdm
+from torchinfo import summary
+
+from relic import ReLIC, relic_loss
+
+from relic.utils import accuracy, get_dataset, get_encoder
+from relic.stl10_eval import STL10Eval
+
+SEED = 42
+
+random.seed(SEED)
+np.random.seed(SEED)
+torch.manual_seed(SEED)
+torch.cuda.manual_seed(SEED)
+
+
+# cosine EMA schedule (increase from tau_base to one) as defined in https://arxiv.org/abs/2010.07922
+# k -> current training step, K -> maximum number of training steps
+def update_gamma(k, K, tau_base):
+    k = torch.tensor(k, dtype=torch.float32)
+    K = torch.tensor(K, dtype=torch.float32)
+
+    tau = 1 - (1 - tau_base) * (torch.cos(torch.pi * k / K) + 1) / 2
+    return tau.item()
+
+
+def train_relic(args):
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+    modify_model = True if "cifar" in args.dataset_name else False
+    encoder = get_encoder(args.encoder_model_name, modify_model)
+    if not args.use_siglip:
+        init_tau, init_b, max_tau = np.log(1), 0, 5
+    else:
+        init_tau, init_b, max_tau = np.log(10), -10, 15
+    relic_model = ReLIC(encoder,
+                        mlp_out_dim=args.proj_out_dim,
+                        mlp_hidden=args.proj_hidden_dim,
+                        init_tau=init_tau, init_b=init_b)
+
+    if args.ckpt_path:
+        model_state = torch.load(args.ckpt_path)
+        relic_model.load_state_dict(model_state)
+    relic_model = relic_model.to(device)
+
+    summary(relic_model, input_size=[(1, 3, 32, 32), (1, 3, 32, 32)])
+
+    params = list(relic_model.online_encoder.parameters()) + [relic_model.tau, relic_model.b]
+    optimizer = torch.optim.Adam(params,
+                                 lr=args.learning_rate,
+                                 weight_decay=args.weight_decay)
+
+    ds = get_dataset(args)
+    train_loader = DataLoader(ds,
+                              batch_size=args.batch_size,
+                              num_workers=multiprocessing.cpu_count() - 8,
+                              drop_last=True,
+                              pin_memory=True,
+                              shuffle=True)
+
+    scaler = GradScaler(enabled=args.fp16_precision)
+
+    stl10_eval = STL10Eval()
+    total_num_steps = (len(train_loader) *
+                       (args.num_epochs + 2)) - args.update_gamma_after_step
+    gamma = args.gamma
+    global_step = 0
+    total_loss = 0.0
+    n_global, n_local = args.num_global_views, args.num_local_views
+    for epoch in range(args.num_epochs):
+        epoch_loss = 0.0
+        epoch_kl_loss = 0.0
+        progress_bar = tqdm(train_loader,
+                            desc=f"Epoch {epoch+1}/{args.num_epochs}")
+        for step, (views, _) in enumerate(progress_bar):
+            views = [v.to(device) for v in views]
+            global_views = views[:n_global]
+            local_views = views[:n_local]
+
+            with autocast(enabled=args.fp16_precision):
+                projections_online = []
+                projections_target = []
+                for view in global_views:
+                    projections_online.append(relic_model.get_online_pred(view))
+                    projections_target.append(relic_model.get_target_pred(view))
+                for view in local_views:
+                    projections_online.append(relic_model.get_online_pred(view))
+                loss = 0
+                # invariance_loss used only for debug
+                invariance_loss = 0
+                scale = 0
+                for i_t, target_pred in enumerate(projections_target):
+                    for i_o, online_pred in enumerate(projections_online):
+                        if i_t != i_o:
+                            relic_loss_, invar_loss = relic_loss(online_pred, target_pred,
+                                                                 relic_model.tau, relic_model.b, 
+                                                                 args.alpha, max_tau=max_tau,
+                                                                 use_siglip=args.use_siglip)
+                            loss += relic_loss_
+                            invariance_loss += invar_loss
+                            scale += 1
+
+                loss /= scale
+                invariance_loss /= scale
+
+            optimizer.zero_grad()
+            scaler.scale(loss).backward()
+            scaler.step(optimizer)
+            scaler.update()
+
+            if global_step > args.update_gamma_after_step and global_step % args.update_gamma_every_n_steps == 0:
+                relic_model.update_params(gamma)
+                gamma = update_gamma(global_step, total_num_steps, args.gamma)
+
+            if global_step <= args.update_gamma_after_step:
+                relic_model.copy_params()
+
+            total_loss += loss.item()
+            epoch_loss += loss.item()
+            avg_loss = total_loss / (global_step + 1)
+            ep_loss = epoch_loss / (step + 1)
+
+            epoch_kl_loss += invariance_loss.item()
+            ep_kl_loss = epoch_kl_loss / (step + 1)
+
+            current_lr = optimizer.param_groups[0]['lr']
+            progress_bar.set_description(
+                f"Epoch {epoch+1}/{args.num_epochs} | "
+                f"Step {global_step+1} | "
+                f"Epoch Loss: {ep_loss:.4f} |"
+                f"Total Loss: {avg_loss:.4f} |"
+                f"KL Loss: {ep_kl_loss:.6f} |"
+                f"Gamma: {gamma:.6f} |"
+                f"Alpha: {args.alpha:.3f} |"
+                f"Temp: {relic_model.tau.exp().item():.3f} |"
+                f"Bias: {relic_model.b.item():.3f} |"
+                f"Lr: {current_lr:.6f}")
+
+            global_step += 1
+            if global_step % args.log_every_n_steps == 0:
+                with torch.no_grad():
+                    x, x_prime = projections_online[0], projections_target[1]
+                    x, x_prime = F.normalize(x, p=2, dim=-1), F.normalize(x_prime, p=2, dim=-1)
+                    logits = torch.mm(x, x_prime.t()) * relic_model.tau.exp().clamp(0, max_tau) + relic_model.b
+                labels = torch.arange(logits.size(0)).to(logits.device)
+                top1, top5 = accuracy(logits, labels, topk=(1, 5))
+                print("#" * 100)
+                print('acc/top1 logits1', top1[0].item())
+                print('acc/top5 logits1', top5[0].item())
+                print("#" * 100)
+
+                torch.save(relic_model.state_dict(),
+                           f"{args.save_model_dir}/relic_model.pth")
+                relic_model.save_encoder(f"{args.save_model_dir}/encoder.pth")
+
+            if global_step % (args.log_every_n_steps * 5) == 0:
+                stl10_eval.evaluate(relic_model)
+                print("!" * 100)
