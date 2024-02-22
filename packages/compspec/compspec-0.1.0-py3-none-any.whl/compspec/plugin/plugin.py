f24b9b264e37e34a7a6bc518342659fb6dfc40a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-22 15:04:31 +0000

    Add package compspec/0.1.0/compspec-0.1.0-py3-none-any.whl

---
+++ packages/compspec/compspec-0.1.0-py3-none-any.whl/compspec/plugin/plugin.py
@@ -0,0 +1,22 @@
+class PluginBase:
+    """
+    A base for a compspec plugin
+    """
+
+    def __init__(self, name):
+        self.name = name
+        # description should be on the class
+
+    def add_arguments(self, subparser):
+        """
+        Add arguments for the plugin to show up in argparse
+
+        This is added by the plugin clas
+        """
+        pass
+
+    def extract(self, args, extra):
+        """
+        The extract interface allows for extraction of metadata into an artifact.
+        """
+        raise NotImplementedError(f"The {self.name} plugin is missing a 'run' function")
