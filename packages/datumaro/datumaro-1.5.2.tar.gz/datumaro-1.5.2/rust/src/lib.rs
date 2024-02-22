f513f95cfea959310a24524f79c6da1da25c3b1f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-22 15:04:08 +0000

    Add package datumaro/1.5.2/datumaro-1.5.2.tar.gz

---
+++ packages/datumaro/datumaro-1.5.2.tar.gz/datumaro-1.5.2/rust/src/lib.rs
@@ -0,0 +1,27 @@
+//  Copyright (C) 2023 Intel Corporation
+//
+//  SPDX-License-Identifier: MIT
+
+mod coco_page_mapper;
+mod datum_page_mapper;
+mod json_section_page_mapper;
+mod page_mapper;
+mod page_maps;
+mod test_helpers;
+mod utils;
+use pyo3::prelude::*;
+
+use crate::coco_page_mapper::CocoPageMapper;
+use crate::datum_page_mapper::DatumPageMapper;
+use crate::json_section_page_mapper::JsonSectionPageMapper;
+
+/// Datumaro Rust API
+#[pymodule]
+#[pyo3(name = "rust_api")]
+fn rust_api(_py: Python<'_>, m: &PyModule) -> PyResult<()> {
+    m.add_class::<CocoPageMapper>()?;
+    m.add_class::<DatumPageMapper>()?;
+    m.add_class::<JsonSectionPageMapper>()?;
+
+    Ok(())
+}
