122888a1655b69f13a13a55b8750646a11272f95
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-22 14:03:06 +0000

    Add package webql/0.2.0/webql-0.2.0-py3-none-any.whl

---
+++ packages/webql/webql-0.2.0-py3-none-any.whl/webql/sync_api/core.py
@@ -0,0 +1,35 @@
+"""
+This module is an entrypoint to WebQL service
+"""
+
+import logging
+
+from webql.sync_api.web import InteractiveItemTypeT, PlaywrightWebDriver, WebDriver
+
+from .session import Session
+
+log = logging.getLogger(__name__)
+
+
+def start_session(
+    url: str,
+    *,
+    web_driver: WebDriver[InteractiveItemTypeT] = PlaywrightWebDriver(),
+) -> Session[InteractiveItemTypeT]:
+    """Start a new synchronous WebQL session.
+
+    Parameters:
+
+    url (str): The URL to start the session with.
+    web_driver (optional): The web driver to use. Defaults to Playwright web driver.
+
+    Returns:
+
+    Session: The new session.
+    """
+    log.debug(f"Starting session with {url}")
+
+    web_driver.start_browser()
+    web_driver.open_url(url)
+    session = Session[InteractiveItemTypeT](web_driver)
+    return session
