8a2d8aa9083f8e19bc66d8cfd25150bee784a3d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-22 05:02:56 +0000

    Add package wagtail-word/1.1.0/wagtail_word-1.1.0.tar.gz

---
+++ packages/wagtail-word/wagtail_word-1.1.0.tar.gz/wagtail_word-1.1.0/wagtail_word/signals.py
@@ -0,0 +1,18 @@
+from django.dispatch import receiver
+from django.db.models.signals import post_migrate
+from wagtail.models import Collection
+
+COLLECTION_NAME = "Wagtail Word"
+
+@receiver(post_migrate)
+def create_default_collection(sender, **kwargs):
+    root: Collection = Collection.get_first_root_node()
+    if not root:
+        root = Collection.add_root(name="Root")
+        
+    c1, _ = Collection.objects.get_or_create(name=COLLECTION_NAME, depth=1)
+    
+    if not root.get_children().filter(name=COLLECTION_NAME).exists():
+        root.add_child(instance=c1)
+
+
