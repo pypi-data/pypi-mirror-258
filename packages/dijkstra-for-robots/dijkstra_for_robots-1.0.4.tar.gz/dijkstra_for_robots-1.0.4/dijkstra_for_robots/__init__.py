7273bd39eedaaa1e876c0df542b866aa75646d88
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-22 03:04:12 +0000

    Add package dijkstra-for-robots/1.0.4/dijkstra_for_robots-1.0.4.tar.gz

---
+++ packages/dijkstra-for-robots/dijkstra_for_robots-1.0.4.tar.gz/dijkstra_for_robots-1.0.4/dijkstra_for_robots/__init__.py
@@ -0,0 +1,13 @@
+"""
+:authors: latwiks
+:license: MIT, see LICENSE for more details
+:copyright: (c) 2024 latwiks
+"""
+
+from .main import *
+
+__author__ = 'latwiks'
+__version__ = '1.0.0'
+__license__ = 'MIT'
+__copyright__ = '(c) 2024 latwiks'
+__email__ = 'latwiks@vk.com'
