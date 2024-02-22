a802b33116c74799be04023026427ad41844e475
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-22 15:04:10 +0000

    Add package pygpt-net/2.0.161/pygpt_net-2.0.161-py3-none-any.whl

---
+++ packages/pygpt-net/pygpt_net-2.0.161-py3-none-any.whl/pygpt_net/app.py
@@ -0,0 +1,193 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# ================================================== #
+# This file is a part of PYGPT package               #
+# Website: https://pygpt.net                         #
+# GitHub:  https://github.com/szczyglis-dev/py-gpt   #
+# MIT License                                        #
+# Created By  : Marcin Szczygliński                  #
+# Updated Date: 2024.02.03 16:00:00                  #
+# ================================================== #
+
+from pygpt_net.launcher import Launcher
+
+# plugins
+from pygpt_net.plugin.audio_azure import Plugin as AudioAzurePlugin
+from pygpt_net.plugin.audio_openai_tts import Plugin as AudioOpenAITTSPlugin
+from pygpt_net.plugin.audio_openai_whisper import Plugin as AudioOpenAIWhisperPlugin
+from pygpt_net.plugin.cmd_code_interpreter import Plugin as CmdCodeInterpreterPlugin
+from pygpt_net.plugin.cmd_custom import Plugin as CmdCustomCommandPlugin
+from pygpt_net.plugin.cmd_files import Plugin as CmdFilesPlugin
+from pygpt_net.plugin.cmd_history import Plugin as CtxHistoryPlugin
+from pygpt_net.plugin.cmd_serial import Plugin as CmdSerialPlugin
+from pygpt_net.plugin.cmd_web_google import Plugin as CmdWebGooglePlugin
+from pygpt_net.plugin.crontab import Plugin as CrontabPlugin
+from pygpt_net.plugin.extra_prompt import Plugin as ExtraPromptPlugin
+from pygpt_net.plugin.idx_llama_index import Plugin as IdxLlamaIndexPlugin
+from pygpt_net.plugin.openai_dalle import Plugin as OpenAIDallePlugin
+from pygpt_net.plugin.openai_vision import Plugin as OpenAIVisionPlugin
+from pygpt_net.plugin.real_time import Plugin as RealTimePlugin
+from pygpt_net.plugin.self_loop import Plugin as SelfLoopPlugin
+
+# LLMs wrappers providers (langchain, llama-index)
+from pygpt_net.provider.llms.anthropic import AnthropicLLM
+from pygpt_net.provider.llms.azure_openai import AzureOpenAILLM
+from pygpt_net.provider.llms.hugging_face import HuggingFaceLLM
+from pygpt_net.provider.llms.llama import Llama2LLM
+from pygpt_net.provider.llms.ollama import OllamaLLM
+from pygpt_net.provider.llms.openai import OpenAILLM
+
+# vector stores providers (llama-index)
+from pygpt_net.provider.vector_stores.chroma import ChromaProvider
+from pygpt_net.provider.vector_stores.elasticsearch import ElasticsearchProvider
+from pygpt_net.provider.vector_stores.pinecode import PinecodeProvider
+from pygpt_net.provider.vector_stores.redis import RedisProvider
+from pygpt_net.provider.vector_stores.simple import SimpleProvider
+
+# data loaders providers (llama-index)
+from pygpt_net.provider.loaders.file_csv import Loader as CsvLoader
+from pygpt_net.provider.loaders.file_docx import Loader as DocxLoader
+from pygpt_net.provider.loaders.file_epub import Loader as EpubLoader
+from pygpt_net.provider.loaders.file_excel import Loader as ExcelLoader
+from pygpt_net.provider.loaders.file_json import Loader as JsonLoader
+from pygpt_net.provider.loaders.file_markdown import Loader as MarkdownLoader
+from pygpt_net.provider.loaders.file_pdf import Loader as PdfLoader
+
+
+def run(**kwargs):
+    """
+    PyGPT launcher.
+
+    :param kwargs: keyword arguments for launcher
+
+    Extending PyGPT with custom plugins, LLMs wrappers, vector stores and data loaders
+
+    - You can pass custom plugin instances, LLMs wrappers and vector store providers to the launcher.
+    - This is useful if you want to extend PyGPT with your own plugins, vectors storage and LLMs.
+
+    To register custom plugins create custom launcher, e.g. "my_launcher.py" and:
+
+    - Pass a list with the plugin instances as 'plugins' keyword argument.
+
+    To register custom LLMs wrappers:
+
+    - Pass a list with the LLMs wrappers instances as 'llms' keyword argument.
+
+    To register custom vector store providers:
+
+    - Pass a list with the vector store provider instances as 'vector_stores' keyword argument.
+
+    To register custom data loaders:
+
+    - Pass a list with the data loader instances as 'loaders' keyword argument.
+
+    Example:
+    --------
+    ::
+
+        # my_launcher.py
+
+        from pygpt_net.app import run
+        from my_plugins import MyCustomPlugin, MyOtherCustomPlugin
+        from my_llms import MyCustomLLM
+        from my_vector_stores import MyCustomVectorStore
+        from my_loaders import MyCustomLoader
+
+        plugins = [
+            MyCustomPlugin(),
+            MyOtherCustomPlugin(),
+        ]
+        llms = [
+            MyCustomLLM(),
+        ]
+        vector_stores = [
+            MyCustomVectorStore(),
+        ]
+        loaders = [
+            MyCustomLoader(),
+        ]
+
+        run(
+            plugins=plugins,
+            llms=llms,
+            vector_stores=vector_stores,
+            loaders=loaders
+        )
+
+    """
+    # initialize app launcher
+    launcher = Launcher()
+    launcher.init()
+
+    # register base plugins
+    launcher.add_plugin(SelfLoopPlugin())
+    launcher.add_plugin(RealTimePlugin())
+    launcher.add_plugin(ExtraPromptPlugin())
+    launcher.add_plugin(AudioAzurePlugin())
+    launcher.add_plugin(AudioOpenAITTSPlugin())
+    launcher.add_plugin(AudioOpenAIWhisperPlugin())
+    launcher.add_plugin(CmdWebGooglePlugin())
+    launcher.add_plugin(CmdFilesPlugin())
+    launcher.add_plugin(CmdCodeInterpreterPlugin())
+    launcher.add_plugin(CmdCustomCommandPlugin())
+    launcher.add_plugin(CmdSerialPlugin())
+    launcher.add_plugin(CtxHistoryPlugin())
+    launcher.add_plugin(OpenAIDallePlugin())
+    launcher.add_plugin(OpenAIVisionPlugin())
+    launcher.add_plugin(IdxLlamaIndexPlugin())
+    launcher.add_plugin(CrontabPlugin())
+
+    # register custom plugins
+    plugins = kwargs.get('plugins', None)
+    if isinstance(plugins, list):
+        for plugin in plugins:
+            launcher.add_plugin(plugin)
+
+    # register base langchain and llama-index LLMs
+    launcher.add_llm(OpenAILLM())
+    launcher.add_llm(AzureOpenAILLM())
+    launcher.add_llm(AnthropicLLM())
+    launcher.add_llm(HuggingFaceLLM())
+    launcher.add_llm(Llama2LLM())
+    launcher.add_llm(OllamaLLM())
+
+    # register custom langchain and llama-index LLMs
+    llms = kwargs.get('llms', None)
+    if isinstance(llms, list):
+        for llm in llms:
+            launcher.add_llm(llm)
+
+    # register base vector store providers (llama-index)
+    launcher.add_vector_store(ChromaProvider())
+    launcher.add_vector_store(ElasticsearchProvider())
+    launcher.add_vector_store(PinecodeProvider())
+    launcher.add_vector_store(RedisProvider())
+    launcher.add_vector_store(SimpleProvider())
+
+    # register custom vector store providers (llama-index)
+    vector_stores = kwargs.get('vector_stores', None)
+    if isinstance(vector_stores, list):
+        for store in vector_stores:
+            launcher.add_vector_store(store)
+
+    # register base data loaders (llama-index)
+    launcher.add_loader(CsvLoader())
+    launcher.add_loader(DocxLoader())
+    launcher.add_loader(EpubLoader())
+    launcher.add_loader(ExcelLoader())
+    launcher.add_loader(JsonLoader())
+    launcher.add_loader(MarkdownLoader())
+    launcher.add_loader(PdfLoader())
+
+    # register custom data loaders (llama-index)
+    loaders = kwargs.get('loaders', None)
+    if isinstance(loaders, list):
+        for loader in loaders:
+            launcher.add_loader(loader)
+
+    # run app
+    launcher.run()
+    
+
+if __name__ == '__main__':
+    run()
---
+++ packages/pygpt-net/pygpt_net-2.0.161-py3-none-any.whl/pygpt_net/core/idx/indexing.py
@@ -0,0 +1,315 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# ================================================== #
+# This file is a part of PYGPT package               #
+# Website: https://pygpt.net                         #
+# GitHub:  https://github.com/szczyglis-dev/py-gpt   #
+# MIT License                                        #
+# Created By  : Marcin Szczygliński                  #
+# Updated Date: 2024.02.20 19:00:00                  #
+# ================================================== #
+
+import os.path
+from pathlib import Path
+from sqlalchemy import text
+from llama_index.indices.base import BaseIndex
+from llama_index import (
+    SimpleDirectoryReader,
+    download_loader,
+)
+from llama_index.readers.schema.base import Document
+from llama_index.readers import BeautifulSoupWebReader
+
+from pygpt_net.provider.loaders.base import BaseLoader
+
+
+class Indexing:
+    def __init__(self, window=None):
+        """
+        Indexing core
+
+        :param window: Window instance
+        """
+        self.window = window
+        self.loaders = {}  # offline loaders
+
+    def register_loader(self, loader: BaseLoader):
+        """
+        Register data loader
+
+        :param loader: loader instance
+        """
+        extensions = loader.extensions  # available extensions
+        for ext in extensions:
+            self.loaders[ext] = loader.get()  # get reader instance
+
+    def get_online_loader(self, ext: str):
+        """
+        Get online loader by extension
+
+        :param ext: file extension
+        """
+        loaders = self.window.core.config.get("llama.hub.loaders")
+        if loaders is None or not isinstance(loaders, list):
+            return None
+        ext = ext.lower()
+        for loader in loaders:
+            check = loader["ext"].lower()
+            if "," in check:
+                extensions = check.replace(" ", "").split(",")
+            else:
+                extensions = [check.strip()]
+            if ext in extensions:
+                return loader["loader"]
+
+    def get_documents(self, path: str) -> list[Document]:
+        """
+        Get documents from path
+
+        :param path: path to data
+        :return: list of documents
+        """
+        self.log("Reading documents from path: {}".format(path))
+        if self.window.core.config.is_compiled():
+            self.log("Compiled version detected - online loaders are disabled. "
+                     "Use Python version for using online loaders.")
+
+        if os.path.isdir(path):
+            reader = SimpleDirectoryReader(
+                input_dir=path,
+                recursive=True,
+                exclude_hidden=False,
+            )
+            documents = reader.load_data()
+        else:
+            ext = os.path.splitext(path)[1][1:]  # get extension
+            online_loader = self.get_online_loader(ext)  # get online loader if available
+            # TODO: in future, add support for online loaders in compiled version
+            if online_loader is not None and not self.window.core.config.is_compiled():
+                self.log("Using online loader for: {}".format(ext))
+                loader = download_loader(online_loader)
+                reader = loader()
+                documents = reader.load_data(file=Path(path))
+            else:  # try offline loaders
+                if ext in self.loaders:
+                    self.log("Using offline loader for: {}".format(ext))
+                    # download_loader cause problems in compiled version
+                    # use offline versions instead
+                    reader = self.loaders[ext]
+                    documents = reader.load_data(file=Path(path))
+                else:
+                    self.log("Using default loader for: {}".format(ext))
+                    reader = SimpleDirectoryReader(input_files=[path])
+                    documents = reader.load_data()
+        return documents
+
+    def index_files(self, index: BaseIndex, path: str = None) -> tuple:
+        """
+        Index all files in directory
+
+        :param index: index instance
+        :param path: path to file or directory
+        :return: dict with indexed files, errors
+        """
+        if self.window.core.config.get("llama.idx.recursive"):
+            return self.index_files_recursive(index, path)
+
+        indexed = {}
+        errors = []
+        files = []
+        if os.path.isdir(path):
+            files = [os.path.join(path, f)
+                     for f in os.listdir(path) if os.path.isfile(os.path.join(path, f))]
+        elif os.path.isfile(path):
+            files = [path]
+
+        for file in files:   # per file to allow use of multiple loaders
+            try:
+                documents = self.get_documents(file)
+                for d in documents:
+                    index.insert(document=d)
+                    indexed[file] = d.id_  # add to index
+                    self.log("Inserted document: {}".format(d.id_))
+            except Exception as e:
+                errors.append(str(e))
+                print(e)
+                print("Error while indexing file: " + file)
+                self.window.core.debug.log(e)
+                continue
+
+        return indexed, errors
+
+    def index_files_recursive(self, index: BaseIndex, path: str = None) -> tuple:
+        """
+        Index all files in directory and subdirectories recursively.
+
+        :param index: index instance
+        :param path: path to file or directory
+        :return: dict with indexed files, errors
+        """
+        indexed = {}
+        errors = []
+
+        if os.path.isdir(path):
+            for root, dirs, files in os.walk(path):
+                for file in files:
+                    file_path = os.path.join(root, file)
+                    try:
+                        documents = self.get_documents(file_path)
+                        for d in documents:
+                            index.insert(document=d)
+                            indexed[file_path] = d.id_  # add to index
+                            self.log("Inserted document: {}".format(d.id_))
+                    except Exception as e:
+                        errors.append(str(e))
+                        print(e)
+                        print("Error while indexing file: " + file_path)
+                        self.window.core.debug.log(e)
+                        continue
+        elif os.path.isfile(path):
+            try:
+                documents = self.get_documents(path)
+                for d in documents:
+                    index.insert(document=d)
+                    indexed[path] = d.id_  # add to index
+                    self.log("Inserted document: {}".format(d.id_))
+            except Exception as e:
+                errors.append(str(e))
+                print(e)
+                print("Error while indexing file: " + path)
+                self.window.core.debug.log(e)
+
+        return indexed, errors
+
+    def get_db_data_from_ts(self, updated_ts: int = 0) -> list:
+        """
+        Get data from database from timestamp
+
+        :param updated_ts: timestamp
+        :return: list of documents
+        """
+        db = self.window.core.db.get_db()
+        documents = []
+        query = f"""
+        SELECT
+            'User: ' || ctx_item.input || '; Assistant: ' || ctx_item.output AS text
+        FROM 
+            ctx_item
+        LEFT JOIN
+            ctx_meta
+        ON
+            ctx_item.meta_id = ctx_meta.id
+        WHERE
+            ctx_meta.updated_ts > {updated_ts}
+        """
+        with db.connect() as connection:
+            result = connection.execute(text(query))
+            for item in result.fetchall():
+                doc_str = ", ".join([str(entry) for entry in item])
+                documents.append(Document(text=doc_str))
+        return documents
+
+    def get_db_data_by_id(self, id: int = 0) -> list:
+        """
+        Get data from database by meta id
+
+        :param id: meta id
+        :return: list of documents
+        """
+        db = self.window.core.db.get_db()
+        documents = []
+        query = f"""
+        SELECT
+            'User: ' || input || '; Assistant: ' || output AS text
+        FROM ctx_item
+        WHERE meta_id = {id}
+        """
+        with db.connect() as connection:
+            result = connection.execute(text(query))
+            for item in result.fetchall():
+                doc_str = ", ".join([str(entry) for entry in item])
+                documents.append(Document(text=doc_str))
+        return documents
+
+    def index_db_by_meta_id(self, index: BaseIndex, id: int = 0) -> (int, list):
+        """
+        Index data from database by meta id
+
+        :param index: index instance
+        :param id: meta id
+        :return: number of indexed documents, errors
+        """
+        errors = []
+        n = 0
+        try:
+            self.log("Indexing documents from database by meta id: {}".format(id))
+            documents = self.get_db_data_by_id(id)
+            for d in documents:
+                index.insert(document=d)
+                self.log("Inserted DB document: {} / {}".format(n+1, len(documents)))
+                n += 1
+        except Exception as e:
+            errors.append(str(e))
+            print(e)
+            self.window.core.debug.log(e)
+        return n, errors
+
+    def index_db_from_updated_ts(self, index: BaseIndex, updated_ts: int = 0) -> (int, list):
+        """
+        Index data from database from timestamp
+
+        :param index: index instance
+        :param updated_ts: timestamp
+        :return: number of indexed documents, errors
+        """
+        errors = []
+        n = 0
+        try:
+            self.log("Indexing documents from database from timestamp: {}".format(updated_ts))
+            documents = self.get_db_data_from_ts(updated_ts)
+            for d in documents:
+                index.insert(document=d)
+                self.log("Inserted DB document: {} / {}".format(n+1, len(documents)))
+                n += 1
+        except Exception as e:
+            errors.append(str(e))
+            print(e)
+            self.window.core.debug.log(e)
+        return n, errors
+
+    def index_urls(self, index: BaseIndex, urls: list) -> (int, list):
+        """
+        Index data from URLs
+
+        :param index: index instance
+        :param urls: list of urls
+        :return: number of indexed documents, errors
+        """
+        errors = []
+        n = 0
+        try:
+            self.log("Indexing URLs: {}".format(", ".join(urls)))
+            documents = BeautifulSoupWebReader().load_data(urls)
+            for d in documents:
+                index.insert(document=d)
+                self.log("Inserted webpage document: {} / {}".format(n+1, len(documents)))
+                n += 1
+        except Exception as e:
+            errors.append(str(e))
+            print(e)
+            self.window.core.debug.log(e)
+        return n, errors
+
+    def log(self, msg: str):
+        """
+        Log info message
+
+        :param msg: message
+        """
+        is_log = False
+        if self.window.core.config.has("log.llama") \
+                and self.window.core.config.get("log.llama"):
+            is_log = True
+        self.window.core.debug.info(msg, not is_log)
+        if is_log:
+            print("[LLAMA-INDEX] {}".format(msg))
