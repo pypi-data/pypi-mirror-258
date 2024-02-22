31f8e72b75f53f9d31aaee13f4be2d6f77e9f408
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-22 03:03:49 +0000

    Add package typing-validation/1.2.10/typing-validation-1.2.10.tar.gz

---
+++ packages/typing-validation/typing-validation-1.2.10.tar.gz/typing-validation-1.2.10/typing_validation/__init__.py
@@ -0,0 +1,33 @@
+"""
+    Runtime validation using type hints.
+"""
+
+__version__ = "1.2.9"
+
+from .inspector import TypeInspector, UnsupportedType
+from .validation import (
+    validate,
+    can_validate,
+    validation_aliases,
+    validated,
+    validated_iter,
+    is_valid,
+)
+from .validation_failure import (
+    get_validation_failure,
+    latest_validation_failure,
+)
+
+# re-export all encodings and functions.
+__all__ = [
+    "validate",
+    "can_validate",
+    "validation_aliases",
+    "is_valid",
+    "validated",
+    "validated_iter",
+    "TypeInspector",
+    "UnsupportedType",
+    "get_validation_failure",
+    "latest_validation_failure",
+]
