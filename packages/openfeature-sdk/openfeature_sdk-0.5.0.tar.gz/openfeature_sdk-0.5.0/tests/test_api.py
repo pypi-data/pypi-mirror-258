03785857ba3c0853cfd8e89acdb745bd3a9995ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-22 04:03:39 +0000

    Add package openfeature-sdk/0.5.0/openfeature_sdk-0.5.0.tar.gz

---
+++ packages/openfeature-sdk/openfeature_sdk-0.5.0.tar.gz/openfeature_sdk-0.5.0/tests/test_api.py
@@ -0,0 +1,230 @@
+from unittest.mock import MagicMock
+
+import pytest
+
+from openfeature.api import (
+    add_hooks,
+    clear_hooks,
+    clear_providers,
+    get_client,
+    get_evaluation_context,
+    get_hooks,
+    get_provider_metadata,
+    set_evaluation_context,
+    set_provider,
+    shutdown,
+)
+from openfeature.evaluation_context import EvaluationContext
+from openfeature.exception import ErrorCode, GeneralError
+from openfeature.hook import Hook
+from openfeature.provider.metadata import Metadata
+from openfeature.provider.no_op_provider import NoOpProvider
+from openfeature.provider.provider import FeatureProvider
+
+
+def test_should_not_raise_exception_with_noop_client():
+    # Given
+    # No provider has been set
+    # When
+    client = get_client()
+
+    # Then
+    assert isinstance(client.provider, NoOpProvider)
+
+
+def test_should_return_open_feature_client_when_configured_correctly():
+    # Given
+    set_provider(NoOpProvider())
+
+    # When
+    client = get_client()
+
+    # Then
+    assert isinstance(client.provider, NoOpProvider)
+
+
+def test_should_try_set_provider_and_fail_if_none_provided():
+    # Given
+    # When
+    with pytest.raises(GeneralError) as ge:
+        set_provider(provider=None)
+
+    # Then
+    assert ge.value.error_message == "No provider"
+    assert ge.value.error_code == ErrorCode.GENERAL
+
+
+def test_should_invoke_provider_initialize_function_on_newly_registered_provider():
+    # Given
+    evaluation_context = EvaluationContext("targeting_key", {"attr1": "val1"})
+    provider = MagicMock(spec=FeatureProvider)
+
+    # When
+    set_evaluation_context(evaluation_context)
+    set_provider(provider)
+
+    # Then
+    provider.initialize.assert_called_with(evaluation_context)
+
+
+def test_should_invoke_provider_shutdown_function_once_provider_is_no_longer_in_use():
+    # Given
+    provider_1 = MagicMock(spec=FeatureProvider)
+    provider_2 = MagicMock(spec=FeatureProvider)
+
+    # When
+    set_provider(provider_1)
+    set_provider(provider_2)
+
+    # Then
+    assert provider_1.shutdown.called
+
+
+def test_should_retrieve_metadata_for_configured_provider():
+    # Given
+    set_provider(NoOpProvider())
+
+    # When
+    metadata = get_provider_metadata()
+
+    # Then
+    assert isinstance(metadata, Metadata)
+    assert metadata.name == "No-op Provider"
+
+
+def test_should_raise_an_exception_if_no_evaluation_context_set():
+    # Given
+    with pytest.raises(GeneralError) as ge:
+        set_evaluation_context(evaluation_context=None)
+    # Then
+    assert ge.value
+    assert ge.value.error_message == "No api level evaluation context"
+    assert ge.value.error_code == ErrorCode.GENERAL
+
+
+def test_should_successfully_set_evaluation_context_for_api():
+    # Given
+    evaluation_context = EvaluationContext("targeting_key", {"attr1": "val1"})
+
+    # When
+    set_evaluation_context(evaluation_context)
+    global_evaluation_context = get_evaluation_context()
+
+    # Then
+    assert global_evaluation_context
+    assert global_evaluation_context.targeting_key == evaluation_context.targeting_key
+    assert global_evaluation_context.attributes == evaluation_context.attributes
+
+
+def test_should_add_hooks_to_api_hooks():
+    # Given
+    hook_1 = MagicMock(spec=Hook)
+    hook_2 = MagicMock(spec=Hook)
+    clear_hooks()
+
+    # When
+    add_hooks([hook_1])
+    add_hooks([hook_2])
+
+    # Then
+    assert get_hooks() == [hook_1, hook_2]
+
+
+def test_should_call_provider_shutdown_on_api_shutdown():
+    # Given
+    provider = MagicMock(spec=FeatureProvider)
+    set_provider(provider)
+
+    # When
+    shutdown()
+
+    # Then
+    assert provider.shutdown.called
+
+
+def test_should_provide_a_function_to_bind_provider_through_domain():
+    # Given
+    provider = MagicMock(spec=FeatureProvider)
+    test_client = get_client("test")
+    default_client = get_client()
+
+    # When
+    set_provider(provider, domain="test")
+
+    # Then
+    assert default_client.provider != provider
+    assert default_client.domain is None
+
+    assert test_client.provider == provider
+    assert test_client.domain == "test"
+
+
+def test_should_not_initialize_provider_already_bound_to_another_domain():
+    # Given
+    provider = MagicMock(spec=FeatureProvider)
+    set_provider(provider, "foo")
+
+    # When
+    set_provider(provider, "bar")
+
+    # Then
+    provider.initialize.assert_called_once()
+
+
+def test_should_shutdown_unbound_provider():
+    # Given
+    provider = MagicMock(spec=FeatureProvider)
+    set_provider(provider, "foo")
+
+    # When
+    other_provider = MagicMock(spec=FeatureProvider)
+    set_provider(other_provider, "foo")
+
+    provider.shutdown.assert_called_once()
+
+
+def test_should_not_shutdown_provider_bound_to_another_domain():
+    # Given
+    provider = MagicMock(spec=FeatureProvider)
+    set_provider(provider, "foo")
+    set_provider(provider, "bar")
+
+    # When
+    other_provider = MagicMock(spec=FeatureProvider)
+    set_provider(other_provider, "foo")
+
+    provider.shutdown.assert_not_called()
+
+
+def test_shutdown_should_shutdown_every_registered_provider_once():
+    # Given
+    provider_1 = MagicMock(spec=FeatureProvider)
+    provider_2 = MagicMock(spec=FeatureProvider)
+    set_provider(provider_1)
+    set_provider(provider_1, "foo")
+    set_provider(provider_2, "bar")
+    set_provider(provider_2, "baz")
+
+    # When
+    shutdown()
+
+    # Then
+    provider_1.shutdown.assert_called_once()
+    provider_2.shutdown.assert_called_once()
+
+
+def test_clear_providers_shutdowns_every_provider_and_resets_default_provider():
+    # Given
+    provider_1 = MagicMock(spec=FeatureProvider)
+    provider_2 = MagicMock(spec=FeatureProvider)
+    set_provider(provider_1)
+    set_provider(provider_2, "foo")
+    set_provider(provider_2, "bar")
+
+    # When
+    clear_providers()
+
+    # Then
+    provider_1.shutdown.assert_called_once()
+    provider_2.shutdown.assert_called_once()
+    assert isinstance(get_client().provider, NoOpProvider)
---
+++ packages/openfeature-sdk/openfeature_sdk-0.5.0.tar.gz/openfeature_sdk-0.5.0/tests/test_client.py
@@ -0,0 +1,184 @@
+from unittest.mock import MagicMock
+
+import pytest
+
+from openfeature.api import add_hooks, clear_hooks, set_provider
+from openfeature.client import OpenFeatureClient
+from openfeature.exception import ErrorCode, OpenFeatureError
+from openfeature.flag_evaluation import Reason
+from openfeature.hook import Hook
+from openfeature.provider.in_memory_provider import InMemoryFlag, InMemoryProvider
+from openfeature.provider.no_op_provider import NoOpProvider
+
+
+@pytest.mark.parametrize(
+    "flag_type, default_value, get_method",
+    (
+        (bool, True, "get_boolean_value"),
+        (str, "String", "get_string_value"),
+        (int, 100, "get_integer_value"),
+        (float, 10.23, "get_float_value"),
+        (
+            dict,
+            {
+                "String": "string",
+                "Number": 2,
+                "Boolean": True,
+            },
+            "get_object_value",
+        ),
+        (
+            list,
+            ["string1", "string2"],
+            "get_object_value",
+        ),
+    ),
+)
+def test_should_get_flag_value_based_on_method_type(
+    flag_type, default_value, get_method, no_op_provider_client
+):
+    # Given
+    # When
+    flag = getattr(no_op_provider_client, get_method)(
+        flag_key="Key", default_value=default_value
+    )
+    # Then
+    assert flag is not None
+    assert flag == default_value
+    assert isinstance(flag, flag_type)
+
+
+@pytest.mark.parametrize(
+    "flag_type, default_value, get_method",
+    (
+        (bool, True, "get_boolean_details"),
+        (str, "String", "get_string_details"),
+        (int, 100, "get_integer_details"),
+        (float, 10.23, "get_float_details"),
+        (
+            dict,
+            {
+                "String": "string",
+                "Number": 2,
+                "Boolean": True,
+            },
+            "get_object_details",
+        ),
+        (
+            list,
+            ["string1", "string2"],
+            "get_object_details",
+        ),
+    ),
+)
+def test_should_get_flag_detail_based_on_method_type(
+    flag_type, default_value, get_method, no_op_provider_client
+):
+    # Given
+    # When
+    flag = getattr(no_op_provider_client, get_method)(
+        flag_key="Key", default_value=default_value
+    )
+    # Then
+    assert flag is not None
+    assert flag.value == default_value
+    assert isinstance(flag.value, flag_type)
+
+
+def test_should_raise_exception_when_invalid_flag_type_provided(no_op_provider_client):
+    # Given
+    # When
+    flag = no_op_provider_client.evaluate_flag_details(
+        flag_type=None, flag_key="Key", default_value=True
+    )
+    # Then
+    assert flag.value
+    assert flag.error_message == "Unknown flag type"
+    assert flag.error_code == ErrorCode.GENERAL
+    assert flag.reason == Reason.ERROR
+
+
+def test_should_pass_flag_metadata_from_resolution_to_evaluation_details():
+    # Given
+    provider = InMemoryProvider(
+        {
+            "Key": InMemoryFlag(
+                "true",
+                {"true": True, "false": False},
+                flag_metadata={"foo": "bar"},
+            )
+        }
+    )
+    set_provider(provider, "my-client")
+
+    client = OpenFeatureClient("my-client", None)
+
+    # When
+    details = client.get_boolean_details(flag_key="Key", default_value=False)
+
+    # Then
+    assert details is not None
+    assert details.flag_metadata == {"foo": "bar"}
+
+
+def test_should_handle_a_generic_exception_thrown_by_a_provider(no_op_provider_client):
+    # Given
+    exception_hook = MagicMock(spec=Hook)
+    exception_hook.after.side_effect = Exception("Generic exception raised")
+    no_op_provider_client.add_hooks([exception_hook])
+    # When
+    flag_details = no_op_provider_client.get_boolean_details(
+        flag_key="Key", default_value=True
+    )
+    # Then
+    assert flag_details is not None
+    assert flag_details.value
+    assert isinstance(flag_details.value, bool)
+    assert flag_details.reason == Reason.ERROR
+    assert flag_details.error_message == "Generic exception raised"
+
+
+def test_should_handle_an_open_feature_exception_thrown_by_a_provider(
+    no_op_provider_client,
+):
+    # Given
+    exception_hook = MagicMock(spec=Hook)
+    exception_hook.after.side_effect = OpenFeatureError(
+        ErrorCode.GENERAL, "error_message"
+    )
+    no_op_provider_client.add_hooks([exception_hook])
+
+    # When
+    flag_details = no_op_provider_client.get_boolean_details(
+        flag_key="Key", default_value=True
+    )
+    # Then
+    assert flag_details is not None
+    assert flag_details.value
+    assert isinstance(flag_details.value, bool)
+    assert flag_details.reason == Reason.ERROR
+    assert flag_details.error_message == "error_message"
+
+
+def test_should_return_client_metadata_with_domain():
+    # Given
+    client = OpenFeatureClient("my-client", None, NoOpProvider())
+    # When
+    metadata = client.get_metadata()
+    # Then
+    assert metadata is not None
+    assert metadata.domain == "my-client"
+
+
+def test_should_call_api_level_hooks(no_op_provider_client):
+    # Given
+    clear_hooks()
+    api_hook = MagicMock(spec=Hook)
+    add_hooks([api_hook])
+
+    # When
+    no_op_provider_client.get_boolean_details(flag_key="Key", default_value=True)
+
+    # Then
+    api_hook.before.assert_called_once()
+    api_hook.after.assert_called_once()
