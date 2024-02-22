49b3c6f6a5349d8798ad354e38e0e7e7f3ddd5d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-22 05:03:11 +0000

    Add package relationalai/0.1/relationalai-0.1.tar.gz

---
+++ packages/relationalai/relationalai-0.1.tar.gz/relationalai-0.1/src/relationalai/__init__.py
@@ -0,0 +1,33 @@
+from .clients import config
+from . import clients
+from . import dsl
+from . import debugging
+from . import metamodel
+from . import rel
+from .loaders import csv
+from . import analysis
+from . import tools
+from . import graphs
+
+def Model(name:str, dry_run:bool=False):
+    cfg = config.Config()
+    platform = cfg.get("platform", "snowflake")
+    if platform == "azure":
+        return clients.azure.Graph(name, dry_run)
+    elif platform == "snowflake":
+        return clients.snowflake.Graph(name, dry_run)
+    else:
+        raise Exception(f"Unknown platform: {platform}")
+
+def Resources(profile:str|None=None, cfg:config.Config|None=None):
+    cfg = cfg or config.Config(profile)
+    platform = cfg.get("platform", "snowflake")
+    if platform == "azure":
+        return clients.azure.Resources(config=cfg)
+    elif platform == "snowflake":
+        return clients.snowflake.Resources(config=cfg)
+    else:
+        raise Exception(f"Unknown platform: {platform}")
+
+def Graph(name:str, dry_run:bool=False):
+    return Model(name, dry_run=dry_run)
---
+++ packages/relationalai/relationalai-0.1.tar.gz/relationalai-0.1/src/relationalai/clients/__init__.py
@@ -0,0 +1,1 @@
+from . import azure, snowflake, test, sqlite, config, client
