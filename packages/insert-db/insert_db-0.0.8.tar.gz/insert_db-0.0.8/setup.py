ed8d888dd02991c2c44eb5b8832e88692a8180ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-22 16:03:47 +0000

    Add package insert-db/0.0.8/insert_db-0.0.8.tar.gz

---
+++ packages/insert-db/insert_db-0.0.8.tar.gz/insert_db-0.0.8/setup.py
@@ -0,0 +1,20 @@
+﻿from setuptools import setup, find_packages
+
+setup(
+    name = 'insert_db',
+    version= '0.0.8',
+    description= ' Connect pandas to postgresql database to insert data',
+    author= 'jerry',
+    author_email='dnl6097@gmail.com',
+    url='https://github.com/wisangyun/myfile.git',
+    install_requires = ['tqdm','sqlalchemy'],
+    packages=find_packages(exclude=[]),
+    keywords=['insert_db','database','postgresql','python','pypi'],
+    python_requires = '>=3.8',
+    package_data={},
+    zip_safe = False,
+    classifiers=[
+        "Programming Language :: Python :: 3"
+    ],
+    include_package_data=True
+)
