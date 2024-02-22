f19967cb9e0a92daf0cd71897715af6861a766e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-22 16:03:48 +0000

    Add package entry-on-kitchen/0.1.0/entry-on-kitchen-0.1.0.tar.gz

---
+++ packages/entry-on-kitchen/entry-on-kitchen-0.1.0.tar.gz/entry-on-kitchen-0.1.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='entry-on-kitchen',
+    version='0.1.0',
+    description='A Python module for interacting with entry blocks',
+    author='Endevre Technologies',
+    author_email='contact@endevre.com',
+    packages=find_packages(),
+    install_requires=['axios'],  # Add any dependencies here
+)
