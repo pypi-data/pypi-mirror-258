6be3bc02102d203f051ae7ad8d7fd6905906b1ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-22 03:04:02 +0000

    Add package wbib/0.3.11/wbib-0.3.11-py2.py3-none-any.whl

---
+++ packages/wbib/wbib-0.3.11-py2.py3-none-any.whl/wbib/queries.py
@@ -0,0 +1,468 @@
+import urllib.parse
+
+
+def format_with_prefix(list_of_qids):
+
+    list_with_prefix = ["wd:" + i for i in list_of_qids]
+    return "{ " + " ".join(list_with_prefix) + " }"
+
+
+def get_selector(info, mode="advanced"):
+    """
+    The selector that decides the scope of the dashboard. It MUST have the keywords
+    ?work and ?author.
+    You can override everything here by adapting the query on WDQS:
+    https://w.wiki/3Cmd
+
+    Args:
+        info: either a dict containing complex information for the selector or a list of QIDs
+        mode: a string representing the mode. If "advanced", then a config is expected for the
+          info parameters. If "basic", a list of QIDs is expected. Defaults to "advanced".
+
+    """
+
+    if mode == "advanced":
+        fields_of_work = info["restriction"]["author_area"]
+
+        if fields_of_work is not None:
+            field_of_work_selector = (
+                """
+                VALUES ?field_of_work """
+                + format_with_prefix(fields_of_work)
+                + """
+                ?author wdt:P101 ?field_of_work.
+                """
+            )
+        else:
+            field_of_work_selector = ""
+
+        topic_of_work = info["restriction"]["topic_of_work"]
+
+        if topic_of_work is not None:
+            topic_of_work_selector = (
+                """
+                VALUES ?topics """
+                + format_with_prefix(topic_of_work)
+                + """
+                ?work wdt:P921/wdt:P279* ?topics.
+                """
+            )
+        else:
+            topic_of_work_selector = ""
+
+        region = info["restriction"]["institution_region"]
+
+        if region is not None:
+            region_selector = (
+                """
+                VALUES ?regions """
+                + format_with_prefix(region)
+                + """
+                ?country wdt:P361* ?regions.
+                ?author ( wdt:P108 | wdt:P463 | wdt:P1416 ) / wdt:P361* ?organization . 
+                ?organization wdt:P17 ?country.
+                """
+            )
+        else:
+            region_selector = ""
+
+        gender = info["restriction"]["gender"]
+        if gender is not None:
+            gender_selector = (
+                """
+                VALUES ?gender """
+                + format_with_prefix(gender)
+                + """
+                ?author wdt:P21 ?gender.
+                """
+            )
+        else:
+            gender_selector = ""
+
+        event = info["restriction"]["event"]
+
+        if event is not None:
+
+            # P823 - speaker
+            # P664 - organizer
+            # P1334 - has participant
+            # ^P710 - inverse of (participated in)
+            event_selector = (
+                """
+                VALUES ?event """
+                + format_with_prefix(event)
+                + """
+                ?event wdt:P823 |  wdt:P664 | wdt:P1344 | ^wdt:P710 ?author.
+                """
+            )
+        else:
+            event_selector = ""
+
+        author_is_topic_of = info["restriction"]["author_is_topic_of"]
+
+        if author_is_topic_of is not None:
+            author_is_topic_of_selector = (
+                """
+                VALUES ?biographical_work """
+                + format_with_prefix(author_is_topic_of)
+                + """
+                ?biographical_work wdt:P921 ?author.
+                """
+            )
+        else:
+            author_is_topic_of_selector = ""
+
+        selector = (
+            field_of_work_selector
+            + topic_of_work_selector
+            + region_selector
+            + gender_selector
+            + event_selector
+            + author_is_topic_of_selector
+            + """
+            ?work wdt:P50 ?author.
+            """
+        )
+    else:
+        selector = f"""
+        VALUES ?work {format_with_prefix(info)} .
+        ?work wdt:P50 ?author .
+        """
+    return selector
+
+
+def render_url(query):
+    return "https://query.wikidata.org/embed.html#" + urllib.parse.quote(query, safe="")
+
+
+def get_query_url_for_author_without_affiliation(info, mode="basic"):
+    query = (
+        """
+# tool: scholia
+# title: Employees and affiliated with a specified organization
+SELECT
+  (SAMPLE(?number_of_works_) AS ?works)
+  (SAMPLE(?wikis_) AS ?wikis)
+  ?author ?authorLabel ?authorDescription
+  (SAMPLE(?orcid_) AS ?orcid)
+WITH {
+  SELECT DISTINCT ?author WHERE {
+"""
+        + get_selector(info, mode)
+        + """
+    
+    MINUS {?author ( wdt:P108 | wdt:P463 | wdt:P1416 ) / wdt:P361* ?organization .}
+  } 
+} AS %authors
+WITH {
+  SELECT
+    (COUNT(?work) AS ?number_of_works_) ?author
+  WHERE {
+    INCLUDE %authors
+    OPTIONAL { ?work wdt:P50 ?author . }
+
+    # No biological pathways; they skew the statistics too much 
+    MINUS { ?work wdt:P31 wd:Q4915012 } 
+  } 
+  GROUP BY ?author
+} AS %authors_and_number_of_works
+WHERE {
+  INCLUDE %authors_and_number_of_works
+  OPTIONAL { ?author wdt:P496 ?orcid_ . }
+  OPTIONAL { ?author wikibase:sitelinks ?wikis_ }
+  SERVICE wikibase:label { bd:serviceParam wikibase:language "en,da,de,es,fr,nl,no,ru,sv,zh" . } 
+}
+GROUP BY ?author ?authorLabel ?authorDescription 
+ORDER BY DESC(?works)
+  """
+    )
+
+    return render_url(query)
+
+
+def get_query_url_for_missing_author_items(info, mode="basic"):
+    query = (
+        """
+  #defaultView:Table
+  SELECT
+    (COUNT(?work) AS ?count) 
+    ?author_name
+    (URI(CONCAT(
+        'https://author-disambiguator.toolforge.org/names_oauth.php?doit=Look+for+author&name=',
+        ENCODE_FOR_URI(?author_name))) AS ?author_resolver_url)
+  WHERE {
+    {
+      SELECT DISTINCT ?author_name {
+    """
+        + get_selector(info, mode)
+        + """
+        ?work wdt:P50 ?author . 
+        ?author skos:altLabel | rdfs:label ?author_name_ .
+        
+        # The SELECT-DISTINCT-BIND trick here is due to Stanislav Kralin
+        # https://stackoverflow.com/questions/53933564
+        BIND (STR(?author_name_) AS ?author_name)
+      }
+      LIMIT 2000
+    }
+    OPTIONAL { ?work wdt:P2093 ?author_name . }
+  }
+  GROUP BY ?author_name 
+  HAVING (?count > 0)
+  ORDER BY DESC(?count)
+  """
+    )
+
+    return render_url(query)
+
+
+def get_query_url_for_articles(info, mode="basic"):
+    query = (
+        """
+  SELECT
+  (MIN(?dates) AS ?date)
+  ?work ?workLabel
+  (GROUP_CONCAT(DISTINCT ?type_label; separator=", ") AS ?type)
+  (SAMPLE(?pages_) AS ?pages)
+  ?venue ?venueLabel
+  (GROUP_CONCAT(DISTINCT ?author_label; separator=", ") AS ?authors)
+  WHERE {
+"""
+        + get_selector(info, mode)
+        + """
+  ?work wdt:P50 ?author_all .
+  OPTIONAL {
+    ?author rdfs:label ?author_label_ . FILTER (LANG(?author_label_) = 'en')
+  }
+  BIND(COALESCE(?author_label_, SUBSTR(STR(?author_all), 32)) AS ?author_label)
+  OPTIONAL { ?work wdt:P31 ?type_ . ?type_ rdfs:label ?type_label . FILTER (LANG(?type_label) = 'en') }
+  ?work wdt:P577 ?datetimes .
+  BIND(xsd:date(?datetimes) AS ?dates)
+  OPTIONAL { ?work wdt:P1104 ?pages_ }
+  OPTIONAL { ?work wdt:P1433 ?venue }
+  SERVICE wikibase:label { bd:serviceParam wikibase:language "en,da,de,es,fr,jp,no,ru,sv,zh". }  
+  }
+  GROUP BY ?work ?workLabel ?venue ?venueLabel
+  ORDER BY DESC(?date)  
+
+  """
+    )
+
+    return render_url(query)
+
+
+def get_query_url_for_topic_bubble(info, mode="basic"):
+
+    query = (
+        """
+
+  #defaultView:BubbleChart
+  SELECT ?score ?topic ?topicLabel
+  WITH {
+    SELECT
+      (SUM(?score_) AS ?score)
+      ?topic
+    WHERE {
+          {
+        SELECT (100 AS ?score_) ?topic WHERE {
+        """
+        + get_selector(info, mode)
+        + """
+          ?work  wdt:P921 ?topic . 
+        }
+      }
+      UNION
+      {
+        SELECT (1 AS ?score_) ?topic WHERE {
+        """
+        + get_selector(info, mode)
+        + """
+          ?citing_work wdt:P2860 ?work .
+          ?citing_work wdt:P921 ?topic . 
+        }
+      }
+    }
+    GROUP BY ?topic
+  } AS %results 
+  WHERE {
+    INCLUDE %results
+    SERVICE wikibase:label { bd:serviceParam wikibase:language "[AUTO_LANGUAGE],en,da,de,es,jp,no,ru,sv,zh". }
+  }
+  ORDER BY DESC(?score)
+  LIMIT 50
+
+  """
+    )
+    return render_url(query)
+
+
+def get_topics_as_table(info, mode="basic"):
+    query_3 = (
+        """
+  #defaultView:Table
+  SELECT ?count ?theme ?themeLabel ?example_work ?example_workLabel
+  WITH {
+    SELECT (COUNT(?work) AS ?count) ?theme (SAMPLE(?work) AS ?example_work)
+    WHERE {
+    """
+        + get_selector(info, mode)
+        + """
+      ?work wdt:P921 ?theme .
+    }
+    GROUP BY ?theme
+  } AS %result
+  WHERE {
+    INCLUDE %result
+    SERVICE wikibase:label { bd:serviceParam wikibase:language "en,da,de,es,fr,jp,nl,no,ru,sv,zh" . } 
+  }
+  ORDER BY DESC(?count) 
+
+  """
+    )
+    return render_url(query_3)
+
+
+def get_query_url_for_venues(info, mode="basic"):
+    query_4 = (
+        """
+
+
+  # Venue statistics for a collection
+  SELECT
+    ?count (SAMPLE(?short_name_) AS ?short_name)
+    ?venue ?venueLabel
+    ?topics ?topicsUrl
+  WITH {
+    SELECT
+      (COUNT(DISTINCT ?work) as ?count)
+      ?venue
+      (GROUP_CONCAT(DISTINCT ?topic_label; separator=", ") AS ?topics)
+    WHERE {
+    """
+        + get_selector(info, mode)
+        + """
+      ?work wdt:P1433 ?venue .
+      OPTIONAL {
+        ?venue wdt:P921 ?topic .
+        ?topic rdfs:label ?topic_label . FILTER(LANG(?topic_label) = 'en') }
+    }
+    GROUP BY ?venue
+  } AS %result
+  WHERE {
+    INCLUDE %result
+    OPTIONAL { ?venue wdt:P1813 ?short_name_ . }
+    SERVICE wikibase:label { bd:serviceParam wikibase:language "en,da,de,es,fr,jp,nl,no,ru,sv,zh". }  
+  } 
+  GROUP BY ?count ?venue ?venueLabel ?topics ?topicsUrl
+  ORDER BY DESC(?count)
+
+  """
+    )
+    return render_url(query_4)
+
+
+def get_query_url_for_locations(info, mode="basic"):
+    query_5 = (
+        """
+#defaultView:Map
+SELECT 
+?organization ?organizationLabel 
+(CONCAT(STR(?count), " result(s)") as ?counts)
+?geo ?layer
+?sample_author ?sample_authorLabel
+?sample_work  ?sample_workLabel
+
+  WITH {
+    SELECT DISTINCT ?organization ?geo 
+    (COUNT(DISTINCT ?work) AS ?count) 
+    (SAMPLE(?work) as ?sample_work) 
+    (SAMPLE(?author) as ?sample_author) WHERE {
+
+    """
+        + get_selector(info, mode)
+        + """
+          ?work wdt:P50 ?author .
+      ?author ( wdt:P108 | wdt:P463 | wdt:P1416 ) / wdt:P361* ?organization . 
+      ?organization wdt:P625 ?geo .
+    }
+    GROUP BY ?organization ?geo ?count
+    ORDER BY DESC (?count)
+    LIMIT 2000
+  } AS %organizations
+  WHERE {
+    INCLUDE %organizations
+    BIND(IF( (?count < 1), "No results", IF((?count < 2), "1 result", IF((?count < 5), "1 < results ≤ 10", IF((?count < 101), "10 < results ≤ 100", IF((?count < 1001), "100 < results ≤ 1000", IF((?count < 10001), "1000 < results ≤ 10000", "10000 or more results") ) ) ) )) AS ?layer )
+    SERVICE wikibase:label { bd:serviceParam wikibase:language "[AUTO_LANGUAGE],en". }        
+  }
+  ORDER BY DESC (?count)
+
+
+  """
+    )
+    return render_url(query_5)
+
+
+def get_query_url_for_citing_authors(info, mode="basic"):
+    query_6 = (
+        """
+  SELECT
+    ?count
+    ?citing_author ?citing_authorLabel
+
+    # Either show the ORCID iD or construct part of a URL to search on the ORCID homepage
+    (COALESCE(?orcid_, CONCAT("orcid-search/quick-search/?searchQuery=", ENCODE_FOR_URI(?citing_authorLabel))) AS ?orcid)
+  WITH {
+    SELECT (COUNT(?citing_work) AS ?count) ?citing_author WHERE {
+    """
+        + get_selector(info, mode)
+        + """
+      ?citing_work wdt:P2860 ?work . 
+      ?citing_work wdt:P50 ?citing_author .
+    }
+    GROUP BY ?citing_author 
+    ORDER BY DESC(?count)
+    LIMIT 500
+  } AS %counts
+  WITH {
+    # An author might have multiple ORCID iDs
+    SELECT
+      ?count
+      ?citing_author
+      (SAMPLE(?orcids) AS ?orcid_)
+    WHERE {
+      INCLUDE %counts
+      OPTIONAL { ?citing_author wdt:P496 ?orcids }
+    }
+    GROUP BY ?count ?citing_author
+  } AS %result
+  WHERE {
+    INCLUDE %result
+
+    SERVICE wikibase:label { bd:serviceParam wikibase:language "en,da,de,es,fr,jp,nl,no,ru,sv,zh". }
+  } 
+  ORDER BY DESC(?count)
+
+
+  """
+    )
+    return render_url(query_6)
+
+
+def get_query_url_for_authors(info, mode="basic"):
+    query_7 = (
+        """
+  SELECT (COUNT(?work) AS ?count) ?author ?authorLabel ?orcids  WHERE {
+  """
+        + get_selector(info, mode)
+        + """
+    ?work wdt:P50 ?author .
+      OPTIONAL { ?author wdt:P496 ?orcids }
+    SERVICE wikibase:label { bd:serviceParam wikibase:language "en,da,de,es,fr,jp,nl,no,ru,sv,zh". }
+
+    }
+  GROUP BY ?author ?authorLabel ?orcids 
+  ORDER BY DESC(?count)
+
+  """
+    )
+    return render_url(query_7)
---
+++ packages/wbib/wbib-0.3.11-py2.py3-none-any.whl/wbib/wbib.py
@@ -0,0 +1,231 @@
+"""Main functions for the use by end users.
+"""
+
+import pandas as pd
+from pathlib import Path
+from wbib import queries, render
+from wikidata2df import wikidata2df
+from jinja2 import Environment, PackageLoader
+
+env = Environment(
+    loader=PackageLoader("wbib", "templates"),
+)
+
+DEFAULT_QUERY_OPTIONS = {
+    "map of institutions": {
+        "label": "map of institutions",
+        "query": queries.get_query_url_for_locations,
+    },
+    "articles": {
+        "label": "articles",
+        "query": queries.get_query_url_for_articles,
+    },
+    "list of authors": {
+        "label": "list of authors",
+        "query": queries.get_query_url_for_authors,
+    },
+    "list of topics": {
+        "label": "list of co-studied topics",
+        "query": queries.get_topics_as_table,
+    },
+    "list of journals": {
+        "label": "list of venues",
+        "query": queries.get_query_url_for_venues,
+    },
+    "curation of author items": {
+        "label": "curation: curate author strings that are not yet linked to items",
+        "query": queries.get_query_url_for_missing_author_items,
+    },
+    "curation of author affiliations": {
+        "label": "curation: add affiliation/employment for authors lacking it",
+        "query": queries.get_query_url_for_author_without_affiliation,
+    },
+}
+
+DEFAULT_SESSIONS = [
+    "map of institutions",
+    "articles",
+    "list of authors",
+    "list of topics",
+    "list of journals",
+    "curation of author items",
+    "curation of author affiliations",
+]
+
+EXAMPLE_PAGES = {"home": {"href": "/", "name": "Home"}}
+
+
+def render_dashboard(
+    info,
+    mode="basic",
+    query_options=DEFAULT_QUERY_OPTIONS,
+    sections_to_add=DEFAULT_SESSIONS,
+    site_title="Wikidata Bib Dashboard",
+    site_subtitle="A dashboard for Wikidata-based bibliometrics for a given set of articles.",
+    filepath=".",
+    pages={},
+):
+    """
+    Renders a plain html string coding for a dashboard with embedded Wikidata SPARQL queries.
+    At the same time, it saves the html to the file system via the filepath argument.
+
+
+    ```
+    DEFAULT_SESSIONS = [
+        "map of institutions",
+        "articles",
+        "list of authors",
+        "list of topics",
+        "list of journals",
+        "curation of author items",
+        "curation of author affiliations",
+    ]
+    ```
+
+    ```
+    DEFAULT_QUERY_OPTIONS = {
+        "map of institutions": {
+            "label": "map of institutions",
+            "query": wbib.queries.get_query_url_for_locations
+        },
+        "articles": {
+            "label": "articles",
+            "query": wbib.queries.get_query_url_for_articles
+        },
+        "list of authors": {
+            "label": "list of authors",
+            "query": wbib.queries.get_query_url_for_authors
+        },
+        "list of topics": {
+            "label": "list of co-studied topics",
+            "query": wbib.queries.get_topics_as_table
+        },
+        "list of journals": {
+            "label": "list of venues",
+            "query": wbib.queries.get_query_url_for_venues
+        },
+        "curation of author items": {
+            "label": "curation: curate author strings that are not yet linked to items",
+            "query": wbib.queries.get_query_url_for_missing_author_items
+        },
+        "curation of author affiliations": {
+            "label": "curation: add affiliation/employment for authors lacking it",
+            "query": queries.get_query_url_for_author_without_affiliation
+        }
+    }
+    ```
+    ```
+    EXAMPLE_PAGES = {"home": {"href": "/", "name": "Home"}}
+    ```
+    Args:
+        info (dict): Either a dict containing complex information for the selector or a list of QIDs.
+        mode (str): A string representing the mode. If "advanced", then a config is expected for the
+            info parameters. If "basic", a list of QIDs is expected. Defaults to "advanced".
+        query_options (dict): A set of queries that might be used by the dashboard.
+            See default for customizing queries or labels.
+        sections_to_add (list): Names of the queries to be included in the dashboard.
+            Standard is to include all.
+        site_title (str): A title for the dashboard (if in "basic" mode)
+        site_subtitle (str): A subtitle for the dashboard (if in "basic" mode)
+        filepath (str): The filepath to write the dashboard to.
+        pages (dict): The pages that will be part of the final dashboard, as to make a simple navbar.
+
+    Returns:
+        str: The html content for a static Wikidata-based dashboard.
+            Note: also saves the file to the file system.
+    """
+
+    if mode == "advanced":
+        if not isinstance(info, dict):
+            raise TypeError(
+                "In advanced mode, 'info' needs to be a dict obtained from the standard yaml file"
+            )
+        else:
+            license_statement = info["license_statement"]
+            scholia_credit_statement = info["scholia_credit"]
+            creator_statement = info["creator_credit"]
+            site_title = info["title"]
+            site_subtitle = info["subtitle"]
+            sections_to_add = info["sections"]
+
+    if mode == "basic":
+
+        license_statement = """
+            This content is available under a <a target="_blank" href="https://creativecommons.org/publicdomain/zero/1.0/"> 
+                Creative Commons CC0</a> license.
+        """
+
+        scholia_credit_statement = """
+        SPARQL queries adapted from <a target="_blank" href="https://scholia.toolforge.org/">Scholia</a>
+        """
+
+        creator_statement = """
+        Dashboard  generated via <a target="_blank" href="https://pypi.org/project/wbib/">Wikidata Bib</a>
+        """
+
+    sections = render.render_sections(sections_to_add, query_options, info, mode)
+
+    template = env.get_template("template.html.jinja")
+    rendered_template = template.render(
+        site_title=site_title,
+        site_subtitle=site_subtitle,
+        sections=sections,
+        license_statement=license_statement,
+        scholia_credit=scholia_credit_statement,
+        creator_statement=creator_statement,
+        pages=pages,
+    )
+
+    filename = "{}.html".format(site_title.lower().strip().replace(" ", "_"))
+    path_to_write = (
+        Path(filepath).joinpath(filename) if filepath is "." else Path(filepath)
+    )
+
+    with open(path_to_write, "w") as html:
+        html.write(rendered_template)
+
+    return rendered_template
+
+
+def convert_doi_to_qid(list_of_dois):
+    """
+    Converts a list of DOI ids to Wikidata QIDs.
+
+    Args:
+      list_of_dois (list): DOIs without prefix. For example:
+        ["10.3897/RIO.2.E9342", "10.3389/fimmu.2019.02736"]
+
+    Returns:
+      dict: A dict with two key-value pairs. The "missing" key contains a set
+          with the DOIs not found, the "qids" key contain a set with the
+          QIDs found on Wikidata.
+    """
+
+    formatted_dois = '{ "' + '" "'.join(list_of_dois) + '"}'
+    query = f"""SELECT ?id ?item  ?itemLabel
+  WHERE {{
+      {{
+      SELECT ?item ?id WHERE {{
+          VALUES ?unformatted_id {formatted_dois}
+          BIND(UCASE(?unformatted_id) AS ?id)
+          ?item wdt:P356 ?id.
+      }}
+      }}
+      SERVICE wikibase:label 
+      {{ bd:serviceParam wikibase:language "[AUTO_LANGUAGE],en". }}}}
+  """
+
+    query_result = wikidata2df(query)
+
+    result = {}
+
+    result["qids"] = list(query_result["item"].values)
+    result["missing"] = []
+
+    for doi in list_of_dois:
+        if doi.upper() not in list(query_result["id"].values):
+            result["missing"].append(doi)
+
+    result["qids"] = set(result["qids"])
+    result["missing"] = set(result["missing"])
+    return result
