104ba92566dab8cbaeba691814204af279de7c76
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-22 15:04:23 +0000

    Add package pytest_robotframework/3.2.1/pytest_robotframework-3.2.1.tar.gz

---
+++ packages/pytest_robotframework/pytest_robotframework-3.2.1.tar.gz/pytest_robotframework-3.2.1/tests/fixtures/test_python/test_catch_errors_decorator_with_non_instance_method/conftest.py
@@ -0,0 +1,27 @@
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
+from robot.api.interfaces import ListenerV3
+from typing_extensions import override
+
+from pytest_robotframework import listener  # pyright:ignore[reportDeprecated]
+
+if TYPE_CHECKING:
+    from robot import result, running
+
+
+@listener  # pyright:ignore[reportDeprecated]
+class Listener(ListenerV3):
+    @staticmethod
+    def static_method():
+        pass
+
+    @classmethod
+    def class_method(cls):
+        pass
+
+    @override
+    def start_test(self, data: running.TestCase, result: result.TestCase):
+        self.static_method()
+        self.class_method()
