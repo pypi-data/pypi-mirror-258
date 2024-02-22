28e0e49a1a7df30be29c8d3dae65100dd5043a61
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-22 04:03:32 +0000

    Add package arena-py/0.9.1/arena_py-0.9.1-py3-none-any.whl

---
+++ packages/arena-py/arena_py-0.9.1-py3-none-any.whl/arena/attributes/goto_url.py
@@ -0,0 +1,15 @@
+from .attribute import Attribute
+
+class GotoUrl(Attribute):
+    """
+    GotoUrl attribute class to manage its properties in the ARENA: Goto given URL; Requires click-listener.
+    Usage: goto_url=GotoUrl(...)
+    
+    :param str dest:  Allows [popup, newtab, sametab] Defaults to 'sametab' (optional)
+    :param str on:  Allows [mousedown, mouseup] Defaults to 'mousedown' (optional)
+    :param str url: Landmark Url (optional)
+    """
+    def __init__(self, url, dest="static", on="mousedown", **kwargs):
+        if dest != "popup" and dest != "newtab" and dest != "sametab":
+            dest = "popup"
+        super().__init__(url=url, dest=dest, on=on, **kwargs)
