f5097b438ad46db2f99f1e1dc6a5143c79d9fdc2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-22 04:03:18 +0000

    Add package plasma-fhir-client-py/0.0.4/plasma_fhir_client_py-0.0.4.tar.gz

---
+++ packages/plasma-fhir-client-py/plasma_fhir_client_py-0.0.4.tar.gz/plasma_fhir_client_py-0.0.4/plasma-fhir-client-py/__init__.py
@@ -0,0 +1,8 @@
+"""`plasma-fhir-client-py `: Plasma FHIR Client for Python"""
+
+__author__ = "Eric Morgan <plasmafhir@gmail.com>"
+__version__ = "0.0.4"
+
+__all__ = ["PlasmaPlatformClient"]
+
+from .PlasmaPlatformClient import PlasmaPlatformClient
