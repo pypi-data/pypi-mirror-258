43ae28650173dbe2243e413f391a6d700ecbd01c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-22 15:04:28 +0000

    Add package fakeoai/0.1.3/FakeOAI-0.1.3.tar.gz

---
+++ packages/fakeoai/FakeOAI-0.1.3.tar.gz/FakeOAI-0.1.3/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+from fakeoai import __version__
+
+with open('README.md', 'r', encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='FakeOAI',
+    version=__version__,
+    python_requires='>=3.7',
+    author='baobao',
+    author_email='yuanbao@fakeopenai.cn',
+    keywords='OpenAI ChatGPT',
+    description='A Package for local deployment of ChatGPT.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/FakeOpenAI/FakeOpenAI',
+    packages=find_packages('fakeoai'),
+    include_package_data=True
+)
