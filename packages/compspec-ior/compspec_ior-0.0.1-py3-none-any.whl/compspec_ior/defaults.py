6007c2cced06ae1b1c136a9874b29e7277bf41c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-22 15:04:31 +0000

    Add package compspec-ior/0.0.1/compspec_ior-0.0.1-py3-none-any.whl

---
+++ packages/compspec-ior/compspec_ior-0.0.1-py3-none-any.whl/compspec_ior/defaults.py
@@ -0,0 +1,4 @@
+# These are required to be given to compspec
+spec_version = "0.0.0"
+schema_url = "https://raw.githubusercontent.com/compspec/compspec-ior/main/compspec_ior/schema.json"
+namespace = "io.compspec.ior"
