fbd77e876071a756a38adf69c4714b3ab3d9a55d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-22 15:03:31 +0000

    Add package zcbot-web-core/0.6/zcbot-web-core-0.6.tar.gz

---
+++ packages/zcbot-web-core/zcbot-web-core-0.6.tar.gz/zcbot-web-core-0.6/setup.py
@@ -0,0 +1,20 @@
+from distutils.core import setup
+from setuptools import find_packages
+
+with open('README.rst', 'r') as f:
+    long_description = f.read()
+
+setup(name='zcbot-web-core',
+      version='0.6',
+      description='zcbot web core for zsodata',
+      long_description=long_description,
+      author='zsodata',
+      author_email='team@zso.io',
+      url='http://www.zsodata.com',
+      install_requires=[],
+      python_requires='>=3.7',
+      license='BSD License',
+      packages=find_packages(),
+      platforms=['all'],
+      include_package_data=True
+      )
