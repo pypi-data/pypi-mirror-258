7dc69e0d569af3fefc1090d2d25228ea58bc01ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-22 17:03:58 +0000

    Add package tllab-common/2024.2.4/tllab_common-2024.2.4.tar.gz

---
+++ packages/tllab-common/tllab_common-2024.2.4.tar.gz/tllab_common-2024.2.4/tllab_common/__init__.py
@@ -0,0 +1,14 @@
+from pathlib import Path
+from importlib.metadata import version
+try:
+    __version__ = version(Path(__file__).parent.name)
+except Exception:
+    __version__ = "unknown"
+
+try:
+    with open(Path(__file__).parent.parent / ".git" / "HEAD") as g:
+        head = g.read().split(":")[1].strip()
+    with open(Path(__file__).parent.parent / ".git" / head) as h:
+        __git_commit_hash__ = h.read().rstrip("\n")
+except Exception:
+    __git_commit_hash__ = "unknown"
