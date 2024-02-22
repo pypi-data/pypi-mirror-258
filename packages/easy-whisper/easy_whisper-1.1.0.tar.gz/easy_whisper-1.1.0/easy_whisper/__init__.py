9a0c6c3f9c4e3de4a6ea48af83f5e979cfb0cb2a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-22 14:03:03 +0000

    Add package easy-whisper/1.1.0/easy_whisper-1.1.0.tar.gz

---
+++ packages/easy-whisper/easy_whisper-1.1.0.tar.gz/easy_whisper-1.1.0/easy_whisper/__init__.py
@@ -0,0 +1,10 @@
+__app_name__ = "easy_whisper"
+__version__ = "1.1.0"       
+__all__ = [
+        '__main__',
+        'ui_share',
+        'cli_ui',
+        'tk_ui',
+        'trans',
+        'api_trans'
+        ]
