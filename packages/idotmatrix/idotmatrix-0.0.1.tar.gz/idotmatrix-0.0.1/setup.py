76234f334936190479572e727d16281c721cd099
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-22 14:03:20 +0000

    Add package idotmatrix/0.0.1/idotmatrix-0.0.1.tar.gz

---
+++ packages/idotmatrix/idotmatrix-0.0.1.tar.gz/idotmatrix-0.0.1/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+setup(
+    name="idotmatrix",
+    version="0.0.1",
+    description="configure any iDotMatrix compatible 16x16 or 32x32 pixel display without the chinese iDotMatrix android / iOS app.",
+    url="https://github.com/shuds13/pyexample",
+    author="Kalle Minkner",
+    author_email="info@kalleminkner.de",
+    license="GPLv3",
+    packages=["idotmatrix"],
+    install_requires=[
+        "bleak",
+        "pillow",
+    ],
+    classifiers=[
+        "Development Status :: 2 - Pre-Alpha",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: POSIX :: Linux",
+        "Programming Language :: Python :: 3",
+    ],
+)
