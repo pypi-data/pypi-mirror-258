b8310b3c9d6a540f55359c20ab08edd19a8e1be8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-22 15:04:30 +0000

    Add package wealthbox/0.3.2/wealthbox-0.3.2.tar.gz

---
+++ packages/wealthbox/wealthbox-0.3.2.tar.gz/wealthbox-0.3.2/wealthbox/__init__.py
@@ -0,0 +1,58 @@
+from json import JSONDecodeError
+import requests
+
+__version__ = '0.1.0'
+
+class WealthBox(object):
+    def __init__(self, token=None):
+        self.token = token
+        self.user_id = None
+        self.base_url = f"https://api.crmworkspace.com/v1/"
+    
+    def api_request(self, endpoint, params=None):
+        url = self.base_url + endpoint
+        page = 1
+        total_pages = 9999999999
+        if params is None:
+            params = {}
+        
+        params.setdefault('per_page', '5000')
+        results = []
+
+        while page <= total_pages:
+            params['page'] = page
+            res = requests.get(url,
+                            params=params,
+                            headers={'ACCESS_TOKEN': self.token})
+            try:
+                res_json = res.json()
+                total_pages = res_json['meta']['total_pages']
+                results.extend(res_json[endpoint])
+                page += 1
+            except:# JSONDecodeError:
+                print(res.text)
+                return results
+
+        return results
+       
+    def get_contacts(self, filters=None):
+        return self.api_request('contacts',params=filters)
+
+    def get_tasks(self, filters=None):
+        return self.api_request('tasks',params=filters)
+
+    def get_my_user_id(self):
+        self.user_id = self.api_request('me')['current_user']['id']
+        return self.user_id
+
+    def get_my_tasks(self):
+        if self.user_id is None:
+            self.get_my_user_id()
+        return self.get_tasks({'assigned_to':self.user_id})
+    
+    def update_contact(self,contact_id,updates_dict):
+        # Update a contact in WealthBox with a given ID and field data
+        res = requests.put(self.base_url + f'contacts/{contact_id}',
+                            json=updates_dict,
+                            headers={'ACCESS_TOKEN': self.token})
+        return res.json()
