2746731dfdb6747c26fd046a8f1110a7a62594db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-22 17:03:58 +0000

    Add package graphene-django-query-optimizer/0.1.10/graphene_django_query_optimizer-0.1.10.tar.gz

---
+++ packages/graphene-django-query-optimizer/graphene_django_query_optimizer-0.1.10.tar.gz/graphene_django_query_optimizer-0.1.10/query_optimizer/typing.py
@@ -0,0 +1,94 @@
+from __future__ import annotations
+
+from typing import (
+    TYPE_CHECKING,
+    Any,
+    Callable,
+    Collection,
+    Generator,
+    Hashable,
+    Iterable,
+    Literal,
+    NamedTuple,
+    Optional,
+    TypeVar,
+    Union,
+)
+
+from graphene.relay.connection import ConnectionOptions
+from graphene_django.types import DjangoObjectTypeOptions
+
+# New in version 3.10
+try:
+    from typing import ParamSpec, TypeAlias, TypeGuard
+except ImportError:
+    from typing_extensions import ParamSpec, TypeAlias, TypeGuard
+
+
+from django.core.handlers.wsgi import WSGIRequest
+from django.db.models import (
+    Field,
+    ForeignKey,
+    ForeignObject,
+    ForeignObjectRel,
+    ManyToManyField,
+    ManyToManyRel,
+    ManyToOneRel,
+    Model,
+    OneToOneField,
+)
+from graphql import GraphQLResolveInfo
+
+if TYPE_CHECKING:
+    from django.contrib.auth.models import AnonymousUser, User
+    from django.contrib.contenttypes.fields import GenericForeignKey, GenericRelation
+
+__all__ = [
+    "Any",
+    "Callable",
+    "Collection",
+    "Generator",
+    "GQLInfo",
+    "Hashable",
+    "Iterable",
+    "Literal",
+    "ModelField",
+    "NamedTuple",
+    "OptimizedDjangoOptions",
+    "Optional",
+    "ParamSpec",
+    "PK",
+    "QueryCache",
+    "StoreStr",
+    "TableName",
+    "ToManyField",
+    "ToOneField",
+    "TypeGuard",
+    "TypeOptions",
+    "TypeVar",
+    "Union",
+]
+
+
+TModel = TypeVar("TModel", bound=Model)
+TableName: TypeAlias = str
+StoreStr: TypeAlias = str
+PK: TypeAlias = Any
+QueryCache: TypeAlias = dict[TableName, dict[StoreStr, dict[PK, TModel]]]
+ModelField: TypeAlias = Union[Field, ForeignObjectRel, "GenericForeignKey"]
+ToManyField: TypeAlias = Union["GenericRelation", ManyToManyField, ManyToOneRel, ManyToManyRel]
+ToOneField: TypeAlias = Union["GenericRelation", ForeignObject, ForeignKey, OneToOneField]
+TypeOptions: TypeAlias = Union[DjangoObjectTypeOptions, ConnectionOptions]
+AnyUser: TypeAlias = Union["User", "AnonymousUser"]
+
+
+class UserHintedWSGIRequest(WSGIRequest):
+    user: AnyUser
+
+
+class GQLInfo(GraphQLResolveInfo):
+    context: UserHintedWSGIRequest
+
+
+class OptimizedDjangoOptions(DjangoObjectTypeOptions):
+    max_complexity: int
---
+++ packages/graphene-django-query-optimizer/graphene_django_query_optimizer-0.1.10.tar.gz/graphene_django_query_optimizer-0.1.10/query_optimizer/utils.py
@@ -0,0 +1,124 @@
+from __future__ import annotations
+
+import logging
+from functools import wraps
+from typing import TYPE_CHECKING
+
+from django.db.models import ForeignKey, QuerySet
+from graphene import Connection
+from graphene.types.definitions import GrapheneObjectType
+from graphene_django import DjangoObjectType
+from graphql.execution.execute import get_field_def
+
+from .errors import OptimizerError
+from .settings import optimizer_settings
+
+if TYPE_CHECKING:
+    from graphql import GraphQLOutputType, SelectionNode
+
+    from .typing import (
+        Callable,
+        Collection,
+        GQLInfo,
+        ModelField,
+        ParamSpec,
+        ToManyField,
+        ToOneField,
+        TypeGuard,
+        TypeVar,
+    )
+
+    T = TypeVar("T")
+    P = ParamSpec("P")
+
+
+__all__ = [
+    "get_field_type",
+    "get_selections",
+    "get_underlying_type",
+    "is_foreign_key_id",
+    "is_optimized",
+    "is_to_many",
+    "is_to_one",
+    "mark_optimized",
+    "mark_unoptimized",
+    "maybe_skip_optimization_on_error",
+    "optimizer_logger",
+    "unique",
+]
+
+
+optimizer_logger = logging.getLogger("query_optimizer")
+
+
+def is_foreign_key_id(model_field: ModelField, name: str) -> bool:
+    return isinstance(model_field, ForeignKey) and model_field.name != name and model_field.get_attname() == name
+
+
+def get_underlying_type(field_type: GraphQLOutputType) -> GrapheneObjectType:
+    while hasattr(field_type, "of_type"):
+        field_type = field_type.of_type
+    return field_type
+
+
+def is_to_many(model_field: ModelField) -> TypeGuard[ToManyField]:
+    return bool(model_field.one_to_many or model_field.many_to_many)
+
+
+def is_to_one(model_field: ModelField) -> TypeGuard[ToOneField]:
+    return bool(model_field.many_to_one or model_field.one_to_one)
+
+
+def get_field_type(info: GQLInfo) -> GrapheneObjectType:
+    field_node = info.field_nodes[0]
+    field_def = get_field_def(info.schema, info.parent_type, field_node)
+    return get_underlying_type(field_def.type)
+
+
+def get_selections(info: GQLInfo) -> tuple[SelectionNode, ...]:
+    field_node = info.field_nodes[0]
+    selection_set = field_node.selection_set
+    return () if selection_set is None else selection_set.selections
+
+
+def unique(items: Collection[T]) -> list[T]:
+    return list(dict.fromkeys(items))
+
+
+def can_optimize(info: GQLInfo) -> bool:
+    return_type = get_underlying_type(info.return_type)
+
+    return isinstance(return_type, GrapheneObjectType) and (
+        issubclass(return_type.graphene_type, (DjangoObjectType, Connection))
+    )
+
+
+def mark_optimized(queryset: QuerySet) -> None:
+    """Mark queryset as optimized so that later optimizers know to skip optimization"""
+    queryset._hints[optimizer_settings.OPTIMIZER_MARK] = True  # type: ignore[attr-defined]
+
+
+def mark_unoptimized(queryset: QuerySet) -> None:  # pragma: no cover
+    """Mark queryset as unoptimized so that later optimizers will run optimization"""
+    queryset._hints.pop(optimizer_settings.OPTIMIZER_MARK, None)  # type: ignore[attr-defined]
+
+
+def is_optimized(queryset: QuerySet) -> bool:
+    """Has the queryset be optimized?"""
+    return queryset._hints.get(optimizer_settings.OPTIMIZER_MARK, False)  # type: ignore[no-any-return, attr-defined]
+
+
+def maybe_skip_optimization_on_error(func: Callable[P, T]) -> Callable[P, T]:
+    @wraps(func)
+    def wrapper(*args: P.args, **kwargs: P.kwargs) -> T:
+        try:
+            return func(*args, **kwargs)
+        except OptimizerError:
+            raise
+        except Exception as error:  # pragma: no cover
+            if optimizer_settings.SKIP_OPTIMIZATION_ON_ERROR:
+                optimizer_logger.info("Something went wrong during the optimization process.", exc_info=error)
+                return args[0]  # original queryset
+            raise
+
+    return wrapper
