a963f5b2629c9cdbaf6a900d1665528e6e2b8b4e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-22 03:04:07 +0000

    Add package multipers/1.2.1/multipers-1.2.1-cp311-cp311-macosx_12_0_universal2.whl

---
+++ packages/multipers/multipers-1.2.1-cp311-cp311-macosx_12_0_universal2.whl/multipers/__init__.py
@@ -0,0 +1,12 @@
+# Doc
+import multipers.simplex_tree_multi
+import multipers.multiparameter_module_approximation
+import multipers.slicer
+import multipers.io
+
+
+# Shortcuts
+from multipers.simplex_tree_multi import SimplexTreeMulti
+from multipers._signed_measure_meta import signed_measure
+from multipers._slicer_meta import Slicer
+from multipers.multiparameter_module_approximation import module_approximation
