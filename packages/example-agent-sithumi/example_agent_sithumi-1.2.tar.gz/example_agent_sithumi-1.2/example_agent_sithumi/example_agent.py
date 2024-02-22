ff61f43bcfdb9e4143c86914db1892c8017d5ca4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-22 15:03:29 +0000

    Add package example-agent-sithumi/1.2/example_agent_sithumi-1.2.tar.gz

---
+++ packages/example-agent-sithumi/example_agent_sithumi-1.2.tar.gz/example_agent_sithumi-1.2/example_agent_sithumi/example_agent.py
@@ -0,0 +1,26 @@
+"""
+This is an example_agent_sithumi for public python module.
+"""
+
+import pandas
+import logging
+
+
+class HelloSithumi:
+    def __init__(self) -> None:
+        """
+        This will initiate the HelloSithumi demo class.
+        """
+        self.name = "Sithumi"
+        self.age = 26
+
+    def run(self) -> str:
+        """
+        This function will return Sithumi's user info.
+        :return:
+            Info: Name and Age as a description.
+        """
+        try:
+            return f"My name is {self.name}. I'm {self.age} years old."
+        except Exception as exception:
+            logging.error(exception)
