b302e8636ad5966c93484a5c3fef433e923bb44e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-22 14:03:30 +0000

    Add package functime/0.9.5/functime-0.9.5-cp38-none-win_amd64.whl

---
+++ packages/functime/functime-0.9.5-cp38-none-win_amd64.whl/functime/__init__.py
@@ -0,0 +1,9 @@
+"functime: Time-series machine learning at scale."
+
+__version__ = "0.9.5"
+
+import logging
+
+from functime.feature_extractors import FeatureExtractor  # noqa: F401
+
+logging.basicConfig(level=logging.INFO)
