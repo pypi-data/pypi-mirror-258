8af63993d549ec1d299c4fc5f326c7d10c34dd01
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-22 17:03:55 +0000

    Add package acme-dns-azure/0.1.1/acme_dns_azure-0.1.1-py3-none-any.whl

---
+++ packages/acme-dns-azure/acme_dns_azure-0.1.1-py3-none-any.whl/acme_dns_azure/___init___.py
@@ -0,0 +1,18 @@
+from acme_dns_azure.data import (
+    RotationResult,
+    DomainReference,
+    RotationCertificate,
+    CertbotResult,
+)
+from acme_dns_azure.client import AcmeDnsAzureClient
+from acme_dns_azure.log import setup_custom_logger
+
+__author__ = "ZEISS Digital Innovation Partners"
+__all__ = [
+    "AcmeDnsAzureClient",
+    "RotationResult",
+    "DomainReference",
+    "RotationCertificate",
+    "CertbotResult",
+    "setup_custom_logger",
+]
