5d08077cbe2b34935694a3cfddf59d3f05725dcb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-22 03:03:44 +0000

    Add package django-url-tokenizer/0.1.16/django-url-tokenizer-0.1.16.tar.gz

---
+++ packages/django-url-tokenizer/django-url-tokenizer-0.1.16.tar.gz/django-url-tokenizer-0.1.16/urltokenizer/models.py
@@ -0,0 +1,26 @@
+from django.conf import settings
+from django.db import models
+
+from .enums import Channel
+
+
+class Log(models.Model):
+    created_at = models.DateTimeField(null=True)
+    checked_at = models.DateTimeField(null=True)
+    token_type = models.CharField(max_length=255, null=True)
+    uidb64 = models.CharField(max_length=255, null=True)
+    hash = models.CharField(max_length=255, unique=True, null=True)
+    name = models.CharField(max_length=255, null=True)
+    email = models.EmailField(null=True)
+    phone = models.CharField(max_length=255, null=True)
+    channel = models.CharField(max_length=255, choices=Channel.choices, null=True)
+    precondition_failed = models.BooleanField(default=False)
+    sent = models.BooleanField(default=False)
+    errors = models.CharField(max_length=255, null=True)
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE, null=True
+    )
+
+    @property
+    def checked(self) -> bool:
+        return self.checked_at is not None
