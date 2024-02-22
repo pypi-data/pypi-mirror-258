11356186d89a6a2e1755717c821fccab31a765f2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-22 14:02:55 +0000

    Add package gemmaai/0.1.36/gemmaai-0.1.36.tar.gz

---
+++ packages/gemmaai/gemmaai-0.1.36.tar.gz/gemmaai-0.1.36/gemmaai/__init__.py
@@ -0,0 +1,12 @@
+from .gemmaai import GemmaAI
+
+__all__ = ["GemmaAI"]
+
+from importlib import metadata
+
+try:
+    __version__ = metadata.version(__package__)
+except metadata.PackageNotFoundError:
+    # Case where package metadata is not available.
+    __version__ = ""
+del metadata  # optional, avoids polluting the results of dir(__package__)
