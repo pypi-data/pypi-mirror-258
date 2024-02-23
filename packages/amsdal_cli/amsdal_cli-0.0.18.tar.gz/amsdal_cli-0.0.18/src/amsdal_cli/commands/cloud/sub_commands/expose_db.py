f7dd92ce02d1671bae21e554e8bb0d93ae18cf40
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-23 06:04:38 +0000

    Add package amsdal_cli/0.0.18/amsdal_cli-0.0.18.tar.gz

---
+++ packages/amsdal_cli/amsdal_cli-0.0.18.tar.gz/amsdal_cli-0.0.18/src/amsdal_cli/commands/cloud/sub_commands/expose_db.py
@@ -0,0 +1,32 @@
+from pathlib import Path
+
+import typer
+from amsdal.errors import AmsdalCloudError
+from amsdal.manager import AmsdalManager
+from amsdal_utils.config.manager import AmsdalConfigManager
+from rich import print
+
+from amsdal_cli.commands.cloud.app import cloud_sub_app
+from amsdal_cli.utils.cli_config import CliConfig
+
+
+@cloud_sub_app.command(name='expose_db')
+def expose_db_command(ctx: typer.Context) -> None:
+    """
+    Add your IP to the whitelist of the database and return the connection configs.
+    """
+
+    cli_config: CliConfig = ctx.meta['config']
+    AmsdalConfigManager().load_config(Path('./config.yml'))
+    manager = AmsdalManager()
+    manager.authenticate()
+
+    try:
+        response = manager.cloud_actions_manager.expose_db(
+            application_uuid=cli_config.application_uuid,
+            application_name=cli_config.application_name,
+        )
+        print(response)
+    except AmsdalCloudError as e:
+        print(f'[red]{e}[/red]')
+        return
