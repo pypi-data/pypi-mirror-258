0e209c68d504c80700b1c38531626e3c316b0528
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-23 17:04:00 +0000

    Add package lumberman/0.45.1/lumberman-0.45.1-py3-none-any.whl

---
+++ packages/lumberman/lumberman-0.45.1-py3-none-any.whl/lumberman/cli/manipulation.py
@@ -0,0 +1,99 @@
+from typing import TYPE_CHECKING, Annotated, Optional
+
+from rich import print
+
+from lumberman.cli.config import ISSUE_CONTROLLER, STACK_MANIPULATOR, STACK_NAVIGATOR, STACK_OP
+from lumberman.cli.location import Location, LocationCLIOption
+
+from .markdown import print_md
+
+if TYPE_CHECKING:
+    from ..issues.provider import Issue
+
+
+def _select_issue() -> "Issue":
+    selected_issue = ISSUE_CONTROLLER.select_issue()
+    print_md(f"# {selected_issue.title!s}")
+    if selected_issue.description:
+        print_md("## Description")
+        description = "\n".join([f"> {line}" for line in selected_issue.description.split("\n")])
+        print_md(description)
+    return selected_issue
+
+
+def insert(location: Annotated[Optional[Location], "typer.Argument()"] = Location.up):
+    """Prompt to create a new item on the current stack. Defaults to creating an item in between the current item and the next item."""
+    with STACK_OP(sync_time="exit", sync_pull_requests=False):
+        selected_issue = _select_issue()
+
+        if location == Location.trunk:
+            STACK_NAVIGATOR.trunk()
+        if location == Location.bottom:
+            STACK_NAVIGATOR.bottom()
+        if location == Location.top:
+            STACK_NAVIGATOR.top()
+        if location == Location.up:
+            pass
+        if location == Location.down:
+            STACK_NAVIGATOR.down()
+
+        STACK_MANIPULATOR.insert(selected_issue)
+        ISSUE_CONTROLLER.label_issue_in_progress(selected_issue)
+        ISSUE_CONTROLLER.provider.assign(selected_issue, assignee="@me")
+
+
+def move():
+    """Move the current item to a new location in the stack."""
+    with STACK_OP(sync_time="exit", sync_pull_requests=True):
+        STACK_MANIPULATOR.move()
+
+
+def delete():
+    """Prompt to delete an item."""
+    with STACK_OP(sync_time="exit", sync_pull_requests=False):
+        STACK_MANIPULATOR.delete()
+
+
+def fork(location: LocationCLIOption = Location.bottom):
+    """Fork into a new stack and add an item. Defaults to forking from the first item in the current stack."""
+    with STACK_OP(sync_time="enter", sync_pull_requests=False):
+        selected_issue = _select_issue()
+
+        if location == Location.bottom:
+            STACK_NAVIGATOR.bottom()
+            STACK_NAVIGATOR.up()
+        elif location == Location.top:
+            STACK_NAVIGATOR.top()
+            STACK_NAVIGATOR.down()
+        elif location == Location.up:
+            pass  # No need to do anything, already in the correct location
+        elif location == Location.down:
+            STACK_NAVIGATOR.down()
+
+        STACK_MANIPULATOR.fork(selected_issue)
+        ISSUE_CONTROLLER.label_issue_in_progress(selected_issue)
+        ISSUE_CONTROLLER.provider.assign(selected_issue, assignee="@me")
+
+
+def new():
+    """Start a new stack on top of trunk."""
+    with STACK_OP(sync_time="none", sync_pull_requests=False):
+        selected_issue = _select_issue()
+        STACK_MANIPULATOR.sync(sync_pull_requests=False)
+        STACK_NAVIGATOR.trunk()
+        STACK_MANIPULATOR.fork(selected_issue)
+        ISSUE_CONTROLLER.label_issue_in_progress(selected_issue)
+        ISSUE_CONTROLLER.provider.assign(selected_issue, assignee="@me")
+
+
+def sync(automerge: bool = False, draft: bool = False, squash: bool = False):
+    """Synchronize all state, ensuring the stack is internally in sync, and in sync with the remote. Creates PRs if needed."""
+    with STACK_OP(sync_time="none", sync_pull_requests=False):
+        STACK_MANIPULATOR.sync(
+            automerge=automerge, squash=squash, draft=draft, sync_pull_requests=True
+        )
+        print(":rocket: [bold green]Stack synced![/bold green]")
+
+
+if __name__ == "__main__":
+    move()
