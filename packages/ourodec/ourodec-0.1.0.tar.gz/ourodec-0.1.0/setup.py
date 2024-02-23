39aaf83ee2e7ba5c74660f66fadcaf8d81bc0352
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-23 15:04:24 +0000

    Add package ourodec/0.1.0/ourodec-0.1.0.tar.gz

---
+++ packages/ourodec/ourodec-0.1.0.tar.gz/ourodec-0.1.0/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='ourodec',
+    version='0.1.0',
+    packages=find_packages(),
+    install_requires=[
+        "numpy>=1.21.0",
+        "tqdm",
+        "transformers>=4.36.0",
+        "datasets>=1.6.2",
+        "human_eval",
+        "lade",
+        "jinja2>=3.1.3"
+    ],
+    author='TsinghuaNLP',
+    author_email='zwl23@mails.tsinghua.edu.cn, huang-yx21@mails.tsinghua.edu.cn, hanxu2022@tsinghua.edu.cn',
+    description='Ouroboros',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+)
