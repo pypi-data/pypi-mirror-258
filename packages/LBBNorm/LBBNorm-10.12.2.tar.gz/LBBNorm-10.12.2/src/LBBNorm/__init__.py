1ea4ed66def94db342557f202a8ced5d6237f2ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-23 03:04:09 +0000

    Add package LBBNorm/10.12.2/LBBNorm-10.12.2.tar.gz

---
+++ packages/LBBNorm/LBBNorm-10.12.2.tar.gz/LBBNorm-10.12.2/src/LBBNorm/__init__.py
@@ -0,0 +1,8 @@
+from .macenko import Macenko
+from .vahadane import Vahadane
+from .adaptive_color_deconvolution import AdaptiveColorDeconvolution
+from .reinhard import Reinhard
+from .modified_reinhard import ModifiedReinhard
+
+
+from .slide_patcher import WholeSlidePatcher
