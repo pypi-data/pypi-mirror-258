486644aedb84ffb90962d63938f8becc679523e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-23 03:04:09 +0000

    Add package supercell-downloader/1/supercell_downloader-1.tar.gz

---
+++ packages/supercell-downloader/supercell_downloader-1.tar.gz/supercell_downloader-1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="supercell_downloader",
+    version="1",
+    description="Library for downloading Supercell game assets directly from server",
+    author="Anybodyy",
+    author_email="polnyjloh76@gmail.com",
+    license="MIT",
+    url="https://github.com/sc-workshop/better-sc-downloader",
+    packages=find_packages()
+)
