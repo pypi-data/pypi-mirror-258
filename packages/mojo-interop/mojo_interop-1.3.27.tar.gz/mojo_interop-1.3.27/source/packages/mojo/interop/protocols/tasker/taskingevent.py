2d1638dc7b9cf0488e20599627b0ad73e441001a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-23 14:03:55 +0000

    Add package mojo-interop/1.3.27/mojo_interop-1.3.27.tar.gz

---
+++ packages/mojo-interop/mojo_interop-1.3.27.tar.gz/mojo_interop-1.3.27/source/packages/mojo/interop/protocols/tasker/taskingevent.py
@@ -0,0 +1,43 @@
+
+from typing import Optional, Union
+
+class TaskingEvent:
+    """
+        A :class:`TaskingEvent` object used to report events.
+    """
+
+    def __init__(self, tasking_id: str, event_name: str, payload: Optional[dict]=None):
+        self._tasking_id = tasking_id
+        self._event_name = event_name
+        self._payload = payload
+        return
+    
+    @property
+    def event_name(self) -> str:
+        return self._event_name
+    
+    @property
+    def payload(self) -> Union[dict, None]:
+        return self._payload
+
+    @property
+    def tasking_id(self) -> str:
+        return self._tasking_id
+        
+    @classmethod
+    def from_dict(self, data: dict) -> "TaskingEvent":
+        self._tasking_id = data["tasking-id"]
+        self._event_name = data["event-name"]
+        self._payload = data["payload"]
+        return
+
+    def as_dict(self) -> dict:
+
+        rtnval = {
+            "event-name": self._event_name,
+            "payload": self._payload,
+            "tasking-id": self._tasking_id
+        }
+
+        return rtnval
+    
