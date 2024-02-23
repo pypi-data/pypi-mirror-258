f3ad5debb7a9106afef8ced5fd734e4c21af6198
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-23 03:04:08 +0000

    Add package langchain_chinese/0.2.11/langchain_chinese-0.2.11.tar.gz

---
+++ packages/langchain_chinese/langchain_chinese-0.2.11.tar.gz/langchain_chinese-0.2.11/langchain_chinese/__init__.py
@@ -0,0 +1,10 @@
+from langchain_chinese.zhipuai.base import (
+    ChatZhipuAI,
+    _convert_dict_to_message
+)
+
+from langchain_chinese.__version__ import __version__
+
+__all__ = [
+    "ChatZhipuAI",
+]
