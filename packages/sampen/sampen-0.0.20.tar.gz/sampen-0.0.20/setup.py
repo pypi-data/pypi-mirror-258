754cd27e23f930c58b966db416ec83c507a9b558
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-23 16:04:07 +0000

    Add package sampen/0.0.20/sampen-0.0.20.tar.gz

---
+++ packages/sampen/sampen-0.0.20.tar.gz/sampen-0.0.20/setup.py
@@ -0,0 +1,41 @@
+from setuptools import setup
+
+
+setup(
+    name='sampen',
+    version='0.0.20',
+    description='A Python module to calculate an estimation of the '
+                'Sample Entropy of a time series.',
+    long_description=open("README.rst").read(),
+    url='https://readthedocs.org/projects/sampen/',
+    download_url='https://github.com/Kinsa/sampen/tarball/0.0.20/',
+    author='Kinsa Creative Incorporated',
+    author_email='enquiries@kinsa.cc',
+    maintainer='Kinsa Creative Incorporated',
+    maintainer_email='enquiries@kinsa.cc',
+    license='GNU',
+    packages=['sampen'],
+    install_requires=[
+        'numpy>=1.9.2',
+    ],
+    keywords=['sample entropy', 'sampen'],
+    test_suite="tests.test_sampen",
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Topic :: Scientific/Engineering :: Bio-Informatics',
+        'Topic :: Scientific/Engineering :: Information Analysis',
+        'Topic :: Scientific/Engineering :: Mathematics',
+    ]
+)
