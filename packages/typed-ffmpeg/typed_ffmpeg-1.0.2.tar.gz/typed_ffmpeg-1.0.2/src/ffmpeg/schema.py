75c42c14f86177ba955e6323ec68344d6e7c60a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-23 15:04:16 +0000

    Add package typed-ffmpeg/1.0.2/typed_ffmpeg-1.0.2.tar.gz

---
+++ packages/typed-ffmpeg/typed_ffmpeg-1.0.2.tar.gz/typed_ffmpeg-1.0.2/src/ffmpeg/schema.py
@@ -0,0 +1,34 @@
+"""
+Defines the basic schema for the ffmpeg command line options.
+"""
+
+import enum
+
+
+class StreamType(enum.Enum):
+    """
+    The type of a stream. (audio or video)
+    """
+
+    audio = "audio"
+    """it is an audio stream"""
+    video = "video"
+    """it is a video stream"""
+
+
+class Default(str):
+    """
+    This is the default value for an option. It is used for annotation purposes only
+    and will not be passed to the ffmpeg command line.
+    """
+
+    ...
+
+
+def _to_tuple(
+    kwargs: dict[str, str | int | float | bool | Default]
+) -> tuple[tuple[str, str | int | float | bool], ...]:
+    """
+    Convert the values of the dictionary to strings.
+    """
+    return tuple((k, v) for k, v in kwargs.items() if not isinstance(v, Default))
