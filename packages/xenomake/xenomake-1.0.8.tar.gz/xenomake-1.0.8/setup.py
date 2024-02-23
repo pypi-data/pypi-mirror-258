c79b3ba8e18393b356a34fb54b672477256d20e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-23 07:04:06 +0000

    Add package xenomake/1.0.8/xenomake-1.0.8.tar.gz

---
+++ packages/xenomake/xenomake-1.0.8.tar.gz/xenomake-1.0.8/setup.py
@@ -0,0 +1,49 @@
+from setuptools import setup
+
+setup(
+    #metadata
+    name='xenomake',
+    version='1.0.8',
+    description='Xenomake: Processing Pipeline for Patient Derived Xenograft (PDX) Spatial Transcriptomics Datasets',
+    url='https://github.com/istrope/Xenomake/',
+    author='Ivy Strope',
+    author_email='ivystrope@gmail.com',
+    packages=['xenomake'],
+    install_requires=['snakemake','argparse','pyyaml','scanpy'],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Science/Research',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3'
+    ],
+    
+    #options
+    python_requires='>=3.10',
+    package_dir={
+        'xenomake':'xenomake'
+    },
+
+    #package data
+    package_data = {
+        'xenomake': ['snakemake/*.smk',
+                     'scripts/*.py',
+                     'scripts/*.sh',
+                     'data/barcodes/*.txt',
+                     'data/barcodes/*.csv',
+                     'data/Drop-seq_tools-2.5.3/*',
+                     'data/Drop-seq_tools-2.5.3/3rdParty/picard/picard.jar',
+                     'data/Drop-seq_tools-2.5.3/jar/dropseq.jar',
+                     'data/Drop-seq_tools-2.5.3/jar/lib/*.jar'
+                     'data/urls/*.txt',
+                     'data/picard.jar',
+                     'data/*.yaml']
+    },
+
+    #entry points
+    entry_points = {
+        'console_scripts': [
+            'xenomake = xenomake.smk:cmdline',
+        ],
+    },
+)
+
