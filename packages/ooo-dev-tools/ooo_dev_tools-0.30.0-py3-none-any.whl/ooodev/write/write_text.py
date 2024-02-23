c32628cd2dbcd491e2fd7b29fc854e00006e0369
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-23 06:03:46 +0000

    Add package ooo-dev-tools/0.30.0/ooo_dev_tools-0.30.0-py3-none-any.whl

---
+++ packages/ooo-dev-tools/ooo_dev_tools-0.30.0-py3-none-any.whl/ooodev/write/write_text.py
@@ -0,0 +1,133 @@
+from __future__ import annotations
+from typing import TYPE_CHECKING, TypeVar, Generic, overload
+import uno
+from com.sun.star.text import XTextRange
+
+from ooodev.adapter.text.relative_text_content_insert_partial import RelativeTextContentInsertPartial
+from ooodev.adapter.text.text_comp import TextComp
+from ooodev.format.inner.style_partial import StylePartial
+from ooodev.proto.component_proto import ComponentT
+from ooodev.loader import lo as mLo
+from ooodev.loader.inst.lo_inst import LoInst
+from ooodev.utils.partial.qi_partial import QiPartial
+from ooodev.utils.partial.lo_inst_props_partial import LoInstPropsPartial
+from ooodev.write.partial.write_doc_prop_partial import WriteDocPropPartial
+from . import write_paragraphs as mWriteParagraphs
+from . import write_text_tables as mWriteTextTables
+
+if TYPE_CHECKING:
+    from com.sun.star.text import XText
+    from com.sun.star.text import XTextContent
+
+T = TypeVar("T", bound="ComponentT")
+
+
+class WriteText(
+    Generic[T],
+    LoInstPropsPartial,
+    WriteDocPropPartial,
+    TextComp,
+    RelativeTextContentInsertPartial,
+    QiPartial,
+    StylePartial,
+):
+    """
+    Represents writer text content.
+
+    Contains Enumeration Access.
+    """
+
+    def __init__(self, owner: T, component: XText, lo_inst: LoInst | None = None) -> None:
+        """
+        Constructor
+
+        Args:
+            owner (T): Owner of this component.
+            component (XText): UNO object that supports ``com.sun.star.text.Text`` service.
+            lo_inst (LoInst, optional): Lo instance. Defaults to ``None``.
+        """
+        if lo_inst is None:
+            lo_inst = mLo.Lo.current_lo
+        self._owner = owner
+        LoInstPropsPartial.__init__(self, lo_inst=lo_inst)
+        if not isinstance(owner, WriteDocPropPartial):
+            raise TypeError("WriteDocPropPartial is not inherited by owner.")
+        WriteDocPropPartial.__init__(self, obj=owner.write_doc)  # type: ignore
+        TextComp.__init__(self, component)  # type: ignore
+        RelativeTextContentInsertPartial.__init__(self, component=component, interface=None)  # type: ignore
+        QiPartial.__init__(self, component=component, lo_inst=self.lo_inst)  # type: ignore
+        StylePartial.__init__(self, component=component)
+
+    def get_paragraphs(self) -> mWriteParagraphs.WriteParagraphs[T]:
+        """Returns the paragraphs of this text."""
+        return mWriteParagraphs.WriteParagraphs(owner=self.owner, component=self.component, lo_inst=self.lo_inst)
+
+    def get_text_tables(self) -> mWriteTextTables.WriteTextTables[T]:
+        """Returns the text tables of this text."""
+        return mWriteTextTables.WriteTextTables(owner=self.owner, component=self.component, lo_inst=self.lo_inst)
+
+    @overload
+    def insert_text_content(self, content: XTextContent, absorb: bool) -> None:
+        """
+        Inserts a content, such as a text table, text frame or text field.
+
+        Args:
+            content (XTextContent): The content to be inserted.
+            absorb (bool): Specifies whether the text spanned by xRange will be replaced.
+                If ``True`` then the content of range will be replaced by content,
+                otherwise content will be inserted at the end of xRange.
+
+        Returns:
+            None:
+        """
+        ...
+
+    @overload
+    def insert_text_content(
+        self,
+        content: XTextContent,
+        absorb: bool,
+        rng: XTextRange,
+    ) -> None:
+        """
+        Inserts a content, such as a text table, text frame or text field.
+
+        Args:
+            content (XTextContent): The content to be inserted.
+            absorb (bool): Specifies whether the text spanned by xRange will be replaced.
+                If ``True`` then the content of range will be replaced by content,
+                otherwise content will be inserted at the end of xRange.
+            rng (XTextRange): The position at which the content is inserted.
+
+        Returns:
+            None:
+        """
+        ...
+
+    def insert_text_content(self, content: XTextContent, absorb: bool, rng: XTextRange | None = None) -> None:
+        """
+        Inserts a content, such as a text table, text frame or text field.
+
+        Args:
+            rng (XTextRange): The position at which the content is inserted.
+            content (XTextContent): The content to be inserted.
+            absorb (bool): Specifies whether the text spanned by xRange will be replaced.
+                If ``True`` then the content of range will be replaced by content,
+                otherwise content will be inserted at the end of xRange.
+
+        Returns:
+            None:
+        """
+        if rng is None:
+            rng = self.lo_inst.qi(XTextRange, self.owner.component)
+            if rng is None:
+                raise TypeError("owner must be XTextRange when rng is None")
+        TextComp.insert_text_content(self, rng, content, absorb)
+
+    # region Properties
+    @property
+    def owner(self) -> T:
+        """Owner of this component."""
+        return self._owner
+
+    # endregion Properties
