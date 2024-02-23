c5e69b65400203125f5100cfceee2ab6ac1c076b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-23 04:04:16 +0000

    Add package pyallel/0.11.0/pyallel-0.11.0.tar.gz

---
+++ packages/pyallel/pyallel-0.11.0.tar.gz/pyallel-0.11.0/src/pyallel/constants.py
@@ -0,0 +1,53 @@
+import os
+import sys
+
+IN_TTY = sys.__stdin__.isatty()
+
+
+if IN_TTY:
+    WHITE_BOLD = "\033[1m"
+    GREEN_BOLD = "\033[1;32m"
+    BLUE_BOLD = "\033[1;34m"
+    RED_BOLD = "\033[1;31m"
+    CLEAR_LINE = "\033[2K"
+    CLEAR_SCREEN = "\033[2J"
+    SAVE_CURSOR = "\033[s"
+    RESTORE_CURSOR = "\033[u"
+    UP_LINE = "\033[1F"
+    RESET_COLOUR = "\033[0m"
+    CARRIAGE_RETURN = "\r"
+    DIM_ON = "\033[2m"
+    DIM_OFF = "\033[22m"
+
+    def COLUMNS() -> int:
+        return os.get_terminal_size().columns
+
+    def LINES() -> int:
+        return os.get_terminal_size().lines
+
+else:
+    WHITE_BOLD = ""
+    GREEN_BOLD = ""
+    BLUE_BOLD = ""
+    RED_BOLD = ""
+    CLEAR_LINE = ""
+    CLEAR_SCREEN = ""
+    SAVE_CURSOR = ""
+    RESTORE_CURSOR = ""
+    UP_LINE = ""
+    RESET_COLOUR = ""
+    CARRIAGE_RETURN = ""
+    DIM_ON = ""
+    DIM_OFF = ""
+
+    def COLUMNS() -> int:
+        return sys.maxsize
+
+    def LINES() -> int:
+        return sys.maxsize
+
+
+ICONS = ("/", "-", "\\", "|")
+# Unicode character bytes to render different symbols in the terminal
+TICK = "\u2713"
+X = "\u2717"
