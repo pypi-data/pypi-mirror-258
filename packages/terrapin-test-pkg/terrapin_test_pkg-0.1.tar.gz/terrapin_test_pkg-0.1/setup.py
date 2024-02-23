50b01b38dcc81ce6bbaa5a51c505894cb42b3b12
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-23 04:03:36 +0000

    Add package terrapin-test-pkg/0.1/terrapin_test_pkg-0.1.tar.gz

---
+++ packages/terrapin-test-pkg/terrapin_test_pkg-0.1.tar.gz/terrapin_test_pkg-0.1/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="terrapin_test_pkg",
+    version="0.1",
+    packages=find_packages(),
+    author="gregorymacharia",
+    author_email="gregorymacharia13@gmail.com",
+    description="A test package",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)
+
+
+# The setup.py file is a standard way to package Python code for distribution.
+# It contains metadata about the package eg: its name, version, and author.
+# It also specifies the package's dependencies and other details.
