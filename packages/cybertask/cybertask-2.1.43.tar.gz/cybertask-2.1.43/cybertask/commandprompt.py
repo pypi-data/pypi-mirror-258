8656ee92585302ad52fbe1cd6bab31ae0f31ef89
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-23 03:03:33 +0000

    Add package cybertask/2.1.43/cybertask-2.1.43.tar.gz

---
+++ packages/cybertask/cybertask-2.1.43.tar.gz/cybertask-2.1.43/cybertask/commandprompt.py
@@ -0,0 +1,34 @@
+from cybertask import config
+from cybertask.health_check import HealthCheck
+if not hasattr(config, "currentMessages"):
+    HealthCheck.setBasicConfig()
+    HealthCheck.saveConfig()
+    #print("Configurations updated!")
+from cybertask.utils.prompts import Prompts
+from cybertask.utils.terminal_system_command_prompt import SystemCommandPrompt
+import platform
+
+def setOsOpenCmd():
+    thisPlatform = platform.system()
+    config.thisPlatform = thisPlatform
+    if config.terminalEnableTermuxAPI:
+        config.open = "termux-share"
+    elif thisPlatform == "Linux":
+        config.open = "xdg-open"
+    elif thisPlatform == "Darwin":
+        config.open = "open"
+    elif thisPlatform == "Windows":
+        config.open = "start"
+    # name macOS
+    if config.thisPlatform == "Darwin":
+        config.thisPlatform = "macOS"
+
+def main():
+    config.systemCommandPromptEntry = ""
+    config.print = print
+    setOsOpenCmd()
+    Prompts()
+    SystemCommandPrompt().run(allowPathChanges=True)
+
+if __name__ == '__main__':
+    main()
