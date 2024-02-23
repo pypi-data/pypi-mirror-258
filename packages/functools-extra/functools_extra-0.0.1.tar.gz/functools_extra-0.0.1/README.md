d46ef3d67b2828c02c48758332eb75422241d097
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-23 04:03:49 +0000

    Add package functools-extra/0.0.1/functools_extra-0.0.1.tar.gz

---
+++ packages/functools-extra/functools_extra-0.0.1.tar.gz/functools_extra-0.0.1/README.md
@@ -0,0 +1,2 @@
+# Functools Extra
+Additional functional tools for python not covered in the [functools](https://docs.python.org/3/library/functools.html) library.
---
+++ packages/functools-extra/functools_extra-0.0.1.tar.gz/functools_extra-0.0.1/functools_extra/pipe.py
@@ -0,0 +1,56 @@
+from typing import overload, TypeVar, Callable, Optional, Union
+from functools import reduce
+
+T = TypeVar("T")
+T1 = TypeVar("T1")
+T2 = TypeVar("T2")
+T3 = TypeVar("T3")
+T4 = TypeVar("T4")
+T5 = TypeVar("T5")
+T6 = TypeVar("T6")
+T7 = TypeVar("T7")
+T8 = TypeVar("T8")
+
+@overload
+def pipe(value: T) -> T:
+    ...
+
+@overload
+def pipe(value: T, func1: Callable[[T], T1]) -> T1:
+    ...
+
+@overload
+def pipe(value: T, func1: Callable[[T], T1], func2: Callable[[T1], T2]) -> T2:
+    ...
+
+@overload
+def pipe(value: T, func1: Callable[[T], T1], func2: Callable[[T1], T2], func3: Callable[[T2], T3]) -> T3:
+    ...
+
+@overload
+def pipe(value: T, func1: Callable[[T], T1], func2: Callable[[T1], T2], func3: Callable[[T2], T3], func4: Callable[[T3], T4]) -> T4:
+    ...
+
+@overload
+def pipe(value: T, func1: Callable[[T], T1], func2: Callable[[T1], T2], func3: Callable[[T2], T3], func4: Callable[[T3], T4], func5: Callable[[T4], T5]) -> T5:
+    ...
+
+@overload
+def pipe(value: T, func1: Callable[[T], T1], func2: Callable[[T1], T2], func3: Callable[[T2], T3], func4: Callable[[T3], T4], func5: Callable[[T4], T5], func6: Callable[[T5], T6]) -> T6:
+    ...
+
+@overload
+def pipe(value: T, func1: Callable[[T], T1], func2: Callable[[T1], T2], func3: Callable[[T2], T3], func4: Callable[[T3], T4], func5: Callable[[T4], T5], func6: Callable[[T5], T6], func7: Callable[[T6], T7]) -> T7:
+    ...
+
+@overload
+def pipe(value: T, func1: Callable[[T], T1], func2: Callable[[T1], T2], func3: Callable[[T2], T3], func4: Callable[[T3], T4], func5: Callable[[T4], T5], func6: Callable[[T5], T6], func7: Callable[[T6], T7], func8: Callable[[T7], T8]) -> T8:
+    ...
+
+def pipe(value: T, func1: Optional[Callable[[T], T1]] = None, func2: Optional[Callable[[T1], T2]] = None, func3: Optional[Callable[[T2], T3]] = None, func4: Optional[Callable[[T3], T4]] = None, func5: Optional[Callable[[T4], T5]] = None, func6: Optional[Callable[[T5], T6]] = None, func7: Optional[Callable[[T6], T7]] = None, func8: Optional[Callable[[T7], T8]] = None) -> Union[T, T1, T2, T3, T4, T5, T6, T7, T8]:
+    funcs = (func for func in (func1, func2, func3, func4, func5, func6, func7, func8) if func is not None)
+    return reduce(pipe_reduce, funcs, value)
+
+def pipe_reduce(val, func):
+    return func(val)
+
