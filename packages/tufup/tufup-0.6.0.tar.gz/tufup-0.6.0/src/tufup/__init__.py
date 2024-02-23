6c9768818ba9411e840de89b6b46f20eb143889b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-23 04:03:50 +0000

    Add package tufup/0.6.0/tufup-0.6.0.tar.gz

---
+++ packages/tufup/tufup-0.6.0.tar.gz/tufup-0.6.0/src/tufup/__init__.py
@@ -0,0 +1,35 @@
+import logging
+import sys
+
+from tufup.repo import cli
+
+# https://packaging.python.org/en/latest/guides/single-sourcing-package-version/
+# https://semver.org/
+__version__ = '0.6.0'
+
+logger = logging.getLogger(__name__)
+
+
+def main(args=None):
+    print(f'tufup version: {__version__}')
+    # default to --help
+    if args is None:
+        args = sys.argv[1:] or ['--help']
+
+    # parse command line arguments
+    options = cli.get_parser().parse_args(args=args)
+
+    # cli debugging
+    if options.debug:
+        logging.basicConfig(level=logging.DEBUG, stream=sys.stdout, force=True)
+
+    # process command
+    try:
+        options.func(options)
+    except Exception:  # noqa
+        logger.exception(f'Failed to process command: {args}')
+
+
+if __name__ == '__main__':
+    logging.basicConfig(level=logging.INFO)
+    main()
