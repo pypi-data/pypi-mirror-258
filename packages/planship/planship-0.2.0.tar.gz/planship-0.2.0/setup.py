9b2d2f4ec622d77f906b2a16b6fa0953969fd489
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-23 16:04:27 +0000

    Add package planship/0.2.0/planship-0.2.0.tar.gz

---
+++ packages/planship/planship-0.2.0.tar.gz/planship-0.2.0/setup.py
@@ -0,0 +1,41 @@
+from pathlib import Path
+
+from setuptools import find_packages, setup  # noqa: H301
+
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+NAME = "planship"
+VERSION = "0.2.0"
+# To install the library, run the following
+#
+# python setup.py install
+#
+# prerequisite: setuptools
+# http://pypi.python.org/pypi/setuptools
+
+REQUIRES = [
+    "planship_openapi_gen >= 1",
+]
+
+project_urls = {
+    "Repository": "https://github.com/planship/planship-python",
+    "Documentation": "https://docs.planship.io",
+}
+
+setup(
+    name=NAME,
+    version=VERSION,
+    description="Planship API client",
+    author="Pawel Wojnarowicz",
+    author_email="pawel@planship.io",
+    url="https://planship.io",
+    keywords=["Planship"],
+    python_requires=">=3.7",
+    install_requires=REQUIRES,
+    packages=find_packages(exclude=["test", "tests"]),
+    include_package_data=True,
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    project_urls=project_urls,
+)
