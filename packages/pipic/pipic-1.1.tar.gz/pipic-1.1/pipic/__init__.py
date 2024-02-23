9b52ee3dd0895bb8994d81b15cd3d9018b26987a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-23 16:04:40 +0000

    Add package pipic/1.1/pipic-1.1.tar.gz

---
+++ packages/pipic/pipic-1.1.tar.gz/pipic-1.1/pipic/__init__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Main module of the pipic package.
+"""
+from ._version import __version__
+
+from ._pipic import *
+import pipic.types as types
+import pipic.consts as consts
+from pipic.ctypes import addressof
+import pipic.interfaces as interfaces
+import pipic.extensions as extensions
