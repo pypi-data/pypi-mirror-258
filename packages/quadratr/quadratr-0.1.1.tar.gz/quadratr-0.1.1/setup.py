06aa341adf2a69209118e615ae5b54fb7b1ce226
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-23 05:04:03 +0000

    Add package quadratr/0.1.1/quadratr-0.1.1.tar.gz

---
+++ packages/quadratr/quadratr-0.1.1.tar.gz/quadratr-0.1.1/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+
+setup(
+name="quadratr",
+version="0.1.1",
+author='Sancho Mir',
+author_email='gugle.html@gmail.com',
+description="A simple Python package to solve quadratic equations",
+packages=find_packages(),
+classifiers=[
+"Programming Language :: Python :: 3",
+"License :: OSI Approved :: MIT License",
+"Operating System :: OS Independent",
+],
+python_requires=">=3.6",
+)
