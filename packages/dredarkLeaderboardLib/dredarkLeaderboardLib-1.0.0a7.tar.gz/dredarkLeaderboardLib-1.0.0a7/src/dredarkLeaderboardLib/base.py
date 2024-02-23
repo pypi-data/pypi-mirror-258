83d02dd10295b4ea0f5152e2395faa62dfbe78c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-23 04:03:43 +0000

    Add package dredarkLeaderboardLib/1.0.0a7/dredarkLeaderboardLib-1.0.0a7.tar.gz

---
+++ packages/dredarkLeaderboardLib/dredarkLeaderboardLib-1.0.0a7.tar.gz/dredarkLeaderboardLib-1.0.0a7/src/dredarkLeaderboardLib/base.py
@@ -0,0 +1,130 @@
+import requests
+from bs4 import BeautifulSoup
+
+from .errors import *
+
+
+class Leaderboard():
+  """
+    This class is used to scrape the Deep Space Airships leaderboards
+    """
+
+  def __init__(self):
+    self.shipData = []
+
+  def scan_Leaderboard(self, url: str, limit=None):
+    """
+        Multiplies two numbers and returns the result.
+    
+        Args:
+            url (str): The leaderboard URL to scrape.
+            limit (int): Limit of how many ships to retrieve from the page.
+    
+        Returns:
+            Nothing. The scanned data is stored in the 'shipData' instance variable.
+        """
+    ship_info = {}
+    if url.count("ship")>=1:
+      response = requests.get(url)
+      soup = BeautifulSoup(response.text, 'html.parser')
+      tables=str(soup.find_all("table", {"class": "leaderboard"})).replace("[","").replace("]","").replace('<table class="leaderboard">','').replace("</table>","").replace("<tr>","").replace("<td>","")
+      lbEntries=tables.split("</tr>")
+      ships=lbEntries
+      for ship in ships:
+        try:
+          rawData = ship.split("</td>")
+          rawData.remove("")
+          data = rawData
+          listEntry={
+            "hex": data[1],
+            "rank": int(data[0].replace("#","")),
+            "score": data[2].split(" ")[0]
+          }
+          self.shipData.append(listEntry)
+        except Exception as e:
+          pass
+    elif url.count("pilot")>=1:
+      response = requests.get(url)
+      soup = BeautifulSoup(response.text, 'html.parser')
+      tables=str(soup.find_all("table", {"class": "leaderboard"})).replace("[","").replace("]","").replace('<table class="leaderboard">','').replace("</table>","").replace("<tr>","").replace("<td>","")
+      lbEntries=tables.split("</tr>")
+      ships=lbEntries
+      for ship in ships:
+        try:
+          rawData = ship.split("</td>")
+          rawData.remove("")
+          data = rawData
+          listEntry={
+            "name": data[1],
+            "rank": int(data[0].replace("#","")),
+            "score": data[2].split(" ")[0]
+          }
+          self.shipData.append(listEntry)
+        except Exception as e:
+          pass
+    elif "clan" in url:
+      response = requests.get(url)
+      soup = BeautifulSoup(response.text, 'html.parser')
+      tables=str(soup.find_all("table", {"class": "leaderboard"})).replace("[","").replace("]","").replace('<table class="leaderboard">','').replace("</table>","").replace("<tr>","").replace("<td>","")
+      lbEntries=tables.split("</tr>")
+      ships=lbEntries
+      for ship in ships:
+        try:
+          rawData = ship.split("</td>")
+          rawData.remove("")
+          data = rawData
+          listEntry={
+            "clan": data[1],
+            "rank": int(data[0].replace("#","")),
+            "score": data[2].split(" ")[0]
+          }
+          self.shipData.append(listEntry)
+        except Exception as e:
+          pass
+    else:
+      response = requests.get(url)
+      soup = BeautifulSoup(response.text, 'html.parser')
+      tables=str(soup.find_all("table", {"class": "leaderboard"})).replace("[","").replace("]","").replace('<table class="leaderboard">','').replace("</table>","").replace("<tr>","").replace("<td>","")
+      lbEntries=tables.split("</tr>")
+      ships=lbEntries
+      for ship in ships:
+        try:
+          rawData = ship.split("</td>")
+          rawData.remove("")
+          data = rawData
+          listEntry={
+            "name": data[1],
+            "rank": int(data[0].replace("#","")),
+            "score": data[2].split(" ")[0]
+          }
+          self.shipData.append(listEntry)
+        except Exception as e:
+          pass
+
+  def return_data(self):
+    """
+        Returns:
+            dict : The dictionary of ship data from the 'shipData' instance variable
+        """
+    return self.shipData
+
+  def fetch_ship(self, searchKey, searchTerm):
+    """
+        Multiplies two numbers and returns the result.
+    
+        Args:
+            searchKey (str): Search for an individual ship by `name, rank, points, or hex (hex code)`. 
+            searchTerm (str): 
+    
+        Returns:
+            Nothing. The scanned data is stored in the 'shipData' instance variable.
+        """
+    viableKeys = ["name", "rank", "points", "hex"]
+    if searchKey not in viableKeys:
+      raise BadSearchKey  #(f"{searchKey} is not in the approved list of searchKeys; Approved List: {viableKeys}")
+    else:
+
+      def scan_data(data, term):
+        return list(filter(lambda x: x.get(searchKey) == term, data))
+
+      return scan_data(self.shipData, searchTerm)
