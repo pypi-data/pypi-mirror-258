861e5a968da5fd3fe4fd8eb82ab007e189270717
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-23 16:04:37 +0000

    Add package jsonable-platform/0.0.1.20240223160240/jsonable_platform-0.0.1.20240223160240-py3-none-any.whl

---
+++ packages/jsonable-platform/jsonable_platform-0.0.1.20240223160240-py3-none-any.whl/jsonable_platform/shared.py
@@ -0,0 +1,58 @@
+from typing import Any, TypeVar
+from json.encoder import JSONEncoder
+from hashlib import sha256
+
+from .type import HashMethods, JSONSupportedBases
+
+# from type import JSONSupportedTypes, JSONSupportedBases, JSONSupportedIterables
+
+ENCODER = JSONEncoder()
+
+
+def json_native_encode(obj: Any) -> str | None:
+    """
+    Get the result of native json encoded, if obj can't be converted to json, return empty (...)
+    :param obj: Object you want to check
+    :return: str -> native jsonable; None -> not native jsonable;
+    """
+
+    try:
+        return ENCODER.encode(obj)
+    except TypeError:
+        return
+
+
+DefaultType = TypeVar('DefaultType')
+
+
+def class_name(obj_or_cls: Any, default: DefaultType = None) -> str | DefaultType:
+    if hasattr(obj_or_cls, '__name__'):
+        return obj_or_cls.__name__
+
+    obj_or_cls = type(obj_or_cls)
+    if hasattr(obj_or_cls, '__name__'):
+        return obj_or_cls.__name__
+
+    return default
+
+
+def hash_class(cls: Any) -> tuple[str, HashMethods]:
+    res = False
+    func = getattr(cls, '__jsonable_hash__', None)
+    if callable(func):
+        res = func()
+
+    cls_name = class_name(cls)
+    if cls_name is None:
+        raise ValueError(f'{cls}\'s name is not defined')
+
+    if isinstance(res, str):
+        return res, 'custom'
+    else:
+        # return sha256(cls_name.encode()).hexdigest(), 'default'
+        return cls_name, 'default'
+
+
+def get_jsonable_keyname(obj: dict):
+    keys = tuple(obj.keys())
+    return keys[0] if len(keys) == 1 else None
---
+++ packages/jsonable-platform/jsonable_platform-0.0.1.20240223160240-py3-none-any.whl/jsonable_platform/type.py
@@ -0,0 +1,66 @@
+from sys import version_info
+from typing import TypedDict, TypeAlias, TypeVar, Literal, Union, Generic
+
+JSONAbleClassID: TypeAlias = str
+
+JSONSupportedBases: TypeAlias = Union[bytes, str, int, float, bool, None]
+JSONSupportedIterables: TypeAlias = Union[
+    list[Union[JSONSupportedBases, 'JSONSupportedIterables', 'JSONAbleABC']],
+    tuple[Union[JSONSupportedBases, 'JSONSupportedIterables', 'JSONAbleABC']],
+    dict[
+        Union[JSONSupportedBases, 'JSONSupportedIterables'],
+        Union[JSONSupportedBases, 'JSONSupportedIterables', 'JSONAbleABC']
+    ]
+]
+JSONSupportedEditableIters: TypeAlias = Union[list, dict]
+
+JSONSupportedTypes: TypeAlias = Union[JSONSupportedBases, JSONSupportedIterables]
+
+# `Self` is Python 3.11+ Only (https://docs.python.org/zh-cn/3/library/typing.html#typing.Self)
+if version_info >= (3, 11):
+    from typing import Self  # type: ignore
+else:
+    from typing_extensions import Self
+
+JSONAbleABCEncodedType = TypeVar('JSONAbleABCEncodedType', bound=JSONSupportedTypes)
+
+
+class JSONAbleABC(Generic[JSONAbleABCEncodedType]):
+    @classmethod
+    def __jsonable_hash__(cls) -> str | None:
+        """
+        Get the hash of the jsonable object
+        :return: Any str or None. if return None, will use the `__name__` of your class
+        """
+        return
+
+    @classmethod
+    def __jsonable_encode__(cls, obj: Self) -> JSONAbleABCEncodedType:
+        """
+        Encode to json, return native jsonable type
+        :parma obj: Any Python object you want to encode
+        :return: *The type of return is the same as the `obj` param at function `__jsonable_decode__`
+        """
+        raise NotImplemented
+
+    @classmethod
+    def __jsonable_decode__(cls, data: JSONAbleABCEncodedType) -> Self:
+        """
+        Decode from json, return any Python object
+        :param data: The data of the object, as same as the return of function `__jsonable_encode__`
+        :return: Any Python object as same as given at the `obj` param at function `__jsonable_encode__`
+        """
+        raise NotImplemented
+
+
+class DefinedClasses(TypedDict):
+    names: dict[JSONAbleClassID, type[JSONAbleABC]]
+    classes: dict[JSONAbleClassID, type[JSONAbleABC]]
+
+
+class JSONAbleEncodedDict(TypedDict):
+    hash: str
+    data: JSONSupportedTypes
+
+
+HashMethods = Literal['default', 'custom']
