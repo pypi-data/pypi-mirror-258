586002c01f5d9265bfb91a4b7adfafdafd43ee7c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-23 05:04:12 +0000

    Add package azfuncblobclient/0.0.6/azfuncblobclient-0.0.6.tar.gz

---
+++ packages/azfuncblobclient/azfuncblobclient-0.0.6.tar.gz/azfuncblobclient-0.0.6/setup.py
@@ -0,0 +1,39 @@
+#  Copyright (c) Microsoft Corporation. All rights reserved.
+#  Licensed under the MIT License.
+
+from setuptools import setup, find_packages
+
+EXTRA_REQUIRES = {
+    'dev': [
+        'flake8~=4.0.1',
+        'flake8-logging-format',
+        'mypy',
+        'pytest',
+        'pytest-cov',
+        'requests==2.*',
+        'coverage'
+    ]
+}
+
+setup(
+    name='azfuncblobclient',
+    version='0.0.6',
+    author='Azure Functions team at Microsoft Corp.',
+    author_email='azurefunctions@microsoft.com',
+    description='Blob Python worker extension for Azure Functions.',
+    packages=find_packages(exclude=[
+        'azure.functions.extension', 'azure.functions',
+        'azure', 'tests', 'samples'
+    ]),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.7',
+    install_requires=[
+        'azfuncbindingbase',
+        'azure-storage-blob'
+    ],
+    extras_require=EXTRA_REQUIRES,
+)
