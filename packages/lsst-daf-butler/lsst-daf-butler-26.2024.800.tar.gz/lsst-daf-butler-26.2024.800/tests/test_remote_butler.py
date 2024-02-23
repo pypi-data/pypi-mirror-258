c2b112d2e7f93e928f3c8eda0c3597e1fbecccb6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-23 03:04:32 +0000

    Add package lsst-daf-butler/26.2024.800/lsst-daf-butler-26.2024.800.tar.gz

---
+++ packages/lsst-daf-butler/lsst-daf-butler-26.2024.800.tar.gz/lsst-daf-butler-26.2024.800/tests/test_remote_butler.py
@@ -0,0 +1,67 @@
+# This file is part of daf_butler.
+#
+# Developed for the LSST Data Management System.
+# This product includes software developed by the LSST Project
+# (http://www.lsst.org).
+# See the COPYRIGHT file at the top-level directory of this distribution
+# for details of code ownership.
+#
+# This software is dual licensed under the GNU General Public License and also
+# under a 3-clause BSD license. Recipients may choose which of these licenses
+# to use; please see the files gpl-3.0.txt and/or bsd_license.txt,
+# respectively.  If you choose the GPL option then the following text applies
+# (but note that there is still no warranty even if you opt for BSD instead):
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import unittest
+from unittest.mock import patch
+
+from lsst.daf.butler import Butler
+from pydantic import ValidationError
+
+try:
+    import httpx
+    from lsst.daf.butler.remote_butler import ButlerServerError, RemoteButler, RemoteButlerFactory
+except ImportError:
+    # httpx is not available in rubin-env yet, so skip these tests if it's not
+    # available
+    RemoteButler = None
+
+
+@unittest.skipIf(RemoteButler is None, "httpx is not installed")
+class RemoteButlerConfigTests(unittest.TestCase):
+    """Test construction of RemoteButler via Butler()"""
+
+    def test_bad_config(self):
+        with self.assertRaises(ValidationError):
+            Butler({"cls": "lsst.daf.butler.remote_butler.RemoteButler", "remote_butler": {"url": "!"}})
+
+
+@unittest.skipIf(RemoteButler is None, "httpx is not installed")
+class RemoteButlerErrorHandlingTests(unittest.TestCase):
+    """Test RemoteButler error handling."""
+
+    def test_internal_server_error(self):
+        butler = RemoteButlerFactory.create_factory_for_url(
+            "https://doesntmatter"
+        ).create_butler_for_access_token("dontcare")
+        with patch.object(butler._client, "request") as mock:
+            mock.side_effect = httpx.HTTPError("unhandled error")
+            with self.assertRaises(ButlerServerError):
+                butler.get_dataset_type("int")
+
+
+if __name__ == "__main__":
+    unittest.main()
