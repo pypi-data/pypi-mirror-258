aabcbc9a63126fcbf62cd82676ab6f4c36321a6d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-23 16:04:46 +0000

    Add package kuzu/0.2.2.dev9/kuzu-0.2.2.dev9.tar.gz

---
+++ packages/kuzu/kuzu-0.2.2.dev9.tar.gz/sdist/kuzu-source/src/planner/plan/append_projection.cpp
@@ -0,0 +1,26 @@
+#include "planner/operator/factorization/flatten_resolver.h"
+#include "planner/operator/logical_projection.h"
+#include "planner/planner.h"
+
+using namespace kuzu::binder;
+
+namespace kuzu {
+namespace planner {
+
+void Planner::appendProjection(const expression_vector& expressionsToProject, LogicalPlan& plan) {
+    for (auto& expression : expressionsToProject) {
+        planSubqueryIfNecessary(expression, plan);
+    }
+    for (auto& expression : expressionsToProject) {
+        auto dependentGroupsPos = plan.getSchema()->getDependentGroupsPos(expression);
+        auto groupsPosToFlatten = factorization::FlattenAllButOne::getGroupsPosToFlatten(
+            dependentGroupsPos, plan.getSchema());
+        appendFlattens(groupsPosToFlatten, plan);
+    }
+    auto projection = make_shared<LogicalProjection>(expressionsToProject, plan.getLastOperator());
+    projection->computeFactorizedSchema();
+    plan.setLastOperator(std::move(projection));
+}
+
+} // namespace planner
+} // namespace kuzu
