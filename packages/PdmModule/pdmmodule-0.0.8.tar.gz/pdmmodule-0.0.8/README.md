da140a9a2f10a309ab3ebcabc46e7a8a116c90f0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-23 05:04:46 +0000

    Add package PdmModule/0.0.8/pdmmodule-0.0.8.tar.gz

---
+++ packages/PdmModule/pdmmodule-0.0.8.tar.gz/pdmmodule-0.0.8/README.md
@@ -0,0 +1,3 @@
+Predictive Maintenance Module for applying a modified Xgboost and Pair detection Techniques in streaming data.
+
+Documentation: [https://pd-mmodule.vercel.app/](https://pd-mmodule.vercel.app/)
---
+++ packages/PdmModule/pdmmodule-0.0.8.tar.gz/pdmmodule-0.0.8/example2.py
@@ -0,0 +1,10 @@
+from PdmModule.PdMApi import PdmModule,PdmModuleStatic
+import pandas as pd
+import numpy as np
+
+streamModule =PdmModule()
+
+for i in range(50):
+    #prediction = streamModule.collect_data(pd.to_datetime("01/01/2023 15:00:15"), np.array([0.52, 0.32,0.22,0.16,0.15,0.12,0.13]), id="1")
+    prediction = streamModule.collect_data(pd.to_datetime("01/01/2023 15:00:15"), np.array([1, 1,1,1,1,1,1]), id="1")
+    print(prediction.toString())
