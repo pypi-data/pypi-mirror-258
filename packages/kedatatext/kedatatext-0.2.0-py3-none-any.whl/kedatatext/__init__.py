22efdc38b4cada0b676041a7d319ca3df874e743
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-23 05:03:25 +0000

    Add package kedatatext/0.2.0/kedatatext-0.2.0-py3-none-any.whl

---
+++ packages/kedatatext/kedatatext-0.2.0-py3-none-any.whl/kedatatext/__init__.py
@@ -0,0 +1,5 @@
+"""Top-level package for Kedata Text Analysis Package."""
+
+# __author__ = "Canggih Puspo Wibowo"
+# __email__ = "cangcappo@gmail.com"
+# __version__ = "0.1.0"
---
+++ packages/kedatatext/kedatatext-0.2.0-py3-none-any.whl/kedatatext/cli.py
@@ -0,0 +1,15 @@
+"""Console script for kedatatext."""
+
+import fire  # pragma: no cover
+
+
+def hello() -> str:  # pragma: no cover
+    return "kedatatext"
+
+
+def main() -> None:  # pragma: no cover
+    fire.Fire(hello)
+
+
+if __name__ == "__main__":
+    main()  # pragma: no cover
