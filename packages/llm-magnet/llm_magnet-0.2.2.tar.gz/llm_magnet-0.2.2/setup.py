d9106693986f4c0e55c1f0ddafcce84278939089
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-23 04:04:07 +0000

    Add package llm-magnet/0.2.2/llm_magnet-0.2.2.tar.gz

---
+++ packages/llm-magnet/llm_magnet-0.2.2.tar.gz/llm_magnet-0.2.2/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+setup(
+    name='llm_magnet',
+    version='0.2.2',
+    description="the small distributed language model toolkit. fine-tune state-of-the-art LLMs anywhere, rapidly.",
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    install_requires=[
+        'torch',
+        'sentence-transformers',
+        'pandas',
+        'tqdm',
+        'spacy',
+        'pymilvus',
+        'boto3',
+        'nats-py',
+        'rich',
+        'xxhash'
+    ],
+    url = 'https://github.com/Prismadic/magnet'
+)
