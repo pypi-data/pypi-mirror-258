2f17c9a2bef9cfde296d6bf21ea1bb76a0dfa4d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-23 15:04:17 +0000

    Add package formant/1.158.35/formant-1.158.35-py2.py3-none-any.whl

---
+++ packages/formant/formant-1.158.35-py2.py3-none-any.whl/formant/sdk/cloud/v2/src/resources/queries/__init__.py
@@ -0,0 +1,4 @@
+
+from formant.sdk.cloud.v2.formant_query_api_client.models import Twist, ImageAnnotation, Odometry, Battery, Image, StreamAggregateDataTags, File, QueryAggregate, HealthStatus, StreamAggregateData, Bitset, Health, StreamDataPointsItem, Health,HealthStatus, Map, Query, StreamDataTags, Path, Vector3, QueryTypesItem, Transform, NumericSetEntry, VideoMimeType, Video, Video,VideoMimeType, Location, BoundingBox, StreamData, StreamDataType, Goal, TransformNode, PointCloud, StreamDataListResponse, Localization, Quaternion, StreamAggregateDataType
+from formant.sdk.cloud.v2.formant_query_api_client.api.query import query_controller_query
+from formant.sdk.cloud.v2.formant_query_api_client.types import *
