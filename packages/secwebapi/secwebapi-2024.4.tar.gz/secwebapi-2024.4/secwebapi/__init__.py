088135aca4b299bd5169866edaa1653ef2b33700
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-23 16:04:37 +0000

    Add package secwebapi/2024.4/secwebapi-2024.4.tar.gz

---
+++ packages/secwebapi/secwebapi-2024.4.tar.gz/secwebapi-2024.4/secwebapi/__init__.py
@@ -0,0 +1,66 @@
+import aiohttp
+import asyncio
+import tldextract
+import ssl
+
+class Secweb:
+    def __init__(self, username='', password='', api_key=''):
+        self.api_key = api_key
+        self.base_url = f"https://secwe.pythonanywhere.com/api/predict/"
+        self.headers = {
+            "X-User": username,
+            "X-Pass": password,
+            "Authorization": f"{self.api_key}",
+        }
+
+        self.domain = None
+        self.prediction = None
+
+        self.response = None
+
+    async def async_get(self, domain):
+        sslcontext = ssl.create_default_context()
+        sslcontext.check_hostname = False
+        sslcontext.verify_mode = ssl.CERT_NONE
+
+        async with aiohttp.ClientSession() as session:
+            async with session.get(f"{self.base_url}{domain}", headers=self.headers, ssl=sslcontext) as response:
+                if response.status == 200:
+                    return self.parse_response(await response.json())
+                elif response.status == 403:
+                    return None, f"Invalid username, password or API key: {response.status}"
+                elif response.status == 429:
+                    return None, f"Rate limit exceeded: {response.status}"
+                else:
+                    return None, f"Request failed with status code {response.status}"
+
+    def parse_response(self, response):
+        if isinstance(response, dict):
+            try:
+                self.domain = response['domain']
+                self.prediction = response['prediction']
+                return self.domain, self.prediction
+            except KeyError:
+                return "Invalid response format"
+        else:
+            return "Response is not a dictionary"
+
+    async def async_read_domains_from_file(self, file_path):
+        self.results = []
+        with open(file_path, 'r') as file:
+            for line in file:
+                domain, *category = line.strip().split()
+                category = ' '.join(category)
+                extracted = tldextract.extract(domain)
+                if not extracted.subdomain and extracted.suffix:
+                    self.domain, self.prediction = await self.get(domain)
+                    print(f"Domain: {self.domain}, Prediction: {self.prediction}")
+                    self.results.append((self.domain, self.prediction))
+                else:
+                    print(f"'{domain}' is not a valid domain.")
+
+    def read_domains_from_file(self, file_path):
+        asyncio.run(self.async_read_domains_from_file(file_path))
+
+    def get(self, domain):
+        return asyncio.run(self.async_get(domain))
