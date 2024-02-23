cc1647cd8f41baddc87ce10ea9d421e1ec44b430
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-23 05:03:57 +0000

    Add package rfhotjup/1.0.1/rfhotjup-1.0.1-py3-none-any.whl

---
+++ packages/rfhotjup/rfhotjup-1.0.1-py3-none-any.whl/rfhotjup/__init__.py
@@ -0,0 +1,13 @@
+from rfhotjup import rfhotjup
+
+def get_stellar_system_param():
+    return rfhotjup.get_planet_param()
+
+def get_star_param():
+    return rfhotjup.get_star_param()
+
+def get_stellar_wind_param():
+    return rfhotjup.get_stellar_wind_param()
+
+def get_planet_param():
+    return rfhotjup.get_planet_param()
