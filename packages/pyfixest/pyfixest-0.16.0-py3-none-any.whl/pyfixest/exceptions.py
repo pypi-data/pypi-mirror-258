b6057f4418d7899aa29060180b78383cd32ddc24
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-23 07:03:43 +0000

    Add package pyfixest/0.16.0/pyfixest-0.16.0-py3-none-any.whl

---
+++ packages/pyfixest/pyfixest-0.16.0-py3-none-any.whl/pyfixest/exceptions.py
@@ -0,0 +1,62 @@
+class FixedEffectInteractionError(Exception):  # noqa: D101
+    pass
+
+
+class CovariateInteractionError(Exception):  # noqa: D101
+    pass
+
+
+class DuplicateKeyError(Exception):  # noqa: D101
+    pass
+
+
+class EndogVarsAsCovarsError(Exception):  # noqa: D101
+    pass
+
+
+class InstrumentsAsCovarsError(Exception):  # noqa: D101
+    pass
+
+
+class UnderDeterminedIVError(Exception):  # noqa: D101
+    pass
+
+
+class UnsupportedMultipleEstimationSyntax(Exception):  # noqa: D101
+    pass
+
+
+class VcovTypeNotSupportedError(Exception):  # noqa: D101
+    pass
+
+
+class MultiEstNotSupportedError(Exception):  # noqa: D101
+    pass
+
+
+class NanInClusterVarError(Exception):  # noqa: D101
+    pass
+
+
+class DepvarIsNotNumericError(Exception):  # noqa: D101
+    pass
+
+
+class NotImplementedError(Exception):  # noqa: D101
+    pass
+
+
+class NonConvergenceError(Exception):  # noqa: D101
+    pass
+
+
+class MatrixNotFullRankError(Exception):  # noqa: D101
+    pass
+
+
+class EmptyDesignMatrixError(Exception):  # noqa: D101
+    pass
+
+
+class InvalidReferenceLevelError(Exception):  # noqa: D101
+    pass
