8d7baf7be68b49db2ce8b2e7b262722606671e7d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-23 03:03:59 +0000

    Add package axtriaalator/1.0/axtriaalator-1.0-py3-none-any.whl

---
+++ packages/axtriaalator/axtriaalator-1.0-py3-none-any.whl/tensorfloe/__init__.py
@@ -0,0 +1,13 @@
+# tensorfloe/__init__.py
+
+from . import spam
+from . import sentiment
+from . import gender
+from . import pos
+from . import word2vec
+from . import skipgram
+from . import chatbot
+from . import summary
+from . import transformer
+from . import latent
+
