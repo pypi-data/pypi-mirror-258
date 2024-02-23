51bbd9a891004a452ab7271a7ada98f132ff45c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-23 03:03:58 +0000

    Add package wg-utilities/5.11.1/wg_utilities-5.11.1-py3-none-any.whl

---
+++ packages/wg-utilities/wg_utilities-5.11.1-py3-none-any.whl/wg_utilities/__init__.py
@@ -0,0 +1,17 @@
+"""Utilities package for my other projects."""
+from __future__ import annotations
+
+name = "wg_utilities"
+
+__version__ = "5.11.1"
+
+__all__ = [
+    "api",
+    "clients",
+    "decorators",
+    "devices",
+    "exceptions",
+    "functions",
+    "loggers",
+    "testing",
+]
