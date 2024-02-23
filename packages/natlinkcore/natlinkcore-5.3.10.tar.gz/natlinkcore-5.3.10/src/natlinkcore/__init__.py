2db9a82656a3fb700c6b31e8bdeb7b1942e34ea6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-23 14:04:01 +0000

    Add package natlinkcore/5.3.10/natlinkcore-5.3.10.tar.gz

---
+++ packages/natlinkcore/natlinkcore-5.3.10.tar.gz/natlinkcore-5.3.10/src/natlinkcore/__init__.py
@@ -0,0 +1,23 @@
+'''Python portion of Natlink, a compatibility module for Dragon Naturally Speaking
+The python stuff including test modules'''
+__version__="5.3.10"
+#pylint:disable=
+from pathlib import Path
+
+def getThisDir(fileOfModule):
+    """get directory of calling module
+    """
+    return Path(fileOfModule).parent
+
+# warningTexts = []
+# def warning(text):
+#     """print warning only once, if warnings is set!
+#     
+#     warnings can be set in the calling functions above...
+#     """
+#     textForeward = text.replace("\\", "/")
+#     if textForeward in warningTexts:
+#         return
+#     warningTexts.append(textForeward)
+#     print(text)
+#     
