9b60f70297944eac1375b0e50f99bce2aa1e938b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-23 16:04:05 +0000

    Add package nlib3/1.19.1/nlib3-1.19.1-py3-none-any.whl

---
+++ packages/nlib3/nlib3-1.19.1-py3-none-any.whl/nlib3/__init__.py
@@ -0,0 +1,8 @@
+from .nlib import *
+
+__copyright__ = "Copyright (C) 2022-2023 Nicoyou"
+__version__ = "1.19.1"
+__license__ = "MIT"
+__author__ = "nicoyou"
+__author_email__ = "nicoyou.info@gmail.com"
+__url__ = "https://github.com/nicoyou/nlib"
