388324286fd70aa6cc55af8a533ebd4daa9e8339
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-23 15:04:40 +0000

    Add package fructose/0.0.4/fructose-0.0.4.tar.gz

---
+++ packages/fructose/fructose-0.0.4.tar.gz/fructose-0.0.4/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(
+    name='fructose',
+    version='0.0.4',
+    packages=["fructose"],
+    package_dir={'': 'src'},
+    install_requires=[
+        'openai',
+        'Jinja2'
+    ],
+    # Additional metadata about your package
+    author='Banana',
+    author_email='erik@banana.dev',
+    description='A package for strongly-typed LLM function calling',
+    url='https://github.com/bananaml/fructose',
+)
