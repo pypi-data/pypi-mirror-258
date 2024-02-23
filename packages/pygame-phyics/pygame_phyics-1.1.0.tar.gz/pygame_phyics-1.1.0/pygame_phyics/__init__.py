fd4913c7e1924bf0e746bda5e2c4a3944c040a9e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-23 16:04:17 +0000

    Add package pygame-phyics/1.1.0/pygame_phyics-1.1.0.tar.gz

---
+++ packages/pygame-phyics/pygame_phyics-1.1.0.tar.gz/pygame_phyics-1.1.0/pygame_phyics/__init__.py
@@ -0,0 +1,16 @@
+
+__version__ = "1.1.0"
+
+PPM =20
+
+from pygame_phyics.manger import Manger
+
+from pygame_phyics.game import Game
+import pygame_phyics.game as game
+
+import pygame_phyics.mouse as mouse
+
+from pygame_phyics.objects import *
+
+from pygame_phyics.sheet import SurfaceSheet
+
