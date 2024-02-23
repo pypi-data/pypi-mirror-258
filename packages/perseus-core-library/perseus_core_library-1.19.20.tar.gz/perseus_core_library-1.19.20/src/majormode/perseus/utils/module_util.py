f891f82dba9ac115ff3f991e4567beade9e9067c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-23 15:04:15 +0000

    Add package perseus-core-library/1.19.20/perseus_core_library-1.19.20.tar.gz

---
+++ packages/perseus-core-library/perseus_core_library-1.19.20.tar.gz/perseus_core_library-1.19.20/src/majormode/perseus/utils/module_util.py
@@ -0,0 +1,130 @@
+# Copyright (C) 2019 Majormode.  All rights reserved.
+#
+# Permission is hereby granted, free of charge, to any person obtaining
+# a copy of this software and associated documentation files (the
+# "Software"), to deal in the Software without restriction, including
+# without limitation the rights to use, copy, modify, merge, publish,
+# distribute, sublicense, and/or sell copies of the Software, and to
+# permit persons to whom the Software is furnished to do so, subject to
+# the following conditions:
+#
+# The above copyright notice and this permission notice shall be
+# included in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+# IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+# CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+# TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+# SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+import importlib
+import sys
+from os import PathLike
+from pathlib import Path
+
+
+def get_project_root_path(
+        module_name: str,
+        source_depth: int = 0
+) -> PathLike:
+    """
+    Return the root path of the Python project.
+
+
+    :param module_name: The name of the current module from which this
+        function is called.  This SHOULD be the Python built-in variable
+        ``__name__``, which evaluates to the name of the current module.
+
+    :param source_depth: The depth of the Python project's source directory
+        from the Python project's root directory.
+
+        Python developers usually create a ``src`` directory at the root of
+        their Python project where they organize their project's modules,
+        rather than putting their sources file directly in the root
+        directory.  For example::
+
+        ```text
+        .
+        ├── CHANGELOG.md
+        ├── LICENSE.md
+        ├── README.md
+        ├── data
+        │   ├── ...
+        │   .
+        │   └── ...
+        ├── poetry.lock
+        ├── pyproject.toml
+        ├── run.py
+        └── src
+            └── ...
+                ├── __init__.py
+                ├── ...
+                .
+                └── ...
+                .   ├── __init__.py
+                .   .
+                .   └── ...
+                └── ...
+        ```
+
+        In this case, the depth of the Python project's source directory
+        is ``1``.
+
+
+    :return: The root path of the Python project.
+
+
+    :raise ValueError: If the module name doesn't refer to a Python
+        package but to a not packaged script (i.e., ``__main__``).
+    """
+    if module_name == '__main__':
+        raise ValueError("A Python packaged module is required")
+
+    if source_depth < 0:
+        raise ValueError("The source depth must be a positive integer")
+
+    module_package_depth = len(module_name.split('.'))
+    module_package_file_name = Path(__file__)
+    project_path = module_package_file_name.parents[module_package_depth + source_depth]
+
+    return project_path
+
+
+def load_module(module_package_name):
+    """
+    Load the specified Python module specified by a string representation.
+
+
+    @note: The function prevents from circular import.
+
+
+    :param module_package_name: A string representation of the Python
+        packaged module (module namespace) using "dotted module names",
+        e.g., `foo.bar`.
+
+
+    :return: An object of the specified module.
+    """
+    return sys.modules.get(module_package_name) or importlib.import_module(module_package_name)
+
+
+def load_class(class_module_package_name):
+    """
+    Dynamically load a Python class specified by the string representation
+    of this class in a Python packaged module (module namespace).
+
+
+    @note: The function prevents from circular import.
+
+
+    :param class_module_package_name: A string representation of the class
+        to load using "dotted module names", e.g., `foo.bar.MyClass`.
+
+
+    :return: The specified class.
+    """
+    module_package_name, class_name = class_module_package_name.rsplit('.', 1)
+
+    return getattr(load_module(module_package_name), class_name)
