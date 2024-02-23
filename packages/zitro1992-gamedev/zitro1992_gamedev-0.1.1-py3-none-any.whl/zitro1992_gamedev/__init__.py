a6ef7effb058a4ac174fbc74e130f3fb903b164a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-23 15:04:49 +0000

    Add package zitro1992-gamedev/0.1.1/zitro1992_gamedev-0.1.1-py3-none-any.whl

---
+++ packages/zitro1992-gamedev/zitro1992_gamedev-0.1.1-py3-none-any.whl/zitro1992_gamedev/__init__.py
@@ -0,0 +1,1 @@
+from first_person_shooter import FirstPersonShooter
---
+++ packages/zitro1992-gamedev/zitro1992_gamedev-0.1.1-py3-none-any.whl/zitro1992_gamedev/first_person_shooter.py
@@ -0,0 +1,6 @@
+class FirstPersonShooter():
+    def __init__(self, name: str) -> None:
+        self.name=name
+
+    def hello_world():
+        return "This is hello world from my first project!"
