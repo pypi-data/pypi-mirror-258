52cca83a2e43f9f4beaf2508c9eb43563447258d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-23 04:04:14 +0000

    Add package prediction-market-agent-tooling/0.2.7/prediction_market_agent_tooling-0.2.7-py3-none-any.whl

---
+++ packages/prediction-market-agent-tooling/prediction_market_agent_tooling-0.2.7-py3-none-any.whl/prediction_market_agent_tooling/tools/gnosis_rpc.py
@@ -0,0 +1,21 @@
+import os
+
+import requests
+from web3.types import Wei
+
+GNOSIS_RPC_URL = os.getenv("GNOSIS_RPC_URL", "https://gnosis-rpc.publicnode.com")
+
+
+def get_balance(address: str) -> Wei:
+    response = requests.post(
+        GNOSIS_RPC_URL,
+        json={
+            "jsonrpc": "2.0",
+            "method": "eth_getBalance",
+            "params": [address, "latest"],
+            "id": 1,
+        },
+        headers={"content-type": "application/json"},
+    ).json()
+    balance = Wei(int(response["result"], 16))  # Convert hex value to int.
+    return balance
