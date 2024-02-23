65ae83a36a99e763f730ad82c73c03d07acb7db0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-23 06:04:46 +0000

    Add package slack-time-localization-bot/0.1.2/slack_time_localization_bot-0.1.2.tar.gz

---
+++ packages/slack-time-localization-bot/slack_time_localization_bot-0.1.2.tar.gz/slack_time_localization_bot-0.1.2/slack_time_localization_bot/config.py
@@ -0,0 +1,7 @@
+import logging
+import os
+
+TIME_FORMAT = "%H:%M"
+LOG_LEVEL = logging.INFO
+SLACK_BOT_TOKEN = os.environ.get("SLACK_BOT_TOKEN")
+SLACK_APP_TOKEN = os.environ.get("SLACK_APP_TOKEN")
