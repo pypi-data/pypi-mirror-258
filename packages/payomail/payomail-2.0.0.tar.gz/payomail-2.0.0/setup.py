313910120d3f63d2c7c525a2f570308fac690339
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-23 05:04:41 +0000

    Add package payomail/2.0.0/payomail-2.0.0.tar.gz

---
+++ packages/payomail/payomail-2.0.0.tar.gz/payomail-2.0.0/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='payomail',
+    version='2.0.0',
+    packages=find_packages(),
+    install_requires=[
+     
+    ],
+    # Metadata
+    author='Roshan Gedam',
+    author_email='roshangedam1998@gmail.com',
+    description='mailing utilty',
+    url='https://github.com/Roshangedam/payomail',
+)
