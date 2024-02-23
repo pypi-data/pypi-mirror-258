1e3e02f9b0b91cd4ea3830ce22c0781c62144321
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-23 15:04:10 +0000

    Add package testbugmaker/0.1/testbugmaker-0.1.tar.gz

---
+++ packages/testbugmaker/testbugmaker-0.1.tar.gz/testbugmaker-0.1/setup.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*- 
+# @Time : 2022/3/22 22:11 
+# @Author : 
+# @File : setup.py
+
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="testbugmaker",
+    version="0.1",
+    author="JackTest",
+    author_email="workspaceelojack@gmail.com",
+    description="test bugmaker platfrom",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/airdrop11t/airdrop_test",
+    packages=setuptools.find_packages(),
+    python_requires='>=3.7.0',
+    install_requires=['kazoo','pyyaml','requests'],
+    license='MIT',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
