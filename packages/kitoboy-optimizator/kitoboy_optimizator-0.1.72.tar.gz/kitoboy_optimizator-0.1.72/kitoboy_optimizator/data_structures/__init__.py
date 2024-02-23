16b136ffb111f0ac7a5d3e7516214f90b9cea4bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-23 03:04:46 +0000

    Add package kitoboy-optimizator/0.1.72/kitoboy_optimizator-0.1.72.tar.gz

---
+++ packages/kitoboy-optimizator/kitoboy_optimizator-0.1.72.tar.gz/kitoboy_optimizator-0.1.72/kitoboy_optimizator/data_structures/__init__.py
@@ -0,0 +1,9 @@
+__all__ = [
+    "DownloadingTask",
+    "OptimizationTask",
+    "BacktestResults"
+]
+
+from .downloading_task import DownloadingTask
+from .optimization_task import OptimizationTask
+from .backtest_results import BacktestResults
