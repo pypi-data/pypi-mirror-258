6687c1fa525aac9f362748cd7acc1af3a58c889d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-23 16:04:23 +0000

    Add package sankuai/4.0.0/sankuai-4.0.0.tar.gz

---
+++ packages/sankuai/sankuai-4.0.0.tar.gz/sankuai-4.0.0/setup.py
@@ -0,0 +1,28 @@
+
+#!/usr/bin/env python
+# coding: utf-8
+
+from setuptools import setup
+
+
+#rn()
+print(111)
+
+setup(
+    name='sankuai',
+    version='4.0.0', # 项�~[��~I~H�~\� 
+    author='xingxing', # 项�~[��~\�~@~E 
+    author_email='duxinglin123@gmail.com', # �~\�~@~Eemail 
+    url='https://github.com/', # 项�~[�代�| ~A�~S�~S
+    description='hello word', # 项�~[��~O~O述 
+    #packages=['portal-api'], # �~L~E�~P~M 
+    install_requires=[],
+        classifiers          = [
+        'Topic :: Security',
+        'Environment :: Console',
+        'Operating System :: OS Independent',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2.7',
+        'Intended Audience :: Developers'
+    ]
+)
