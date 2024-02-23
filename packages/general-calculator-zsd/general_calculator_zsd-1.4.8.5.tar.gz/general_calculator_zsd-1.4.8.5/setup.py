b9043d709a3d1d4374a85f2442b25b74ae6d3c72
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-23 16:04:23 +0000

    Add package general-calculator-zsd/1.4.8.5/general_calculator_zsd-1.4.8.5.tar.gz

---
+++ packages/general-calculator-zsd/general_calculator_zsd-1.4.8.5.tar.gz/general_calculator_zsd-1.4.8.5/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='general_calculator_zsd',
+    version='1.4.8.5',
+    author='harden.jiang',
+    author_email='zsd0830@163.com',
+    description='添加微信上传',
+    long_description_content_type="""text/markdown""",
+    url='',
+    packages=find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ]
+)
+
