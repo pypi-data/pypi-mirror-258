bef87a197e84d962464f01414daeda6bda799413
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-23 05:04:06 +0000

    Add package fgpyo/0.1.3/fgpyo-0.1.3.tar.gz

---
+++ packages/fgpyo/fgpyo-0.1.3.tar.gz/fgpyo-0.1.3/fgpyo/util/tests/test_inspect.py
@@ -0,0 +1,68 @@
+from typing import Optional
+
+import attr
+import pytest
+
+from fgpyo.util.inspect import attr_from
+from fgpyo.util.inspect import attribute_has_default
+from fgpyo.util.inspect import attribute_is_optional
+
+
+@attr.s(auto_attribs=True, frozen=True)
+class Name:
+    required: str
+    custom_parser: str
+    optional_no_default: Optional[str]
+    optional_with_default_none: Optional[str] = None
+    optional_with_default_some: Optional[str] = "foo"
+
+
+def test_attr_from() -> None:
+    name = attr_from(
+        cls=Name,
+        kwargs={"required": "required", "custom_parser": "before"},
+        parsers={str: lambda value: "after" if value == "before" else value},
+    )
+    assert name.required == "required"
+    assert name.custom_parser == "after"
+    assert name.optional_no_default is None
+    assert name.optional_with_default_none is None
+    assert name.optional_with_default_some == "foo"
+
+
+def test_attribute_is_optional() -> None:
+    fields_dict = attr.fields_dict(Name)
+    assert not attribute_is_optional(fields_dict["required"])
+    assert not attribute_is_optional(fields_dict["custom_parser"])
+    assert attribute_is_optional(fields_dict["optional_no_default"])
+    assert attribute_is_optional(fields_dict["optional_with_default_none"])
+    assert attribute_is_optional(fields_dict["optional_with_default_some"])
+
+
+def test_attribute_has_default() -> None:
+    fields_dict = attr.fields_dict(Name)
+    assert not attribute_has_default(fields_dict["required"])
+    assert not attribute_has_default(fields_dict["custom_parser"])
+    assert attribute_has_default(fields_dict["optional_no_default"])
+    assert attribute_has_default(fields_dict["optional_with_default_none"])
+    assert attribute_has_default(fields_dict["optional_with_default_some"])
+
+
+class Foo:
+    pass
+
+
+@attr.s(auto_attribs=True, frozen=True)
+class Bar:
+    foo: Foo
+
+
+# Test for regression #94 - the call to attr_from succeeds when the check for None type
+# in inspect._get_parser is done incorrectly.
+def test_attr_from_custom_type_without_parser_fails() -> None:
+    with pytest.raises(AssertionError):
+        attr_from(
+            cls=Bar,
+            kwargs={"foo": ""},
+            parsers={},
+        )
