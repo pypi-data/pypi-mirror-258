3dff8786a3aee426e49e098c5c538aff83e774e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-23 03:04:02 +0000

    Add package spakky-core/0.1.1/spakky_core-0.1.1-py3-none-any.whl

---
+++ packages/spakky-core/spakky_core-0.1.1-py3-none-any.whl/spakky/core/mutability.py
@@ -0,0 +1,25 @@
+from dataclasses import field, dataclass
+
+from typing_extensions import dataclass_transform
+
+from spakky.core.generics import AnyT
+
+
+@dataclass_transform(
+    eq_default=False,
+    kw_only_default=True,
+    frozen_default=False,
+    field_specifiers=(field,),
+)
+def mutable(cls: type[AnyT]) -> type[AnyT]:
+    return dataclass(frozen=False, kw_only=True, eq=False)(cls)
+
+
+@dataclass_transform(
+    eq_default=False,
+    kw_only_default=True,
+    frozen_default=True,
+    field_specifiers=(field,),
+)
+def immutable(cls: type[AnyT]) -> type[AnyT]:
+    return dataclass(frozen=True, kw_only=True, eq=False)(cls)
