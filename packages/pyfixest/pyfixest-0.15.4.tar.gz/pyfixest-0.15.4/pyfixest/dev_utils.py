39ebf904b73c95275c951e328b159213d9ac6431
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-23 07:03:43 +0000

    Add package pyfixest/0.15.4/pyfixest-0.15.4.tar.gz

---
+++ packages/pyfixest/pyfixest-0.15.4.tar.gz/pyfixest-0.15.4/pyfixest/dev_utils.py
@@ -0,0 +1,24 @@
+from typing import Union
+
+import pandas as pd
+
+try:
+    import polars as pl
+
+    DataFrameType = Union[pd.DataFrame, pl.DataFrame]
+except ImportError:
+    DataFrameType = pd.DataFrame
+
+
+def _polars_to_pandas(data: DataFrameType) -> pd.DataFrame:
+    if not isinstance(data, pd.DataFrame):
+        try:
+            import polars as pl  # noqa: F401
+
+            data = data.to_pandas()
+        except ImportError:
+            raise ImportError(
+                "Polars is not installed. Please install Polars to use it as an alternative."
+            )
+
+    return data
---
+++ packages/pyfixest/pyfixest-0.15.4.tar.gz/pyfixest-0.15.4/pyfixest/exceptions.py
@@ -0,0 +1,62 @@
+class FixedEffectInteractionError(Exception):
+    pass
+
+
+class CovariateInteractionError(Exception):
+    pass
+
+
+class DuplicateKeyError(Exception):
+    pass
+
+
+class EndogVarsAsCovarsError(Exception):
+    pass
+
+
+class InstrumentsAsCovarsError(Exception):
+    pass
+
+
+class UnderDeterminedIVError(Exception):
+    pass
+
+
+class UnsupportedMultipleEstimationSyntax(Exception):
+    pass
+
+
+class VcovTypeNotSupportedError(Exception):
+    pass
+
+
+class MultiEstNotSupportedError(Exception):
+    pass
+
+
+class NanInClusterVarError(Exception):
+    pass
+
+
+class DepvarIsNotNumericError(Exception):
+    pass
+
+
+class NotImplementedError(Exception):
+    pass
+
+
+class NonConvergenceError(Exception):
+    pass
+
+
+class MatrixNotFullRankError(Exception):
+    pass
+
+
+class EmptyDesignMatrixError(Exception):
+    pass
+
+
+class InvalidReferenceLevelError(Exception):
+    pass
