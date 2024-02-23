9684e9c65990db41835dfebb04b2bae1673e7fa4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-23 04:04:36 +0000

    Add package aiondata/0.1.3/aiondata-0.1.3.tar.gz

---
+++ packages/aiondata/aiondata-0.1.3.tar.gz/aiondata-0.1.3/aiondata/__init__.py
@@ -0,0 +1,16 @@
+from .bindingdb import BindingDB
+from .moleculenet import (
+    Tox21,
+    ESOL,
+    FreeSolv,
+    Lipophilicity,
+    QM7,
+    QM8,
+    QM9,
+    MUV,
+    HIV,
+    BACE,
+    BBBP,
+    SIDER,
+    ClinTox,
+)
