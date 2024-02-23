6c98180a9e9463cf0f431e19ce27bbe770aed6f7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-23 04:03:42 +0000

    Add package mdmparis-defense-finder/1.2.2/mdmparis_defense_finder-1.2.2-py3-none-any.whl

---
+++ packages/mdmparis-defense-finder/mdmparis_defense_finder-1.2.2-py3-none-any.whl/defense_finder_posttreat/best_solution.py
@@ -0,0 +1,64 @@
+import os
+import csv
+
+from macsypy.serialization import TsvSystemSerializer
+from macsypy.registries import split_def_name
+
+def get(tmp_dir):
+    results = os.listdir(tmp_dir)
+    acc = []
+
+    for family_dir in results:
+        family_path = os.path.join(tmp_dir, family_dir)
+        acc = acc + parse_best_solution(family_path)
+
+    return format_best_solution(acc)
+
+def uncomment(csvfile):
+    """
+    generator which yield lines in macsyfinder/best_solution files but skip comments or empty lines
+
+    :param csvfile: the csv to parse
+    :type cvsfile: file object
+    """
+    for line in csvfile:
+        uncommented = line.split('#')[0].strip()
+        if uncommented:
+            yield uncommented
+
+
+def parse_best_solution(dir):
+    """
+    :param dir: the macsyfinder result directory path
+    :type dir: str
+    """
+    delimiter = '\t'
+    with open(os.path.join(dir, 'best_solution.tsv')) as tsv_file:
+        tsv = csv.DictReader(uncomment(tsv_file),
+                             fieldnames=get_best_solution_keys(delimiter=delimiter),
+                             delimiter=delimiter)
+        try:
+            next(tsv)
+        except StopIteration:
+            return []
+        data = list(tsv)
+    return data
+
+
+def get_best_solution_keys(delimiter='\t'):
+    return TsvSystemSerializer.header.split(delimiter)
+
+
+def format_best_solution(p):
+    out = []
+    for l in p:
+        gene_ref = l['model_fqn']
+        gene_ref_elements = split_def_name(gene_ref)
+        type = gene_ref_elements[-2]
+        subtype = gene_ref_elements[-1]
+        native_keys = list(filter(lambda i: i not in ['type', 'subtype'], get_best_solution_keys()))
+        new_line = { key: l[key] for key in native_keys }
+        new_line['type'] = type
+        new_line['subtype'] = subtype
+        out.append(new_line)
+    return out
---
+++ packages/mdmparis-defense-finder/mdmparis_defense_finder-1.2.2-py3-none-any.whl/defense_finder_posttreat/df_hmmer.py
@@ -0,0 +1,71 @@
+import os
+import csv
+
+def get_hit_sort_attr(hit):
+    return hit['hit_id']
+
+def remove_duplicates(hmmer_hits):
+    temp_list = []
+    for i in hmmer_hits:
+        if i not in temp_list:
+            temp_list.append(i)
+    return temp_list
+
+def export_defense_finder_hmmer_hits(tmp_dir, outdir, filename):
+    paths = get_hmmer_paths(tmp_dir)
+    hmmer_hits = []
+    for path in paths:
+        d = parse_hmmer_results_file(path)
+        hmmer_hits = hmmer_hits + remove_duplicates(d)
+    sorted_hmmer_hits = sorted(hmmer_hits, key=get_hit_sort_attr)
+    hmmer_hits_list = hmmer_to_list(sorted_hmmer_hits)
+    write_defense_finder_hmmer(hmmer_hits_list, outdir, filename)
+
+def write_defense_finder_hmmer(hmmer_hits_list, outdir, filename):
+    filepath = os.path.join(outdir,  f'{filename}_defense_finder_hmmer.tsv')
+    with open(filepath, 'w') as defense_finder_hmmer_file:
+        write = csv.writer(defense_finder_hmmer_file, delimiter='\t')
+        write.writerows(hmmer_hits_list)
+        defense_finder_hmmer_file.close()
+
+def get_hmmer_keys():
+    return ['hit_id', 'replicon', 'hit_pos', 'hit_sequence_length', 'gene_name','i_eval','hit_score','hit_profile_cov','hit_seq_cov','hit_begin_match','hit_end_match']
+
+def parse_hmmer_results_file(path):
+    tsv_file = open(path)
+    tsv = csv.reader(tsv_file, delimiter='\t')
+    data = []
+    for row in tsv:
+        if not row[0].startswith('#'):
+            data.append(row)
+    tsv_file.close()
+    out = []
+    for l in data:
+        if not l: continue
+        line_as_dict = {}
+        for idx, val in enumerate(get_hmmer_keys()):
+            line_as_dict[val] = l[idx]
+        out.append(line_as_dict)
+    return out
+
+def get_hmmer_paths(results_dir):
+    family_dirs = os.listdir(results_dir)
+    files = []
+    for family_dir in family_dirs:
+        hmmer_results_dir = os.path.join(results_dir, family_dir, 'hmmer_results')
+        with os.scandir(hmmer_results_dir) as it:
+            for entry in it:
+                if entry.name.endswith('extract') and entry.is_file():
+                    files.append(entry)
+    return list(map(lambda i: i.path, files))
+
+
+def hmmer_to_list(hmmer_hits):
+    header = get_hmmer_keys()
+    out = [header]
+    for s in hmmer_hits:
+        l = []
+        for key in header:
+            l.append(s[key])
+        out.append(l)
+    return out
