a73add1825908508d6ad2cb9226eaab7867b0979
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-23 06:04:45 +0000

    Add package selenium-web-browser/0.0.2/selenium_web_browser-0.0.2.tar.gz

---
+++ packages/selenium-web-browser/selenium_web_browser-0.0.2.tar.gz/selenium_web_browser-0.0.2/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    description = fh.read()
+
+setup(
+    name="selenium_web_browser",
+    version="v0.0.2",
+    author="Eren Mustafa Özdal",
+    author_email="eren.060737@gmail.com",
+    packages=find_packages(where="src"),
+    package_dir={"": "src"},
+    description="Module covering Selenium browser",
+    long_description=description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/erenmustafaozdal/selenium-browser",
+    license='MIT',
+    python_requires='>=3.11',
+    install_requires=["selenium>=4.18.1", "webdriver-manager>=4.0.1"],
+)
