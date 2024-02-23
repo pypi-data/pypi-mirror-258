109458b27e76b329207833d28dd862e1f30b7d6c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-23 05:04:00 +0000

    Add package payomail/6.0.0/payomail-6.0.0.tar.gz

---
+++ packages/payomail/payomail-6.0.0.tar.gz/payomail-6.0.0/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name='payomail',
+    version='6.0.0',
+    packages=find_packages(),
+    
+    # Metadata
+    author='Roshan Gedam',
+    author_email='roshangedam1998@gmail.com',
+    description='mailing utilty',
+    url='https://github.com/Roshangedam/payomail',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+)
+
+# for add in publish pacakge use commands
+
+# py setup.py sdist
+
+# twine upload dist/*
+
+# give your api token key
+
+# done
