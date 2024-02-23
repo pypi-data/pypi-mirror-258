47188db8ebab5319ab3d9811016294a9c827748d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-23 03:03:50 +0000

    Add package teo/0.2.6/teo-0.2.6-cp312-cp312-manylinux_2_28_x86_64.whl

---
+++ packages/teo/teo-0.2.6-cp312-cp312-manylinux_2_28_x86_64.whl/teo/__init__.py
@@ -0,0 +1,21 @@
+from .teo import (
+    App, Namespace, HandlerGroup, Model, Field, Relation, Property, Enum, 
+    EnumMember, Response, Request, ReadOnlyHeaderMap, ReadWriteHeaderMap, 
+    HandlerMatch, RequestCtx, ObjectId, Range, OptionVariant, EnumVariant, 
+    File, Pipeline, InterfaceEnumVariant, Pipeline, serve_static_files
+)
+from typing import TypeVar, Union
+from signal import signal, SIGINT
+from sys import exit
+
+
+T = TypeVar('T')
+
+Enumerable = Union[T, list[T]]
+
+async def _main(self):
+    await self._run()
+
+App.run = _main
+
+signal(SIGINT, lambda _, __: exit(0))
