0a6ee221f292a4ae22fc7b46d9c1a7703396715e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-23 14:04:16 +0000

    Add package finisterra/1.0.2/finisterra-1.0.2-py3-none-any.whl

---
+++ packages/finisterra/finisterra-1.0.2-py3-none-any.whl/finisterra/utils/filesystem.py
@@ -0,0 +1,14 @@
+import os
+
+
+def create_version_file(path):
+    file_name = os.path.join(path, "versions.tf")
+    with open(file_name, "w") as version_file:
+        version_file.write('terraform {\n')
+        version_file.write('  required_providers {\n')
+        version_file.write('  aws = {\n')
+        version_file.write('  source  = "hashicorp/aws"\n')
+        version_file.write('  version = "~> 5.33.0"\n')
+        version_file.write('}\n')
+        version_file.write('}\n')
+        version_file.write('}\n')
