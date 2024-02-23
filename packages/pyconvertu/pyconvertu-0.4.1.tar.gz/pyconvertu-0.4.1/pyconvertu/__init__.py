7f5da76009a26af01c14590bb30bba2941ccceb7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-23 04:04:27 +0000

    Add package pyconvertu/0.4.1/pyconvertu-0.4.1.tar.gz

---
+++ packages/pyconvertu/pyconvertu-0.4.1.tar.gz/pyconvertu-0.4.1/pyconvertu/__init__.py
@@ -0,0 +1,3 @@
+from pyconvertu.pyconvertu import convert              # main function
+from pyconvertu.pyconvertu import classification       # classification
+from pyconvertu.pyconvertu import info                 # info, metadata
