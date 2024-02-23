1286d302f3da0220c8631330ed290b8fee07bb20
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-23 15:04:16 +0000

    Add package otx/1.4.5/otx-1.4.5-cp310-cp310-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/otx/otx-1.4.5-cp310-cp310-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/otx/algorithms/segmentation/utils/__init__.py
@@ -0,0 +1,9 @@
+"""Collection of utils for task implementation in Segmentation Task."""
+
+# Copyright (C) 2023 Intel Corporation
+# SPDX-License-Identifier: Apache-2.0
+
+
+from .metadata import get_seg_model_api_configuration
+
+__all__ = ["get_seg_model_api_configuration"]
