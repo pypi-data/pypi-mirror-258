1d8b5e43ebf1dc8c352322163ab6199135514edb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-23 03:04:01 +0000

    Add package rated-python/0.0.2/rated-python-0.0.2.tar.gz

---
+++ packages/rated-python/rated-python-0.0.2.tar.gz/rated-python-0.0.2/src/rated/ethereum/blocks.py
@@ -0,0 +1,80 @@
+from __future__ import annotations
+
+from typing import Iterator, Any, Dict
+
+from rated.base import APIResource
+from rated.client import json_to_instance
+from rated.ethereum.datatypes import Block as EthBlock
+
+
+class Blocks(APIResource):
+    """
+    Blocks allows one to dig into the individual slots and blocks in Ethereum since the Merge.
+    Metrics include validator rewards from the consensus and execution layers, MEV data, and missed reward estimates.
+    """
+
+    path = "/blocks"
+
+    def all(
+        self,
+        *,
+        from_slot: int | None = None,
+        size: int | None = None,
+        follow_next: bool = False,
+    ) -> Iterator[EthBlock]:
+        """
+        Get all blocks
+
+        Examples:
+            >>> from rated import Rated
+            >>> from rated.ethereum import MAINNET
+            >>>
+            >>> RATED_KEY = "ey..."
+            >>> r = Rated(RATED_KEY)
+            >>> eth = r.ethereum(network=MAINNET)
+            >>> blocks = eth.blocks.all(from_slot=500, size=10)
+            >>> for block in blocks:
+            >>>     print(f"{block.total_rewards = }")
+
+        Args:
+            from_slot: Start slot
+            size: Number of results included per page
+            follow_next: Whether to follow pagination or not
+
+        Yields:
+            An iterator over all blocks
+        """
+        params: Dict[str, Any] = {"from": from_slot, "size": size}
+        return self.client.yield_paginated_results(
+            self.resource_path,
+            params=params,
+            cls=EthBlock,
+            follow_next=follow_next,
+        )
+
+
+class Block(APIResource):
+    path = "/blocks"
+
+    def get(self, slot: int) -> EthBlock:
+        """
+        Get a block by consensus slot number
+
+        Examples:
+            >>> from rated import Rated
+            >>> from rated.ethereum import MAINNET
+            >>>
+            >>> RATED_KEY = "ey..."
+            >>> r = Rated(RATED_KEY)
+            >>> eth = r.ethereum(network=MAINNET)
+            >>> block = eth.block.get(500)
+            >>> print(f"{block.total_rewards = }")
+
+        Args:
+            slot: Consensus slot number
+
+        Returns:
+            A single block
+        """
+        data = self.client.get(f"{self.resource_path}/{slot}")
+        return json_to_instance(data, EthBlock)
---
+++ packages/rated-python/rated-python-0.0.2.tar.gz/rated-python-0.0.2/src/rated/ethereum/p2p.py
@@ -0,0 +1,89 @@
+from __future__ import annotations
+
+from typing import Iterator
+
+from rated.base import APIResource
+from rated.client import json_to_instance
+from rated.ethereum.datatypes import (
+    P2PGeographicalDistribution,
+    P2PHostingProviderDistribution,
+)
+from rated.ethereum.enums import DistributionType
+
+
+class P2P(APIResource):
+    """
+    Querying into aggregated stats on Ethereum's peer-to-peer networking layer
+    """
+
+    path = "/p2p"
+
+    def geographical_distribution(
+        self,
+        distribution_type: DistributionType = DistributionType.PROS,
+    ) -> Iterator[P2PGeographicalDistribution]:
+        """
+        Retrieves a list of countries and the respective share of the validator set based in those countries
+
+        Examples:
+            >>> from rated import Rated
+            >>> from rated.ethereum import MAINNET
+            >>>
+            >>> RATED_KEY = "ey..."
+            >>> r = Rated(RATED_KEY)
+            >>> eth = r.ethereum(network=MAINNET)
+            >>> geo_dist = eth.p2p.geographical_distribution()
+            >>> for dist in geo_dist:
+            >>>     print(f"{dist.country = }, {dist.validator_share = }")
+
+        Args:
+            distribution_type: The type of distribution
+
+        Yields:
+            Geographical distribution
+        """
+        url = f"{self.resource_path}/geographical"
+        params = {"distType": distribution_type.value}
+        data = self.client.get(url, params=params)
+        for item in data:
+            yield json_to_instance(item, P2PGeographicalDistribution)
+
+    def hosting_provider_distribution(
+        self,
+        *,
+        from_rank: int | None = None,
+        size: int | None = None,
+        distribution_type: DistributionType = DistributionType.PROS,
+        follow_next: bool = False,
+    ) -> Iterator[P2PHostingProviderDistribution]:
+        """
+        Retrieves a list of hosting providers and their respective share of the validator set
+
+        Examples:
+            >>> from rated import Rated
+            >>> from rated.ethereum import MAINNET
+            >>>
+            >>> RATED_KEY = "ey..."
+            >>> r = Rated(RATED_KEY)
+            >>> eth = r.ethereum(network=MAINNET)
+            >>> provider_dist = eth.p2p.hosting_provider_distribution(size=5)
+            >>> for dist in provider_dist:
+            >>>     print(f"{dist.hosting_provider = }, {dist.validator_share = }")
+
+        Args:
+            from_rank:
+            size: Number of results included per page
+            distribution_type: The type of distribution
+            follow_next: Whether to follow pagination or not
+
+        Yields:
+            Hosting provider distribution
+        """
+        url = f"{self.resource_path}/hostingProvider"
+        params = {"from": from_rank, "size": size, "distType": distribution_type.value}
+        return self.client.yield_paginated_results(
+            url,
+            params=params,
+            cls=P2PHostingProviderDistribution,
+            follow_next=follow_next,
+        )
