d8a17bf73de182f49c63f6b0e4cbf66f4adb43f4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-23 05:04:05 +0000

    Add package pysimmmulator/0.3.0/pysimmmulator-0.3.0.tar.gz

---
+++ packages/pysimmmulator/pysimmmulator-0.3.0.tar.gz/pysimmmulator-0.3.0/src/pysimmmulator/__init__.py
@@ -0,0 +1,12 @@
+__title__ = "CheetahPy"
+__author__ = "RyanAugust"
+__license__ = "MIT"
+__copyright__ = "Copyright 2024"
+
+import os
+
+with open(os.path.join(os.path.dirname(__file__), "VERSION")) as version_file:
+    __version__ = version_file.read().strip()
+
+from .simulate import simmmulate
+from .load_parameters import load_config, define_basic_params
---
+++ packages/pysimmmulator/pysimmmulator-0.3.0.tar.gz/pysimmmulator-0.3.0/tests/test_cli.py
@@ -0,0 +1,11 @@
+from pysimmmulator import simmmulate, command_line
+import subprocess
+
+
+def test_cli():
+    subprocess.run(["pysimmm", "-i", "example_config.yaml", "-o", "."])
+    import os
+    #cleanup
+    os.remove("./mmm_input_df.csv")
+    os.remove("./channel_roi.csv")
+    assert 1 == 1
