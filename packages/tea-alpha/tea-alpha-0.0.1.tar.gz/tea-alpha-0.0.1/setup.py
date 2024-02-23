49abae1f030a66ef182ad0ff9f3a42ca6f3b960a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-23 16:04:47 +0000

    Add package tea-alpha/0.0.1/tea-alpha-0.0.1.tar.gz

---
+++ packages/tea-alpha/tea-alpha-0.0.1.tar.gz/tea-alpha-0.0.1/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="tea-alpha",
+    version="0.0.1",
+    keywords=['Exception'],
+    license='MIT license',
+    author="alphagot812",
+    author_email="alphagot@163.com",
+    install_requires=[],
+    url="https://github.com/alphagot812/tea-alpha",
+    packages=find_packages(),
+    description="Exception"
+)
