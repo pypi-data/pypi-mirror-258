fcbcfbd9cc0833412a36f0d90749de6c2875d9d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-23 15:04:35 +0000

    Add package ast-grep-cli/0.19.2/ast_grep_cli-0.19.2.tar.gz

---
+++ packages/ast-grep-cli/ast_grep_cli-0.19.2.tar.gz/ast_grep_cli-0.19.2/crates/cli/src/lsp.rs
@@ -0,0 +1,37 @@
+use crate::config::{find_rules, register_custom_language};
+use crate::error::ErrorContext as EC;
+use anyhow::{Context, Result};
+use ast_grep_lsp::{Backend, LspService, Server};
+
+async fn run_language_server_impl() -> Result<()> {
+  // env_logger::init();
+  register_custom_language(None)?;
+  let stdin = tokio::io::stdin();
+  let stdout = tokio::io::stdout();
+  let config = find_rules(None, None).unwrap_or_default();
+
+  let (service, socket) = LspService::build(|client| Backend::new(client, config))
+    .custom_method("ast-grep/search", Backend::search)
+    .finish();
+  Server::new(stdin, stdout, socket).serve(service).await;
+  Ok(())
+}
+
+pub fn run_language_server() -> Result<()> {
+  tokio::runtime::Builder::new_multi_thread()
+    .enable_all()
+    .build()
+    .context(EC::StartLanguageServer)?
+    .block_on(async { run_language_server_impl().await })
+}
+
+#[cfg(test)]
+mod test {
+  use super::*;
+
+  #[test]
+  #[ignore = "test lsp later"]
+  fn test_lsp_start() {
+    assert!(run_language_server().is_err())
+  }
+}
