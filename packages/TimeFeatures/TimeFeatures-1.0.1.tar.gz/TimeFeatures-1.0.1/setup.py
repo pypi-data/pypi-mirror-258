dfce4eafa5c859f417ba9bec6ecb1ab64655be0c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-23 15:04:15 +0000

    Add package TimeFeatures/1.0.1/TimeFeatures-1.0.1.tar.gz

---
+++ packages/TimeFeatures/TimeFeatures-1.0.1.tar.gz/TimeFeatures-1.0.1/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(name="TimeFeatures",
+      packages=["timefeatures.widgets"],
+      package_data={"timefeatures.widgets": ["icons/*.svg", "icons/*.png"]},
+      entry_points={"orange.widgets": "Time-Features = timefeatures.widgets"},
+      version="1.0.1",
+      author="Alejandro Rivas García",
+      author_email="alejandrorivasgarcia@gmail.com",
+      keywords=[
+    'orange3 add-on','timefeatures','graph','time series'
+],
+      url="https://github.com/alervgr/Orange-TimeFeatures"
+      )
