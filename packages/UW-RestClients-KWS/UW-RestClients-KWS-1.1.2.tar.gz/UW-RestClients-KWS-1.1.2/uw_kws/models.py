02f3af761ac643b83ad63ec8d2a5c4955704b873
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-23 05:04:47 +0000

    Add package UW-RestClients-KWS/1.1.2/UW-RestClients-KWS-1.1.2.tar.gz

---
+++ packages/UW-RestClients-KWS/UW-RestClients-KWS-1.1.2.tar.gz/UW-RestClients-KWS-1.1.2/uw_kws/models.py
@@ -0,0 +1,27 @@
+# Copyright 2024 UW-IT, University of Washington
+# SPDX-License-Identifier: Apache-2.0
+
+from restclients_core import models
+import dateutil.parser
+
+
+class Key(models.Model):
+    algorithm = models.CharField(max_length=100)
+    cipher_mode = models.CharField(max_length=100)
+    expiration = models.DateTimeField()
+    key_id = models.CharField(max_length=100)
+    key = models.CharField(max_length=100)
+    size = models.SmallIntegerField()
+    url = models.CharField(max_length=1000)
+
+    @staticmethod
+    def from_json(data):
+        key = Key()
+        key.algorithm = data["Algorithm"]
+        key.cipher_mode = data["CipherMode"]
+        key.expiration = dateutil.parser.parse(data["Expiration"])
+        key.key_id = data["ID"]
+        key.key = data["Key"]
+        key.size = data["KeySize"]
+        key.url = data["KeyUrl"]
+        return key
