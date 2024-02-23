634559daccc267c5e6fff7e6cdd262baa793b528
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-23 06:04:09 +0000

    Add package zyte-spider-templates/0.7.1/zyte_spider_templates-0.7.1-py3-none-any.whl

---
+++ packages/zyte-spider-templates/zyte_spider_templates-0.7.1-py3-none-any.whl/zyte_spider_templates/spiders/base.py
@@ -0,0 +1,122 @@
+from enum import Enum
+from importlib.metadata import version
+from typing import Any, Dict, Optional
+
+import scrapy
+from pydantic import BaseModel, Field
+from scrapy.crawler import Crawler
+
+from zyte_spider_templates._geolocations import (
+    GEOLOCATION_OPTIONS_WITH_CODE,
+    Geolocation,
+)
+from zyte_spider_templates.documentation import document_enum
+
+# Higher priority than command-line-defined settings (40).
+ARG_SETTING_PRIORITY: int = 50
+
+
+@document_enum
+class ExtractFrom(str, Enum):
+    httpResponseBody: str = "httpResponseBody"
+    """Use HTTP responses. Cost-efficient and fast extraction method, which
+    works well on many websites."""
+
+    browserHtml: str = "browserHtml"
+    """Use browser rendering. Often provides the best quality."""
+
+
+class BaseSpiderParams(BaseModel):
+    url: str = Field(
+        title="URL",
+        description="Initial URL for the crawl. Enter the full URL including http(s), "
+        "you can copy and paste it from your browser. Example: https://toscrape.com/",
+        pattern=r"^https?://[^:/\s]+(:\d{1,5})?(/[^\s]*)*(#[^\s]*)?$",
+    )
+    geolocation: Optional[Geolocation] = Field(
+        title="Geolocation",
+        description="ISO 3166-1 alpha-2 2-character string specified in "
+        "https://docs.zyte.com/zyte-api/usage/reference.html#operation/extract/request/geolocation.",
+        default=None,
+        json_schema_extra={
+            "enumMeta": {
+                code: {
+                    "title": GEOLOCATION_OPTIONS_WITH_CODE[code],
+                }
+                for code in Geolocation
+            }
+        },
+    )
+    max_requests: Optional[int] = Field(
+        description=(
+            "The maximum number of Zyte API requests allowed for the crawl.\n"
+            "\n"
+            "Requests with error responses that cannot be retried or exceed "
+            "their retry limit also count here, but they incur in no costs "
+            "and do not increase the request count in Scrapy Cloud."
+        ),
+        default=100,
+        json_schema_extra={
+            "widget": "request-limit",
+        },
+    )
+    extract_from: Optional[ExtractFrom] = Field(
+        title="Extraction source",
+        description=(
+            "Whether to perform extraction using a browser request "
+            "(browserHtml) or an HTTP request (httpResponseBody)."
+        ),
+        default=None,
+        json_schema_extra={
+            "enumMeta": {
+                ExtractFrom.browserHtml: {
+                    "title": "browserHtml",
+                    "description": "Use browser rendering. Often provides the best quality.",
+                },
+                ExtractFrom.httpResponseBody: {
+                    "title": "httpResponseBody",
+                    "description": "Use HTTP responses. Cost-efficient and fast extraction method, which works well on many websites.",
+                },
+            },
+        },
+    )
+
+
+class BaseSpider(scrapy.Spider):
+    custom_settings: Dict[str, Any] = {
+        "ZYTE_API_TRANSPARENT_MODE": True,
+        "_ZYTE_API_USER_AGENT": f"zyte-spider-templates/{version('zyte-spider-templates')}",
+    }
+
+    metadata: Dict[str, Any] = {
+        "template": True,
+        "title": "Base",
+        "description": "Base template.",
+    }
+
+    _NEXT_PAGE_PRIORITY: int = 100
+
+    @classmethod
+    def from_crawler(cls, crawler: Crawler, *args, **kwargs) -> scrapy.Spider:
+        spider = super().from_crawler(crawler, *args, **kwargs)
+
+        if spider.args.geolocation:
+            # We set the geolocation in ZYTE_API_PROVIDER_PARAMS for injected
+            # dependencies, and in ZYTE_API_AUTOMAP_PARAMS for page object
+            # additional requests.
+            for component in ("AUTOMAP", "PROVIDER"):
+                default_params = spider.settings.getdict(f"ZYTE_API_{component}_PARAMS")
+                default_params["geolocation"] = spider.args.geolocation
+                spider.settings.set(
+                    f"ZYTE_API_{component}_PARAMS",
+                    default_params,
+                    priority=ARG_SETTING_PRIORITY,
+                )
+
+        if spider.args.max_requests:
+            spider.settings.set(
+                "ZYTE_API_MAX_REQUESTS",
+                spider.args.max_requests,
+                priority=ARG_SETTING_PRIORITY,
+            )
+        return spider
