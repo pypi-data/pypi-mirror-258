73c54fdfef23a716bc090a629ddbd3f5bf803cc4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-23 05:03:41 +0000

    Add package policyengine-us/0.678.0/policyengine-us-0.678.0.tar.gz

---
+++ packages/policyengine-us/policyengine-us-0.678.0.tar.gz/policyengine-us-0.678.0/policyengine_us/variables/household/income/household/household_tax_before_refundable_credits.py
@@ -0,0 +1,37 @@
+from policyengine_us.model_api import *
+
+
+class household_tax_before_refundable_credits(Variable):
+    value_type = float
+    entity = Household
+    label = "total tax before refundable credits"
+    documentation = "Total tax liability before refundable credits."
+    unit = USD
+    definition_period = YEAR
+
+    adds = "gov.household_tax_before_refundable_credits"
+
+    def formula(household, period, parameters):
+        p = parameters(period)
+        added_components = p.gov.household_tax_before_refundable_credits
+        flat_tax = p.gov.contrib.ubi_center.flat_tax
+        if p.simulation.reported_state_income_tax:
+            added_components = [
+                "employee_payroll_tax",
+                "self_employment_tax",
+                "income_tax_before_refundable_credits",  # Federal.
+                "flat_tax",
+                # State tax reported in ASEC includes refundable credits.
+                "spm_unit_state_tax_reported",
+            ]
+        if flat_tax.abolish_payroll_tax:
+            added_components = [
+                c for c in added_components if c != "employee_payroll_tax"
+            ]
+        if flat_tax.abolish_federal_income_tax:
+            added_components = [
+                c
+                for c in added_components
+                if c != "income_tax_before_refundable_credits"
+            ]
+        return add(household, period, added_components)
