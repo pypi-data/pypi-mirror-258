14923695b48be6bde881cae9bea344a4276a02d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-23 05:04:11 +0000

    Add package ltd-conveyor/0.8.2/ltd-conveyor-0.8.2.tar.gz

---
+++ packages/ltd-conveyor/ltd-conveyor-0.8.2.tar.gz/ltd-conveyor-0.8.2/src/ltdconveyor/keeper/exceptions.py
@@ -0,0 +1,10 @@
+"""Exceptions related to the LTD Keeper.
+"""
+
+__all__ = ("KeeperError",)
+
+from ..exceptions import ConveyorError
+
+
+class KeeperError(ConveyorError):
+    """Error raised because of issues using the LTD Keeper API."""
