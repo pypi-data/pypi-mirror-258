c002a8f3d8b6687976eb72759969bb11801a8090
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-24 04:04:21 +0000

    Add package jaxonloader/0.1.6/jaxonloader-0.1.6-py3-none-any.whl

---
+++ packages/jaxonloader/jaxonloader-0.1.6-py3-none-any.whl/jaxonloader/dataset.py
@@ -0,0 +1,29 @@
+from abc import ABC, abstractmethod
+from typing import Any
+
+from jaxtyping import Array
+
+
+class Dataset(ABC):
+    @abstractmethod
+    def __len__(self) -> int:
+        raise NotImplementedError
+
+    @abstractmethod
+    def __getitem__(self, idx: int) -> Any:
+        raise NotImplementedError
+
+
+class StandardDataset(Dataset):
+    def __init__(self, *columns: Array):
+        self.columns = columns
+        if not all(len(c) == len(columns[0]) for c in columns):
+            raise ValueError("All columns must have the same length")
+        if len(columns) == 0:
+            raise ValueError("At least one column is required")
+
+    def __len__(self):
+        return len(self.columns[0])
+
+    def __getitem__(self, idx: int):
+        return tuple(c[idx] for c in self.columns)
