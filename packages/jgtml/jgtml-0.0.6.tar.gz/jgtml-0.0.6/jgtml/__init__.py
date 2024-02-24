597e6c4c4ca2b1744f40754e8f5bd610fdb2f07a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-24 05:06:31 +0000

    Add package jgtml/0.0.6/jgtml-0.0.6.tar.gz

---
+++ packages/jgtml/jgtml-0.0.6.tar.gz/jgtml-0.0.6/jgtml/__init__.py
@@ -0,0 +1,13 @@
+# jgtml
+__version__ = "0.0.6"
+import sys
+import os
+
+sys.path.insert(0, os.path.abspath(os.path.dirname(__file__)))
+
+from calc import (
+    calculate_target_variable_min_max as calc_target_from_df,
+    pto_target_calculation as calc_target_to_file,
+)
+
+# from jgtpy import JGTPDS as pds,JGTADS as ads,JGTPDSP as pds
