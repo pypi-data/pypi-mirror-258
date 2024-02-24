44dd32d3c2389bd17b63c83dfa790e78e5f1adb5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-24 16:04:38 +0000

    Add package jaxonloader/0.1.8/jaxonloader-0.1.8.tar.gz

---
+++ packages/jaxonloader/jaxonloader-0.1.8.tar.gz/jaxonloader-0.1.8/jaxonloader/dataset.py
@@ -0,0 +1,39 @@
+from abc import ABC, abstractmethod
+from dataclasses import dataclass
+from typing import Any
+
+from jaxtyping import Array
+
+
+@dataclass
+class Dataset(ABC):
+    @abstractmethod
+    def __len__(self) -> int:
+        raise NotImplementedError
+
+    @abstractmethod
+    def __getitem__(self, idx: int) -> Any:
+        raise NotImplementedError
+
+
+@dataclass
+class StandardDataset(Dataset):
+    columns: tuple[Array, ...]
+
+    def __init__(self, *columns: Array):
+        self.columns = columns
+        if len(columns) == 0:
+            raise ValueError("At least one column is required")
+
+    def __len__(self) -> int:
+        return len(self.columns[0])
+
+    def __getitem__(self, idx: int) -> tuple:
+        res_tuple = ()
+        for c in self.columns:
+            if idx >= len(c):
+                res_tuple += (None,)
+            else:
+                res_tuple += (c[idx],)
+
+        return res_tuple
