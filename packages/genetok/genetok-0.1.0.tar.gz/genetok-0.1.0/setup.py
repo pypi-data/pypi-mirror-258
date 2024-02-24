76dcd913b8d3481c2b4d9d4957edb2aed4a71c6e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-24 15:05:09 +0000

    Add package genetok/0.1.0/genetok-0.1.0.tar.gz

---
+++ packages/genetok/genetok-0.1.0.tar.gz/genetok-0.1.0/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+with open("genetok/README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+with open("genetok/requirements.txt", "r", encoding="utf-8") as fh:
+    requirements = fh.readlines()
+
+setup(
+    name="genetok",
+    version="0.1.0",
+    author="Daniel Losey",
+    author_email="danieljlosey@gmail.com",
+    description="A Genetic Algorithm Tokenizer",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/dadukhankevin/genetok",
+    packages=find_packages(),
+    python_requires=">=3.6",
+    install_requires=requirements,
+)
