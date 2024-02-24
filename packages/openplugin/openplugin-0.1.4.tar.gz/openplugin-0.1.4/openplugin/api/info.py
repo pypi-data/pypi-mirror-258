7ea28224d5f10b86ec3ef367aebf76caba3d8480
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-24 04:04:52 +0000

    Add package openplugin/0.1.4/openplugin-0.1.4.tar.gz

---
+++ packages/openplugin/openplugin-0.1.4.tar.gz/openplugin-0.1.4/openplugin/api/info.py
@@ -0,0 +1,39 @@
+import datetime
+from fastapi import APIRouter
+from fastapi.responses import JSONResponse
+
+# Create a FastAPI router instance
+router = APIRouter(
+    dependencies=[],
+    responses={404: {"description": "Not found"}},
+)
+
+
+@router.get("/info")
+def info():
+    # time taken to run this function
+    start = datetime.datetime.now()
+    response = JSONResponse(
+        status_code=200,
+        content={"message": "OpenPlugin API"},
+    )
+    end = datetime.datetime.now()
+    elapsed_time = end - start
+    return JSONResponse(
+        status_code=200,
+        content={
+            "message": "OpenPlugin API",
+            "start_time": start.strftime("%Y-%m-%d %H:%M:%S"),
+            "end_time": end.strftime("%Y-%m-%d %H:%M:%S"),
+            "time_taken_seconds": elapsed_time.seconds,
+            "time_taken_ms": elapsed_time.microseconds,
+        },
+    )
+
+
+@router.get("/")
+def version():
+    return JSONResponse(
+        status_code=200,
+        content={"message": "OpenPlugin API"},
+    )
