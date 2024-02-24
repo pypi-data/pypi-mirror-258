db4527ccc6685bfed1942a386d9b68266604894c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-24 15:05:26 +0000

    Add package jgtml/0.0.11/jgtml-0.0.11.tar.gz

---
+++ packages/jgtml/jgtml-0.0.11.tar.gz/jgtml-0.0.11/tests/test-plot.py
@@ -0,0 +1,38 @@
+#%% Imports
+import pandas as pd
+import os
+#import jgtml as jml
+from jgtml import  jtc
+
+from jgtml import jplt
+from jgtpy import JGTADS as ads
+
+import tlid
+tlid_tag = tlid.get_minutes()
+
+
+crop_end_dt=None;crop_start_dt=None
+
+I_raw = os.getenv('I')
+T_raw = os.getenv('T')
+
+if I_raw is None or T_raw is None:
+    raise ValueError("Environment variables 'I' and 'T' must be set.")
+
+instruments = I_raw.split(',')
+timeframes = T_raw.split(',')
+
+
+
+print("Processing", I_raw, T_raw)
+for i in instruments:
+    for t in timeframes:
+        print("Processing POV:" , i, t)
+        #jtc.pto_target_calculation(i,t)
+        c,a,d=ads.plot(i,t,show=False)
+        p=jplt.an_bivariate_plot00(d)
+
+
+
+
+# %%
