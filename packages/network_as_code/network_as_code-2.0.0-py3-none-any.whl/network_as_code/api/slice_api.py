512c4cd4e9c3e6463184530f2085eb2468712dc3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-24 03:05:06 +0000

    Add package network_as_code/2.0.0/network_as_code-2.0.0-py3-none-any.whl

---
+++ packages/network_as_code/network_as_code-2.0.0-py3-none-any.whl/network_as_code/api/slice_api.py
@@ -0,0 +1,227 @@
+# Copyright 2023 Nokia
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import json
+import os
+import pdb
+import httpx
+
+from typing import Optional, Any
+from pydantic import BaseModel
+
+from ..errors import error_handler
+
+class Throughput(BaseModel):
+    """
+    A class representing the `Throughput` API field.
+
+    #### Public Attributes:
+            guaranteed (float): the guaranteed throughput in kbps
+            maximum (float): the maximum throughput in kbps
+    """
+
+    guaranteed: Optional[float]
+    maximum: Optional[float]
+
+
+class SliceAPI:
+    def __init__(self, base_url: str, rapid_key: str, rapid_host: str) -> None:
+        self.client = httpx.Client(
+            base_url=base_url,
+            headers={
+                "content-type": "application/json",
+                "X-RapidAPI-Key": rapid_key,
+                "X-RapidAPI-Host": rapid_host,
+            },
+        )
+
+    def create(
+        self,
+        network_id,
+        slice_info,
+        notification_url,
+        name: Optional[str] = None,
+        notification_auth_token: Optional[str] = None,
+        area_of_service: Optional[Any] = None,
+        slice_downlink_throughput: Optional[Throughput] = None,
+        slice_uplink_throughput: Optional[Throughput] = None,
+        device_downlink_throughput: Optional[Throughput] = None,
+        device_uplink_throughput: Optional[Throughput] = None,
+        max_data_connections: Optional[int] = None,
+        max_devices: Optional[int] = None,
+    ):
+        body = {
+            "networkIdentifier": dict(network_id),
+            "sliceInfo": self.convert_slice_info_obj(slice_info),
+            "notificationUrl": notification_url,
+        }
+
+        if name:
+            body["name"] = name
+
+        if area_of_service:
+            body["areaOfService"] = self.convert_area_of_service_obj(area_of_service)
+
+        if notification_auth_token:
+            body["notificationAuthToken"] = notification_auth_token
+
+        if max_data_connections:
+            body["maxDataConnections"] = max_data_connections
+
+        if max_devices:
+            body["maxDevices"] = max_devices
+
+        if slice_downlink_throughput:
+            body["sliceDownlinkThroughput"] = self.convert_throughput_obj(
+                slice_downlink_throughput
+            )
+
+        if slice_uplink_throughput:
+            body["sliceUplinkThroughput"] = self.convert_throughput_obj(
+                slice_uplink_throughput
+            )
+
+        if device_uplink_throughput:
+            body["deviceUplinkThroughput"] = self.convert_throughput_obj(
+                device_uplink_throughput
+            )
+
+        if device_downlink_throughput:
+            body["deviceDownlinkThroughput"] = self.convert_throughput_obj(
+                device_downlink_throughput
+            )
+
+        response = self.client.post(url="/slices", json=body)
+
+        error_handler(response)
+
+        return response
+
+    def getAll(self):
+        res = self.client.get(
+            url="/slices",
+        )
+
+        error_handler(res)
+
+        return res
+
+    def get(self, slice_id: str):
+        res = self.client.get(
+            url=f"/slices/{slice_id}",
+        )
+
+        error_handler(res)
+
+        return res
+
+    def activate(self, slice_id: str):
+        res = self.client.post(
+            url=f"/slices/{slice_id}/activate",
+        )
+
+        error_handler(res)
+
+        return res
+
+    def deactivate(self, slice_id: str):
+        return self.client.post(
+            url=f"/slices/{slice_id}/deactivate",
+        )
+
+    def delete(self, slice_id: str):
+        res = self.client.delete(
+            url=f"/slices/{slice_id}",
+        )
+
+        error_handler(res)
+
+        return res
+
+    def convert_area_of_service_obj(self, areaOfService):
+        polygons = []
+
+        for point in areaOfService.polygon:
+            polygons.append({"lat": point.latitude, "lon": point.longitude})
+
+        return {"polygon": polygons}
+
+    def convert_slice_info_obj(self, sliceInfo):
+        return {k: str(v) for k, v in dict(sliceInfo).items()}
+
+    def convert_throughput_obj(self, throughput: Throughput):
+        return {k: float(v) for k, v in dict(throughput).items()}
+
+
+def delete_none(_dict):
+    """Delete None values recursively from all of the dictionaries"""
+    for key, value in list(_dict.items()):
+        if isinstance(value, dict):
+            delete_none(value)
+        elif value is None:
+            del _dict[key]
+        elif isinstance(value, list):
+            for v_i in value:
+                if isinstance(v_i, dict):
+                    delete_none(v_i)
+
+    return _dict
+
+
+class AttachAPI:
+    def __init__(self, base_url: str, rapid_key: str, rapid_host: str) -> None:
+        self.client = httpx.Client(
+            base_url=base_url,
+            headers={"X-RapidAPI-Key": rapid_key, "X-RapidAPI-Host": rapid_host},
+        )
+
+    def attach(
+        self,
+        device,
+        slice_id: str,
+        notification_url: str,
+        notification_auth_token: Optional[str] = None,
+    ):
+        res = self.client.post(
+            url=f"/slice/{slice_id}/attach",
+            json=delete_none(
+                {
+                    "phoneNumber": device.phone_number,
+                    "notificationUrl": notification_url,
+                    "notificationAuthToken": notification_auth_token,
+                }
+            ),
+        )
+
+        error_handler(res)
+
+    def detach(
+        self,
+        device,
+        slice_id: str,
+        notification_url: str,
+        notification_auth_token: Optional[str] = None,
+    ):
+        res = self.client.post(
+            url=f"/slice/{slice_id}/detach",
+            json=delete_none(
+                {
+                    "phoneNumber": device.phone_number,
+                    "notificationUrl": notification_url,
+                    "notificationAuthToken": notification_auth_token,
+                }
+            ),
+        )
+
+        error_handler(res)
---
+++ packages/network_as_code/network_as_code-2.0.0-py3-none-any.whl/network_as_code/models/slice.py
@@ -0,0 +1,333 @@
+# Copyright 2023 Nokia
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from os import access
+from urllib.error import HTTPError
+from pydantic import BaseModel, EmailStr, PrivateAttr, Field, ValidationError
+from typing import Dict, List, Union, Optional
+from enum import Enum
+
+from ..api import APIClient
+from ..models.session import QoDSession
+from ..models.location import CivicAddress, Location
+from ..models.device import Device
+from ..errors import (
+    NotFound,
+    InvalidParameter,
+    NotFound,
+    AuthenticationException,
+    ServiceError,
+    error_handler,
+)
+
+
+class NetworkIdentifier(BaseModel):
+    """
+    A class representing the `NetworkIdentifier` model.
+
+    #### Public Attributes:
+            mnc (str): the `mnc` of a network identifier object.
+            mcc (Optional[str]): the `mcc` of a network identifier object.
+    """
+
+    mnc: str
+    mcc: str
+
+
+class SliceInfo(BaseModel):
+    """
+    A class representing the `SliceInfo` model.
+
+    #### Public Attributes:
+            service_type (str): the service type of a slice object, Example: `eMBB`
+            differentiator (Optional[str]): the differentiator of a slice object.
+    """
+
+    service_type: str
+    differentiator: Optional[str]
+
+
+class Throughput(BaseModel):
+    """
+    A class representing the `Throughput` model.
+
+    #### Public Attributes:
+            guaranteed (float): the guaranteed throughput in kbps
+            maximum (float): the maximum throughput in kbps
+    """
+
+    guaranteed: Optional[float]
+    maximum: Optional[float]
+
+
+class Point(BaseModel):
+    """
+    A class representing the `Point` model.
+
+    #### Public Attributes:
+            longitude (Union[float, int]): the `longitude` of a point object.
+            latitude (Union[float, int]): the `latitude` of a point object.
+    """
+
+    longitude: Union[float, int] = Field(serialization_alias="lon")
+    latitude: Union[float, int] = Field(serialization_alias="lat")
+
+
+class AreaOfService(BaseModel):
+    """
+    A class representing the `AreaOfService` model.
+
+    #### Public Attributes:
+            polygon (List[Point]): the `polygon` value of an area of service object.
+    """
+
+    polygon: List[Point]
+
+
+class Slice(BaseModel, arbitrary_types_allowed=True):
+    """
+    A class representing the `Slice` model.
+
+    #### Private Attributes:
+        _api(APIClient): An API client object.
+        _sessions(List[Session]): List of device session instances.
+
+    #### Public Attributes:
+        sid (optional): String ID of the slice
+        state (str): State of the slice (ie. NOT_SUBMITTED)
+        name (optional): Optional short name for the slice. Must be ASCII characters, digits and dash. Like name of an event, such as "Concert-2029-Big-Arena".
+        networkIdentifier (NetworkIdentifier): Name of the network
+        sliceInfo (SliceInfo): Purpose of this slice
+        areaOfService (AreaOfService): Location of the slice
+        maxDataConnections (optional): Optional maximum number of data connection sessions in the slice.
+        maxDevices (optional): Optional maximum number of devices using the slice.
+        sliceDownlinkThroughput (optional): Optional throughput object
+        sliceUplinkThroughput (optional): Optional throughput object
+        deviceDownlinkThroughput (optional): Optional throughput object
+        deviceUplinkThroughput: (optional): Optional throughput object
+
+
+    #### Public Methods:
+        activate (None): Activate a network slice.
+        attach (): Attach a network slice to a device.
+        deactivate (None): Deactivate a network slice. The slice state must be active to be able to perform this operation.
+        delete (None): Delete network slice. The slice state must not be active to perform this operation.
+        refresh (None): Refresh the state of the network slice.
+
+    #### Callback Functions:
+        on_creation ():
+        on_event ():
+
+    """
+
+    _api: APIClient = PrivateAttr()
+    _sessions: List[QoDSession] = PrivateAttr()
+    sid: Optional[str]
+    state: str
+    name: Optional[str] = Field(
+        None,
+        description='Optional short name for the slice. Must be ASCII characters, digits and dash. Like name of an event, such as "Concert-2029-Big-Arena".',
+        min_length=4,
+        max_length=64,
+        regex="^[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]$",
+    )
+    network_identifier: NetworkIdentifier
+    slice_info: SliceInfo
+    area_of_service: Optional[AreaOfService] = Field(
+        None, description="Area which the network slice is intended to serve"
+    )
+    max_data_connections: Optional[int] = Field(
+        None,
+        description="Maximum number of data connection sessions in the slice.",
+        ge=0,
+    )
+    max_devices: Optional[int] = Field(
+        None, description="Maximum number of devices using the slice.", ge=0
+    )
+    slice_downlink_throughput: Optional[Throughput] = None
+    slice_uplink_throughput: Optional[Throughput] = None
+    device_downlink_throughput: Optional[Throughput] = None
+    device_uplink_throughput: Optional[Throughput] = None
+
+    def __init__(self, api: APIClient, **data) -> None:
+        super().__init__(**data)
+        self._api = api
+        self._sessions = []
+
+    def activate(self) -> None:
+        """Activate network slice.
+
+        #### Args:
+            None
+
+        #### Example:
+            ```python
+            slice.activate()
+            ```
+        """
+        if self.name:
+            return self._api.slicing.activate(self.name)
+    
+    def deactivate(self) -> None:
+        """Deactivate network slice.
+
+        #### Args:
+            None
+
+        #### Example:
+            ```python
+            slice.deactivate()
+            ```
+        """
+        if self.name:
+            return self._api.slicing.deactivate(self.name)
+    
+    def delete(self) -> None:
+        """Delete network slice.
+
+        #### Args:
+            None
+
+        #### Example:
+            ```python
+            slice.delete()
+            ```
+        """
+        if self.name:
+            return self._api.slicing.delete(self.name)
+
+    def refresh(self) -> None:
+        """Refresh state of the network slice.
+
+        #### Args:
+            None
+
+        #### Example:
+            ```python
+            slice.refresh()
+            ```
+        """
+        slice_data = self._api.slicing.get(self.name)
+        self.state = slice_data.json()["state"]
+
+    def attach(
+        self,
+        device: Device,
+        notification_url: str,
+        notification_auth_token: Optional[str] = None,
+    ) -> None:
+        """Attach network slice.
+
+        #### Args:
+            device (Device): Device object that the slice is being attached to
+
+        #### Example:
+            ```python
+            device = client.devices.get("testuser@open5glab.net", ipv4_address = DeviceIpv4Addr(public_address="1.1.1.2", private_address="1.1.1.2", public_port=80))
+            slice.attach(device)
+            ```
+        """
+        self._api.slice_attach.attach(
+            device, self.name, notification_url, notification_auth_token
+        )
+
+    def detach(
+        self,
+        device: Device,
+        notification_url: str,
+        notification_auth_token: Optional[str] = None,
+    ) -> None:
+        """Detach network slice.
+
+        #### Args:
+            None
+
+        #### Example:
+            ```python
+            device = client.devices.get("testuser@open5glab.net", ipv4_address = DeviceIpv4Addr(public_address="1.1.1.2", private_address="1.1.1.2", public_port=80))
+            slice.attach(device)
+            slice.detach()
+            ```
+        """
+        self._api.slice_attach.detach(
+            device, self.name, notification_url, notification_auth_token
+        )
+
+    @staticmethod
+    def network_identifier_from_dict(networkIdentifierDict: Optional[Dict[str, str]]):
+        """Returns a `NetworkIdentifier` instance.
+
+        Assigns the `mcc` and `mnc`.
+        #### Args:
+            networkIdentifierDict (Dict[str, str]): A Network Identifier object with `mcc` and `mnc` values.
+        """
+        if networkIdentifierDict:
+            return NetworkIdentifier(
+                mcc=networkIdentifierDict["mcc"], mnc=networkIdentifierDict["mnc"]
+            )
+        else:
+            return None
+
+    @staticmethod
+    def slice_info_from_dict(sliceInfoDict: Optional[Dict[str, str]]):
+        """Returns a `SliceInfo` instance.
+
+        Assigns the `service_type` and `differentiator`.
+        #### Args:
+            sliceInfoDict (Dict[str, str]): A Slice Info object with `service_type` and `differentiator` values.
+        """
+        if sliceInfoDict:
+            return SliceInfo(
+                service_type=sliceInfoDict["service_type"],
+                differentiator=sliceInfoDict["differentiator"],
+            )
+        else:
+            return None
+
+    @staticmethod
+    def area_of_service_from_dict(areaOfServiceDict: Optional[Dict[str, List[Dict[str, float]]]]) -> AreaOfService | None:
+        """Returns a `AreaOfService` instance.
+
+        Assigns the `polygon`.
+        #### Args:
+            areaOfServiceDict (Dict[str, List[Dict[str, float]]]): An Area Of Service object with polygon list value.
+        """
+        if areaOfServiceDict:
+            polygon = areaOfServiceDict["polygon"]
+            return AreaOfService(
+                    polygon=[
+                        Point(latitude=polygon[0]["lat"], longitude=polygon[0]["lon"]),
+                        Point(latitude=polygon[1]["lat"], longitude=polygon[1]["lon"]),
+                        Point(latitude=polygon[2]["lat"], longitude=polygon[2]["lon"]),
+                        Point(latitude=polygon[3]["lat"], longitude=polygon[3]["lon"]),
+                    ]
+            )
+        else:
+            return None
+
+    @staticmethod
+    def throughput(throughputdict: Optional[Dict[str, float]]):
+        """Returns a `Throughput` instance.
+
+        Assigns the `guaranteed` and `maximum`.
+        #### Args:
+            throughputDict (Dict[str, float]): A Throughput object with `guaranteed` and `maximum` values.
+        """
+        if throughputdict:
+            return Throughput(
+                guaranteed=throughputdict.get("guaranteed"),
+                maximum=throughputdict.get("maximum"),
+            )
+        else:
+            return None
