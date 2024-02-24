5c08a332744a925b7f181516388ee5bc0f2eb4fa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-24 16:04:25 +0000

    Add package codereviewbot/0.4/codereviewbot-0.4.tar.gz

---
+++ packages/codereviewbot/codereviewbot-0.4.tar.gz/codereviewbot-0.4/setup.py
@@ -0,0 +1,14 @@
+import setuptools
+setuptools.setup(
+ name="codereviewbot",
+ version='0.4',
+ author="Sriram",
+ author_email="Sriram.Chembrolu@powerschool.com",
+ description="This is a code review bot. Where you can review your code or other's code and get the feedback.",
+ packages=setuptools.find_packages(),
+ classifiers=[
+ "Programming Language :: Python :: 3",
+ "License :: OSI Approved :: MIT License",
+ "Operating System :: OS Independent",
+ ],
+)
