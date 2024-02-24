809444a11fe52a9ba6e1a02054b0f630933611ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-24 15:04:22 +0000

    Add package alist-sdk/0.30.5/alist_sdk-0.30.5-py3-none-any.whl

---
+++ packages/alist-sdk/alist_sdk-0.30.5-py3-none-any.whl/alist_sdk/__init__.py
@@ -0,0 +1,5 @@
+from .client import Client
+from .async_client import AsyncClient
+from .models import *
+from .err import *
+from .version import __version__
---
+++ packages/alist-sdk/alist_sdk-0.30.5-py3-none-any.whl/alist_sdk/version.py
@@ -0,0 +1,14 @@
+"""
+
+0.30.4:
+    path_lib.AlistPath.write_* 支持as_task参数
+
+0.30.5:
+    path_lib.AlistPath.read_* 自动跟随重定向，确保读取真实文件内容
+    path_lib.AlistPath.as_download_uri() 返回添加签名
+    
+"""
+
+__version__ = "0.30.5"
+
+ALIST_VERSION = "v3.30.0"
