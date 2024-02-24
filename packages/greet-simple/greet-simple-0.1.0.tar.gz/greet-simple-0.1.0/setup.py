c62abcb797f1896c28ed7777c8a67c82ef826a07
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-24 06:04:25 +0000

    Add package greet-simple/0.1.0/greet-simple-0.1.0.tar.gz

---
+++ packages/greet-simple/greet-simple-0.1.0.tar.gz/greet-simple-0.1.0/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="greet-simple",
+    version="0.1.0",
+    description="A simple package for greeting people.",
+    author="Your Name",
+    author_email="your@email.com",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
