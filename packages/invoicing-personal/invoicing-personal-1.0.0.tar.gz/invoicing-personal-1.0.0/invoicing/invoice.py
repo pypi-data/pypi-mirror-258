431f7da5220abb1ae98164687ba336b4976c7d96
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-24 04:04:23 +0000

    Add package invoicing-personal/1.0.0/invoicing-personal-1.0.0.tar.gz

---
+++ packages/invoicing-personal/invoicing-personal-1.0.0.tar.gz/invoicing-personal-1.0.0/invoicing/invoice.py
@@ -0,0 +1,80 @@
+import pandas as pd
+import glob
+from fpdf import FPDF
+from pathlib import Path
+import os
+
+
+def generate(invoices_path,pdfs_path,image_path):
+    """
+    This function converts Excel file invoices to PDF invoices.
+
+    :param invoices_path:
+    :param pdfs_path:
+    :param image_path:
+    :return:
+    """
+
+    filepaths = glob.glob(f"{invoices_path}/*.xlsx")
+
+    for filepath in filepaths:
+
+        pdf = FPDF(orientation="P", unit="mm", format="A4")
+        pdf.add_page()
+
+        filename = Path(filepath).stem
+        invoice_nr, date = filename.split("-")
+
+        pdf.set_font(family="Times", size=16, style="B")
+        pdf.cell(w=50, h=8, txt=f"Invoice nr.{invoice_nr}", ln=1)
+
+        pdf.set_font(family="Times", size=16, style="B")
+        pdf.cell(w=50, h=8, txt=f"Date: {date}", ln=1)
+
+        df = pd.read_excel(filepath, sheet_name="Sheet 1")
+
+        # Add a header
+        columns = df.columns
+        columns = [item.replace("_", " ").title() for item in columns]
+        pdf.set_font(family="Times", size=10, style="B")
+        pdf.set_text_color(80, 80, 80)
+        pdf.cell(w=30, h=8, txt=columns[0], border=1)
+        pdf.cell(w=70, h=8, txt=columns[1], border=1)
+        pdf.cell(w=30, h=8, txt=columns[2], border=1)
+        pdf.cell(w=30, h=8, txt=columns[3], border=1)
+        pdf.cell(w=30, h=8, txt=columns[4], border=1, ln=1)
+
+        # Add rows to the table
+        for index, row in df.iterrows():
+            pdf.set_font(family="Times", size=10)
+            pdf.set_text_color(80, 80, 80)
+            pdf.cell(w=30, h=8, txt=str(row[df.columns[0]]), border=1)  # product_id
+            pdf.cell(w=70, h=8, txt=str(row[df.columns[1]]), border=1)  # product_name
+            pdf.cell(w=30, h=8, txt=str(row[df.columns[2]]), border=1)  # amount_purchased
+            pdf.cell(w=30, h=8, txt=str(row[df.columns[3]]), border=1)  # price_per_unit
+            pdf.cell(w=30, h=8, txt=str(row[df.columns[4]]), border=1, ln=1)  # total_price
+
+        # Sum of total_price
+        total_sum = df[df.columns[4]].sum()
+        pdf.set_font(family="Times", size=10)
+        pdf.set_text_color(80, 80, 80)
+        pdf.cell(w=30, h=8, txt="", border=1)
+        pdf.cell(w=70, h=8, txt="", border=1)
+        pdf.cell(w=30, h=8, txt="", border=1)
+        pdf.cell(w=30, h=8, txt="", border=1)
+        pdf.cell(w=30, h=8, txt=str(total_sum), border=1, ln=1)
+
+        # Add total sum sentence
+        pdf.set_font(family="Times", size=10, style="B")
+        pdf.cell(w=30, h=8, txt=f"The total price is {total_sum}", ln=1)
+
+        # Add company name and logo
+        pdf.set_font(family="Times", size=14, style="B")
+        pdf.cell(w=25, h=8, txt=f"PythonHow")
+        pdf.image(image_path, w=10)
+
+        try:
+            os.makedirs(pdfs_path)
+        except FileExistsError:
+            pass
+        pdf.output(f"{pdfs_path}/{filename}.pdf")
