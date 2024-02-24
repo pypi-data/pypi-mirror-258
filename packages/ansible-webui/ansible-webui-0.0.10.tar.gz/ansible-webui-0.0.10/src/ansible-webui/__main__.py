2a6c23bc6f263793d56c72742e60cdb2a77703da
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-24 05:05:46 +0000

    Add package ansible-webui/0.0.10/ansible-webui-0.0.10.tar.gz

---
+++ packages/ansible-webui/ansible-webui-0.0.10.tar.gz/ansible-webui-0.0.10/src/ansible-webui/__main__.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+
+if __name__ == '__main__':
+    # pylint: disable=E0401
+    from sys import argv as sys_argv
+    from sys import exit as sys_exit
+    from sys import path as sys_path
+    from os import path as os_path
+
+    try:
+        from main import main
+
+    except ModuleNotFoundError:
+        sys_path.append(os_path.dirname(os_path.abspath(__file__)))
+        from main import main
+
+    from aw.config.main import VERSION
+
+    if len(sys_argv) > 1:
+        if sys_argv[1] in ['--version', '-v']:
+            from cli_init import init_cli
+            from cli import _print_version
+            init_cli()
+            _print_version()
+            sys_exit(0)
+
+    print(f'Ansible-WebUI Version {VERSION}')
+    main()
