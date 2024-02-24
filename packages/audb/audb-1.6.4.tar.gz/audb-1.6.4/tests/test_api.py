49cb4cb91ad5bafc8f719063c6ee1ebad14903f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-24 03:05:22 +0000

    Add package audb/1.6.4/audb-1.6.4.tar.gz

---
+++ packages/audb/audb-1.6.4.tar.gz/audb-1.6.4/tests/test_api.py
@@ -0,0 +1,31 @@
+import os
+
+import audeer
+
+import audb
+
+
+def test_available(repository):
+    # Broken database in repo
+    name = "non-existing-database"
+    path = os.path.join(
+        repository.host,
+        repository.name,
+        name,
+    )
+    path = audeer.mkdir(path)
+    df = audb.available()
+    os.rmdir(path)
+    assert len(df) == 0
+
+    # Non existing repo
+    name = "non-existing-repo"
+    audb.config.REPOSITORIES = [
+        audb.Repository(
+            name=name,
+            host=repository.host,
+            backend=repository.backend,
+        )
+    ]
+    df = audb.available()
+    assert len(df) == 0
