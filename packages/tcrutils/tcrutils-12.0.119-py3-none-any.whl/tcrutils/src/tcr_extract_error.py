2542b9e75832809bffc9221f1b49de210bd93ac3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-24 14:03:59 +0000

    Add package tcrutils/12.0.119/tcrutils-12.0.119-py3-none-any.whl

---
+++ packages/tcrutils/tcrutils-12.0.119-py3-none-any.whl/tcrutils/src/tcr_extract_error.py
@@ -0,0 +1,14 @@
+import traceback
+
+
+def extract_error(e: Exception, pattern='%s: %s', *, raw=False) -> tuple[str, str] | str:
+  if callable(e):
+    e = e()
+  one = e.__class__.__name__
+  two = str(e)
+  return (one, two) if raw else pattern % (one, two)
+
+
+def extract_traceback(e: Exception) -> str:
+  traceback_details = traceback.format_tb(e.__traceback__)
+  return ''.join(traceback_details)
