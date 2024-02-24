f6b730f6265f77604e6a62c8715fb182450f063b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-24 14:04:33 +0000

    Add package recast-atlas/0.4.0/recast_atlas-0.4.0-py3-none-any.whl

---
+++ packages/recast-atlas/recast_atlas-0.4.0-py3-none-any.whl/recastatlas/exceptions.py
@@ -0,0 +1,10 @@
+class FailedRunException(Exception):
+    """
+    raised when an a run has not succeeded
+    """
+
+
+class BackendNotAvailableException(Exception):
+    """
+    raise when a Backend is ill-configured
+    """
