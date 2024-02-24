3693ef14bfec8205cdc8ad8c89591a6de28a53e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-24 04:04:45 +0000

    Add package lalisa/0.1/lalisa-0.1.tar.gz

---
+++ packages/lalisa/lalisa-0.1.tar.gz/lalisa-0.1/setup.py
@@ -0,0 +1,19 @@
+from distutils.core import setup
+setup(
+  name = 'lalisa',
+  packages = ['lalisa'],
+  version = '0.1',
+  license='MIT',
+  description = 'DESCRIPTION',
+  author = 'nshout',
+  url = 'https://github.com/lalisa-studio/lalisa',
+  keywords = ['protection', 'security', 'obfuscation'],
+  install_requires=[],
+  classifiers=[
+    'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
+    'Intended Audience :: Developers',      # Define that your audience are developers
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3.11',
+  ],
+)
