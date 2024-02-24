1e167d6cc0ad81905fd18295076247bd60ac3d92
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-24 05:05:28 +0000

    Add package cloudcix/0.13.5/cloudcix-0.13.5.tar.gz

---
+++ packages/cloudcix/cloudcix-0.13.5.tar.gz/cloudcix-0.13.5/cloudcix/api/plot.py
@@ -0,0 +1,24 @@
+from cloudcix.client import Client
+
+
+class Plot:
+    """
+    The Plot Application exposes a REST API capable of managing historial measurement data
+    """
+    _application_name = 'plot'
+    category = Client(
+        _application_name,
+        'category/',
+    )
+    reading = Client(
+        _application_name,
+        'reading/',
+    )
+    source = Client(
+        _application_name,
+        'source/',
+    )
+    unit = Client(
+        _application_name,
+        'unit/',
+    )
