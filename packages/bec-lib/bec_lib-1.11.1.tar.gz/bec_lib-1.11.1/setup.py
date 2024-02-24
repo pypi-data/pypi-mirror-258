3bd53189e2066f20ded35cbaf909da90ba823e68
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-24 06:04:43 +0000

    Add package bec-lib/1.11.1/bec_lib-1.11.1.tar.gz

---
+++ packages/bec-lib/bec_lib-1.11.1.tar.gz/bec_lib-1.11.1/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup
+
+__version__ = "1.11.1"
+
+if __name__ == "__main__":
+    setup(
+        install_requires=[
+            "numpy",
+            "scipy",
+            "msgpack",
+            "requests",
+            "typeguard>=4.0.1",
+            "pyyaml",
+            "redis",
+            "toolz",
+            "rich",
+            "pylint",
+            "loguru",
+            "psutil",
+            "fpdf",
+            "fastjsonschema",
+            "lmfit",
+        ],
+        extras_require={
+            "dev": ["pytest", "pytest-random-order", "coverage", "pandas", "black", "pylint"]
+        },
+        entry_points={"console_scripts": ["bec-channel-monitor = bec_lib:channel_monitor_launch"]},
+        package_data={"bec_lib.tests": ["*.yaml"], "bec_lib.configs": ["*.yaml", "*.json"]},
+        version=__version__,
+    )
