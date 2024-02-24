443940539c155c0e17ece6f02b648cc42cbb2972
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-24 14:04:28 +0000

    Add package pitcherflask/0.0.2/pitcherflask-0.0.2.tar.gz

---
+++ packages/pitcherflask/pitcherflask-0.0.2.tar.gz/pitcherflask-0.0.2/setup.py
@@ -0,0 +1,23 @@
+import os
+from setuptools import setup
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+    name = "pitcherflask",
+    version = "0.0.2",
+    author = "divine-architect",
+    author_email = "solarhatesbeing@gmail.com",
+    description = ("Build a basic flask project without wasting time on setup."),
+    license = "MIT",
+    keywords = "flask web api",
+    url = "http://github.com/divine-architect/pitcherflask",
+    packages=[],
+    long_description=read('README.md'),
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Topic :: Utilities",
+        "License :: OSI Approved :: MIT License",
+    ],
+)
