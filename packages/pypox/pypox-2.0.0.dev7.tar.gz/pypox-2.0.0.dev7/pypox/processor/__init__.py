24a7b4015d97f581502009b910954510709260f0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-24 15:04:54 +0000

    Add package pypox/2.0.0.dev7/pypox-2.0.0.dev7.tar.gz

---
+++ packages/pypox/pypox-2.0.0.dev7.tar.gz/pypox-2.0.0.dev7/pypox/processor/__init__.py
@@ -0,0 +1,8 @@
+"""
+    This module contains the processors used by the pypox framework.
+
+    The processors handle different aspects of the HTTP request and response, such as cookies, headers, JSON data,
+    path parameters, query parameters, and websockets.
+
+    Optional dependencies include PydanticProcessor, HTMXProcessor, and JinjaProcessor.
+"""
