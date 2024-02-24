589298cbb8889e71dba243866fd6620cf2f80060
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-24 05:05:29 +0000

    Add package KloockyDE/2.5/KloockyDE-2.5.tar.gz

---
+++ packages/KloockyDE/KloockyDE-2.5.tar.gz/KloockyDE-2.5/src/KloockyDE/__init__.py
@@ -0,0 +1,8 @@
+# This is version 2.5
+# 2.3 does not show up for some reason
+# I reorganized the package for Version 2.0
+# 2.1 added some functions I forgot to add
+# 2.2 - 2.4 had to be made because I fucked the required packages up
+#       Apparently, packages os and time could not be installed?
+# 2.4 did not work because internal scripts could not import each other
+# 2.5 fixed that
