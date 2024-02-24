d2721b188a4873f12e89ea15fea571a49089c901
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-24 15:04:25 +0000

    Add package fibonacciwangxiaoyanrust/0.1.0/fibonacciwangxiaoyanrust-0.1.0-cp310-none-win_amd64.whl

---
+++ packages/fibonacciwangxiaoyanrust/fibonacciwangxiaoyanrust-0.1.0-cp310-none-win_amd64.whl/fibonacciwangxiaoyanrust/__init__.py
@@ -0,0 +1,5 @@
+from .fibonacciwangxiaoyanrust import *
+
+__doc__ = fibonacciwangxiaoyanrust.__doc__
+if hasattr(fibonacciwangxiaoyanrust, "__all__"):
+    __all__ = fibonacciwangxiaoyanrust.__all__
