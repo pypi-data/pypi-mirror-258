5dafdfcb53d193b034bd59e63eeb0f0e699c71cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-24 16:04:36 +0000

    Add package fast-aug/0.1.0/fast_aug-0.1.0-cp38-cp38-manylinux_2_17_aarch64.manylinux2014_aarch64.whl

---
+++ packages/fast-aug/fast_aug-0.1.0-cp38-cp38-manylinux_2_17_aarch64.manylinux2014_aarch64.whl/fast_aug/text/__init__.py
@@ -0,0 +1,15 @@
+# Generated content DO NOT EDIT
+from .. import text
+
+BaseTextAugmenter = text.BaseTextAugmenter
+CharsRandomDeleteAugmenter = text.CharsRandomDeleteAugmenter
+CharsRandomInsertAugmenter = text.CharsRandomInsertAugmenter
+CharsRandomSubstituteAugmenter = text.CharsRandomSubstituteAugmenter
+CharsRandomSwapAugmenter = text.CharsRandomSwapAugmenter
+WordsRandomDeleteAugmenter = text.WordsRandomDeleteAugmenter
+WordsRandomInsertAugmenter = text.WordsRandomInsertAugmenter
+WordsRandomSubstituteAugmenter = text.WordsRandomSubstituteAugmenter
+WordsRandomSwapAugmenter = text.WordsRandomSwapAugmenter
+
+__all__ = text.__all__
+__doc__ = text.__doc__
