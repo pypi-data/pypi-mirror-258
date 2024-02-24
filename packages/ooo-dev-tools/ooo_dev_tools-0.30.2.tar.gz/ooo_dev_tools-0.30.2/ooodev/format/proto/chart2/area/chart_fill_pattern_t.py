a0f5f67cd4cd8ce857742d5df9dac7ba16102422
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-24 15:04:44 +0000

    Add package ooo-dev-tools/0.30.2/ooo_dev_tools-0.30.2.tar.gz

---
+++ packages/ooo-dev-tools/ooo_dev_tools-0.30.2.tar.gz/ooo_dev_tools-0.30.2/ooodev/format/proto/chart2/area/chart_fill_pattern_t.py
@@ -0,0 +1,54 @@
+from __future__ import annotations
+from typing import Any, overload, TYPE_CHECKING
+
+from ooodev.mock.mock_g import DOCS_BUILDING
+from ...area.fill_pattern_t import FillPatternT
+
+if TYPE_CHECKING or DOCS_BUILDING:
+    try:
+        from typing import Protocol
+    except ImportError:
+        from typing_extensions import Protocol
+    from com.sun.star.chart2 import XChartDocument
+    from com.sun.star.awt import XBitmap
+    from ooodev.format.inner.preset.preset_pattern import PresetPatternKind
+
+else:
+    Protocol = object
+    PresetPatternKind = Any
+    XBitmap = Any
+    XChartDocument = Any
+
+
+
+class ChartFillPatternT(FillPatternT, Protocol):
+    """Fill Pattern Protocol"""
+
+    def __init__(
+        self,
+        chart_doc: XChartDocument,
+        *,
+        bitmap: XBitmap | None = None,
+        name: str = "",
+        tile: bool = True,
+        stretch: bool = False,
+        auto_name: bool = False,
+    ) -> None:
+
+        ...
+
+    @overload
+    @classmethod
+    def from_obj(cls, chart_doc: XChartDocument, obj: Any) -> ChartFillPatternT: ...
+
+    @overload
+    @classmethod
+    def from_obj(cls, chart_doc: XChartDocument,obj: Any, **kwargs) -> ChartFillPatternT: ...
+
+    @overload
+    @classmethod
+    def from_preset(cls, chart_doc: XChartDocument, preset: PresetPatternKind) -> ChartFillPatternT: ...
+
+    @overload
+    @classmethod
+    def from_preset(cls, chart_doc: XChartDocument, preset: PresetPatternKind, **kwargs) -> ChartFillPatternT: ...
