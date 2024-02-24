e2fed04818589a3e5ef061c876b235a9b1531fb3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-24 04:04:53 +0000

    Add package adx-logging-handler/1.0.2/adx-logging-handler-1.0.2.tar.gz

---
+++ packages/adx-logging-handler/adx-logging-handler-1.0.2.tar.gz/adx-logging-handler-1.0.2/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="adx-logging-handler",
+    version="1.0.2",
+    description="A python logging handler for Azure Data explorer",
+    package_dir={"": "src"},
+    packages=find_packages(where="src"),
+    install_requires=["azure-kusto-data", "azure-kusto-ingest"],
+)
