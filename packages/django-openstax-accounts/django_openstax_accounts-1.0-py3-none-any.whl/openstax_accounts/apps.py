6afd058cfb4022658214233aeef8be4d8f43f565
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-24 14:03:57 +0000

    Add package django-openstax-accounts/1.0/django_openstax_accounts-1.0-py3-none-any.whl

---
+++ packages/django-openstax-accounts/django_openstax_accounts-1.0-py3-none-any.whl/openstax_accounts/apps.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+
+
+class AccountsConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'openstax_accounts'
+    label = 'accounts'
