615410ac2233921ab8e35d3b7037e2a5a2412733
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-24 04:04:46 +0000

    Add package imod/0.15.3/imod-0.15.3-py3-none-any.whl

---
+++ packages/imod/imod-0.15.3-py3-none-any.whl/imod/data/__init__.py
@@ -0,0 +1,15 @@
+# from .synthetic import
+from .sample_data import (
+    ahn,
+    circle,
+    fluxes,
+    head_observations,
+    hondsrug_drainage,
+    hondsrug_initial,
+    hondsrug_layermodel,
+    hondsrug_meteorology,
+    hondsrug_river,
+    imod5_projectfile_data,
+    lakes_shp,
+    twri_output,
+)
