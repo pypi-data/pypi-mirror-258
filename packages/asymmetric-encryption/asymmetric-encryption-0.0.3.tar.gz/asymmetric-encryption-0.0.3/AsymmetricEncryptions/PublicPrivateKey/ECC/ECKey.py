de97b29a8fdc7bbfdcdbaa722999ae43c684e9c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-24 04:04:58 +0000

    Add package asymmetric-encryption/0.0.3/asymmetric-encryption-0.0.3.tar.gz

---
+++ packages/asymmetric-encryption/asymmetric-encryption-0.0.3.tar.gz/asymmetric-encryption-0.0.3/AsymmetricEncryptions/PublicPrivateKey/ECC/ECKey.py
@@ -0,0 +1,10 @@
+from __future__ import annotations
+from AsymmetricEncryptions.PublicPrivateKey.ECC.EllipticCurveNISTP256 import EllipticCurveNISTP256
+import secrets
+class ECKey:
+    def __init__(self) -> None:
+        self.private_key = secrets.randbelow(EllipticCurveNISTP256().n)
+        self.public_key = EllipticCurveNISTP256().g() * self.private_key
+
+    def __str__(self):
+        return f"{self.private_key = }\nself.public_key = {self.public_key }"
