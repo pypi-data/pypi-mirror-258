f6aa60ed9df118164c93d7d66cae31368305a86f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-24 14:04:04 +0000

    Add package lego-workflows/0.0.10/lego_workflows-0.0.10-py3-none-any.whl

---
+++ packages/lego-workflows/lego_workflows-0.0.10-py3-none-any.whl/lego_workflows/components.py
@@ -0,0 +1,36 @@
+"""Workflow definition components."""
+from __future__ import annotations
+
+from abc import ABC, abstractmethod
+from dataclasses import dataclass
+from typing import Generic, TypeVar
+
+
+@dataclass(frozen=True)
+class ResponseComponent:
+    """Workflow response data."""
+
+
+class DomainEvent(ABC):
+    """Worflow event."""
+
+    @abstractmethod
+    async def publish(self) -> None:
+        """Publish event."""
+
+
+class DomainError(Exception):
+    """Raised when a user violates a business rule."""
+
+
+R = TypeVar("R", bound=ResponseComponent)
+
+
+@dataclass(frozen=True)
+class CommandComponent(Generic[R]):
+    """Workflow input data."""
+
+    @abstractmethod
+    async def run(self, events: list[DomainEvent]) -> R:
+        """Execute workflow."""
+        raise NotImplementedError
