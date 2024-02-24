7cbe6d7a19ae9cb08181cc12caaa886b11587a08
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-24 05:05:20 +0000

    Add package flake8-pydantic/0.1.0.post0/flake8_pydantic-0.1.0.post0-py3-none-any.whl

---
+++ packages/flake8-pydantic/flake8_pydantic-0.1.0.post0-py3-none-any.whl/flake8_pydantic/plugin.py
@@ -0,0 +1,21 @@
+from __future__ import annotations
+
+import ast
+from importlib.metadata import version
+from typing import Any, Iterator
+
+from .visitor import Visitor
+
+
+class Plugin:
+    name = "flake8-pydantic"
+    version = version(name)
+
+    def __init__(self, tree: ast.AST) -> None:
+        self._tree = tree
+
+    def run(self) -> Iterator[tuple[int, int, str, type[Any]]]:
+        visitor = Visitor()
+        visitor.visit(self._tree)
+        for error in visitor.errors:
+            yield *error.as_flake8_error(), type(self)
