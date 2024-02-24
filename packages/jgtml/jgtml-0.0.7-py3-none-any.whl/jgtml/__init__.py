811a551a19abcce951d159309938b0ae58cbb21c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-24 05:06:37 +0000

    Add package jgtml/0.0.7/jgtml-0.0.7-py3-none-any.whl

---
+++ packages/jgtml/jgtml-0.0.7-py3-none-any.whl/jgtml/__init__.py
@@ -0,0 +1,19 @@
+# jgtml
+__version__ = "0.0.7"
+import sys
+import os
+
+sys.path.insert(0, os.path.abspath(os.path.dirname(__file__)))
+
+from jtc import (
+    calculate_target_variable_min_max as calc_target_from_df,
+    pto_target_calculation as calc_target_to_file,
+)
+
+# from jgtpy import JGTPDS as pds,JGTADS as ads,JGTPDSP as pds
+
+def __init__():
+    """
+    Initialize the jgtml module.
+    """
+    pass
