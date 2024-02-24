e2047328d8aba62d21a24a1af076c44448272c40
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-24 15:04:39 +0000

    Add package fast-dash/0.2.9/fast_dash-0.2.9-py3-none-any.whl

---
+++ packages/fast-dash/fast_dash-0.2.9-py3-none-any.whl/fast_dash/__init__.py
@@ -0,0 +1,48 @@
+"""Top-level package for Fast Dash."""
+
+__author__ = """Kedar Dabhadkar"""
+__email__ = "kedar@fastdash.app"
+__version__ = "0.2.9"
+
+from fast_dash.Components import (
+    Graph,
+    Image,
+    Slider,
+    Text,
+    TextArea,
+    Upload,
+    UploadImage,
+    acknowledge_image_component,
+    dbc,
+    dcc,
+    dmc,
+    html,
+    PIL,
+    Chat,
+    Table
+)
+import dash
+from fast_dash.fast_dash import FastDash, fastdash
+from fast_dash.utils import Fastify
+
+__all__ = [
+    "FastDash",
+    "fastdash",
+    "Fastify",
+    "Text",
+    "TextArea",
+    "Slider",
+    "Upload",
+    "acknowledge_image_component",
+    "UploadImage",
+    "Image",
+    "Graph",
+    "dcc",
+    "dbc",
+    "dmc",
+    "html",
+    "dash",
+    "PIL",
+    "Chat",
+    "Table"
+]
