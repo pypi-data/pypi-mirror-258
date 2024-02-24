89b44746893f2a484279f9cba7624190305300d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-24 15:04:22 +0000

    Add package keraas/1.8/keraas-1.8.tar.gz

---
+++ packages/keraas/keraas-1.8.tar.gz/keraas-1.8/keraas/__init__.py
@@ -0,0 +1,34 @@
+def read_file(filename):
+    with open(filename, 'r') as file:
+        print(file.read())
+
+def cgan():
+    read_file('cgan.txt')
+
+def mod1():
+    read_file('mod1.txt')
+
+def diffusion():
+    read_file('diffusion.txt')
+
+def rcnn():
+    read_file('rcnn.txt')
+
+def face():
+    read_file('face.txt')
+
+def image():
+    read_file('image.txt')
+
+def stable():
+    read_file('stable.txt')
+
+def vae():
+    read_file('vae.txt')
+
+def yoloc():
+    read_file('yoloc.txt')
+
+def yolo():
+    read_file('yolo.txt')
+
