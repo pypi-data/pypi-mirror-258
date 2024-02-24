043e988e21c5b758149149d91dfbeb55f06717f0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-24 04:04:50 +0000

    Add package composio-autogen/0.0.3/composio-autogen-0.0.3.tar.gz

---
+++ packages/composio-autogen/composio-autogen-0.0.3.tar.gz/composio-autogen-0.0.3/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = 'composio-autogen',
+    version = '0.0.3',
+    author = 'Utkarsh',
+    author_email = 'utkarsh@composio.dev',
+    description = 'Provides integrations skill with 50+ services in autogen',
+    long_description = open('README.md').read(),
+    long_description_content_type = 'text/markdown',
+    url = 'https://github.com/SamparkAI/autogen-composio-skills',
+    classifiers = [
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: OS Independent'
+    ],
+    python_requires = '>=3.7',
+    include_package_data = True,
+    scripts = ['composio-autogen'],
+)
