b33d995c41ec558a652fcb90a6e0e2e83eb4e0d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-24 15:04:30 +0000

    Add package izio-cli/0.1.40/izio_cli-0.1.40-py3-none-any.whl

---
+++ packages/izio-cli/izio_cli-0.1.40-py3-none-any.whl/izio_cli/values/dot_net_payload.py
@@ -0,0 +1,7 @@
+def envPayload():
+    return """
+#ToDo: change to your APM credentials
+ELASTIC_APM_SERVICE_NAME=teste_quatro
+ELASTIC_APM_SERVER_URLS=http://localhost:8200
+ELASTIC_APM_SECRET_TOKEN=hashToken
+"""
