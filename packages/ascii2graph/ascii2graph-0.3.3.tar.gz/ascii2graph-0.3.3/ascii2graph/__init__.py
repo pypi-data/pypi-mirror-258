436c8fc9900cdb726a6ef40b7422956237b6deae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-24 03:05:22 +0000

    Add package ascii2graph/0.3.3/ascii2graph-0.3.3.tar.gz

---
+++ packages/ascii2graph/ascii2graph-0.3.3.tar.gz/ascii2graph-0.3.3/ascii2graph/__init__.py
@@ -0,0 +1,9 @@
+"""
+Converts ASCII text to a graph (represented as a dictionary of connections and
+angles). It can represent directed and undirected graphs.
+"""
+
+
+from .graph import graph
+
+__version__ = "0.3.3"
