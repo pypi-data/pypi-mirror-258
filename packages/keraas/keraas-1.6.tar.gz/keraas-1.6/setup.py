d66b097c2118793b57a9ca84a5f2f233568203e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-24 15:04:21 +0000

    Add package keraas/1.6/keraas-1.6.tar.gz

---
+++ packages/keraas/keraas-1.6.tar.gz/keraas-1.6/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='keraas',
+    version='1.6',
+    packages=find_packages(),
+    package_data={
+        'keraas': ['*.txt']
+    },
+    author='Your Name',
+    author_email='your.email@example.com',
+    description='A brief description of your package',
+    long_description=open('README.md').read(),
+     long_description_content_type='text/markdown',
+    url='http://example.com',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Text Processing :: Linguistic',
+    ],
+    keywords='keraas',
+)
