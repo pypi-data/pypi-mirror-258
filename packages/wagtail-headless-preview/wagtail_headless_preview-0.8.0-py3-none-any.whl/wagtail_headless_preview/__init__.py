d6b9e2f19095049c9d659fdf5e26e50dcd1a7c28
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-24 04:04:59 +0000

    Add package wagtail-headless-preview/0.8.0/wagtail_headless_preview-0.8.0-py3-none-any.whl

---
+++ packages/wagtail-headless-preview/wagtail_headless_preview-0.8.0-py3-none-any.whl/wagtail_headless_preview/__init__.py
@@ -0,0 +1,13 @@
+VERSION = (0, 8, 0)
+__version__ = ".".join(map(str, VERSION))
+
+
+def setup():
+    import warnings
+
+    from .deprecation import removed_in_next_version_warning
+
+    warnings.simplefilter("default", removed_in_next_version_warning)
+
+
+setup()
---
+++ packages/wagtail-headless-preview/wagtail_headless_preview-0.8.0-py3-none-any.whl/wagtail_headless_preview/deprecation.py
@@ -0,0 +1,10 @@
+class RemovedInWagtailHeadlessPreview090Warning(DeprecationWarning):
+    pass
+
+
+class RemovedInWagtailHeadlessPreview010Warning(PendingDeprecationWarning):
+    pass
+
+
+removed_in_next_version_warning = RemovedInWagtailHeadlessPreview090Warning
+pending_deprecation_warning = RemovedInWagtailHeadlessPreview010Warning
