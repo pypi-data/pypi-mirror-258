d3d51786cc5cf2ade6787cc4517cda982300475b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-24 05:05:47 +0000

    Add package jgtml/0.0.4/jgtml-0.0.4-py3-none-any.whl

---
+++ packages/jgtml/jgtml-0.0.4-py3-none-any.whl/jgtml/__init__.py
@@ -0,0 +1,10 @@
+# jgtml
+__version__ = '0.0.4'
+import sys
+import os
+
+sys.path.insert(0, os.path.abspath(os.path.dirname(__file__)))
+
+from calc import calculate_target_variable_min_max as calc_targets
+
+#from jgtpy import JGTPDS as pds,JGTADS as ads,JGTPDSP as pds
