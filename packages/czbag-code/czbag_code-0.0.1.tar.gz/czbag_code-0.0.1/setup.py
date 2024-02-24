34121bbeddfe50e48877a78e2193a9e454577942
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-24 15:04:45 +0000

    Add package czbag-code/0.0.1/czbag_code-0.0.1.tar.gz

---
+++ packages/czbag-code/czbag_code-0.0.1.tar.gz/czbag_code-0.0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='czbag_code',
+      version='0.0.1',
+      description='Test Project',
+      packages=['czbag_code'],
+      author_email='czbag.ton@proton.me',
+      zip_safe=False)
