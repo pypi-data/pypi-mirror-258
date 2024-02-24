3924b1e9170ae0b58cb2224588e32e576434fb52
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-24 03:04:42 +0000

    Add package nrfapy/0.5/nrfapy-0.5-py2.py3-none-any.whl

---
+++ packages/nrfapy/nrfapy-0.5-py2.py3-none-any.whl/nrfapy/__init__.py
@@ -0,0 +1,8 @@
+"""Top-level package for nrfapy."""
+
+__author__ = """Simon M Mudd, Simon Moulds and Daniel N Goldberg"""
+__email__ = 'simon.m.mudd@ed.ac.uk'
+__version__ = '0.5'
+
+
+from .nrfapy import *
