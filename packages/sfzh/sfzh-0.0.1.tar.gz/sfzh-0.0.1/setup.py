32fe0b8c8afb4b493f5045af6c37538302e8b999
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-24 03:04:53 +0000

    Add package sfzh/0.0.1/sfzh-0.0.1.tar.gz

---
+++ packages/sfzh/sfzh-0.0.1.tar.gz/sfzh-0.0.1/setup.py
@@ -0,0 +1,17 @@
+import setuptools  
+  
+setuptools.setup(  
+    name="sfzh",
+    author="ykm",
+    author_email="18678617683@163.com",
+    description="中国公民身份证号码的解析，校验与生成",
+    version="0.0.1",
+    url="http://tbstxl.wikidot.com/sfzh",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3',
+    ]
+)
