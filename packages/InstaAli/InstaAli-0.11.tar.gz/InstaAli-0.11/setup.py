5627a2bc9e9bd8bf4ee17ac42797e5446b54b8ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-24 15:04:54 +0000

    Add package InstaAli/0.11/InstaAli-0.11.tar.gz

---
+++ packages/InstaAli/InstaAli-0.11.tar.gz/InstaAli-0.11/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='InstaAli',
+      version='0.11',
+      description='operation & greeting library',
+      packages=['InstaBek'],
+      author_email='dasturchiyigit2002@gmail.com',
+      zip_safe=False)
