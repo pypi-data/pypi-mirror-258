851a780b8ed4a03342971be1e0d0f1124eb00c1e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-24 03:04:46 +0000

    Add package poetry-polylith-plugin/1.17.2/poetry_polylith_plugin-1.17.2-py3-none-any.whl

---
+++ packages/poetry-polylith-plugin/poetry_polylith_plugin-1.17.2-py3-none-any.whl/polylith/poetry/commands/libs.py
@@ -0,0 +1,43 @@
+from pathlib import Path
+
+from poetry.console.commands.command import Command
+from polylith import commands, configuration, info, repo
+from polylith.poetry.commands.check import command_options
+from polylith.poetry.internals import filter_projects_data, find_third_party_libs
+
+
+class LibsCommand(Command):
+    name = "poly libs"
+    description = "Show third-party libraries used in the workspace."
+
+    options = command_options
+
+    def print_report(self, root: Path, ns: str, data: dict) -> bool:
+        name = data["name"]
+        path = data["path"]
+
+        options = {"strict": self.option("strict"), "alias": self.option("alias")}
+
+        try:
+            third_party_libs = find_third_party_libs(self.poetry, path)
+            merged = {
+                **data,
+                **{"deps": {"items": third_party_libs, "source": "poetry.lock"}},
+            }
+
+            return commands.libs.run(root, ns, merged, options)
+        except ValueError as e:
+            self.line_error(f"{name}: <error>{e}</error>")
+            return False
+
+    def handle(self) -> int:
+        directory = self.option("directory")
+        root = repo.get_workspace_root(Path.cwd())
+        ns = configuration.get_namespace_from_config(root)
+
+        all_projects_data = info.get_projects_data(root, ns)
+        projects_data = filter_projects_data(self.poetry, directory, all_projects_data)
+
+        results = {self.print_report(root, ns, data) for data in projects_data}
+
+        return 0 if all(results) else 1
