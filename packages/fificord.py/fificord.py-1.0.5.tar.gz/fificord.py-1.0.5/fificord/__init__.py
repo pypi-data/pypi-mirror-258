2c15e564d32ae1a037a9a906a94cc7503ab29ab7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-24 15:04:38 +0000

    Add package fificord.py/1.0.5/fificord.py-1.0.5.tar.gz

---
+++ packages/fificord.py/fificord.py-1.0.5.tar.gz/fificord.py-1.0.5/fificord/__init__.py
@@ -0,0 +1,12 @@
+import tls_client
+
+class Status:
+    Idle = "idle"
+    Dnd = "dnd"
+    Invisible = "invisible"
+    Online = "online"
+    Mobile = "mobile"
+    Vr = "vr"
+
+
+session = tls_client.Session()
