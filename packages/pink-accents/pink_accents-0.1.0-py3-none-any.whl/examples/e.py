e86fe6cb2e6c6b5b546aa40b881f799d29ee2d92
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-24 05:05:34 +0000

    Add package pink-accents/0.1.0/pink_accents-0.1.0-py3-none-any.whl

---
+++ packages/pink-accents/pink_accents-0.1.0-py3-none-any.whl/examples/e.py
@@ -0,0 +1,43 @@
+# original accent created by: PotatoAlienOf13
+# commit: https://github.com/Fogapod/pink/commit/0de155c932ac5bc1f1eefc329c831db9d569730c
+
+import random
+
+from typing import Generator
+
+from pink_accents import Accent, Match, ReplacementContext
+
+CURSED_ES = "EĒÊËÈÉ"
+
+
+def cursed_e_generator(count: int) -> Generator[str, None, None]:
+    yield from random.choices(CURSED_ES, k=count)
+
+
+def next_cursed_e(ctx: ReplacementContext[Generator[str, None, None]]) -> str:
+    if ctx.state is None:
+        # this is a little hack for knowing the amount of e's we will need ahead of time.
+        # this number will usually be greater than the actual amount of e's used because
+        # not all letters are replaced, but the advantage is using random.choices only
+        # once
+        ctx.state = cursed_e_generator(len(ctx.source))
+
+    return next(ctx.state)
+
+
+def e(m: Match) -> str:
+    if m.severity < 5:
+        return "e" if m.original.islower() else "E"
+
+    elif m.severity < 10:
+        return "E"
+
+    return next_cursed_e(m.context)
+
+
+class E(Accent):
+    """Eeeeee eeeeeeeeeee eee eeee."""
+
+    PATTERNS = {
+        r"[a-z]": e,
+    }
