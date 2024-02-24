3b501d9d8f0d6a124779a8a4958e86b608142c42
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-24 05:05:20 +0000

    Add package coloraura/1.0.0/coloraura-1.0.0.tar.gz

---
+++ packages/coloraura/coloraura-1.0.0.tar.gz/coloraura-1.0.0/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='coloraura',
+    version='1.0.0',
+    packages=find_packages(),
+    author='VisuallySynced',
+    description='A module for Python 3 with text styling with color and gradient support',
+    url='https://github.com/visuallysynced/coloraura',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Operating System :: OS Independent',
+    ],
+)
