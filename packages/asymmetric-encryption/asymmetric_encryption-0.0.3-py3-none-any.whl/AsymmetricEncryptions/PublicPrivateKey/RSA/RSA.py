e66470ed0d028082edc2dd9655fb9250b9e4de09
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-24 04:04:58 +0000

    Add package asymmetric-encryption/0.0.3/asymmetric_encryption-0.0.3-py3-none-any.whl

---
+++ packages/asymmetric-encryption/asymmetric_encryption-0.0.3-py3-none-any.whl/AsymmetricEncryptions/PublicPrivateKey/RSA/RSA.py
@@ -0,0 +1,40 @@
+from __future__ import annotations
+from AsymmetricEncryptions.PublicPrivateKey.RSA.RSAKey import RSAKey
+from AsymmetricEncryptions.General import BytesAndInts
+
+class RSA:
+    def __init__(self, key: RSAKey) -> None:
+        self.key = key
+
+    @staticmethod
+    def generate_key_pair(nBit) -> RSAKey and RSAKey:
+        # RSAKey.new returns a pair of priv and pub
+        Priv: RSAKey = RSAKey.new(nBit)
+        Pub: RSAKey = Priv.public
+        return Priv, Pub
+
+
+    def encrypt(self, msg: bytes, assertion=True) -> bytes:
+        int_msg: int = BytesAndInts.byte2Int(msg)
+        if assertion: assert int_msg < self.key.n
+        cipher: int = pow(int_msg, self.key.e, self.key.n)
+        return BytesAndInts.int2Byte(cipher)
+
+    def decrypt(self, cipher: bytes) -> bytes:
+        assert self.key.has_private
+        int_cipher: int = BytesAndInts.byte2Int(cipher)
+        m: int = pow(int_cipher, self.key.d, self.key.n)
+        return BytesAndInts.int2Byte(m)
+
+    def sign(self, msg: bytes, assertion=True) -> bytes:
+        assert self.key.has_private
+        int_msg: int = BytesAndInts.byte2Int(msg)
+        if assertion: assert int_msg < self.key.n
+        s: int = pow(int_msg, self.key.d, self.key.n)
+        return BytesAndInts.int2Byte(s)
+
+    def verify(self, s, og_msg: bytes) -> None:
+        int_s: int = BytesAndInts.byte2Int(s)
+        cipher: int = pow(int_s, self.key.e, self.key.n)
+        assert cipher == BytesAndInts.byte2Int(og_msg
+                                               )
