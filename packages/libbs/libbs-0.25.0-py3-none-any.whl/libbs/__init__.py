10eaecf5732a7cfd164a162e74cc1d8b943d3ca1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-24 06:04:10 +0000

    Add package libbs/0.25.0/libbs-0.25.0-py3-none-any.whl

---
+++ packages/libbs/libbs-0.25.0-py3-none-any.whl/libbs/__init__.py
@@ -0,0 +1,8 @@
+__version__ = "0.25.0"
+
+import logging
+logging.getLogger("libbs").addHandler(logging.NullHandler())
+from libbs.logger import Loggers
+loggers = Loggers()
+del Loggers
+del logging
