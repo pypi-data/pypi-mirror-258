7492232b07c25da0cff245614632c1cf1b9a7495
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-24 14:03:59 +0000

    Add package abalytics/2.1.1/abalytics-2.1.1.tar.gz

---
+++ packages/abalytics/abalytics-2.1.1.tar.gz/abalytics-2.1.1/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='abalytics',
+    version='2.1.1',
+    author='Janis Berneker',
+    packages=find_packages(),
+    license='LICENSE',
+    description='Advanced A/B Testing Statistical Analytics',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    install_requires=[
+        'pandas>=1.5.2',
+        'scipy>=1.10.1',
+        'statsmodels>=0.14.1',
+        'scikit-posthocs>=0.8.1',
+        'pingouin>=0.5.4'
+    ],
+    python_requires='>=3.8',
+    project_urls={
+        'Repository': 'https://github.com/BigBerny/abalytics',
+    },
+)
