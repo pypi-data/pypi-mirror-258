3916b3425bb44d9fa31c455b3b092be043eee890
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-24 03:04:47 +0000

    Add package aiida-worktree/0.1.4/aiida_worktree-0.1.4-py3-none-any.whl

---
+++ packages/aiida-worktree/aiida_worktree-0.1.4-py3-none-any.whl/aiida_worktree/decorator.py
@@ -0,0 +1,276 @@
+from typing import Any
+from aiida_worktree.utils import get_executor
+from aiida.engine.processes.functions import calcfunction, workfunction
+from aiida.engine.processes.calcjobs import CalcJob
+from aiida.engine.processes.workchains import WorkChain
+from aiida.orm.nodes.process.calculation.calcfunction import CalcFunctionNode
+from aiida.orm.nodes.process.workflow.workfunction import WorkFunctionNode
+from aiida.engine.processes.ports import PortNamespace
+
+
+def add_input_recursive(inputs, port, prefix=None):
+    """Add input recursively."""
+    if prefix is None:
+        port_name = port.name
+    else:
+        port_name = f"{prefix}.{port.name}"
+    if isinstance(port, PortNamespace):
+        # TODO the default value is {} could cause problem, because the address of the dict is the same,
+        # so if you change the value of one port, the value of all the ports of other nodes will be changed
+        # consider to use None as default value
+        inputs.append(
+            ["General", port_name, {"property": ["General", {"default": {}}]}]
+        )
+        for value in port.values():
+            add_input_recursive(inputs, value, prefix=port_name)
+    else:
+        inputs.append(["General", port_name])
+    return inputs
+
+
+def build_node(ndata):
+    """Register a node from a AiiDA component."""
+    from node_graph.decorator import create_node
+    from aiida_worktree.node import Node
+    import cloudpickle as pickle
+    import inspect
+
+    path, executor_name, = ndata.pop(
+        "path"
+    ).rsplit(".", 1)
+    ndata["executor"] = {"path": path, "name": executor_name}
+    executor, type = get_executor(ndata["executor"])
+    # print(executor)
+    if inspect.isfunction(executor):
+        # calcfunction and workfunction
+        if getattr(executor, "node_class", False):
+            if executor.node_class is CalcFunctionNode:
+                ndata["node_type"] = "calcfunction"
+            elif executor.node_class is WorkFunctionNode:
+                ndata["node_type"] = "workfunction"
+        else:
+            ndata["node_type"] = "normal"
+    else:
+        if issubclass(executor, CalcJob):
+            ndata["node_type"] = "calcjob"
+        elif issubclass(executor, WorkChain):
+            ndata["node_type"] = "workchain"
+        else:
+            ndata["node_type"] = "normal"
+    inputs = []
+    outputs = []
+    spec = executor.spec()
+    for _key, port in spec.inputs.ports.items():
+        add_input_recursive(inputs, port)
+    kwargs = [input[1] for input in inputs]
+    for _key, port in spec.outputs.ports.items():
+        outputs.append(["General", port.name])
+    if spec.inputs.dynamic:
+        ndata["var_kwargs"] = spec.inputs.dynamic
+        inputs.append(
+            ["General", spec.inputs.dynamic, {"property": ["General", {"default": {}}]}]
+        )
+    # print("kwargs: ", kwargs)
+    ndata["node_class"] = Node
+    ndata["kwargs"] = kwargs
+    ndata["inputs"] = inputs
+    ndata["outputs"] = outputs
+    ndata["identifier"] = ndata.pop("identifier", ndata["executor"]["name"])
+    # TODO In order to reload the WorkTree from process, "is_pickle" should be True
+    # so I pickled the function here, but this is not necessary
+    # we need to update the node_graph to support the path and name of the function
+    executor = {
+        "executor": pickle.dumps(executor),
+        "type": ndata["node_type"],
+        "is_pickle": True,
+    }
+    ndata["executor"] = executor
+    node = create_node(ndata)
+    return node
+
+
+# decorator with arguments indentifier, args, kwargs, properties, inputs, outputs, executor
+def decorator_node(
+    identifier=None,
+    node_type="Normal",
+    properties=None,
+    inputs=None,
+    outputs=None,
+    catalog="Others",
+    executor_type="function",
+):
+    """Generate a decorator that register a function as a node.
+
+    Attributes:
+        indentifier (str): node identifier
+        catalog (str): node catalog
+        args (list): node args
+        kwargs (dict): node kwargs
+        properties (list): node properties
+        inputs (list): node inputs
+        outputs (list): node outputs
+    """
+    from aiida_worktree.node import Node
+
+    properties = properties or []
+    inputs = inputs or []
+    outputs = outputs or [["General", "result"]]
+
+    def decorator(func):
+        import cloudpickle as pickle
+        from node_graph.decorator import generate_input_sockets, create_node
+
+        nonlocal identifier
+
+        if identifier is None:
+            identifier = func.__name__
+        # use cloudpickle to serialize function
+        executor = {
+            "executor": pickle.dumps(func),
+            "type": "function",
+            "is_pickle": True,
+        }
+        # Get the args and kwargs of the function
+        args, kwargs, var_args, var_kwargs, _inputs = generate_input_sockets(
+            func, inputs, properties
+        )
+        # I don't know why isinstance(func.node_class, CalcFunctionNode) is False
+        # print("func: ", func)
+        # print("node_class: ", func.node_class)
+        if hasattr(func, "node_class"):
+            if func.node_class is CalcFunctionNode:
+                node_type = "calcfunction"
+            elif func.node_class is WorkFunctionNode:
+                node_type = "workfunction"
+        else:
+            node_type = "Normal"
+        ndata = {
+            "node_class": Node,
+            "identifier": identifier,
+            "node_type": node_type,
+            "args": args,
+            "kwargs": kwargs,
+            "var_args": var_args,
+            "var_kwargs": var_kwargs,
+            "properties": properties,
+            "inputs": _inputs,
+            "outputs": outputs,
+            "executor": executor,
+            "catalog": catalog,
+        }
+        node = create_node(ndata)
+        func.identifier = identifier
+        func.node = node
+        return func
+
+    return decorator
+
+
+# decorator with arguments indentifier, args, kwargs, properties, inputs, outputs, executor
+def decorator_node_group(
+    identifier=None,
+    properties=None,
+    inputs=None,
+    outputs=None,
+    catalog="Others",
+    executor_type="function",
+):
+    """Generate a decorator that register a node group as a node.
+
+    Attributes:
+        indentifier (str): node identifier
+        catalog (str): node catalog
+        properties (list): node properties
+        inputs (list): node inputs
+        outputs (list): node outputs
+    """
+    from aiida_worktree.node import Node
+
+    properties = properties or []
+    inputs = inputs or []
+    outputs = outputs or []
+
+    def decorator(func):
+        import cloudpickle as pickle
+        from node_graph.decorator import generate_input_sockets, create_node
+
+        nonlocal identifier, inputs, outputs
+
+        if identifier is None:
+            identifier = func.__name__
+        # use cloudpickle to serialize function
+        func.identifier = identifier
+        func.group_outputs = outputs
+        executor = {
+            "executor": pickle.dumps(func),
+            "type": executor_type,
+            "is_pickle": True,
+        }
+        # Get the args and kwargs of the function
+        args, kwargs, var_args, var_kwargs, _inputs = generate_input_sockets(
+            func, inputs, properties
+        )
+        # nt = func()
+        # inputs = [[nt.nodes[input[0]].inputs[input[1]].identifier, input[2]] for input in group_inputs]
+        # outputs = [[nt.nodes[output[0]].outputs[output[1]].identifier, output[2]] for output in group_outputs]
+        # node_inputs = [["General", input[2]] for input in inputs]
+        node_outputs = [["General", output[1]] for output in outputs]
+        # print(node_inputs, node_outputs)
+        #
+        node_type = "worktree"
+        ndata = {
+            "node_class": Node,
+            "identifier": identifier,
+            "args": args,
+            "kwargs": kwargs,
+            "var_args": var_args,
+            "var_kwargs": var_kwargs,
+            "node_type": node_type,
+            "properties": properties,
+            "inputs": _inputs,
+            "outputs": node_outputs,
+            "executor": executor,
+            "catalog": catalog,
+        }
+        node = create_node(ndata)
+        node.group_inputs = inputs
+        node.group_outputs = outputs
+        func.node = node
+        return func
+
+    return decorator
+
+
+class NodeDecoratorCollection:
+    """Collection of node decorators."""
+
+    node = staticmethod(decorator_node)
+    group = staticmethod(decorator_node_group)
+
+    __call__: Any = node  # Alias '@node' to '@node.node'.
+
+    @staticmethod
+    def calcfunction(**kwargs):
+        def decorator(func):
+            # First, apply the calcfunction decorator
+            calcfunc_decorated = calcfunction(func)
+            # Then, apply node decorator
+            node_decorated = node(**kwargs)(calcfunc_decorated)
+
+            return node_decorated
+
+        return decorator
+
+    @staticmethod
+    def workfunction(**kwargs):
+        def decorator(func):
+            # First, apply the workfunction decorator
+            calcfunc_decorated = workfunction(func)
+            node_decorated = node(**kwargs)(calcfunc_decorated)
+
+            return node_decorated
+
+        return decorator
+
+
+node = NodeDecoratorCollection()
---
+++ packages/aiida-worktree/aiida_worktree-0.1.4-py3-none-any.whl/aiida_worktree/worktree.py
@@ -0,0 +1,274 @@
+import node_graph
+import aiida
+from aiida_worktree.nodes import node_pool
+import time
+
+
+class WorkTree(node_graph.NodeGraph):
+    """Build a node-based workflow AiiDA's worktree engine.
+
+    The class extends from NodeGraph and provides methods to run,
+    submit tasks, wait for tasks to finish, and update the process status.
+    It is used to handle various states of a worktree process and provides
+    convenient operations to interact with it.
+
+    Attributes:
+        process (aiida.orm.ProcessNode): The process node that represents the process status and other details.
+        state (str): The current state of the worktree process.
+        pk (int): The primary key of the process node.
+    """
+
+    node_pool = node_pool
+
+    def __init__(self, name="WorkTree", **kwargs):
+        """
+        Initialize a WorkTree instance.
+
+        Args:
+            name (str, optional): The name of the WorkTree. Defaults to 'WorkTree'.
+            **kwargs: Additional keyword arguments to be passed to the WorkTree class.
+        """
+        super().__init__(name, **kwargs)
+        self.ctx = {}
+        self.worktree_type = "NORMAL"
+        self.sequence = []
+        self.conditions = []
+        self.process = None
+        self.restart_process = None
+        self.max_number_jobs = 1000000
+
+    def run(self, inputs=None):
+        """
+        Run the AiiDA worktree process and update the process status. The method uses AiiDA's engine to run
+        the process and then calls the update method to update the state of the process.
+        """
+        from aiida_worktree.engine.worktree import WorkTree as WorkTreeEngine
+        from aiida_worktree.utils import merge_properties
+        from aiida.manage import manager
+
+        # set node inputs
+        if inputs is not None:
+            for name, input in inputs.items():
+                self.nodes[name].set(input)
+        # One can not run again if the process is alreay created. otherwise, a new process node will
+        # be created again.
+        if self.process is not None:
+            print("Your worktree is already created. Please use the submit() method.")
+            return
+        wtdata = self.to_dict()
+        merge_properties(wtdata)
+        inputs = {"wt": wtdata}
+        # init a process
+        runner = manager.get_manager().get_runner()
+        process_inited = WorkTreeEngine(runner=runner, inputs=inputs)
+        self.process = process_inited.node
+        # save worktree data into process node
+        self.save_to_base(wtdata)
+        result = aiida.engine.run(process_inited)
+        self.update()
+        return result
+
+    def submit(self, inputs=None, wait=False, timeout=60, restart=False, new=False):
+        """Submit the AiiDA worktree process and optionally wait for it to finish.
+        Args:
+            wait (bool): Wait for the process to finish.
+            timeout (int): The maximum time in seconds to wait for the process to finish. Defaults to 60.
+            restart (bool): Restart the process, it will check the modified nodes and reset them,
+                and then only re-run the modified nodes.
+            new (bool): Submit a new process.
+        """
+        from aiida.manage import get_manager
+
+        # set node inputs
+        if inputs is not None:
+            for name, input in inputs.items():
+                self.nodes[name].set(input)
+
+        process_controller = get_manager().get_process_controller()
+        # Create a new submission
+        if self.process is not None and new:
+            self.reset()
+        # Create a restart submission
+        # save the current process node as restart_process
+        # so that the WorkTreeSaver can compare the difference, and reset the modified nodes
+        if restart:
+            self.restart_process = self.process
+            self.process = None
+        # save the worktree to the process node
+        self.save()
+        if self.process.process_state.value.upper() not in ["CREATED"]:
+            return "Error!!! The process has already been submitted and finished."
+        # launch the process, send the task to RabbitMA
+        # TODO in case of "[ERROR] Process<3705> is unreachable."
+        process_controller.continue_process(self.process.pk)
+        if wait:
+            self.wait(timeout=timeout)
+
+    def save(self, metadata=None):
+        """Save the udpated worktree to the process
+        This is only used for a running worktree.
+        Save the AiiDA worktree process and update the process status.
+        """
+        from aiida_worktree.engine.worktree import WorkTree as WorkTreeEngine
+        from aiida_worktree.utils import merge_properties
+
+        wtdata = self.to_dict()
+        merge_properties(wtdata)
+        metadata = metadata or {}
+        inputs = {"wt": wtdata, "metadata": metadata}
+        if self.process is None:
+            # init a process node
+            process_inited = WorkTreeEngine(inputs=inputs)
+            process_inited.runner.persister.save_checkpoint(process_inited)
+            self.process = process_inited.node
+            self.process_inited = process_inited
+            print(f"WorkTree node created, PK: {self.process.pk}")
+        self.save_to_base(wtdata)
+        self.update()
+
+    def save_to_base(self, wtdata):
+        """Save new wtdata to base.extras.
+        It will first check the difference, and reset nodes if needed.
+        """
+        from aiida_worktree.utils.analysis import WorkTreeSaver
+
+        saver = WorkTreeSaver(
+            self.process, wtdata, restart_process=self.restart_process
+        )
+        saver.save()
+
+    def to_dict(self):
+        wtdata = super().to_dict()
+        self.ctx["sequence"] = self.sequence
+        # only alphanumeric and underscores are allowed
+        wtdata["ctx"] = {
+            key.replace(".", "__"): value for key, value in self.ctx.items()
+        }
+        wtdata["worktree_type"] = self.worktree_type
+        wtdata["conditions"] = self.conditions
+        wtdata["max_number_jobs"] = self.max_number_jobs
+
+        return wtdata
+
+    def wait(self, timeout=50):
+        """
+        Periodically checks and waits for the AiiDA worktree process to finish until a given timeout.
+
+        Args:
+            timeout (int): The maximum time in seconds to wait for the process to finish. Defaults to 50.
+        """
+
+        start = time.time()
+        self.update()
+        while self.state not in (
+            "PAUSED",
+            "FINISHED",
+            "FAILED",
+            "CANCELLED",
+            "EXCEPTED",
+        ):
+            time.sleep(0.5)
+            self.update()
+            if time.time() - start > timeout:
+                return
+
+    def update(self):
+        """
+        Update the current state and primary key of the process node as well as the state, node and primary key
+        of the nodes that are outgoing from the process node. This includes updating the state of process nodes
+        linked to the current process, and data nodes linked to the current process.
+        """
+        self.state = self.process.process_state.value.upper()
+        outgoing = self.process.base.links.get_outgoing()
+        for link in outgoing.all():
+            node = link.node
+            # the link is added in order
+            # so the restarted node will be the last one
+            # thus the node is correct
+            if isinstance(node, aiida.orm.ProcessNode) and getattr(
+                node, "process_state", False
+            ):
+                self.nodes[link.link_label].process = node
+                self.nodes[link.link_label].state = node.process_state.value.upper()
+                self.nodes[link.link_label].node = node
+                self.nodes[link.link_label].pk = node.pk
+                self.nodes[link.link_label].ctime = node.ctime
+                self.nodes[link.link_label].mtime = node.mtime
+            elif isinstance(node, aiida.orm.Data):
+                label = link.link_label.split("__", 1)[1]
+                if label in self.nodes.keys():
+                    self.nodes[label].state = "FINISHED"
+                    self.nodes[label].node = node
+                    self.nodes[label].pk = node.pk
+
+    @property
+    def pk(self):
+        return self.process.pk if self.process else None
+
+    @classmethod
+    def load(cls, pk):
+        """
+        Load the process node with the given primary key.
+
+        Args:
+            pk (int): The primary key of the process node.
+        """
+        from aiida_worktree.utils import get_worktree_data
+
+        process = aiida.orm.load_node(pk)
+        wtdata = get_worktree_data(process)
+        if wtdata is None:
+            print("No worktree data found in the process node.")
+            return
+        wt = cls.from_dict(wtdata)
+        wt.process = process
+        wt.update()
+        return wt
+
+    def show(self):
+        """
+        Print the current state of the worktree process.
+        """
+        from tabulate import tabulate
+
+        table = []
+        self.update()
+        for node in self.nodes:
+            table.append([node.name, node.pk, node.state])
+        print("-" * 80)
+        print("WorkTree: {}, PK: {}, State: {}".format(self.name, self.pk, self.state))
+        print("-" * 80)
+        # show nodes
+        print("Nodes:")
+        print(tabulate(table, headers=["Name", "PK", "State"]))
+        print("-" * 80)
+
+    def pause(self):
+        """Pause the worktree."""
+        # from aiida.engine.processes import control
+        # try:
+        # control.pause_processes([self.process])
+        import os
+
+        os.system("verdi process pause {}".format(self.process.pk))
+
+    def pause_nodes(self, nodes):
+        """
+        Pause the given nodes
+        """
+
+    def play_nodes(self, nodes):
+        """
+        Play the given nodes
+        """
+
+    def reset(self):
+        """Reset the worktree."""
+
+        self.process = None
+        for node in self.nodes:
+            node.reset()
+        self.sequence = []
+        self.conditions = []
+        self.ctx = {}
+        self.state = "CREATED"
