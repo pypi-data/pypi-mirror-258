0d8f54a78c8979c3621b8a9dfdf4cf4613207702
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-24 04:04:57 +0000

    Add package MAMMOth-commons/0.0.7/MAMMOth_commons-0.0.7-py3-none-any.whl

---
+++ packages/MAMMOth-commons/MAMMOth_commons-0.0.7-py3-none-any.whl/mammoth/datasets/csv.py
@@ -0,0 +1,19 @@
+import numpy as np
+from fairbench.bench.loader import features
+from typing import Dict
+import pandas as pd
+from kfp import dsl
+
+
+class CSV:
+    integration = dsl.Dataset
+
+    def __init__(self, data, numeric, categorical, labels):
+        self.data = data
+        self.numeric = numeric
+        self.categorical = categorical
+        self.labels = labels
+        self.cols = numeric + categorical
+
+    def to_features(self):
+        return features(self.data, self.numeric, self.categorical).astype(np.float64)
