704c60c90e7ebd32fdc3b102f3693b96f366c2f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-24 05:06:32 +0000

    Add package chemax/0.0.1/chemax-0.0.1-py3-none-any.whl

---
+++ packages/chemax/chemax-0.0.1-py3-none-any.whl/chemax/datas/abundant_nuclide.py
@@ -0,0 +1,345 @@
+"""
+The most abundant stable nuclide of all elements
+"""
+ABU = {
+    "H": {
+        "symbol": "H[1]",
+        "abund": 99.99
+    },
+    "D": {
+        "symbol": "H[2]",
+        "abund": 0.015
+    },
+    "T": {
+        "symbol": "H[3]",
+        "abund": 0.0
+    },
+    "He": {
+        "symbol": "He[4]",
+        "abund": 100.0
+    },
+    "Li": {
+        "symbol": "Li[7]",
+        "abund": 92.58
+    },
+    "Be": {
+        "symbol": "Be[9]",
+        "abund": 100.0
+    },
+    "B": {
+        "symbol": "B[11]",
+        "abund": 80.2
+    },
+    "C": {
+        "symbol": "C[12]",
+        "abund": 98.9
+    },
+    "N": {
+        "symbol": "N[14]",
+        "abund": 99.63
+    },
+    "O": {
+        "symbol": "O[16]",
+        "abund": 99.76
+    },
+    "F": {
+        "symbol": "F[19]",
+        "abund": 100.0
+    },
+    "Ne": {
+        "symbol": "Ne[20]",
+        "abund": 90.6
+    },
+    "Na": {
+        "symbol": "Na[23]",
+        "abund": 100.0
+    },
+    "Mg": {
+        "symbol": "Mg[24]",
+        "abund": 78.9
+    },
+    "Al": {
+        "symbol": "Al[27]",
+        "abund": 100.0
+    },
+    "Si": {
+        "symbol": "Si[28]",
+        "abund": 92.23
+    },
+    "P": {
+        "symbol": "P[31]",
+        "abund": 100.0
+    },
+    "S": {
+        "symbol": "S[32]",
+        "abund": 95.02
+    },
+    "Cl": {
+        "symbol": "Cl[35]",
+        "abund": 75.77
+    },
+    "Ar": {
+        "symbol": "Ar[40]",
+        "abund": 99.6
+    },
+    "K": {
+        "symbol": "K[39]",
+        "abund": 93.2
+    },
+    "Ca": {
+        "symbol": "Ca[40]",
+        "abund": 96.95
+    },
+    "Sc": {
+        "symbol": "Sc[45]",
+        "abund": 100.0
+    },
+    "Ti": {
+        "symbol": "Ti[48]",
+        "abund": 73.8
+    },
+    "Cr": {
+        "symbol": "Cr[52]",
+        "abund": 83.79
+    },
+    "V": {
+        "symbol": "V[51]",
+        "abund": 99.75
+    },
+    "Fe": {
+        "symbol": "Fe[56]",
+        "abund": 91.72
+    },
+    "Mn": {
+        "symbol": "Mn[55]",
+        "abund": 100.0
+    },
+    "Ni": {
+        "symbol": "Ni[58]",
+        "abund": 68.27
+    },
+    "Co": {
+        "symbol": "Co[59]",
+        "abund": 100.0
+    },
+    "Cu": {
+        "symbol": "Cu[63]",
+        "abund": 69.17
+    },
+    "Zn": {
+        "symbol": "Zn[64]",
+        "abund": 48.6
+    },
+    "Ga": {
+        "symbol": "Ga[69]",
+        "abund": 60.1
+    },
+    "Ge": {
+        "symbol": "Ge[74]",
+        "abund": 36.5
+    },
+    "Se": {
+        "symbol": "Se[80]",
+        "abund": 49.6
+    },
+    "As": {
+        "symbol": "As[75]",
+        "abund": 100.0
+    },
+    "Kr": {
+        "symbol": "Kr[84]",
+        "abund": 57.0
+    },
+    "Br": {
+        "symbol": "Br[79]",
+        "abund": 50.69
+    },
+    "Sr": {
+        "symbol": "Sr[88]",
+        "abund": 82.58
+    },
+    "Rb": {
+        "symbol": "Rb[85]",
+        "abund": 72.17
+    },
+    "Y": {
+        "symbol": "Y[89]",
+        "abund": 100.0
+    },
+    "Zr": {
+        "symbol": "Zr[90]",
+        "abund": 51.45
+    },
+    "Mo": {
+        "symbol": "Mo[98]",
+        "abund": 24.13
+    },
+    "Nb": {
+        "symbol": "Nb[93]",
+        "abund": 100.0
+    },
+    "Ru": {
+        "symbol": "Ru[102]",
+        "abund": 31.6
+    },
+    "Pd": {
+        "symbol": "Pd[106]",
+        "abund": 27.33
+    },
+    "Rh": {
+        "symbol": "Rh[103]",
+        "abund": 100.0
+    },
+    "Cd": {
+        "symbol": "Cd[114]",
+        "abund": 28.73
+    },
+    "Ag": {
+        "symbol": "Ag[107]",
+        "abund": 51.84
+    },
+    "Sn": {
+        "symbol": "Sn[120]",
+        "abund": 32.4
+    },
+    "In": {
+        "symbol": "In[115]",
+        "abund": 95.7
+    },
+    "Te": {
+        "symbol": "Te[130]",
+        "abund": 33.8
+    },
+    "Sb": {
+        "symbol": "Sb[121]",
+        "abund": 57.3
+    },
+    "Xe": {
+        "symbol": "Xe[132]",
+        "abund": 26.9
+    },
+    "I": {
+        "symbol": "I[127]",
+        "abund": 100.0
+    },
+    "Ba": {
+        "symbol": "Ba[138]",
+        "abund": 71.7
+    },
+    "Cs": {
+        "symbol": "Cs[133]",
+        "abund": 100.0
+    },
+    "Ce": {
+        "symbol": "Ce[140]",
+        "abund": 88.48
+    },
+    "La": {
+        "symbol": "La[139]",
+        "abund": 99.91
+    },
+    "Pr": {
+        "symbol": "Pr[141]",
+        "abund": 100.0
+    },
+    "Nd": {
+        "symbol": "Nd[142]",
+        "abund": 27.13
+    },
+    "Sm": {
+        "symbol": "Sm[152]",
+        "abund": 26.7
+    },
+    "Eu": {
+        "symbol": "Eu[153]",
+        "abund": 52.2
+    },
+    "Gd": {
+        "symbol": "Gd[158]",
+        "abund": 24.84
+    },
+    "Dy": {
+        "symbol": "Dy[164]",
+        "abund": 28.2
+    },
+    "Tb": {
+        "symbol": "Tb[159]",
+        "abund": 100.0
+    },
+    "Er": {
+        "symbol": "Er[166]",
+        "abund": 33.6
+    },
+    "Ho": {
+        "symbol": "Ho[165]",
+        "abund": 100.0
+    },
+    "Yb": {
+        "symbol": "Yb[174]",
+        "abund": 31.8
+    },
+    "Tm": {
+        "symbol": "Tm[169]",
+        "abund": 100.0
+    },
+    "Hf": {
+        "symbol": "Hf[180]",
+        "abund": 35.2
+    },
+    "Lu": {
+        "symbol": "Lu[175]",
+        "abund": 97.4
+    },
+    "W": {
+        "symbol": "W[184]",
+        "abund": 30.67
+    },
+    "Ta": {
+        "symbol": "Ta[181]",
+        "abund": 99.99
+    },
+    "Os": {
+        "symbol": "Os[192]",
+        "abund": 41.0
+    },
+    "Re": {
+        "symbol": "Re[187]",
+        "abund": 62.6
+    },
+    "Pt": {
+        "symbol": "Pt[195]",
+        "abund": 33.8
+    },
+    "Ir": {
+        "symbol": "Ir[193]",
+        "abund": 62.7
+    },
+    "Hg": {
+        "symbol": "Hg[202]",
+        "abund": 29.65
+    },
+    "Au": {
+        "symbol": "Au[197]",
+        "abund": 100.0
+    },
+    "Tl": {
+        "symbol": "Tl[205]",
+        "abund": 70.48
+    },
+    "Pb": {
+        "symbol": "Pb[208]",
+        "abund": 52.4
+    },
+    "Bi": {
+        "symbol": "Bi[209]",
+        "abund": 100.0
+    },
+    "Th": {
+        "symbol": "Th[232]",
+        "abund": 100.0
+    },
+    "U": {
+        "symbol": "U[238]",
+        "abund": 99.27
+    }
+}
