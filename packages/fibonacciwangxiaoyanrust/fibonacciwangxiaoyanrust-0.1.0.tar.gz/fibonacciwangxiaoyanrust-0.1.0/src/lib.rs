af65781572c3e985437df762d44e50f2c3c43324
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-24 15:04:26 +0000

    Add package fibonacciwangxiaoyanrust/0.1.0/fibonacciwangxiaoyanrust-0.1.0.tar.gz

---
+++ packages/fibonacciwangxiaoyanrust/fibonacciwangxiaoyanrust-0.1.0.tar.gz/fibonacciwangxiaoyanrust-0.1.0/src/lib.rs
@@ -0,0 +1,33 @@
+use pyo3::prelude::*;
+
+
+#[pyfunction]
+fn fibonacci_normal_rust(n: u64) -> u64 {
+    if n <= 1 {
+      n
+    } else {
+        fibonacci_normal_rust(n - 1) + fibonacci_normal_rust(n - 2)
+    }
+  }
+
+
+  #[pyfunction]
+  fn fibonacci_optimized_rust(n: u64) -> u64 {
+    let mut a = 0;
+    let mut b = 1;
+    for _ in 0..n {
+        let temp = a;
+        a = b;
+        b += temp;
+    }
+    a
+}
+
+
+#[pymodule]
+fn fibonacciwangxiaoyanrust(_py: Python, m: &PyModule) -> PyResult<()> {
+    m.add_function(wrap_pyfunction!(fibonacci_normal_rust, m)?)?;
+    m.add_function(wrap_pyfunction!(fibonacci_optimized_rust, m)?)?;
+
+    Ok(())
+}
