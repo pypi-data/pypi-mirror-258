45b9b43ef89d85e3012a82e5cd1b877bbe1294a9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-24 05:06:59 +0000

    Add package chic-lib/0.1.6/chic_lib-0.1.6-py3-none-any.whl

---
+++ packages/chic-lib/chic_lib-0.1.6-py3-none-any.whl/chic/__init__.py
@@ -0,0 +1,19 @@
+"""
+chic: A Python library for coarse-graining hybrid and inorganic frameworks.
+
+This library provides tools for working with complex structures, allowing for
+efficient analysis and manipulation of hybrid and inorganic framework materials.
+"""
+
+
+from .structure import Structure
+from .net import Net
+
+
+__version__ = "0.1.7"
+__author__ = "Thomas C. Nicholas"
+__email__ = "thomas.nicholas@chem.ox.ac.uk"
+__url__ = "https://github.com/tcnicholas/chic"
+
+
+__all__ = ['Structure', 'Net']
