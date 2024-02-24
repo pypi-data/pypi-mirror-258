83de36d54da0b6a283a7daddcb6a43767a7f56bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-24 15:04:20 +0000

    Add package backstrip/0.2.0/backstrip-0.2.0-py2.py3-none-any.whl

---
+++ packages/backstrip/backstrip-0.2.0-py2.py3-none-any.whl/backstrip/__init__.py
@@ -0,0 +1,15 @@
+__author__ = "Matthew Andres Moreno"
+__copyright__ = "Copyright 2024, Matthew Andres Moreno"
+__credits__ = ["Matthew Andres Moreno"]
+__license__ = "MIT"
+__version__ = "0.2.0"
+__maintainer__ = "Matthew Andres Moreno"
+__email__ = "m.more500@gmail.com"
+
+from ._backplot import backplot
+from ._backstrip import backstrip
+
+__all__ = [
+    "backplot",
+    "backstrip",
+]
