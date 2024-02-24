b8b3fbd9b510ce0a6d5e3368f405ee29b193491b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-24 04:04:41 +0000

    Add package django-snowflake/3.2/django_snowflake-3.2-py3-none-any.whl

---
+++ packages/django-snowflake/django_snowflake-3.2-py3-none-any.whl/django_snowflake/__init__.py
@@ -0,0 +1,13 @@
+__version__ = '3.2'
+
+# Check Django compatibility before other imports which may fail if the
+# wrong version of Django is installed.
+from .utils import check_django_compatability
+
+check_django_compatability()
+
+from .functions import register_functions  # noqa
+from .lookups import register_lookups  # noqa
+
+register_functions()
+register_lookups()
