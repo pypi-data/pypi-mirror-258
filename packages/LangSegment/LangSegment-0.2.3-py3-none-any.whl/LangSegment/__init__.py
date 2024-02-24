137400dda878585bd33b443622c0dcf091b5c159
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-24 15:04:21 +0000

    Add package LangSegment/0.2.3/LangSegment-0.2.3-py3-none-any.whl

---
+++ packages/LangSegment/LangSegment-0.2.3-py3-none-any.whl/LangSegment/__init__.py
@@ -0,0 +1,8 @@
+from .LangSegment import LangSegment,getTexts,classify,getCounts,printList,setLangfilters,getLangfilters,setfilters,getfilters,setPriorityThreshold,getPriorityThreshold,setEnablePreview,getEnablePreview
+
+# release
+__version__ = '0.2.3'
+
+
+# develop
+__develop__ = 'dev-0.0.1'
