79c06fc98fef375a24e638d6057f3de4cfdd268a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-24 14:04:11 +0000

    Add package MathRedframe/0.0.1/MathRedframe-0.0.1.tar.gz

---
+++ packages/MathRedframe/MathRedframe-0.0.1.tar.gz/MathRedframe-0.0.1/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+name="MathRedframe",
+version="0.0.1",
+description="A simple Python package for math operations and geometry calculations by Redframe",
+packages=find_packages(),
+classifiers=[
+"Programming Language :: Python :: 3",
+"License :: OSI Approved :: MIT License",
+"Operating System :: OS Independent",
+],
+python_requires=">=3.6",
+)
