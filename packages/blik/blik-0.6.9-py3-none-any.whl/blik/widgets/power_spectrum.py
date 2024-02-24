e71ba13e6f9dfe0abf2683bfbac9fd66b636c5a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-24 04:04:40 +0000

    Add package blik/0.6.9/blik-0.6.9-py3-none-any.whl

---
+++ packages/blik/blik-0.6.9-py3-none-any.whl/blik/widgets/power_spectrum.py
@@ -0,0 +1,33 @@
+from typing import TYPE_CHECKING
+
+import dask.array as da
+import numpy as np
+from magicgui import magic_factory
+from scipy.fft import fftn, fftshift, ifftshift
+
+if TYPE_CHECKING:
+    import napari
+
+
+@magic_factory(
+    auto_call=False,
+    call_button="Calculate",
+)
+def power_spectrum(
+    image: "napari.layers.Image",
+) -> "napari.types.LayerDataTuple":
+    """
+    Power spectrum (log scale) of the image.
+
+    First centers in real space on the origin to remove shift effects.
+    """
+    axes = (1, 2) if image.metadata["stack"] else None
+    raw = da.compute(image.data)[0]
+    power_spectrum = np.abs(
+        fftshift(fftn(ifftshift(raw, axes=axes), axes=axes), axes=axes)
+    )
+    return (
+        np.log(power_spectrum + 1),
+        {"name": f"{image.name} - power spectrum", "scale": image.scale},
+        "image",
+    )
