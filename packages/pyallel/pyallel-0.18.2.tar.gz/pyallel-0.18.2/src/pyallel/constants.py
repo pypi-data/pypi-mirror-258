d64d6e656e9a37d2a4d857abe9a55e4a00a371b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-24 05:05:43 +0000

    Add package pyallel/0.18.2/pyallel-0.18.2.tar.gz

---
+++ packages/pyallel/pyallel-0.18.2.tar.gz/pyallel-0.18.2/src/pyallel/constants.py
@@ -0,0 +1,28 @@
+import os
+import sys
+
+IN_TTY = sys.stdout.isatty()
+CLEAR_LINE = "\033[2K"
+UP_LINE = "\033[1F"
+
+if IN_TTY:
+
+    def COLUMNS() -> int:
+        return os.get_terminal_size().columns
+
+    def LINES() -> int:
+        return os.get_terminal_size().lines
+
+else:
+
+    def COLUMNS() -> int:
+        return sys.maxsize
+
+    def LINES() -> int:
+        return sys.maxsize
+
+
+ICONS = ("/", "-", "\\", "|")
+# Unicode character bytes to render different symbols in the terminal
+TICK = "\u2713"
+X = "\u2717"
