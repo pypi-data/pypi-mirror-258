e8c608fe4a45e626fecdafbf84e48c412269189f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-25 14:05:04 +0000

    Add package OZI/0.0.275/OZI-0.0.275.tar.gz

---
+++ packages/OZI/OZI-0.0.275.tar.gz/OZI-0.0.275/ozi/scripts/core_metadata_template.py
@@ -0,0 +1,27 @@
+# noqa: INP001
+# ozi/scripts/core_metadata_template.py
+# Part of the OZI Project, under the Apache License v2.0 with LLVM Exceptions.
+# See LICENSE.txt for license information.
+# SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+"""python template: check core metadata"""
+import os
+import pathlib
+import sys
+
+if sys.version_info >= (3, 11):  # pragma: no cover
+    import tomllib as toml
+elif sys.version_info < (3, 11):  # pragma: no cover
+    import tomli as toml
+
+# pylint: disable=consider-using-with
+source = pathlib.Path(
+    os.path.relpath(
+        os.path.join('/', os.environ.get('MESON_SOURCE_ROOT', os.path.relpath('..'))),
+        '/',
+    ),
+)
+project_file = open(source / 'pyproject.toml', 'rb')
+pyproject_toml = toml.load(project_file)
+project_file.close()
+core_metadata = pyproject_toml.get('project', {'optional_dependencies': {}})
+print(core_metadata.get('optional_dependencies', {'todo': []}).get('@0@', 'fail'))
