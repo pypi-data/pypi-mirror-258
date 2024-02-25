1fd97a77393a4b0579194b6aadfeb35e9ce50522
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-25 03:04:36 +0000

    Add package tree-sitter-requirements/0.3.0/tree_sitter_requirements-0.3.0-cp38-abi3-musllinux_1_1_x86_64.whl

---
+++ packages/tree-sitter-requirements/tree_sitter_requirements-0.3.0-cp38-abi3-musllinux_1_1_x86_64.whl/tree_sitter_requirements/__init__.py
@@ -0,0 +1,9 @@
+"""pip requirements parser"""
+
+from ._core import parse, query, highlights
+
+__author__ = 'ObserverOfTime'
+__version__ = '0.1.0'
+__license__ = 'MIT'
+
+__all__ = ['parse', 'query', 'highlights']
