bf0f3c48fb30b8ef9c6725e59751c583a1f8960a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-25 03:04:45 +0000

    Add package desktop-env/0.1.2/desktop_env-0.1.2.tar.gz

---
+++ packages/desktop-env/desktop_env-0.1.2.tar.gz/desktop_env-0.1.2/desktop_env/evaluators/getters/file.py
@@ -0,0 +1,100 @@
+import os
+from typing import Dict, List, Set
+from typing import Optional, Any, Union
+
+import requests
+
+
+def get_cloud_file(env, config: Dict[str, Any]) -> Union[str, List[str]]:
+    """
+    Config:
+        path (str|List[str]): the url to download from
+        dest (str|List[str])): file name of the downloaded file
+        multi (bool) : optional. if path and dest are lists providing
+          information of multiple files. defaults to False
+        gives (List[int]): optional. defaults to [0]. which files are directly
+          returned to the metric. if len==1, str is returned; else, list is
+          returned.
+    """
+
+    if not config.get("multi", False):
+        paths: List[str] = [config["path"]]
+        dests: List[str] = [config["dest"]]
+    else:
+        paths: List[str] = config["path"]
+        dests: List[str] = config["dest"]
+    cache_paths: List[str] = []
+
+    gives: Set[int] = set(config.get("gives", [0]))
+
+    for i, (p, d) in enumerate(zip(paths, dests)):
+        _path = os.path.join(env.cache_dir, d)
+        if i in gives:
+            cache_paths.append(_path)
+
+        if os.path.exists(_path):
+            #return _path
+            continue
+
+        url = p
+        response = requests.get(url, stream=True)
+        response.raise_for_status()
+
+        with open(_path, 'wb') as f:
+            for chunk in response.iter_content(chunk_size=8192):
+                if chunk:
+                    f.write(chunk)
+
+    return cache_paths[0] if len(cache_paths)==1 else cache_paths
+
+
+def get_vm_file(env, config: Dict[str, Any]) -> Union[Optional[str], List[Optional[str]]]:
+    """
+    Config:
+        path (str): absolute path on the VM to fetch
+        dest (str): file name of the downloaded file
+        multi (bool) : optional. if path and dest are lists providing
+          information of multiple files. defaults to False
+        gives (List[int]): optional. defaults to [0]. which files are directly
+          returned to the metric. if len==1, str is returned; else, list is
+          returned.
+    """
+
+    if not config.get("multi", False):
+        paths: List[str] = [config["path"]]
+        dests: List[str] = [config["dest"]]
+    else:
+        paths: List[str] = config["path"]
+        dests: List[str] = config["dest"]
+    cache_paths: List[str] = []
+
+    gives: Set[int] = set(config.get("gives", [0]))
+
+    for i, (p, d) in enumerate(zip(paths, dests)):
+        _path = os.path.join(env.cache_dir, d)
+
+        file = env.controller.get_file(p)
+        if file is None:
+            #return None
+            # raise FileNotFoundError("File not found on VM: {:}".format(config["path"]))
+            if i in gives:
+                cache_paths.append(None)
+            continue
+
+        if i in gives:
+            cache_paths.append(_path)
+        with open(_path, "wb") as f:
+            f.write(file)
+
+    return cache_paths[0] if len(cache_paths)==1 else cache_paths
+
+
+def get_cache_file(env, config: Dict[str, str]) -> str:
+    """
+    Config:
+        path (str): relative path in cache dir
+    """
+
+    _path = os.path.join(env.cache_dir, config["path"])
+    assert os.path.exists(_path)
+    return _path
---
+++ packages/desktop-env/desktop_env-0.1.2.tar.gz/desktop_env-0.1.2/desktop_env/evaluators/metrics/vscode.py
@@ -0,0 +1,130 @@
+from typing import Dict
+import json, copy
+
+def check_json_keybindings(actual: str, expected: str, **options) -> float:
+    """
+    Args:
+        actual (str): path to result text file
+        expected (str): expected dict{}
+
+    Return:
+        float: the score
+    """
+    def direct_load_json(fp):
+        try:
+            with open(fp, 'r') as f:
+                data = json.load(f)
+            return data
+        except:
+            return None
+    
+    def skip_first_line_load_json(fp):
+        try:
+            with open(fp, 'r') as f:
+                f.readline()
+                data = json.load(f)
+            return data
+        except:
+            return None
+
+    for func in [direct_load_json, skip_first_line_load_json]:
+        data = func(actual)
+        if data is not None and type(data) == list:
+            break
+    else:
+        return 0.0
+    expected = expected['expected']
+    if expected in data:
+        return 1.0
+    else:
+        return 0.0
+
+
+def check_json_settings(actual: str, expected: str, **options) -> float:
+    """
+    Args:
+        actual (str): path to result text file
+        expected (dict): expected dict{}, containing key "expect"
+
+    Return:
+        float: the score
+    """
+    if not actual:
+        return 0.
+
+    with open(actual, 'r') as f:
+        data = json.load(f)
+    
+    expect = expected['expected']
+    data_copy = copy.deepcopy(data)
+    data_copy.update(expect)
+    if data == data_copy:
+        return 1.0
+    else:
+        return 0.0
+
+
+def compare_text_file(actual: str, expected: str, **options) -> float:
+    """
+    Args:
+        actual (str): path to result text file
+        expected (str): path to gold text file
+
+    Return:
+        float: the score
+    """
+    if not actual:
+        return 0.
+
+    with open(actual) as f1:
+        actual_text = f1.read()
+    with open(expected) as f2:
+        expected_text = f2.read()
+
+    if actual_text == expected_text:
+        return 1.0
+    return 0.0
+
+
+def compare_config(actual: str, rules: Dict, **options) -> float:
+    if not actual:
+        return 0.
+
+    with open(actual) as f1:
+        actual_text = f1.read()
+
+    if actual_text == rules['expected']:
+        return 1.0
+    return 0.0
+
+
+def compare_answer(actual: str, rules: Dict, **options) -> float:
+    """
+    Args:
+        actual (str): result string
+        expected (str): gold string
+
+    Return:
+        float: the score
+    """
+    if not actual:
+        return 0.
+
+    if actual == rules['expected']:
+        return 1.0
+
+    # TODO: can use text embedding to get non-zero return
+    return 0.0
+
+
+def is_extension_installed(actual: str, rules: Dict, **options):
+    if rules['type'] == 'contain':
+        if rules['expected'] in actual:
+            return 1.0
+        return 0.0
+    elif rules['type'] == 'not_contain':
+        if rules['expected'] not in actual:
+            return 1.0
+        return 0.0
+    else:
+        raise NotImplementedError
