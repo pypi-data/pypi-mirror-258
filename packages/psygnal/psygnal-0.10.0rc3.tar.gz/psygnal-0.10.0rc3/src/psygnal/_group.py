58748da349fa04c40fec831e72fa4a1faee2c591
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-25 03:04:49 +0000

    Add package psygnal/0.10.0rc3/psygnal-0.10.0rc3.tar.gz

---
+++ packages/psygnal/psygnal-0.10.0rc3.tar.gz/psygnal-0.10.0rc3/src/psygnal/_group.py
@@ -0,0 +1,419 @@
+"""A SignalGroup class that allows connecting to all SignalInstances on the class.
+
+Note that unlike a slot/callback connected to SignalInstance.connect, a slot connected
+to SignalGroup.connect does *not* receive the direct arguments that were emitted by a
+given SignalInstance. Instead, the slot/callback will receive an EmissionInfo named
+tuple, which contains `.signal`: the SignalInstance doing the emitting, and `.args`:
+the args that were emitted.
+
+"""
+
+from __future__ import annotations
+
+import warnings
+from typing import (
+    TYPE_CHECKING,
+    Any,
+    Callable,
+    ClassVar,
+    ContextManager,
+    Iterable,
+    Iterator,
+    Literal,
+    Mapping,
+    NamedTuple,
+)
+
+from psygnal._signal import Signal, SignalInstance, _SignalBlocker
+
+from ._mypyc import mypyc_attr
+
+if TYPE_CHECKING:
+    from psygnal._weak_callback import WeakCallback
+
+__all__ = ["EmissionInfo", "SignalGroup"]
+
+
+class EmissionInfo(NamedTuple):
+    """Tuple containing information about an emission event.
+
+    Attributes
+    ----------
+    signal : SignalInstance
+    args: tuple
+    """
+
+    signal: SignalInstance
+    args: tuple[Any, ...]
+
+
+class SignalRelay(SignalInstance):
+    """Special SignalInstance that can be used to connect to all signals in a group.
+
+    This class will rarely be instantiated by a user (or anything other than a
+    SignalGroup).
+
+    Parameters
+    ----------
+    signals : Mapping[str, SignalInstance]
+        A mapping of signal names to SignalInstance instances.
+    instance : Any, optional
+        An object to which this `SignalRelay` is bound, by default None
+    """
+
+    def __init__(
+        self, signals: Mapping[str, SignalInstance], instance: Any = None
+    ) -> None:
+        super().__init__(signature=(EmissionInfo,), instance=instance)
+        self._signals = signals
+        self._sig_was_blocked: dict[str, bool] = {}
+
+    def _append_slot(self, slot: WeakCallback) -> None:
+        super()._append_slot(slot)
+        if len(self._slots) == 1:
+            self._connect_relay()
+
+    def _connect_relay(self) -> None:
+        # silence any warnings about failed weakrefs (will occur in compiled version)
+        with warnings.catch_warnings():
+            warnings.simplefilter("ignore")
+            for sig in self._signals.values():
+                sig.connect(
+                    self._slot_relay, check_nargs=False, check_types=False, unique=True
+                )
+
+    def _remove_slot(self, slot: int | WeakCallback | Literal["all"]) -> None:
+        super()._remove_slot(slot)
+        if not self._slots:
+            self._disconnect_relay()
+
+    def _disconnect_relay(self) -> None:
+        for sig in self._signals.values():
+            sig.disconnect(self._slot_relay)
+
+    def _slot_relay(self, *args: Any) -> None:
+        if emitter := Signal.current_emitter():
+            info = EmissionInfo(emitter, args)
+            self._run_emit_loop((info,))
+
+    def connect_direct(
+        self,
+        slot: Callable | None = None,
+        *,
+        check_nargs: bool | None = None,
+        check_types: bool | None = None,
+        unique: bool | str = False,
+        max_args: int | None = None,
+    ) -> Callable[[Callable], Callable] | Callable:
+        """Connect `slot` to be called whenever *any* Signal in this group is emitted.
+
+        Params are the same as `psygnal.SignalInstance.connect`.  It's probably
+        best to check whether all signals are uniform (i.e. have the same signature).
+
+        Parameters
+        ----------
+        slot : Callable
+            A callable to connect to this signal.  If the callable accepts less
+            arguments than the signature of this slot, then they will be discarded when
+            calling the slot.
+        check_nargs : bool | None
+            If `True` and the provided `slot` requires more positional arguments than
+            the signature of this Signal, raise `TypeError`. by default `True`.
+        check_types : bool | None
+            If `True`, An additional check will be performed to make sure that types
+            declared in the slot signature are compatible with the signature
+            declared by this signal, by default `False`.
+        unique : bool | str
+            If `True`, returns without connecting if the slot has already been
+            connected.  If the literal string "raise" is passed to `unique`, then a
+            `ValueError` will be raised if the slot is already connected.
+            By default `False`.
+        max_args : int, optional
+            If provided, `slot` will be called with no more more than `max_args` when
+            this SignalInstance is emitted.  (regardless of how many arguments are
+            emitted).
+
+        Returns
+        -------
+        Union[Callable[[Callable], Callable], Callable]
+            [description]
+        """
+
+        def _inner(slot: Callable) -> Callable:
+            for sig in self._signals.values():
+                sig.connect(
+                    slot,
+                    check_nargs=check_nargs,
+                    check_types=check_types,
+                    unique=unique,
+                    max_args=max_args,
+                )
+            return slot
+
+        return _inner if slot is None else _inner(slot)
+
+    def block(self, exclude: Iterable[str | SignalInstance] = ()) -> None:
+        """Block this signal and all emitters from emitting."""
+        super().block()
+        for name, sig in self._signals.items():
+            if exclude and sig in exclude or name in exclude:
+                continue
+            self._sig_was_blocked[name] = sig._is_blocked
+            sig.block()
+
+    def unblock(self) -> None:
+        """Unblock this signal and all emitters, allowing them to emit."""
+        super().unblock()
+        for name, sig in self._signals.items():
+            if not self._sig_was_blocked.pop(name, False):
+                sig.unblock()
+
+    def blocked(
+        self, exclude: Iterable[str | SignalInstance] = ()
+    ) -> ContextManager[None]:
+        """Context manager to temporarily block all emitters in this group.
+
+        Parameters
+        ----------
+        exclude : iterable of str or SignalInstance, optional
+            An iterable of signal instances or names to exempt from the block,
+            by default ()
+        """
+        return _SignalBlocker(self, exclude=exclude)
+
+    def disconnect(self, slot: Callable | None = None, missing_ok: bool = True) -> None:
+        """Disconnect slot from all signals.
+
+        Parameters
+        ----------
+        slot : callable, optional
+            The specific slot to disconnect.  If `None`, all slots will be disconnected,
+            by default `None`
+        missing_ok : bool, optional
+            If `False` and the provided `slot` is not connected, raises `ValueError.
+            by default `True`
+
+        Raises
+        ------
+        ValueError
+            If `slot` is not connected and `missing_ok` is False.
+        """
+        for sig in self._signals.values():
+            sig.disconnect(slot, missing_ok)
+        super().disconnect(slot, missing_ok)
+
+
+# NOTE
+# To developers. Avoid adding public names to this class, as it is intended to be
+# a container for user-determined names.  If names must be added, try to prefix
+# with "psygnal_" to avoid conflicts with user-defined names.
+@mypyc_attr(allow_interpreted_subclasses=True)
+class SignalGroup:
+    """A collection of signals that can be connected to as a single unit.
+
+    This class is not intended to be instantiated directly.  Instead, it should be
+    subclassed, and the subclass should define Signal instances as class attributes.
+    The SignalGroup will then automatically collect these signals and provide a
+    SignalRelay instance (at `group.all`) that can be used to connect to all of the
+    signals in the group.
+
+    This class is used in both the EventedModels and the evented dataclass patterns.
+    See also: `psygnal.SignalGroupDescriptor`, which provides convenient and explicit
+    way to create a SignalGroup on a dataclass-like class.
+
+    Parameters
+    ----------
+    instance : Any, optional
+        An object to which this `SignalGroup` is bound, by default None
+
+    Attributes
+    ----------
+    all : SignalRelay
+        A special SignalRelay instance that can be used to connect to all signals in
+        this group.
+
+    Examples
+    --------
+    ```python
+    from psygnal import Signal, SignalGroup
+
+    class MySignals(SignalGroup):
+        sig1 = Signal()
+        sig2 = Signal()
+
+    group = MySignals()
+    group.all.connect(print) # connect to all signals in the group
+
+    list(group)                  # ['sig1', 'sig2']
+    len(group)                   # 2
+    group.sig1 is group['sig1']  # True
+    ```
+    """
+
+    _psygnal_signals: ClassVar[Mapping[str, Signal]]
+    _psygnal_instances: dict[str, SignalInstance]
+    _psygnal_uniform: ClassVar[bool] = False
+
+    def __init__(self, instance: Any = None) -> None:
+        cls = type(self)
+        if not hasattr(cls, "_psygnal_signals"):  # pragma: no cover
+            raise TypeError(
+                "Cannot instantiate `SignalGroup` directly.  Use a subclass instead."
+            )
+
+        self._psygnal_instances = {
+            name: sig.__get__(self, cls) for name, sig in cls._psygnal_signals.items()
+        }
+        self._psygnal_relay = SignalRelay(self._psygnal_instances, instance)
+
+    def __init_subclass__(cls, strict: bool = False) -> None:
+        """Collects all Signal instances on the class under `cls._psygnal_signals`."""
+        cls._psygnal_signals = {
+            k: val
+            for k, val in getattr(cls, "__dict__", {}).items()
+            if isinstance(val, Signal)
+        }
+
+        if conflicts := {k for k in cls._psygnal_signals if k.startswith("_psygnal")}:
+            warnings.warn(
+                "Signal names may not begin with '_psygnal'. "
+                f"Skipping signals: {conflicts}",
+                stacklevel=2,
+            )
+            for key in conflicts:
+                del cls._psygnal_signals[key]
+
+        if "all" in cls._psygnal_signals:
+            warnings.warn(
+                "Name 'all' is reserved for the SignalRelay. You cannot use this "
+                "name on to access a SignalInstance on a SignalGroup. (You may still "
+                "access it at `group['all']`).",
+                UserWarning,
+                stacklevel=2,
+            )
+            delattr(cls, "all")
+
+        if "psygnals_uniform" in cls._psygnal_signals:
+            raise NameError(
+                "Name 'psygnals_uniform' is reserved.  You cannot use this "
+                "name as a signal on a SignalGroup"
+            )
+
+        cls._psygnal_uniform = _is_uniform(cls._psygnal_signals.values())
+        if strict and not cls._psygnal_uniform:
+            raise TypeError(
+                "All Signals in a strict SignalGroup must have the same signature"
+            )
+        super().__init_subclass__()
+
+    @property
+    def all(self) -> SignalRelay:
+        """SignalInstance that can be used to connect to all signals in this group.
+
+        Examples
+        --------
+        ```python
+        from psygnal import Signal, SignalGroup
+
+        class MySignals(SignalGroup):
+            sig1 = Signal()
+            sig2 = Signal()
+
+        group = MySignals()
+        group.sig2.connect(...)  # connect to a single signal by name
+        group.all.connect(...)  # connect to all signals in the group
+        """
+        return self._psygnal_relay
+
+    # TODO: change type hint to -> SignalInstance after completing deprecation of
+    # direct access to names on SignalRelay object
+    def __getattr__(self, name: str) -> Any:
+        # Note, technically these lines aren't actually needed because of Signal's
+        # descriptor protocol: Accessing a name on a group instance will first look
+        # the instance's __dict__, and then in the class's __dict__, which
+        # will call Signal.__get__ and return the SignalInstance.
+        # these lines are here as a reminder to developers (and safeguard?).
+        if name != "_psygnal_instances" and name in self._psygnal_instances:
+            return self._psygnal_instances[name]  # pragma: no cover
+
+        if name != "_psygnal_relay" and hasattr(self._psygnal_relay, name):
+            warnings.warn(
+                f"Accessing SignalInstance attribute {name!r} on a SignalGroup is "
+                f"deprecated. Access it on the `group.all` attribute instead. e.g. "
+                f"`group.all.{name}`. This will be an error in v0.11.",
+                FutureWarning,
+                stacklevel=2,
+            )
+            return getattr(self._psygnal_relay, name)
+
+        raise AttributeError(f"{type(self).__name__!r} has no signal named {name!r}")
+
+    @property
+    def signals(self) -> Mapping[str, SignalInstance]:
+        """DEPRECATED: A mapping of signal names to SignalInstance instances."""
+        # TODO: deprecate this property
+        warnings.warn(
+            "Accessing the `signals` property on a SignalGroup is deprecated. "
+            "Use __iter__ to iterate over all signal names, and __getitem__ or getattr "
+            "to access signal instances. This will be an error in a future.",
+            FutureWarning,
+            stacklevel=2,
+        )
+        return self._psygnal_instances
+
+    def __len__(self) -> int:
+        """Return the number of signals in the group (not including the relay)."""
+        return len(self._psygnal_signals)
+
+    def __getitem__(self, item: str) -> SignalInstance:
+        """Get a signal instance by name."""
+        return self._psygnal_instances[item]
+
+    def __iter__(self) -> Iterator[str]:
+        """Yield the names of all signals in the group."""
+        return iter(self._psygnal_signals)
+
+    def __contains__(self, item: str) -> bool:
+        """Return True if the group contains a signal with the given name."""
+        # this is redundant with __iter__ and can be removed, but only after
+        # removing the deprecation warning in __getattr__
+        return item in self._psygnal_signals
+
+    def __repr__(self) -> str:
+        """Return repr(self)."""
+        name = self.__class__.__name__
+        return f"<SignalGroup {name!r} with {len(self)} signals>"
+
+    @classmethod
+    def psygnals_uniform(cls) -> bool:
+        """Return true if all signals in the group have the same signature."""
+        return cls._psygnal_uniform
+
+    @classmethod
+    def is_uniform(cls) -> bool:
+        """Return true if all signals in the group have the same signature."""
+        warnings.warn(
+            "The `is_uniform` method on SignalGroup is deprecated. Use "
+            "`psygnals_uniform` instead. This will be an error in v0.11.",
+            FutureWarning,
+            stacklevel=2,
+        )
+        return cls._psygnal_uniform
+
+    def __deepcopy__(self, memo: dict[int, Any]) -> SignalGroup:
+        # TODO:
+        # This really isn't a deep copy. Should we also copy connections?
+        # a working deepcopy is important for pydantic support, but in most cases
+        # it will be a group without any signals connected
+        return type(self)(instance=self._psygnal_relay.instance)
+
+
+def _is_uniform(signals: Iterable[Signal]) -> bool:
+    """Return True if all signals have the same signature."""
+    seen: set[tuple[str, ...]] = set()
+    for s in signals:
+        v = tuple(str(p.annotation) for p in s.signature.parameters.values())
+        if seen and v not in seen:  # allow zero or one
+            return False
+        seen.add(v)
+    return True
---
+++ packages/psygnal/psygnal-0.10.0rc3.tar.gz/psygnal-0.10.0rc3/tests/test_group.py
@@ -0,0 +1,290 @@
+from copy import deepcopy
+from unittest.mock import Mock, call
+
+import pytest
+
+try:
+    from typing import Annotated  # py39
+except ImportError:
+    Annotated = None
+
+from psygnal import EmissionInfo, Signal, SignalGroup
+
+
+class MyGroup(SignalGroup):
+    sig1 = Signal(int)
+    sig2 = Signal(str)
+
+
+def test_signal_group() -> None:
+    assert not MyGroup.psygnals_uniform()
+    with pytest.warns(
+        FutureWarning, match="The `is_uniform` method on SignalGroup is deprecated"
+    ):
+        assert not MyGroup.is_uniform()
+    group = MyGroup()
+    assert not group.psygnals_uniform()
+    assert list(group) == ["sig1", "sig2"]  # testing __iter__
+    assert group.sig1 is group["sig1"]
+
+    assert repr(group) == "<SignalGroup 'MyGroup' with 2 signals>"
+
+    with pytest.raises(AttributeError, match="'MyGroup' has no signal named 'sig3'"):
+        group.sig3  # noqa: B018
+
+
+def test_uniform_group() -> None:
+    """In a uniform group, all signals must have the same signature."""
+
+    class MyStrictGroup(SignalGroup, strict=True):
+        sig1 = Signal(int)
+        sig2 = Signal(int)
+
+    assert MyStrictGroup.psygnals_uniform()
+    group = MyStrictGroup()
+    assert group.psygnals_uniform()
+    assert set(group) == {"sig1", "sig2"}
+
+    with pytest.raises(TypeError) as e:
+
+        class BadGroup(SignalGroup, strict=True):
+            sig1 = Signal(str)
+            sig2 = Signal(int)
+
+    assert str(e.value).startswith("All Signals in a strict SignalGroup must")
+
+
+@pytest.mark.skipif(Annotated is None, reason="requires typing.Annotated")
+def test_nonhashable_args() -> None:
+    """Test that non-hashable annotations are allowed in a SignalGroup"""
+
+    class MyGroup(SignalGroup):
+        sig1 = Signal(Annotated[int, {"a": 1}])  # type: ignore
+        sig2 = Signal(Annotated[float, {"b": 1}])  # type: ignore
+
+    assert not MyGroup.psygnals_uniform()
+
+    with pytest.raises(TypeError):
+
+        class MyGroup2(SignalGroup, strict=True):
+            sig1 = Signal(Annotated[int, {"a": 1}])  # type: ignore
+            sig2 = Signal(Annotated[float, {"b": 1}])  # type: ignore
+
+
+@pytest.mark.parametrize("direct", [True, False])
+def test_signal_group_connect(direct: bool) -> None:
+    mock = Mock()
+    group = MyGroup()
+    if direct:
+        # the callback wants the emitted arguments directly
+
+        with pytest.warns(
+            FutureWarning,
+            match="Accessing SignalInstance attribute 'connect_direct' on a SignalGroup"
+            " is deprecated",
+        ):
+            group.connect_direct(mock)
+    else:
+        # the callback will receive an EmissionInfo tuple
+        # (SignalInstance, arg_tuple)
+        group.all.connect(mock)
+    group.sig1.emit(1)
+    group.sig2.emit("hi")
+
+    assert mock.call_count == 2
+    # if connect_with_info was used, the callback will be given an EmissionInfo
+    # tuple that contains the args as well as the signal instance used
+    if direct:
+        expected_calls = [call(1), call("hi")]
+    else:
+        expected_calls = [
+            call(EmissionInfo(group.sig1, (1,))),
+            call(EmissionInfo(group.sig2, ("hi",))),
+        ]
+    mock.assert_has_calls(expected_calls)
+
+
+def test_signal_group_connect_no_args() -> None:
+    """Test that group.all.connect can take a callback that wants no args"""
+    group = MyGroup()
+    count = []
+
+    def my_slot() -> None:
+        count.append(1)
+
+    group.all.connect(my_slot)
+    group.sig1.emit(1)
+    group.sig2.emit("hi")
+    assert len(count) == 2
+
+
+def test_group_blocked() -> None:
+    group = MyGroup()
+
+    mock1 = Mock()
+    mock2 = Mock()
+
+    group.all.connect(mock1)
+    group.sig1.connect(mock2)
+    group.sig1.emit(1)
+
+    mock1.assert_called_once_with(EmissionInfo(group.sig1, (1,)))
+    mock2.assert_called_once_with(1)
+
+    mock1.reset_mock()
+    mock2.reset_mock()
+
+    group.sig2.block()
+    assert group.sig2._is_blocked
+
+    with group.all.blocked():
+        group.sig1.emit(1)
+        assert group.sig1._is_blocked
+
+    assert not group.sig1._is_blocked
+    # the blocker should have restored subthings to their previous states
+    assert group.sig2._is_blocked
+
+    mock1.assert_not_called()
+    mock2.assert_not_called()
+
+
+def test_group_blocked_exclude() -> None:
+    """Test that we can exempt certain signals from being blocked."""
+    group = MyGroup()
+
+    mock1 = Mock()
+    mock2 = Mock()
+
+    group.sig1.connect(mock1)
+    group.sig2.connect(mock2)
+
+    with group.all.blocked(exclude=("sig2",)):
+        group.sig1.emit(1)
+        group.sig2.emit("hi")
+    mock1.assert_not_called()
+    mock2.assert_called_once_with("hi")
+
+
+def test_group_disconnect_single_slot() -> None:
+    """Test that we can disconnect single slots from groups."""
+    group = MyGroup()
+
+    mock1 = Mock()
+    mock2 = Mock()
+
+    group.sig1.connect(mock1)
+    group.sig2.connect(mock2)
+
+    group.all.disconnect(mock1)
+    group.sig1.emit()
+    mock1.assert_not_called()
+
+    group.sig2.emit()
+    mock2.assert_called_once()
+
+
+def test_group_disconnect_all_slots() -> None:
+    """Test that we can disconnect all slots from groups."""
+    group = MyGroup()
+
+    mock1 = Mock()
+    mock2 = Mock()
+
+    group.sig1.connect(mock1)
+    group.sig2.connect(mock2)
+
+    group.all.disconnect()
+    group.sig1.emit()
+    group.sig2.emit()
+
+    mock1.assert_not_called()
+    mock2.assert_not_called()
+
+
+def test_weakref() -> None:
+    """Make sure that the group doesn't keep a strong reference to the instance."""
+    import gc
+
+    class T: ...
+
+    obj = T()
+    group = MyGroup(obj)
+    assert group.all.instance is obj
+    del obj
+    gc.collect()
+    assert group.all.instance is None
+
+
+def test_group_deepcopy() -> None:
+    class T:
+        def method(self) -> None: ...
+
+    obj = T()
+    group = MyGroup(obj)
+    assert deepcopy(group) is not group  # but no warning
+
+    group.all.connect(obj.method)
+
+    # with pytest.warns(UserWarning, match="does not copy connected weakly"):
+    group2 = deepcopy(group)
+
+    assert not len(group2.all)
+    mock = Mock()
+    mock2 = Mock()
+    group.all.connect(mock)
+    group2.all.connect(mock2)
+
+    group2.sig1.emit(1)
+    mock.assert_not_called()
+    mock2.assert_called_with(EmissionInfo(group2.sig1, (1,)))
+
+    mock2.reset_mock()
+    group.sig1.emit(1)
+    mock.assert_called_with(EmissionInfo(group.sig1, (1,)))
+    mock2.assert_not_called()
+
+
+def test_group_conflicts() -> None:
+    with pytest.warns(UserWarning, match="Signal names may not begin with '_psygnal'"):
+
+        class MyGroup(SignalGroup):
+            _psygnal_thing = Signal(int)
+            other_signal = Signal(int)
+
+    assert "_psygnal_thing" not in MyGroup._psygnal_signals
+    assert "other_signal" in MyGroup._psygnal_signals
+
+
+def test_delayed_relay_connect() -> None:
+    group = MyGroup()
+    mock = Mock()
+    gmock = Mock()
+    assert len(group.sig1) == 0
+
+    group.sig1.connect(mock)
+    # group relay hasn't been connected to sig1 or sig2 yet
+    assert len(group.sig1) == 1
+    assert len(group.sig2) == 0
+
+    group.all.connect(gmock)
+    # NOW the relay is connected
+    assert len(group.sig1) == 2
+    assert len(group.sig2) == 1
+    method = group.sig1._slots[-1].dereference()
+    assert method
+    assert method.__name__ == "_slot_relay"
+
+    group.sig1.emit(1)
+    mock.assert_called_once_with(1)
+    gmock.assert_called_once_with(EmissionInfo(group.sig1, (1,)))
+
+    group.all.disconnect(gmock)
+    assert len(group.sig1) == 1
+    assert len(group.all) == 0
+
+    mock.reset_mock()
+    gmock.reset_mock()
+    group.sig1.emit(1)
+    mock.assert_called_once_with(1)
+    gmock.assert_not_called()
