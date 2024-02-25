f94a1e50a6f9a4cdd28235ff85a68be96a1e03d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-25 03:04:43 +0000

    Add package pyBunniApi/0.1.2.1/pyBunniApi-0.1.2.1.tar.gz

---
+++ packages/pyBunniApi/pyBunniApi-0.1.2.1.tar.gz/pyBunniApi-0.1.2.1/pyBunniApi/resources/contacts.py
@@ -0,0 +1,22 @@
+from typing import Any, TYPE_CHECKING, List
+
+from pyBunniApi.objects.contact import Contact
+
+if TYPE_CHECKING:
+    from pyBunniApi.client import Client
+
+
+class Contacts:
+    def __init__(self, bunni_api: "Client"):
+        self.bunni_api = bunni_api
+
+    def list(self) -> list[dict[str, Any]] | List[Contact]:
+        if self.bunni_api.TYPED:
+            return self.typed_list()
+        return self.untyped_list()
+
+    def untyped_list(self) -> List[dict[str, Any]]:
+        return self.bunni_api.create_http_request('contacts/list')['items']
+
+    def typed_list(self) -> List[Contact]:
+        return [Contact(**contact) for contact in self.bunni_api.create_http_request('contacts/list')['items']]
