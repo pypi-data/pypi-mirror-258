368c909038b897a930328eedc0d09925bb856c60
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-25 03:04:36 +0000

    Add package pySWXF/0.1.13/pyswxf-0.1.13-py3-none-any.whl

---
+++ packages/pySWXF/pyswxf-0.1.13-py3-none-any.whl/pySWXF/cbwe.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep 10 12:24:39 2019
+
+"""
+import numpy as np
+def cbwe(y1,dy1,y2,dy2):
+    if (dy1 == 0 ) and (dy2 == 0):
+        return (y1+y2)/2, 0
+    elif (dy1 == 0):
+        return y2, dy2
+    elif (dy2 == 0):
+        return y1, dy1
+    else:
+        lg = len(y1)
+        y = (y1/dy1**2+y2[0:lg]/dy2[0:lg]**2)/(1/dy1**2+1/dy2[0:lg]**2)
+        dy = np.sqrt(1/(1/dy1**2 + 1/dy2[0:lg]**2))
+        np.append(y,y2[lg:])
+        np.append(dy,dy2[lg:])
+        return y, dy
+def cbwe_s(y1,dy1,y2,dy2):
+    # cbwe for scalers
+    y = (y1/dy1**2+y2/dy2**2)/(1/dy1**2+1/dy2**2)
+    dy = np.sqrt(1/(1/dy1**2 + 1/dy2**2))
+    return y,dy
+
+   
