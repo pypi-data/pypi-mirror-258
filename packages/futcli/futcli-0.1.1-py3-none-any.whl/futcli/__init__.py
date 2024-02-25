b3134656c3a5f83faba8f901500d5be763ceb48c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-25 14:05:35 +0000

    Add package futcli/0.1.1/futcli-0.1.1-py3-none-any.whl

---
+++ packages/futcli/futcli-0.1.1-py3-none-any.whl/futcli/__init__.py
@@ -0,0 +1,8 @@
+# futcli/__init__.py
+
+__title__ = "futcli"
+__version__ = "0.1.1"
+__author__ = "Edwin V. Amilasan Jr."
+__author_email__ = "ejamilasan@gmail.com"
+__license__ = "GNU GPL v3"
+__copyright__ = "Copyright 2024 Edwin V. Amilasan Jr."
---
+++ packages/futcli/futcli-0.1.1-py3-none-any.whl/futcli/futcli.py
@@ -0,0 +1,77 @@
+import argparse
+import json
+
+from tabulate import tabulate
+
+from .evolutions import get_evolution_items
+from .sbc import get_sbc_items, get_sbc_types
+
+
+def format_output(data, output_format):
+    """Format the output to either json or table."""
+    if output_format == "json":
+        print(json.dumps(data, indent=4))
+    else:
+        print(tabulate(data, headers="keys", tablefmt="grid"))
+
+
+def get_output(data_type, option, output_format):
+    """Retrieve data based on the specified type and option."""
+    if data_type == "sbc":
+        sbc_data = get_sbc_items()
+        if option is None:
+            combined_data = [item for sublist in sbc_data.values() for item in sublist]
+            format_output(combined_data, output_format)
+        elif option in sbc_data:
+            format_output(sbc_data[option], output_format)
+        else:
+            print("Invalid SBC option.")
+    elif data_type == "evolutions":
+        evolutions_data = get_evolution_items()
+        formatted_evolutions = []
+        for item in evolutions_data:
+            formatted_item = {
+                key: (json.dumps(value, indent=4) if isinstance(value, dict) else value)
+                for key, value in item.items()
+            }
+            formatted_evolutions.append(formatted_item)
+        format_output(formatted_evolutions, output_format)
+    else:
+        print("Invalid data type.")
+
+
+def futcli():
+    """Command-line interface for fetching FIFA Ultimate Team data."""
+    parser = argparse.ArgumentParser(
+        formatter_class=argparse.RawDescriptionHelpFormatter
+    )
+    parser.add_argument(
+        "-o",
+        "--output",
+        choices=["table", "json"],
+        default="table",
+        help="Choose the output format (table or json).",
+    )
+
+    subparsers = parser.add_subparsers(
+        dest="data_type", title="args", metavar="[sbc.{options}, evolutions]"
+    )
+    for sbc_type in get_sbc_types():
+        subparsers.add_parser(f"sbc.{sbc_type}", help=f"Outputs list of SBC {sbc_type}")
+    subparsers.add_parser("sbc", help="Outputs list of all SBC types")
+    subparsers.add_parser("evolutions", help="Outputs list of all active Evolutions")
+
+    args = parser.parse_args()
+
+    if args.data_type:
+        if "." in args.data_type:
+            data_type, option = args.data_type.split(".")
+        else:
+            data_type = args.data_type
+            option = None
+
+        get_output(data_type, option, args.output)
+
+
+if __name__ == "__main__":
+    futcli()
