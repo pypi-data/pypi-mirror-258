684f5a719cc9017cd38b66c98dcf9286510d9b7a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-25 15:04:50 +0000

    Add package git-picker/0.3.9/git-picker-0.3.9.tar.gz

---
+++ packages/git-picker/git-picker-0.3.9.tar.gz/git-picker-0.3.9/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = 'git-picker',
+    packages = find_packages(exclude=['examples']),
+    version = '0.3.9',
+    license='MIT',
+    description = 'git picker',
+    author = 'JiauZhang',
+    author_email = 'jiauzhang@163.com',
+    url = 'https://github.com/JiauZhang/git-picker',
+    long_description=open("README.md", "r", encoding="utf-8").read(),
+    long_description_content_type = 'text/markdown',
+    keywords = [
+        'GitHub',
+    ],
+    install_requires=[
+        'httpx',
+    ],
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.8',
+    ],
+)
