edba9acd1785f8533339dbc97ae0173b6abf4263
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-25 14:04:24 +0000

    Add package zrb/0.7.2/zrb-0.7.2-py3-none-any.whl

---
+++ packages/zrb/zrb-0.7.2-py3-none-any.whl/zrb/builtin/__init__.py
@@ -0,0 +1,35 @@
+from zrb.builtin import (
+    base64,
+    devtool,
+    env,
+    eval,
+    explain,
+    generator,
+    git,
+    md5,
+    process,
+    project,
+    say,
+    schedule,
+    ubuntu,
+    update,
+    version,
+    watch_changes,
+)
+
+assert base64
+assert env
+assert eval
+assert git
+assert md5
+assert explain
+assert project
+assert ubuntu
+assert update
+assert version
+assert devtool
+assert generator
+assert process
+assert say
+assert watch_changes
+assert schedule
