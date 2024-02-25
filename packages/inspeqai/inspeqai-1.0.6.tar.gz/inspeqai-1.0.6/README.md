5e92b39247dc7d87055d16669ad1019e47dea867
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-25 15:05:00 +0000

    Add package inspeqai/1.0.6/inspeqai-1.0.6.tar.gz

---
+++ packages/inspeqai/inspeqai-1.0.6.tar.gz/inspeqai-1.0.6/README.md
@@ -0,0 +1,107 @@
+## Project Description
+
+### Inspeq
+
+### Installation
+
+```sh
+pip install inspeqai
+```
+
+### Get API keys
+
+Get your API keys from <a href="https://app.inspeq.ai/" target="_blank">Here</a>
+
+
+### Usage
+
+```py
+
+from inspeq.client import Evaluator
+
+
+#initialization 
+inspeq_eval = Evaluator(sdk_api_key="YOUR_INSPEQ_API_KEY")
+
+
+# Example input data
+input_data = {
+    "llm_input_query": "your_llm_input_query",
+    "llm_input_context": "your_llm_input_context",
+    "llm_output": "your_llm_output",
+}
+
+'''Note : Do not change the structure of input data keep the structure as it
+is. Put your data at places of your_llm_input_context, your_llm_input_query
+and your_llm_output to  with the help of our evaluation metrices.
+
+'''
+print("\n   grammatical_correctness is:")
+print(inspeq_eval.grammatical_correctness(input_data))
+#get all metrices in one function
+
+inspeq_instance.get_all_metrices(input_data)
+
+
+```
+
+### All Metrics provided by Inspeq sdk
+
+```py
+
+print("\n  a. factual_consistency is:")
+print(inspeq_eval.factual_consistency(input_data))
+
+print("\n b. answer_relevance is:")
+print(inspeq_eval.answer_relevance(input_data))
+
+print("\n c. response_tone is:")
+print(inspeq_eval.response_tone(input_data))
+
+print("\n  d. grammatical_correctness is:")
+print(inspeq_eval.grammatical_correctness(input_data))
+
+print("\n e. fluency is:")
+print(inspeq_eval.fluency(input_data))
+
+print("\n f. do_not_use_keywords is:")
+
+print(inspeq_eval.do_not_use_keywords(input_data))
+
+print("\n g. word_limit_test is:")
+print(inspeq_eval.word_limit_test(input_data))
+
+print("\n h.  conceptual_similarity is:")
+print(inspeq_eval.conceptual_similarity(input_data))
+
+```
+
+
+
+###  Supported Features 
+Metrices: 
+
+-  Factual Consistency:
+  Check if the generated text is consistent with known facts.
+
+-  Grammatical Correctness:
+  Assess the grammatical accuracy of the generated text.
+
+-  Do Not Use Keywords:
+  Identify and evaluate the use of specific keywords or phrases.
+
+-  Fluency:
+  Assess the overall smoothness and fluency of the generated text
+
+-  Answer Relevance:
+  Determine the relevance of the generated text in the context of a given query or
+
+-  Word Limit Test:
+  Check if the generated text adheres to specified word limits.
+
+-  Response Tone:
+  Assess the tone and style of the generated response.
+  
+-  Conceptual Similarity:
+  Measure how closely the generated text aligns with the intended conceptual content.
+
---
+++ packages/inspeqai/inspeqai-1.0.6.tar.gz/inspeqai-1.0.6/inspeq/client.py
@@ -0,0 +1,123 @@
+import logging
+import requests
+
+API_URL ="https://api.inspeq.ai"
+
+# Configure logging
+logger = logging.getLogger(__name__)
+
+
+def handle_request_exceptions(func):
+    def wrapper(*args, **kwargs):
+        try:
+            response = func(*args, **kwargs)
+            if response is None:
+                logger.error("API request returned NoneType.")
+                return None
+            if isinstance(response, (str, float)):
+                logger.error(f"Unexpected response received: {response}")
+                return None
+            if response.status_code == 401:
+                logger.error("API key is not valid.")
+                return None
+            response.raise_for_status()
+            return response.json()["data"]
+        except requests.exceptions.RequestException as e:
+            error_msg = str(e)
+            if "for url:" in error_msg:
+                error_msg = error_msg.split("for url:")[
+                    0
+                ].strip()  # Extracts the error message
+            logger.error(f"Request failed: {error_msg}")
+        except Exception as e:
+            logger.error(f"An unexpected error occurred: {e}")
+            raise
+
+    return wrapper
+
+
+class Evaluator:
+    def __init__(self, sdk_api_key, projectId=None):
+        self.sdk_api_key = sdk_api_key
+        self.projectId = projectId
+        self.API_URL = API_URL
+
+    @handle_request_exceptions
+    def make_api_request(self, endpoint, input_data):
+        url = f"{self.API_URL}/api/v1/sdk/{endpoint}"
+        params = {"secret_key": self.sdk_api_key}
+        if self.projectId is not None:
+            params["projectId"] = self.projectId
+        return requests.post(url, params=params, json=input_data, verify=False)
+    
+    def word_limit_test(self, input_data):
+        url = f"{self.API_URL}/api/v1/sdk/word_limit_test"
+        params = {"secret_key": self.sdk_api_key}
+        if self.projectId is not None:
+            params["projectId"] = self.projectId
+
+        response = requests.post(
+            url, params=params, json=input_data,verify=False
+        )
+        response.raise_for_status()
+
+        return response.json()["data"]['word_limit_test']
+    
+
+    def grammatical_correctness(self, input_data):
+        return self.make_api_request("grammatical_correctness", input_data)
+
+    def do_not_use_keywords(self, input_data):
+        return self.make_api_request("do_not_use_keywords", input_data)
+
+    def fluency(self, input_data):
+        return self.make_api_request("answer_fluency", input_data)
+
+    def answer_relevance(self, input_data):
+        return self.make_api_request("answer_relevance", input_data)
+    
+    def response_tone(self, input_data):
+        url = f"{self.API_URL}/api/v1/sdk/response_tone"
+
+        params = {"secret_key": self.sdk_api_key}
+        if self.projectId is not None:
+            params["projectId"] = self.projectId
+
+        response = requests.post(
+            url, params=params, json=input_data,verify=False
+        )
+        response.raise_for_status()
+
+        return response.json()["data"]["tone"]
+
+    def factual_consistency(self, input_data):
+        return self.make_api_request("factual_consistency", input_data)
+
+    def conceptual_similarity(self, input_data):
+        return self.make_api_request("conceptual_similarity", input_data)
+    
+    def get_all_metrices(self, input_data):
+        print("\n  a. factual_consistency is:")
+        print(self.factual_consistency(input_data))
+
+        print("\n b. answer_relevance is:")
+        print(self.answer_relevance(input_data))
+
+        print("\n c. response_tone is:")
+        print(self.response_tone(input_data))
+
+        print("\n  d. grammatical_correctness is:")
+        print(self.grammatical_correctness(input_data))
+
+        print("\n e. fluency is:")
+        print(self.fluency(input_data))
+
+        print("\n f. do_not_use_keywords is:")
+
+        print(self.do_not_use_keywords(input_data))
+
+        print("\n g. word_limit_test is:")
+        print(self.word_limit_test(input_data))
+
+        print("\n h.  conceptual_similarity is:")
+        print(self.conceptual_similarity(input_data))
