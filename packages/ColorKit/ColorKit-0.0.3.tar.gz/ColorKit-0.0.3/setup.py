66b03e6e31e059bf76da545dcd1676d12cce374f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-25 04:05:45 +0000

    Add package ColorKit/0.0.3/ColorKit-0.0.3.tar.gz

---
+++ packages/ColorKit/ColorKit-0.0.3.tar.gz/ColorKit-0.0.3/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.0.3'
+
+DESCRIPTION = 'A comprehensive Python package for advanced color manipulation and analysis'
+
+# Read the contents of the README.md file
+with open("/Volumes/Benjamin/Programming/Python/ColorKit/ColorKit/README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+# Setting up
+setup(
+    name="ColorKit",
+    version=VERSION,
+    author="Benjamin Mark",
+    author_email="bzmarkovits@yahoo.com",
+    description=DESCRIPTION,
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    install_requires=[],
+    keywords=["color conversion", "RGB", "HEX", "CMYK", "HSL", "LAB", "LUV", "XYZ", "YIQ", "HSV", "YUV", "YCbCr", "LCH", "LMS", "color toolkit", "color space", "Python color", "color manipulation", "color analysis", "color utility","python","color"],
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ],
+    license="MIT" 
+)
