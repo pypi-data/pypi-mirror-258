38c9bf5ce8c1aedf18834b52f5002601e1314747
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-25 04:06:02 +0000

    Add package llamakit/0.0.1/llamakit-0.0.1.tar.gz

---
+++ packages/llamakit/llamakit-0.0.1.tar.gz/llamakit-0.0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='llamakit',
+    version='0.0.1',
+    description='llamakit',
+    author='LM Studio',
+    author_email='team@lmstudio.ai',
+    zip_safe=False
+)
