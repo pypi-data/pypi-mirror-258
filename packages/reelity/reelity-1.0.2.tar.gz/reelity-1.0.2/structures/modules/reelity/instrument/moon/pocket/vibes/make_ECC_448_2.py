cb7cadd3e8b89fc507d3e0282f5ffc11869d244f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-25 14:04:20 +0000

    Add package reelity/1.0.2/reelity-1.0.2.tar.gz

---
+++ packages/reelity/reelity-1.0.2.tar.gz/reelity-1.0.2/structures/modules/reelity/instrument/moon/pocket/vibes/make_ECC_448_2.py
@@ -0,0 +1,59 @@
+
+'''
+	import reelity.instrument.moon.pocket.vibes.make_ECC_448_2 as make_ECC_448_2
+	moon_connection = make_ECC_448_2.start (
+		name = "",
+		seed = ""
+	)
+'''
+
+import pymongo
+
+import reelity.instrument.moon.connect as moon_connect
+import reelity.modules.EEC_448_2.keys as EEC_448_2_key_creator	
+
+def start (
+	name = "",
+	seed = ""
+):
+	moon_connection = moon_connect.start ()
+	vibes = moon_connection ["pocket"] ["vibes"]
+	
+	keys = EEC_448_2_key_creator.create (
+		seed = seed
+	)
+	address = "ECC_448_2:" + keys ["public"] ["hexadecimal string"]
+
+
+	existing_document = collection.find_one ({
+		"address": address
+	})
+
+	# If the document doesn't exist, insert it
+	if not existing_document:
+		result = collection.insert_one (new_document)
+		print ("Document inserted")
+	else:
+		print ("Document with the specified field already exists")
+	
+	if existing_document:
+		return {
+			"status": 
+		}
+	
+	vibes.insert_one ({
+		"name": name,
+		"format": "ECC_448_2",
+		"address": "ECC_448_2:" + keys ["public"] ["hexadecimal string"],
+		"public": {
+			"hexadecimal string": keys ["public"] ["hexadecimal string"]
+		},
+		"private": {
+			"hexadecimal string": keys ["private"] ["hexadecimal string"]
+		},
+		"seed": {
+			"hexadecimal string": seed
+		}
+	})
+
+	return;
