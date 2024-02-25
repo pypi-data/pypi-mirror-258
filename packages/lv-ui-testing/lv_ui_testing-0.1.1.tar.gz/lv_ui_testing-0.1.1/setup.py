2a5d2798125fa292f23b25f6b755c118fddd4035
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-25 15:05:01 +0000

    Add package lv-ui-testing/0.1.1/lv_ui_testing-0.1.1.tar.gz

---
+++ packages/lv-ui-testing/lv_ui_testing-0.1.1.tar.gz/lv_ui_testing-0.1.1/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+setup(
+name='lv_ui_testing',
+version='0.1.1',
+author='Thomas Zilliox',
+author_email='thomas.zilliox67@gmail.com',
+description='A LabVIEW User Interface Testing framework.',
+packages=find_packages(),
+classifiers=[
+'Programming Language :: Python :: 3',
+'License :: OSI Approved :: MIT License',
+'Operating System :: OS Independent',
+],
+python_requires='>=3.6',
+)
