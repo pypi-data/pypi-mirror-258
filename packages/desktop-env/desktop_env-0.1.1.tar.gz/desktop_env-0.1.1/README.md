7d9d39fef904e7b053da3f97d0c201a27a496dc4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-25 03:04:45 +0000

    Add package desktop-env/0.1.1/desktop_env-0.1.1.tar.gz

---
+++ packages/desktop-env/desktop_env-0.1.1.tar.gz/desktop_env-0.1.1/README.md
@@ -0,0 +1,88 @@
+# DesktopEnv: An Environment towards Human-like Computer Task Mastery
+<p align="center">
+    <b>SLOGAN</b>
+</p>
+
+<p align="center">
+<a href="">Website</a> •
+<a href="">Paper</a>
+</p>
+
+![Overview](media/overview.png)
+
+## Updates
+- 2024-03-01: 
+
+## Install
+1. Install VMWare and configure `vmrun` command:
+Please refer to [guidance](https://docs.google.com/document/d/1KBdeZwmZs2Vi_Wsnngb3Wf1-RiwMMpXTftwMqP2Ztak/edit#heading=h.uh0x0tkl7fuw)
+
+2. Install the environment package, download the examples and the virtual machine image.
+```bash
+pip install desktop_env
+mkdir -p ~/.desktop_env
+wget xxxx
+wget xxxx
+```
+
+## Quick Start
+Run the following minimal example to interact with the environment:
+```python
+import json
+from desktop_env.envs.desktop_env import DesktopEnv
+
+with open("evaluation_examples/examples/gimp/f723c744-e62c-4ae6-98d1-750d3cd7d79d.json", "r", encoding="utf-8") as f:
+    example = json.load(f)
+
+env = DesktopEnv(
+    path_to_vm=r"path_to_vm",
+    action_space="computer_13",
+    task_config=example
+)
+observation = env.reset()
+
+observation, reward, done, info = env.step({"action_type": "CLICK", "parameters": {"button": "right", "num_clicks": 1}})
+```
+
+## Annotation Tool Usage
+We provide an annotation tool to help you annotate the examples.
+
+## Agent Usage
+We provide a simple agent to interact with the environment. You can use it as a starting point to build your own agent.
+
+## Road map of infra (Proposed)
+
+- [x] Explore VMWare, and whether it can be connected and control through mouse package
+- [x] Explore Windows and MacOS, whether it can be installed
+  - MacOS is closed source and cannot be legally installed
+  - Windows is available legally and can be installed
+- [x] Build gym-like python interface for controlling the VM
+- [x] Recording of actions (mouse movement, click, keyboard) for humans to annotate, and we can replay it and compress it
+- [x] Build a simple task, e.g. open a browser, open a website, click on a button, and close the browser
+- [x] Set up a pipeline and build agents implementation (zero-shot) for the task
+- [x] Start to design on which tasks inside the DesktopENv to focus on, start to wrap up the environment to be public
+- [x] Start to annotate the examples for ~~training~~ and testing
+- [x] Error handling during file passing and file opening, etc.
+- [x] Add accessibility tree from the OS into the observation space
+- [x] Add pre-process and post-process action support for benchmarking setup and evaluation
+- [ ] Multiprocess support, this can enable the reinforcement learning to be more efficient
+- [ ] Experiment logging and visualization system
+- [ ] Add more tasks, maybe scale to 300 for v1.0.0, and create a dynamic leaderboard
+
+## Road map of benchmark, tools and resources (Proposed)
+- [ ] Improve the annotation tool base on DuckTrack, make it more robust which align on accessibility tree
+- [ ] Annotate the steps of doing the task
+- [ ] Build a website for the project
+- [ ] Crawl all resources we explored from the internet, and make it easy to access
+- [ ] Set up ways for community to contribute new examples
+
+## Citation
+If you find this environment useful, please consider citing our work:
+```
+@article{DesktopEnv,
+  title={},
+  author={},
+  journal={arXiv preprint arXiv:xxxx.xxxx},
+  year={2024}
+}
+```
---
+++ packages/desktop-env/desktop_env-0.1.1.tar.gz/desktop_env-0.1.1/desktop_env/evaluators/metrics/general.py
@@ -0,0 +1,219 @@
+import csv
+import functools
+import json
+import operator
+import re
+import sqlite3
+from numbers import Number
+from typing import Callable, Any, Union
+from typing import Dict, List, Pattern
+
+import lxml.etree
+from lxml.cssselect import CSSSelector
+from lxml.etree import _Element
+from rapidfuzz import fuzz
+
+from .utils import _match_record, _match_value_to_rule
+
+
+def check_include_exclude(result: str, rules: Dict[str, List[str]]) -> float:
+    if result is None:
+        return 0.
+
+    print(result, rules)
+    include = rules.get("include", [])
+    exclude = rules.get("exclude", [])
+    if all(r in result for r in include) and all(r not in result for r in exclude):
+        return 1.
+    else:
+        return 0.
+
+
+def exact_match(result, rules) -> float:
+    expect = rules["expected"]
+    print(result, expect)
+
+    if result == expect:
+        return 1.
+    else:
+        return 0.
+
+def is_in_list(result, rules) -> float:
+    expect = rules["expected"]
+    if expect in result:
+        return 1.
+    else:
+        return 0.
+    
+def fuzzy_match(result, rules) -> float:
+    expect = rules["expected"]
+
+    return fuzz.ratio(result, expect) / 100.
+
+
+def check_csv(result: str, rules: Dict[str, List[Dict[str, str]]]) -> float:
+    """
+    Args:
+        result (str): path to csv file
+        rules (Dict[str, List[Dict[str, str]]]): dict like
+          {
+            "expect": [{key: value}]
+            "unexpect": [{key: value}]
+          }
+
+    Returns:
+        float
+    """
+
+    if result is None:
+        return 0.
+
+    expect_metrics = [False] * len(rules.get("expect", []))
+    unexpect_metric = True
+    with open(result) as f:
+        reader = csv.DictReader(f)
+
+        for rcd in reader:
+            for i, r in enumerate(rules.get("expect", [])):
+                expect_metrics[i] = expect_metrics[i] or _match_record(r, rcd)
+            unexpect_metric = unexpect_metric and not any(_match_record(r, rcd) for r in rules.get("unexpect", []))
+    return float(all(expect_metrics) and unexpect_metric)
+
+
+def check_list(result: str, rules: Dict[str, List[str]]) -> float:
+    """
+    Args:
+        result (str): path to list file
+        rules (Dict[str, List[str]]): dict like
+          {
+            "expect": list of str as regexes
+            "unexpect": list of str as regexes
+          }
+
+    Returns:
+        float
+    """
+
+    if result is None:
+        return 0.
+
+    expect_patterns: List[Pattern[str]] = [re.compile(ptt) for ptt in rules.get("expect", [])]
+    unexpect_patterns: List[Pattern[str]] = [re.compile(ptt) for ptt in rules.get("unexpect", [])]
+
+    expect_metrics = [False] * len(expect_patterns)
+    unexpect_metric = True
+    with open(result) as f:
+        for l in f:
+            for i, r in enumerate(expect_patterns):
+                expect_metrics[i] = expect_metrics[i] or (r.search(l) is not None)
+            unexpect_metric = unexpect_metric and all(r.search(l) is None for r in unexpect_patterns)
+    return float(all(expect_metrics) and unexpect_metric)
+
+
+_accessibility_ns_map = {"st": "uri:deskat:state.at-spi.gnome.org"
+    , "attr": "uri:deskat:attributes.at-spi.gnome.org"
+    , "cp": "uri:deskat:component.at-spi.gnome.org"
+    , "doc": "uri:deskat:document.at-spi.gnome.org"
+    , "docattr": "uri:deskat:attributes.document.at-spi.gnome.org"
+    , "txt": "uri:deskat:text.at-spi.gnome.org"
+    , "val": "uri:deskat:value.at-spi.gnome.org"
+    , "act": "uri:deskat:action.at-spi.gnome.org"
+                         }
+
+
+def check_accessibility_tree(result: str, rules: Dict[str, Any]) -> float:
+    """
+    Args:
+        result (str): XML of GNOME Accessibility Tree
+        rules (Dict[str, Any]): dict like
+          {
+            "selectors": list of str as CSS selectors, will be connected by ", "
+              to form a composite selector. Only one from `selectors` and
+              `xpath` is needed. If both are present, `xpath` takes the
+              priority.
+            "xpath": str as xpath. Only one from `selectors` and `xpath` is
+              needed. If both are present, `xpath` takes the priority.
+            "text": str as the expected text content of the selected element.
+            "exact": bool specifying whether exact match or fuzzy match should
+              be performed. defaults to True
+          }
+
+    Returns:
+        float
+    """
+
+    at: _Element = lxml.etree.fromstring(result)
+    if "xpath" in rules:
+        elements: List[_Element] = at.xpath(rules["xpath"], namespaces=_accessibility_ns_map)
+    elif "selectors" in rules:
+        selector = CSSSelector(", ".join(rules["selectors"]), namespaces=_accessibility_ns_map)
+        elements: List[_Element] = selector(at)
+    else:
+        raise ValueError("At least one of xpath and selectors is required")
+
+    if len(elements) == 0:
+        return 0.
+
+    if "text" in rules:
+        match_func: Callable[[str], Number] = functools.partial(operator.eq if rules["exact"] \
+                                                                    else (lambda a, b: fuzz.ratio(a, b) / 100.)
+                                                                , rules["text"]
+                                                                )
+        match_score: Number = 0
+        for elm in elements:
+            match_score = max(match_score, match_func(elm.text or None))
+    else:
+        match_score = 1.
+
+    return float(match_score)
+
+
+# def check_existence(result: str, *args) -> float:
+# return 1. - (result is None)
+
+def run_sqlite3(result: str, rules: Dict[str, Any]) -> float:
+    connection: sqlite3.Connection = sqlite3.connect(result)
+    cursor: sqlite3.Cursor = connection.execute(rules["sql"])
+    return float(cursor.fetchone()[0] or 0)
+
+
+def check_json(result: str, rules: Dict[str, List[Dict[str, Union[List[str], str]]]]) -> float:
+    """
+    Args:
+        result (str): path to json file
+        rules (Dict[str, List[Dict[str, Union[List[str], str]]]]): dict like
+          {
+            "expect": [
+                {
+                    "key": list of str
+                    "method": str
+                    "ref": something
+                }
+            ],
+            "unexpect": <the same as `expect`
+          }
+
+    Returns:
+        float
+    """
+
+    if result is None:
+        return 0.
+    with open(result) as f:
+        result: Dict[str, Any] = json.load(f)
+
+    expect_rules = rules.get("expect", {})
+    unexpect_rules = rules.get("unexpect", {})
+
+    metric = True
+    for r in expect_rules:
+        value = result
+        for k in r["key"]:
+            value = value[k]
+        metric = metric and _match_value_to_rule(value, r)
+    for r in unexpect_rules:
+        value = result
+        for k in r["key"]:
+            value = value[k]
+        metric = metric and not _match_value_to_rule(value, r)
+    return metric
