f44fa4eb530c67d7efdba1d5234862f5d4ca62f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-25 03:04:57 +0000

    Add package TikTokMarketing/0.1.1/TikTokMarketing-0.1.1.tar.gz

---
+++ packages/TikTokMarketing/TikTokMarketing-0.1.1.tar.gz/TikTokMarketing-0.1.1/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+setup(
+name='TikTokMarketing',
+version='0.1.1',
+author='Casper Crause',
+author_email='ccrause07@gmail.com',
+description='A package designed to connect to the TikTok Marketing API and return data to report on.',
+packages=find_packages(),
+classifiers=[
+'Programming Language :: Python :: 3',
+'License :: OSI Approved :: MIT License',
+'Operating System :: OS Independent',
+],
+python_requires='>=3.10.0',
+)
