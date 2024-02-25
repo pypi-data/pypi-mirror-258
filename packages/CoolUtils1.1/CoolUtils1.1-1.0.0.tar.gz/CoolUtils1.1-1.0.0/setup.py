9c128c946c8ddf67ea063d2629635c0a90fafedb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-25 14:05:01 +0000

    Add package CoolUtils1.1/1.0.0/CoolUtils1.1-1.0.0.tar.gz

---
+++ packages/CoolUtils1.1/CoolUtils1.1-1.0.0.tar.gz/CoolUtils1.1-1.0.0/setup.py
@@ -0,0 +1,16 @@
+# setup.py
+
+from setuptools import setup, find_packages
+
+setup(
+    name='CoolUtils1.1',
+    version='1.0.0',
+    description='A collection of cool and useful utilities.',
+    author='kingsmile',
+    author_email='wusicheng@gmail.com',
+    packages=find_packages(),
+    install_requires=[
+        'requests>=2.25.0',
+        'numpy>=1.21.0',
+    ],
+)
