3738048f31d8f4dd60b28ee6a78968fe701869e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-25 03:05:09 +0000

    Add package AlekSIS-App-Cursus/0.1.dev0/aleksis_app_cursus-0.1.dev0-py3-none-any.whl

---
+++ packages/AlekSIS-App-Cursus/aleksis_app_cursus-0.1.dev0-py3-none-any.whl/aleksis/apps/cursus/frontend/components/SubjectChip.vue
@@ -0,0 +1,28 @@
+<script>
+export default {
+  name: "SubjectChip",
+  props: {
+    subject: {
+      type: Object,
+      required: true,
+    },
+    shortName: {
+      type: Boolean,
+      required: false,
+      default: false,
+    },
+    appendIcon: {
+      type: String,
+      default: null,
+      required: false,
+    },
+  },
+};
+</script>
+
+<template>
+  <v-chip :color="subject.colourBg" :text-color="subject.colourFg">
+    {{ shortName ? subject.shortName : subject.name }}
+    <v-icon right v-if="appendIcon">{{ appendIcon }}</v-icon>
+  </v-chip>
+</template>
