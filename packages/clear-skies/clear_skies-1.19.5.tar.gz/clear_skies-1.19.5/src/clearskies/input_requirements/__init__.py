29dee5abb049736b5e7f82e5ac008c77cba256bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-25 03:04:45 +0000

    Add package clear-skies/1.19.5/clear_skies-1.19.5.tar.gz

---
+++ packages/clear-skies/clear_skies-1.19.5.tar.gz/clear_skies-1.19.5/src/clearskies/input_requirements/__init__.py
@@ -0,0 +1,30 @@
+from ..binding_config import BindingConfig
+from .minimum_length import MinimumLength
+from .maximum_length import MaximumLength
+from .required import Required
+from .requirement import Requirement
+from .unique import Unique
+
+
+def minimum_length(minimum_length):
+    return BindingConfig(MinimumLength, minimum_length)
+
+
+def maximum_length(maximum_length):
+    return BindingConfig(MaximumLength, maximum_length)
+
+
+def required():
+    return BindingConfig(Required)
+
+
+def unique():
+    return BindingConfig(Unique)
+
+
+__all__ = [
+    "minimum_length",
+    "maximum_length",
+    "required",
+    "unique",
+]
