e21cff766a6d9b0ae513d9059b8c35ad90d3ed5e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-25 04:05:15 +0000

    Add package ecs-engine/0.3.0/ecs_engine-0.3.0.tar.gz

---
+++ packages/ecs-engine/ecs_engine-0.3.0.tar.gz/ecs_engine-0.3.0/ecs_engine/__init__.py
@@ -0,0 +1,7 @@
+from .entity_admin import EcsAdmin
+from .entity import Entity
+from .system import System
+from .component import Component, SingletonComponent
+from .component_pool import ComponentPool
+from .events import EventBus
+
