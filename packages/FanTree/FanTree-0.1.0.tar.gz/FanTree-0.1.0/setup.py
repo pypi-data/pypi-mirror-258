17ef4008e3095d9c8d05cdf9fba5183a2009f547
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-25 03:05:14 +0000

    Add package FanTree/0.1.0/FanTree-0.1.0.tar.gz

---
+++ packages/FanTree/FanTree-0.1.0.tar.gz/FanTree-0.1.0/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+setup(
+    name='FanTree',
+    version='0.1.0',    
+    description='A module about decision tree',
+    url='https://github.com/fanfansmilkyway/Decision-Tree',
+    author='ShiYuan Fan',
+    author_email='fanfansmilkyway@gmail.com',
+    license='BSD 2-clause',
+    packages=['trees'],
+    install_requires=[],
+
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: BSD License',  
+        'Operating System :: POSIX :: Linux',        
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
+)
