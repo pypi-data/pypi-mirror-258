0cd9d66aff219ff989f7aabf4a9180a794c4ccd2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-25 15:04:43 +0000

    Add package jsonable-platform/0.0.2.20240225175423/jsonable_platform-0.0.2.20240225175423.tar.gz

---
+++ packages/jsonable-platform/jsonable_platform-0.0.2.20240225175423.tar.gz/jsonable_platform-0.0.2.20240225175423/jsonable_platform/__init__.py
@@ -0,0 +1,6 @@
+from .core import *
+
+from .type import (
+    JSONAbleABC, JSONSupportedTypes, JSONSupportedBases, JSONSupportedIterables,
+    JSONAbleABCEncodedType, Self, JSONSupportedEditableIters
+)
