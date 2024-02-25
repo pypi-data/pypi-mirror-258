823177fc2cac38e23284655c122bcf45aeda2f2d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-25 03:04:50 +0000

    Add package PersonlaGPT/1.0.0/PersonlaGPT-1.0.0.tar.gz

---
+++ packages/PersonlaGPT/PersonlaGPT-1.0.0.tar.gz/PersonlaGPT-1.0.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="PersonlaGPT",
+    version="1.0.0",
+    author="Subahdip Jana",
+    author_email="subhadipjana69@gmail.com",
+    
+    )
