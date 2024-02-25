b06ad30e9f535089e5f9eaae22b7ddb3b450e027
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-25 14:05:07 +0000

    Add package alist-sdk/0.30.6/alist_sdk-0.30.6-py3-none-any.whl

---
+++ packages/alist-sdk/alist_sdk-0.30.6-py3-none-any.whl/alist_sdk/__init__.py
@@ -0,0 +1,6 @@
+from .client import Client
+from .async_client import AsyncClient
+from .models import *
+from .err import *
+from .version import __version__
+from .path_lib import AlistPath, PureAlistPath, AlistServer, login_server
