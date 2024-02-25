862ee564da698ff3fc0da16cd02863095549c421
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-25 03:04:46 +0000

    Add package pyBunniApi/1.2.2/pyBunniApi-1.2.2-py3-none-any.whl

---
+++ packages/pyBunniApi/pyBunniApi-1.2.2-py3-none-any.whl/pyBunniApi/objects/category.py
@@ -0,0 +1,11 @@
+class Category:
+    def __init__(self, id: str, name: str, color: str, ledgerNumber: str):
+        self.id = id
+        self.name = name
+        self.color = color
+        self.ledger_number = ledgerNumber
+
+    id: str
+    name: str
+    color: str
+    ledger_number: str
---
+++ packages/pyBunniApi/pyBunniApi-1.2.2-py3-none-any.whl/pyBunniApi/objects/project.py
@@ -0,0 +1,9 @@
+class Project:
+    def __init__(self, id: str, color: str, name: str):
+        self.id = id
+        self.color = color
+        self.name = name
+
+    id: str | None
+    color: str
+    name: str
