3e23477166a2ec88dd3e3e4b6c6b733394430e18
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-25 15:05:05 +0000

    Add package aequilibrae/1.0.1/aequilibrae-1.0.1.tar.gz

---
+++ packages/aequilibrae/aequilibrae-1.0.1.tar.gz/aequilibrae-1.0.1/setup.py
@@ -0,0 +1,122 @@
+import importlib.util as iutil
+import platform
+from os.path import join
+
+import numpy as np
+from Cython.Distutils import build_ext
+from Cython.Build import cythonize
+from setuptools import Extension
+from setuptools import setup, find_packages
+
+with open("__version__.py") as f:
+    exec(f.read())
+
+include_dirs = [np.get_include()]
+if iutil.find_spec("pyarrow") is not None:
+    import pyarrow as pa
+
+    include_dirs.append(pa.get_include())
+
+is_win = "WINDOWS" in platform.platform().upper()
+is_mac = any(e in platform.platform().upper() for e in ["MACOS", "DARWIN"])
+prefix = "/" if is_win else "-f"
+cpp_std = "/std:c++17" if is_win else "-std=c++17"
+compile_args = [cpp_std, f"{prefix}openmp"]
+compile_args += ["-Wno-unreachable-code"] if is_mac else []
+link_args = [f"{prefix}openmp"]
+
+ext_mod_aon = Extension(
+    "aequilibrae.paths.AoN",
+    [join("aequilibrae", "paths", "AoN.pyx")],
+    extra_compile_args=compile_args,
+    extra_link_args=link_args,
+    define_macros=[("NPY_NO_DEPRECATED_API", "NPY_1_7_API_VERSION")],
+    include_dirs=include_dirs,
+    language="c++",
+)
+
+
+ext_mod_ipf = Extension(
+    "aequilibrae.distribution.ipf_core",
+    [join("aequilibrae", "distribution", "ipf_core.pyx")],
+    extra_compile_args=compile_args,
+    extra_link_args=link_args,
+    define_macros=[("NPY_NO_DEPRECATED_API", "NPY_1_7_API_VERSION")],
+    include_dirs=include_dirs,
+    language="c++",
+)
+
+ext_mod_put = Extension(
+    "aequilibrae.paths.public_transport",
+    [join("aequilibrae", "paths", "public_transport.pyx")],
+    extra_compile_args=compile_args,
+    extra_link_args=link_args,
+    define_macros=[("NPY_NO_DEPRECATED_API", "NPY_1_7_API_VERSION")],
+    include_dirs=include_dirs,
+    language="c++",
+)
+
+ext_mod_graph_building = Extension(
+    "aequilibrae.paths.graph_building",
+    [join("aequilibrae", "paths", "graph_building.pyx")],
+    extra_compile_args=compile_args,
+    extra_link_args=link_args,
+    define_macros=[("NPY_NO_DEPRECATED_API", "NPY_1_7_API_VERSION")],
+    include_dirs=include_dirs,
+    language="c++",
+)
+
+
+with open("requirements.txt", "r") as fl:
+    install_requirements = [x.strip() for x in fl.readlines()]
+
+pkgs = list(find_packages())
+
+pkg_data = {
+    "aequilibrae.reference_files": ["spatialite.sqlite", "nauru.zip", "sioux_falls.zip", "coquimbo.zip"],
+    "aequilibrae.paths": ["parameters.pxi", "*.pyx"],
+    "aequilibrae.distribution": ["*.pyx"],
+    "aequilibrae": ["./parameters.yml", "../requirements.txt"],
+    "aequilibrae.project": [
+        "database_specification/network/tables/*.*",
+        "database_specification/network/triggers/*.*",
+        "database_specification/transit/tables/*.*",
+        "database_specification/transit/triggers/*.*",
+    ],
+}
+loose_modules = ["__version__", "parameters"]
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+if __name__ == "__main__":
+    setup(
+        name="aequilibrae",
+        version=release_version,  # noqa: F821
+        install_requires=install_requirements,
+        packages=pkgs,
+        package_dir={"": "."},
+        py_modules=loose_modules,
+        package_data=pkg_data,
+        zip_safe=False,
+        description="A package for transportation modeling",
+        long_description=long_description,
+        author="Pedro Camargo",
+        author_email="c@margo.co",
+        url="https://github.com/AequilibraE/aequilibrae",
+        license="See LICENSE.TXT",
+        license_files=("LICENSE.TXT",),
+        classifiers=[
+            "Programming Language :: Python",
+            "Programming Language :: Python :: 3.8",
+            "Programming Language :: Python :: 3.9",
+            "Programming Language :: Python :: 3.10",
+            "Programming Language :: Python :: 3.11",
+            "Programming Language :: Python :: 3.12",
+        ],
+        cmdclass={"build_ext": build_ext},
+        ext_modules=cythonize(
+            [ext_mod_aon, ext_mod_ipf, ext_mod_put, ext_mod_graph_building],
+            compiler_directives={"language_level": "3str"},
+        ),
+    )
