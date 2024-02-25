9eb043c27b0c1e2e68111cd8aaef6540e11b3590
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-25 03:04:36 +0000

    Add package ftmq/0.6.0/ftmq-0.6.0-py3-none-any.whl

---
+++ packages/ftmq/ftmq-0.6.0-py3-none-any.whl/ftmq/model/__init__.py
@@ -0,0 +1,5 @@
+from ftmq.model.coverage import Coverage
+from ftmq.model.dataset import Catalog, Dataset, Maintainer, Publisher, Resource
+from ftmq.model.proxy import Entity
+
+__all__ = [Catalog, Coverage, Dataset, Entity, Maintainer, Publisher, Resource]
