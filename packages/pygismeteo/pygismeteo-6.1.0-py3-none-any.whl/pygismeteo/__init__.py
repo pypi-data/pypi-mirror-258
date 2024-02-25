710a66c9e2efe759ea385fef801170024dd046a5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-25 15:04:58 +0000

    Add package pygismeteo/6.1.0/pygismeteo-6.1.0-py3-none-any.whl

---
+++ packages/pygismeteo/pygismeteo-6.1.0-py3-none-any.whl/pygismeteo/__init__.py
@@ -0,0 +1,10 @@
+"""Обёртка для Gismeteo API."""
+
+from __future__ import annotations
+
+from pygismeteo_base import models
+
+from . import types
+from ._gismeteo import Gismeteo
+
+__all__ = ("Gismeteo", "models", "types")
