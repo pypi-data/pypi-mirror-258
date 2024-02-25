da14b0ad02629e69f813ebd05fae94e185419991
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-25 04:06:06 +0000

    Add package hexcrypt/0.2/hexcrypt-0.2.tar.gz

---
+++ packages/hexcrypt/hexcrypt-0.2.tar.gz/hexcrypt-0.2/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='hexcrypt',
+	version='0.2',
+	description='A simple hex module for encrypting and decrypting.',
+	author='Matel Matel',
+	packages=['hexcrypt'],
+	zip_safe=False)
