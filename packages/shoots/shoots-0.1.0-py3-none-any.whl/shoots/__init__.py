05b1c70affeea87455ecfedaae82e7f83e1b3c25
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-25 03:04:48 +0000

    Add package shoots/0.1.0/shoots-0.1.0-py3-none-any.whl

---
+++ packages/shoots/shoots-0.1.0-py3-none-any.whl/shoots/__init__.py
@@ -0,0 +1,7 @@
+from .shoots_server import ShootsServer
+from .shoots_client import ShootsClient, PutMode, BucketDeleteMode
+
+__all__ = ['ShootsServer', 
+           'ShootsClient', 
+           'PutMode', 
+           'BucketDeleteMode']
