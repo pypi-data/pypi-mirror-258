0e0d7e3ea299f57fa4aa64024622fd339fcc1957
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-25 04:05:21 +0000

    Add package cybertask/2.1.61/cybertask-2.1.61.tar.gz

---
+++ packages/cybertask/cybertask-2.1.61.tar.gz/cybertask-2.1.61/cybertask/plugins/ask gemini pro.py
@@ -0,0 +1,35 @@
+"""
+LetMeDoIt AI Plugin - ask gemini pro
+
+Ask Google Gemini Pro for information
+
+[FUNCTION_CALL]
+"""
+
+
+from cybertask import config
+from cybertask.geminipro import GeminiPro
+
+def ask_gemini_pro(function_args):
+    config.stopSpinning()
+    query = function_args.get("query") # required
+    GeminiPro(temperature=config.llmTemperature).run(query)
+    return ""
+
+functionSignature = {
+    "name": "ask_gemini_pro",
+    "description": "Ask Gemini Pro to chat or provide information",
+    "parameters": {
+        "type": "object",
+        "properties": {
+            "query": {
+                "type": "string",
+                "description": "The original request in detail, including any supplementary information",
+            },
+        },
+        "required": ["query"],
+    },
+}
+
+config.addFunctionCall(name="ask_gemini_pro", signature=functionSignature, method=ask_gemini_pro)
+config.inputSuggestions.append("Ask Gemini Pro: ")
