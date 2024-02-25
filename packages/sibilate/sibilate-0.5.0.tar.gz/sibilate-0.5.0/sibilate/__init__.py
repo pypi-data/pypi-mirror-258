9446dc3a4e6c198c97be5aa1bfcf599986d4b1ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-25 03:04:37 +0000

    Add package sibilate/0.5.0/sibilate-0.5.0.tar.gz

---
+++ packages/sibilate/sibilate-0.5.0.tar.gz/sibilate-0.5.0/sibilate/__init__.py
@@ -0,0 +1,9 @@
+"""
+Subtitle generator using whisper LLM
+"""
+
+__author__ = """Brendan Strejcek"""
+__email__ = 'brendan@datagazing.com'
+__version__ = '0.5.0'
+
+from .sibilate import main  # noqa F401
