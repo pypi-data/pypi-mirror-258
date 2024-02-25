8927670376be14875b70c7905f77e0c7955c8bcc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-25 04:05:46 +0000

    Add package incredible0124/0.0.4/incredible0124-0.0.4.tar.gz

---
+++ packages/incredible0124/incredible0124-0.0.4.tar.gz/incredible0124-0.0.4/README.md
@@ -0,0 +1,2 @@
+# incredible
+incredible library
---
+++ packages/incredible0124/incredible0124-0.0.4.tar.gz/incredible0124-0.0.4/setup.py
@@ -0,0 +1,16 @@
+#!/usr/local/bin/python3
+# coding: utf-8
+ 
+
+from setuptools import setup
+
+setup(
+    name='incredible0124',
+    version='0.0.4',
+     url='https://github.dev/Louis0124/incredible',
+    install_requires=[
+        'Click',
+        'requests',
+        'paho-mqtt'
+    ]
+)
