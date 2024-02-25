549d7334d3a55d2a405f955d3bda18a58a10d0be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-25 03:04:46 +0000

    Add package tweepy-self/1.6.3/tweepy_self-1.6.3.tar.gz

---
+++ packages/tweepy-self/tweepy_self-1.6.3.tar.gz/tweepy_self-1.6.3/twitter/utils/__init__.py
@@ -0,0 +1,36 @@
+from .file import (
+    copy_file,
+    load_lines,
+    load_json,
+    load_toml,
+    write_lines,
+    write_json,
+    to_json,
+)
+from .html import (
+    parse_unlock_html,
+    parse_oauth_html,
+)
+from .other import (
+    remove_at_sign,
+    tweet_url,
+    to_datetime,
+    hidden_value,
+)
+
+
+__all__ = [
+    "copy_file",
+    "load_lines",
+    "load_json",
+    "load_toml",
+    "write_lines",
+    "write_json",
+    "to_json",
+    "parse_unlock_html",
+    "parse_oauth_html",
+    "remove_at_sign",
+    "tweet_url",
+    "to_datetime",
+    "hidden_value",
+]
