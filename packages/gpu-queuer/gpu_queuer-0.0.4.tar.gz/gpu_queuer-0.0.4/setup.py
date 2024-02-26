e3933ffd9dcd9af9c2f44766287855138e8b7e93
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-26 14:06:10 +0000

    Add package gpu-queuer/0.0.4/gpu_queuer-0.0.4.tar.gz

---
+++ packages/gpu-queuer/gpu_queuer-0.0.4.tar.gz/gpu_queuer-0.0.4/setup.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+from setuptools import setup, find_packages
+
+setup(
+    name='gpu_queuer',
+    version='0.0.4',
+    author='deng1fan',
+    author_email='dengyifan@iie.ac.cn',
+    url='https://github.com/deng1fan',
+    description=u'Deep learning tools',
+    long_description=open("README.md", "r", encoding='utf-8').read(),
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    install_requires=[
+        'nvitop',
+        'redis',
+        'rich',
+        'psutil',
+        'setproctitle',
+    ],
+    exclude=["*.tests", "*.tests.*", "tests"],
+    include_package_data=True,
+    python_requires='>=3.6',
+    keywords=['gpu', 'queuer'],
+)
