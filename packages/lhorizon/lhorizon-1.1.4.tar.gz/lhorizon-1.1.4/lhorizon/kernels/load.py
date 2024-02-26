a72edf469df207e485325fb5f6b08b3ef89e21ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-26 15:05:43 +0000

    Add package lhorizon/1.1.4/lhorizon-1.1.4.tar.gz

---
+++ packages/lhorizon/lhorizon-1.1.4.tar.gz/lhorizon-1.1.4/lhorizon/kernels/load.py
@@ -0,0 +1,19 @@
+import os
+
+import spiceypy as spice
+
+
+def load_metakernel(fn='lhorizon_metakernel.tm'):
+    """
+    convenience wrapper for `spiceypy.furnsh()` and thus SPICE `FURNSH`.
+    it's impossible to accurately 'target' paths in a flexible way inside a
+    SPICE metakernel; this sweeps directory structure messiness under the rug.
+    """
+    cwd = os.getcwd()
+    os.chdir(os.path.dirname(__file__))
+    spice.furnsh(fn)
+    os.chdir(cwd)
+
+
+# TODO: or we could write an extended furnsh() replacement that dynamically
+#   assembled a metakernel. maybe.
