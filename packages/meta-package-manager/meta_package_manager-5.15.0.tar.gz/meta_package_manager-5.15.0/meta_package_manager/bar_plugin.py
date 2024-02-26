aa062a1a95262a04c361da7d86a8273f1447bf7a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-26 03:07:45 +0000

    Add package meta-package-manager/5.15.0/meta_package_manager-5.15.0.tar.gz

---
+++ packages/meta-package-manager/meta_package_manager-5.15.0.tar.gz/meta_package_manager-5.15.0/meta_package_manager/bar_plugin.py
@@ -0,0 +1,402 @@
+#!/usr/bin/env python3
+# <xbar.title>Meta Package Manager</xbar.title>
+# <xbar.version>v5.15.0</xbar.version>
+# <xbar.author>Kevin Deldycke</xbar.author>
+# <xbar.author.github>kdeldycke</xbar.author.github>
+# <xbar.desc>List outdated packages and manage upgrades.</xbar.desc>
+# <xbar.dependencies>python,mpm</xbar.dependencies>
+# <xbar.image>https://i.imgur.com/B5wdxIc.png</xbar.image>
+# <xbar.abouturl>
+#    https://kdeldycke.github.io/meta-package-manager/bar-plugin.html
+# </xbar.abouturl>
+# <xbar.var>
+#   boolean(VAR_SUBMENU_LAYOUT=false):
+#   Group packages into a sub-menu for each manager.
+# </xbar.var>
+# <xbar.var>
+#   boolean(VAR_TABLE_RENDERING=true):
+#   Aligns package names and versions in a table for easier visual parsing.
+# </xbar.var>
+#
+# XXX Deactivate font-related options for Xbar. Default variable value does not allow
+# XXX `=` character in Xbar. See: https://github.com/matryer/xbar/issues/832
+# <!--xbar.var>
+#   string(VAR_DEFAULT_FONT=""):
+#   Default font to use for non-monospaced text.
+# </xbar.var-->
+# <!--xbar.var>
+#   string(VAR_MONOSPACE_FONT="font=Menlo size=12"):
+#   Default configuration for monospace fonts, including errors.
+#   Is used for table rendering.
+# </xbar.var-->
+# <swiftbar.environment>
+#   [
+#       VAR_SUBMENU_LAYOUT: false,
+#       VAR_TABLE_RENDERING: true,
+#       VAR_DEFAULT_FONT: ,
+#       VAR_MONOSPACE_FONT: font=Menlo size=12
+#   ]
+# </swiftbar.environment>
+"""Xbar and SwiftBar plugin for Meta Package Manager (i.e. the :command:`mpm` CLI).
+
+Default update cycle should be set to several hours so we have a chance to get
+user's attention once a day. Higher frequency might ruin the system as all
+checks are quite resource intensive, and Homebrew might hit GitHub's API calls
+quota.
+
+- `Xbar automatically bridge plugin options
+  <https://xbarapp.com/docs/2021/03/14/variables-in-xbar.html>`_ between its UI
+  and environment variable on script execution.
+
+- This is `in progress for SwiftBar
+  <https://github.com/swiftbar/SwiftBar/issues/160>`_.
+"""
+
+from __future__ import annotations
+
+import argparse
+import os
+import re
+import sys
+from configparser import RawConfigParser
+from functools import cached_property
+from operator import methodcaller
+from shutil import which
+from subprocess import run
+from textwrap import dedent
+from unittest.mock import patch
+
+python_min_version = (3, 8, 0)
+"""Minimal requirement is aligned to mpm."""
+
+
+def v_to_str(version_tuple: tuple[int, ...] | None) -> str:
+    """Transforms into a string a tuple of integers representing a version."""
+    if not version_tuple:
+        return "None"
+    return ".".join(map(str, version_tuple))
+
+
+if sys.version_info < python_min_version:
+    msg = (
+        f"Bar plugin ran with Python {sys.version}, but requires "
+        f"Python >= {v_to_str(python_min_version)}"
+    )
+    raise SystemError(msg)
+
+
+class MPMPlugin:
+    """Implements the minimal code necessary to locate and call the ``mpm`` CLI on the
+    system.
+
+    Once ``mpm`` is located, we can rely on it to produce the main output of the plugin.
+
+    The output must supports both `Xbar dialect
+    <https://github.com/matryer/xbar-plugins/blob/main/CONTRIBUTING.md#plugin-api>`_
+    and `SwiftBar dialect <https://github.com/swiftbar/SwiftBar#plugin-api>`_.
+    """
+
+    mpm_min_version = (5, 0, 0)
+    """Mpm v5.0.0 was the first version taking care of the complete layout rendering."""
+
+    @staticmethod
+    def extended_environment() -> dict[str, str]:
+        """Returns a tweaked environment extending global path to find non-default
+        system-wide binaries.
+
+        macOS does not put ``/usr/local/bin`` or ``/opt/local/bin`` in the ``PATH`` for
+        GUI apps. For some package managers this is a problem. Additionally Homebrew and
+        Macports are using different paths. So, to make sure we can always get to the
+        necessary binaries, we overload the path. Current preference order would equate
+        to Homebrew, Macports, then system.
+        """
+        # Cast to dict to make a copy and prevent modification of the global
+        # environment.
+        env_copy = dict(os.environ)
+        env_copy["PATH"] = ":".join(
+            (
+                # Homebrew Apple silicon.
+                "/opt/homebrew/bin",
+                "/opt/homebrew/sbin",
+                # Homebrew Intel.
+                "/usr/local/bin",
+                "/usr/local/sbin",
+                # Macports.
+                "/opt/local/bin",
+                "/opt/local/sbin",
+                # System.
+                os.environ.get("PATH", ""),
+            ),
+        )
+        return env_copy
+
+    @staticmethod
+    def getenv_str(var, default: str | None = None) -> str | None:
+        """Utility to get environment variables.
+
+        Note that all environment variables are strings. Always returns a lowered-case
+        string.
+        """
+        value = os.environ.get(var, None)
+        if value is None:
+            return default
+        return str(value).lower()
+
+    @staticmethod
+    def getenv_bool(var, default: bool = False) -> bool:
+        """Utility to normalize boolean environment variables.
+
+        Relies on ``configparser.RawConfigParser.BOOLEAN_STATES`` to translate strings
+        into boolean. See:
+        https://github.com/python/cpython/blob/89192c4/Lib/configparser.py#L597-L599
+        """
+        value = MPMPlugin.getenv_str(var)
+        if value is None:
+            return default
+        return RawConfigParser.BOOLEAN_STATES[value]
+
+    @staticmethod
+    def normalize_params(
+        font_string: str,
+        valid_ids: set[str] | None = None,
+    ) -> str:
+        if valid_ids is None:
+            valid_ids = {"color", "font", "size"}
+        valid_params = {}
+        for param in font_string.split():
+            param_id, param_value = param.split("=", 1)
+            if param_id in valid_ids:
+                valid_params[param_id] = param_value
+        return " ".join(f"{k}={v}" for k, v in valid_params.items())
+
+    @cached_property
+    def table_rendering(self) -> bool:
+        """Aligns package names and versions, like a table, for easier visual parsing.
+
+        If ``True``, will aligns all items using a fixed-width font.
+        """
+        return self.getenv_bool("VAR_TABLE_RENDERING", True)
+
+    @cached_property
+    def default_font(self) -> str:
+        """Make it easier to change font, sizes and colors of the output."""
+        return self.normalize_params(
+            self.getenv_str("VAR_DEFAULT_FONT", ""),  # type: ignore
+        )
+
+    @cached_property
+    def monospace_font(self) -> str:
+        """Make it easier to change font, sizes and colors of the output."""
+        return self.normalize_params(
+            self.getenv_str("VAR_MONOSPACE_FONT", "font=Menlo size=12"),  # type: ignore
+        )
+
+    @cached_property
+    def error_font(self) -> str:
+        """Error font is based on monospace font."""
+        return self.normalize_params(f"{self.monospace_font} color=red")
+
+    @cached_property
+    def is_swiftbar(self) -> bool:
+        """SwiftBar is kind enough to tell us about its presence."""
+        return self.getenv_bool("SWIFTBAR")
+
+    @cached_property
+    def python_path(self) -> str:
+        """Returns the system's Python binary path.
+
+        This plugin being run from Python, we have the one called by Xbar/SwiftBar to
+        fallback to (i.e. ``sys.executable``). But before that, we attempt to locate it
+        by respecting the environment variables.
+        """
+        for bin_name in ("python", "python3", sys.executable):
+            py_path = which(bin_name)
+            if py_path:
+                return py_path
+        raise FileNotFoundError("No Python binary found on the system.")
+
+    @cached_property
+    def mpm_exec(self) -> tuple[str, ...]:
+        """Search for mpm execution alternatives, either direct ``mpm`` call or as an
+        executable Python module."""
+        # XXX Local debugging and development.
+        # return "poetry", "run", "mpm"
+        mpm_exec = which("mpm")
+        if mpm_exec:
+            return (mpm_exec,)
+        return self.python_path, "-m", "meta_package_manager"
+
+    def check_mpm(
+        self,
+    ) -> tuple[bool, tuple[int, ...] | None, bool, str | Exception | None]:
+        """Test-run mpm execution and extract its version."""
+        error: str | Exception | None = None
+        try:
+            process = run(
+                # Output a color-less versionjust in case the script is not run in a
+                # non-interactive shell, or Click/Click-Extra autodetection fails.
+                (*self.mpm_exec, "--no-color", "--version"),
+                capture_output=True,
+                encoding="utf-8",
+            )
+            error = process.stderr
+        except FileNotFoundError as ex:
+            error = ex
+
+        installed = False
+        mpm_version = None
+        up_to_date = False
+        # Is mpm CLI installed on the system?
+        if not process.returncode and not error:
+            installed = True
+            # This regular expression is designed to extract the version number,
+            # whether it is surrounded by ANSI color escape sequence or not.
+            match = re.compile(
+                r"""
+                .+                      # Any string
+                \                       # A space
+                version                 # The "version" string
+                \                       # A space
+                [^\.]*?                 # Any minimal (non-greedy) string without a dot
+                (?P<version>[0-9\.]+)   # Version composed of numbers and dots
+                [^\.]*?                 # Any minimal (non-greedy) string without a dot
+                $                       # End of the string
+                """,
+                re.VERBOSE | re.MULTILINE,
+            ).search(process.stdout)
+            if match:
+                version_string = match.groupdict()["version"]
+                mpm_version = tuple(map(int, version_string.split(".")))
+                # Is mpm too old?
+                if mpm_version >= self.mpm_min_version:
+                    up_to_date = True
+
+        return installed, mpm_version, up_to_date, error
+
+    @staticmethod
+    def pp(label: str, *args: str) -> None:
+        """Print one menu-line with the Xbar/SwiftBar dialect.
+
+        First argument is the menu-line label, separated by a pipe to all other non-
+        empty parameters, themselves separated by a space.
+
+        Skip printing of the line if label is empty.
+        """
+        if label.strip():
+            print(
+                # Do not strip the label to keep character alignements, especially in
+                # table rendering and Python tracebacks.
+                label,
+                "|",
+                *(line for line in map(methodcaller("strip"), args) if line),
+                sep=" ",
+            )
+
+    @staticmethod
+    def print_error_header() -> None:
+        """Generic header for blocking error."""
+        MPMPlugin.pp("❗️", "dropdown=false")
+        print("---")
+
+    def print_error(self, message: str | Exception, submenu: str = "") -> None:
+        """Print a formatted error message line by line.
+
+        A red, fixed-width font is used to preserve traceback and exception layout. For
+        compactness, the block message is dedented and empty lines are skipped.
+
+        Message is always casted to a string as we allow passing of exception objects
+        and have them rendered.
+        """
+        for line in map(methodcaller("rstrip"), dedent(str(message)).splitlines()):
+            if line:
+                self.pp(
+                    f"{submenu}{line}",
+                    self.error_font,
+                    "trim=false",
+                    "ansi=false",
+                    "emojize=false",
+                    "symbolize=false" if self.is_swiftbar else "",
+                )
+
+    def print_menu(self) -> None:
+        """Print the main menu."""
+        mpm_installed, _, mpm_up_to_date, error = self.check_mpm()
+        if not mpm_installed or not mpm_up_to_date:
+            self.print_error_header()
+            if error:
+                self.print_error(error)
+                print("---")
+            action_msg = "Install" if not mpm_installed else "Upgrade"
+            min_version_str = v_to_str(self.mpm_min_version)
+            self.pp(
+                f"{action_msg} mpm >= v{min_version_str}",
+                f"shell={self.python_path}",
+                "param1=-m",
+                "param2=pip",
+                "param3=install",
+                "param4=--upgrade",
+                # XXX This seems broken beyond repair. No amount of workaround works.
+                # See:
+                # https://github.com/matryer/xbar/issues/831
+                # https://github.com/swiftbar/SwiftBar/issues/308
+                # Fallback to the only version that is working on SwiftBar.
+                f'param5=\\"meta-package-manager>={min_version_str}\\"',
+                self.error_font,
+                "refresh=true",
+                "terminal=true",
+            )
+            return
+
+        # Force a sync of all local package databases.
+        run((*self.mpm_exec, "--verbosity", "ERROR", "sync"))
+
+        # Fetch outdated packages from all package managers available on the system.
+        # We defer all rendering to mpm itself so it can compute more intricate layouts.
+        process = run(
+            # We silence all errors but the CRITICAL ones. All others will be captured
+            # by mpm in --plugin-output mode and rendered back into each manager
+            # section.
+            (*self.mpm_exec, "--verbosity", "CRITICAL", "outdated", "--plugin-output"),
+            capture_output=True,
+            encoding="utf-8",
+        )
+
+        # Bail-out immediately on errors related to mpm self-execution or if mpm is
+        # not able to produce any output.
+        if process.stderr or not process.stdout:
+            self.print_error_header()
+            self.print_error(process.stderr)
+        else:
+            # Capturing the output of mpm and re-printing it will introduce an extra
+            # line returns, hence the extra rstrip() call.
+            print(process.stdout.rstrip())
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--check-mpm",
+        action="store_true",
+        help="Locate mpm on the system and check its version.",
+    )
+    args = parser.parse_args()
+
+    # Wrap plugin execution with our custom environment variables to avoid
+    # environment leaks.
+    with patch.dict("os.environ", MPMPlugin.extended_environment()):
+        plugin = MPMPlugin()
+
+        if args.check_mpm:
+            mpm_installed, mpm_version, mpm_up_to_date, error = plugin.check_mpm()
+            if not mpm_installed:
+                raise FileNotFoundError(error)
+            if not mpm_up_to_date:
+                msg = (
+                    f"{plugin.mpm_exec} is too old: "
+                    f"{v_to_str(mpm_version)} < {v_to_str(plugin.mpm_min_version)}"
+                )
+                raise ValueError(msg)
+            print(f"{' '.join(plugin.mpm_exec)} v{v_to_str(mpm_version)}")
+
+        else:
+            plugin.print_menu()
---
+++ packages/meta-package-manager/meta_package_manager-5.15.0.tar.gz/meta_package_manager-5.15.0/meta_package_manager/tests/conftest.py
@@ -0,0 +1,304 @@
+# Copyright Kevin Deldycke <kevin@deldycke.com> and contributors.
+#
+# This program is Free Software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+
+
+from __future__ import annotations
+
+from functools import partial
+from operator import attrgetter
+from pathlib import Path
+from typing import TYPE_CHECKING
+
+import pytest
+from click_extra.platforms import is_macos
+
+# Pre-load invocation helpers to be used as pytest's fixture.
+from click_extra.tests.conftest import extra_runner  # noqa: F401
+from pytest import fixture, param
+
+from meta_package_manager.cli import mpm
+from meta_package_manager.pool import manager_classes, pool
+
+if TYPE_CHECKING:
+    from _pytest.config import Config
+
+""" Fixtures, configuration and helpers for tests. """
+
+
+def pytest_addoption(parser):
+    """Add custom command line options.
+
+    Based on `Pytest's documentation examples
+    <https://docs.pytest.org/en/latest/example/simple.html#control-skipping-of-tests-according-to-command-line-option>`_.
+
+    By default, runs non-destructive tests and skips destructive ones.
+    """
+    parser.addoption(
+        "--run-destructive",
+        action="store_true",
+        default=False,
+        help="Run the subset of tests that are marked as destructive.",
+    )
+    parser.addoption(
+        "--skip-destructive",
+        action="store_true",
+        default=False,
+        help="Skip the subset of tests that are marked as destructive. "
+        "Takes precedence over --run-destructive.",
+    )
+
+    parser.addoption(
+        "--run-non-destructive",
+        action="store_true",
+        default=True,
+        help="Run the subset of tests that are marked as non-destructive.",
+    )
+    parser.addoption(
+        "--skip-non-destructive",
+        action="store_true",
+        default=False,
+        help="Skip the subset of tests that are marked as non-destructive. "
+        "Takes precedence over --run-non-destructive.",
+    )
+
+
+def pytest_configure(config):
+    """Register custom markers."""
+    config.addinivalue_line(
+        "markers",
+        "destructive: mark test as being destructive, "
+        "i.e. modifying the system they run on.",
+    )
+
+
+def solve_destructive_options(config: Config) -> tuple[bool, bool]:
+    """Solve the destructive options to determine which tests to run."""
+    run_destructive = config.getoption("--run-destructive")
+    run_non_destructive = config.getoption("--run-non-destructive")
+
+    # Skip options take precedence over run options.
+    if config.getoption("--skip-destructive"):
+        run_destructive = False
+    if config.getoption("--skip-non-destructive"):
+        run_non_destructive = False
+
+    if not run_destructive and not run_non_destructive:
+        msg = (
+            "Both destructive and non-destructive tests were skipped. No tests to run."
+        )
+        raise ValueError(msg)
+
+    return run_destructive, run_non_destructive
+
+
+def pytest_collection_modifyitems(config, items):
+    """Either skip or run destructive tests based on command line options."""
+    run_destructive, run_non_destructive = solve_destructive_options(config)
+
+    # Skip destructive tests.
+    if not run_destructive:
+        skip_destructive = pytest.mark.skip(reason="skip destructive tests")
+        for item in items:
+            if "destructive" in item.keywords:
+                item.add_marker(skip_destructive)
+
+    # Skip non-destructive tests.
+    if not run_non_destructive:
+        skip_non_destructive = pytest.mark.skip(reason="skip non-destructive tests")
+        for item in items:
+            if "destructive" not in item.keywords:
+                item.add_marker(skip_non_destructive)
+
+
+def pytest_report_header(config: Config, start_path, startdir) -> tuple[str, ...]:
+    """Display destructive options status in test report header."""
+    run_destructive = config.getoption("--run-destructive")
+    skip_destructive = config.getoption("--skip-destructive")
+    run_non_destructive = config.getoption("--run-non-destructive")
+    skip_non_destructive = config.getoption("--skip-non-destructive")
+    run_destructive_tests, run_non_destructive_tests = solve_destructive_options(config)
+    return (
+        f"--run-destructive={run_destructive}",
+        f"--skip-destructive={skip_destructive}",
+        f"--run-non-destructive={run_non_destructive}",
+        f"--skip-non-destructive={skip_non_destructive}",
+        f"Run destructive tests: {run_destructive_tests}",
+        f"Run non-destructive tests: {run_non_destructive_tests}",
+    )
+
+
+@fixture()
+def invoke(extra_runner):  # noqa: F811
+    return partial(extra_runner.invoke, mpm)
+
+
+@fixture(scope="class")
+def subcmd():
+    """Fixture used in ``test_cli_*.py`` files to set the subcommand arguments in all
+    CLI calls.
+
+    Must returns a string or an iterable of strings. Defaults to ``None``, which allows
+    tests relying on this fixture to selectively skip running.
+    """
+    return
+
+
+PACKAGE_IDS = {
+    "apm": "markdown-pdf",
+    "apt": "wget",
+    "apt-mint": "exiftool",
+    # https://github.com/Homebrew/homebrew-core/blob/master/Formula/meta-package-manager.rb
+    "brew": "meta-package-manager",
+    "cargo": "colorous",
+    "cask": "pngyu",
+    "choco": "ccleaner",
+    "composer": "illuminate/contracts",
+    "dnf": "usd",
+    "emerge": "dev-vcs/git",
+    "flatpak": "org.gnome.Dictionary",
+    "gem": "markdown",
+    "mas": "747648890",  # Telegram
+    "npm": "raven",
+    "opkg": "enigma2-hotplug",
+    "pacaur": "manjaro-hello",
+    "pacman": "manjaro-hello",
+    # https://aur.archlinux.org/packages/meta-package-manager
+    "paru": "meta-package-manager",
+    # https://pypi.org/project/meta-package-manager
+    "pip": "meta-package-manager",
+    # https://pypi.org/project/meta-package-manager
+    "pipx": "meta-package-manager",
+    "pkg": "dmg2img",
+    "scoop": "7zip",
+    "snap": "standard-notes",
+    "steamcmd": "740",
+    "vscode": "tamasfe.even-better-toml",
+    "yarn": "awesome-lint",
+    # https://aur.archlinux.org/packages/meta-package-manager
+    "yay": "meta-package-manager",
+    "yum": "usd",
+    "zypper": "git",
+}
+"""List of existing package IDs to install for each supported package manager.
+
+Only to be used for destructive tests.
+"""
+
+assert set(PACKAGE_IDS) == set(pool.all_manager_ids)
+
+# Collection of pre-computed parametrized decorators.
+
+all_managers = pytest.mark.parametrize("manager", pool.values(), ids=attrgetter("id"))
+available_managers = pytest.mark.parametrize(
+    "manager",
+    tuple(m for m in pool.values() if m.available),
+    ids=attrgetter("id"),
+)
+
+all_manager_ids = pytest.mark.parametrize("manager_id", pool.all_manager_ids)
+maintained_manager_ids = pytest.mark.parametrize(
+    "manager_id",
+    pool.maintained_manager_ids,
+)
+default_manager_ids = pytest.mark.parametrize("manager_id", pool.default_manager_ids)
+unsupported_manager_ids = pytest.mark.parametrize(
+    "manager_id",
+    pool.unsupported_manager_ids,
+)
+
+manager_classes = pytest.mark.parametrize(  # type: ignore[assignment]
+    "manager_class",
+    manager_classes,
+    ids=attrgetter("name"),
+)
+
+all_manager_ids_and_dummy_package = pytest.mark.parametrize(
+    "manager_id,package_id",
+    (param(*v, id=v[0]) for v in PACKAGE_IDS.items()),
+)
+available_managers_and_dummy_package = pytest.mark.parametrize(
+    "manager,package_id",
+    (param(m, PACKAGE_IDS[mid], id=mid) for mid, m in pool.items() if m.available),
+)
+
+
+# XXX Deactivate hack while we investigate.
+# @pytest.hookimpl(hookwrapper=True)
+def pytest_runtest_teardown(item, nextitem):
+    """Force the reset, after each subcommand test, of NPM's cached CLI path.
+
+    This hack is applied on macOS only, in which parallel tests are messing up with the
+    location of the system's NPM binary. This ends up in cryptic test suite failures,
+    as the ``npm`` CLI is moved around during execution:
+
+    .. code-block:: pytb
+        Traceback (most recent call last):
+          File ".../site-packages/click/testing.py", line 408, in invoke
+            return_value = cli.main(args=args or (), prog_name=prog_name, **extra)
+          File ".../site-packages/click_extra/commands.py", line 161, in main
+            return super().main(*args, **kwargs)
+          File ".../site-packages/click/core.py", line 1055, in main
+            rv = self.invoke(ctx)
+          File ".../site-packages/click_extra/commands.py", line 214, in invoke
+            return super().invoke(ctx)
+          File ".../site-packages/click/core.py", line 1657, in invoke
+            return _process_result(sub_ctx.command.invoke(sub_ctx))
+          File ".../site-packages/click/core.py", line 1404, in invoke
+            return ctx.invoke(self.callback, **ctx.params)
+          File ".../site-packages/click/core.py", line 760, in invoke
+            return __callback(*args, **kwargs)
+          File ".../site-packages/click/decorators.py", line 26, in new_func
+            return f(get_current_context(), *args, **kwargs)
+          File ".../meta_package_manager/cli.py", line 864, in upgrade
+            output = manager.upgrade()
+          File ".../meta_package_manager/base.py", line 863, in upgrade
+            return self.run(cli, extra_env=self.extra_env)
+          File ".../meta_package_manager/base.py", line 551, in run
+            code, output, error = run_cmd(
+          File ".../site-packages/click_extra/run.py", line 111, in run_cmd
+            process = subprocess.run(
+          File ".../python3.8/subprocess.py", line 493, in run
+            with Popen(*popenargs, **kwargs) as process:
+          File ".../python3.8/subprocess.py", line 858, in __init__
+            self._execute_child(args, executable, preexec_fn, close_fds,
+          File ".../python3.8/subprocess.py", line 1704, in _execute_child
+            raise child_exception_type(errno_num, err_msg, err_filename)
+        FileNotFoundError: [Errno 2] No such file or directory: '/usr/local/bin/npm'
+
+    Clearing the ``cli_path`` cached property forces ``mpm`` to re-search the ``npm``
+    binary on the system.
+    """
+    # Let pytest finish the test as-is.
+    outcome = yield  # noqa: F841
+
+    # Only inspect tests that are part of a suit wrapped in a class, and only on macOS.
+    if item.parent.cls and is_macos():
+        # Work around circular imports.
+        from .test_cli import CLISubCommandTests
+
+        # Only apply this hack to subcommand tests.
+        if CLISubCommandTests in item.parent.cls.mro():
+            npm = pool.get("npm")
+            # Inspect the internal dict to avoid calling the property or ``getattr``,
+            # as both will trigger the caching mechanism.
+            if "cli_path" in npm.__dict__:
+                # Reset cached cli_path to force re-detection of the CLI.
+                del npm.cli_path
+
+            # Force recompute of the CLI path.
+            cli_path = npm.cli_path
+            if cli_path:
+                assert isinstance(npm.cli_path, Path)
