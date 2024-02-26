29592fdafcdf831c956bbb5c94aa2ae110dad366
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-26 14:06:14 +0000

    Add package pytest-nunit/1.0.7/pytest-nunit-1.0.7.tar.gz

---
+++ packages/pytest-nunit/pytest-nunit-1.0.7.tar.gz/pytest-nunit-1.0.7/setup.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import codecs
+import os
+
+from setuptools import find_packages, setup
+
+
+def read(fname):
+    file_path = os.path.join(os.path.dirname(__file__), fname)
+    return codecs.open(file_path, encoding='utf-8').read()
+
+
+def getversion():
+    if 'BUILD_VERSION' in os.environ:
+        return os.environ['BUILD_VERSION']
+    else:
+        return "1.0.7"
+
+
+setup(
+    name='pytest-nunit',
+    version=getversion(),
+    author='Anthony Shaw',
+    author_email='anthonyshaw@apache.org',
+    maintainer='Anthony Shaw',
+    maintainer_email='anthonyshaw@apache.org',
+    license='MIT License',
+    url='https://github.com/pytest-dev/pytest-nunit',
+    description='A pytest plugin for generating NUnit3 test result XML output',
+    long_description=read('README.rst'),
+    packages=find_packages(exclude=('test*',)),
+    python_requires='>=3.6',
+    install_requires=['pytest>=4.6.0', 'attrs'],
+    extras_require={
+        ':python_version=="2.7"': ['enum34>=1.1.6'],
+        'dev': [
+            'xmlschema==1.0.13',
+            'pytest',
+            'pytest-cov',
+        ],
+    },
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Framework :: Pytest',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Testing',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy',
+        'Operating System :: OS Independent',
+        'License :: OSI Approved :: MIT License',
+    ],
+    entry_points={
+        'pytest11': [
+            'nunit = pytest_nunit.plugin',
+        ],
+    },
+)
