2f7b0aa33b056dbdeb16cd85c02d18a05d65fdf9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-26 04:06:37 +0000

    Add package pnorm/0.0.0b0/pnorm-0.0.0b0-py3-none-any.whl

---
+++ packages/pnorm/pnorm-0.0.0b0-py3-none-any.whl/pnorm/__init__.py
@@ -0,0 +1,12 @@
+from .credentials import PostgresCredentials  # type: ignore
+from .exceptions import *
+from .types import *
+
+...  # type: ignore
+
+from .client import PostgresClient
+from .contexts import *
+
+# https://github.com/dagster-io/dagster/blob/master/python_modules/libraries/dagster-aws/dagster_aws/redshift/resources.py
+# https://github.com/jmoiron/sqlx
+# https://jmoiron.github.io/sqlx/
