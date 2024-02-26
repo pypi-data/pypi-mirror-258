573672ecf123ddcf19d91667c2239bc5e93d84aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-26 16:05:45 +0000

    Add package pih-ws/0.1/pih-ws-0.1.tar.gz

---
+++ packages/pih-ws/pih-ws-0.1.tar.gz/pih-ws-0.1/plb_rw_ntf_build.py
@@ -0,0 +1,6 @@
+import ipih
+
+from build_tools import build
+from PolibasePersonReviewNotificationService.const import SD
+
+build(SD)
---
+++ packages/pih-ws/pih-ws-0.1.tar.gz/pih-ws-0.1/ws_build.py
@@ -0,0 +1,6 @@
+import ipih
+
+from build_tools import build
+from WSService.const import SD
+
+build(SD)
