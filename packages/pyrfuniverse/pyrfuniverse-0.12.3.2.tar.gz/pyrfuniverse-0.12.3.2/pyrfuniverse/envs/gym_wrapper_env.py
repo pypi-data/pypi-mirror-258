6b3bdbd44a8c72da6e509811d833ddc51954afd7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-26 16:05:31 +0000

    Add package pyrfuniverse/0.12.3.2/pyrfuniverse-0.12.3.2.tar.gz

---
+++ packages/pyrfuniverse/pyrfuniverse-0.12.3.2.tar.gz/pyrfuniverse-0.12.3.2/pyrfuniverse/envs/gym_wrapper_env.py
@@ -0,0 +1,37 @@
+from pyrfuniverse.envs.base_env import RFUniverseBaseEnv
+import pyrfuniverse.attributes as attr
+try:
+    import gymnasium as gym
+except ImportError:
+    print("This feature requires gymnasium, please install with `pip install gymnasium`")
+    raise
+
+
+class RFUniverseGymWrapper(RFUniverseBaseEnv, gym.Env):
+    def __init__(
+        self,
+        executable_file: str = None,
+        scene_file: str = None,
+        assets: list = [],
+        graphics: bool = True,
+        port: int = 5004,
+        proc_id=0,
+        log_level=1,
+        ext_attr: list[type(attr.BaseAttr)] = [],
+        check_version: bool = True
+    ):
+        RFUniverseBaseEnv.__init__(
+            self,
+            executable_file=executable_file,
+            scene_file=scene_file,
+            assets=assets,
+            graphics=graphics,
+            port=port,
+            proc_id=proc_id,
+            log_level=log_level,
+            ext_attr=ext_attr,
+            check_version=check_version
+        )
+
+    def close(self):
+        RFUniverseBaseEnv.close(self)
