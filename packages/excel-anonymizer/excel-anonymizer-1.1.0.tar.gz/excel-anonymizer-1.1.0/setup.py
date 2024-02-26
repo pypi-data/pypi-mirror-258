0ae955e785841f222e2b2f74db9ad730337f1415
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-26 03:07:20 +0000

    Add package excel-anonymizer/1.1.0/excel-anonymizer-1.1.0.tar.gz

---
+++ packages/excel-anonymizer/excel-anonymizer-1.1.0.tar.gz/excel-anonymizer-1.1.0/setup.py
@@ -0,0 +1,39 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with open("README.md", "r") as fh:
+    LONG_DESCRIPTION = fh.read()
+
+VERSION = '1.1.0'
+DESCRIPTION = 'Anonymizes an Excel file and synthesizes new data in its place'
+
+# Setting up
+setup(
+    name="excel-anonymizer",
+    version=VERSION,
+    author="Siddharth Bhatia",
+    author_email="",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=LONG_DESCRIPTION,
+    keywords=['python', 'excel', 'anonymization', 'security', 'data science', 'cybersecurity'],
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Education",
+        "Intended Audience :: Information Technology",
+        "Programming Language :: Python :: 3 :: Only",
+        "Operating System :: Unix",
+        "Operating System :: POSIX :: Linux",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+        "Topic :: Office/Business :: Financial :: Spreadsheet",
+    ],
+    install_requires=[
+        "presidio_analyzer",
+        "presidio_anonymizer",
+    ]
+)
