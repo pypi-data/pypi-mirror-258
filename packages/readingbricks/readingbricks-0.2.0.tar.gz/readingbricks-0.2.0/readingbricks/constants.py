d2fde731e8924b328bec8b0ffa650813798e2a0a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-26 03:07:05 +0000

    Add package readingbricks/0.2.0/readingbricks-0.2.0.tar.gz

---
+++ packages/readingbricks/readingbricks-0.2.0.tar.gz/readingbricks-0.2.0/readingbricks/constants.py
@@ -0,0 +1,12 @@
+"""
+Store global constants.
+
+Author: Nikolay Lysenko
+"""
+
+
+LANGUAGES_FOR_STEMMER = {'en': 'english', 'ru': 'russian'}
+MARKDOWN_DIR_NAME = "markdown_notes"
+TAG_COUNTS_FILE_NAME = "tag_counts.tsv"
+TAG_TO_NOTES_DB_FILE_NAME = "tag_to_notes.db"
+TF_IDF_DB_FILE_NAME = "tf_idf.db"
