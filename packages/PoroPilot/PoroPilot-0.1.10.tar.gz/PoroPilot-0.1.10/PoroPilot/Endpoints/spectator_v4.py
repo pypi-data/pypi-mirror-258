072c800e6e663f60e309c5ddd5c05b7f85206f53
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-26 14:06:07 +0000

    Add package PoroPilot/0.1.10/PoroPilot-0.1.10.tar.gz

---
+++ packages/PoroPilot/PoroPilot-0.1.10.tar.gz/PoroPilot-0.1.10/PoroPilot/Endpoints/spectator_v4.py
@@ -0,0 +1,17 @@
+from ..RequestHandler import RequestHandler
+from datetime import datetime
+
+class SpectatorApi:
+    ENDPOINTS = {
+        'BY_SUMMONER': '/lol/spectator/v4/active-games/by-summoner/{}',
+        'BY_FEATURED_GAMES': '/lol/spectator/v4/featured-games'
+    }
+
+    def __init__(self, region, api_key, use_cache=True):
+        self.request_handler = RequestHandler(api_key, region, False, use_cache=use_cache)
+
+    def by_summoner(self, summoner_id):
+        return self.request_handler.make_request(self.ENDPOINTS['BY_SUMMONER'].format(summoner_id))
+    
+    def feature_games(self):
+        return self.request_handler.make_request(self.ENDPOINTS['BY_FEATURED_GAMES'])
