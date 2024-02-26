5c6cad513a42189faa9900f8a5a4fedc5d36ee95
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-26 04:06:20 +0000

    Add package RiotWrapper/0.1.1/RiotWrapper-0.1.1.tar.gz

---
+++ packages/RiotWrapper/RiotWrapper-0.1.1.tar.gz/RiotWrapper-0.1.1/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(
+    name='RiotWrapper',
+    version='0.1.1',
+    description='Interface for Riot API',
+    url='https://github.com/anuiit/RiotWrapper',
+    author='anui',
+    author_email='',
+    keywords=['Riot', 'API', 'wrapper'],
+    license='BSD 2-clause',
+    packages=['RiotWrapper'],
+    install_requires=['requests', 'requests-cache'],
+    python_requires='>=3.6',
+    long_description="Your detailed description goes here.",
+
+)
