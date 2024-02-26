f48f19b07a38b71fb437880430c2af22daeb2e8c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-26 15:06:02 +0000

    Add package dsptoolbox/0.2.16/dsptoolbox-0.2.16.tar.gz

---
+++ packages/dsptoolbox/dsptoolbox-0.2.16.tar.gz/dsptoolbox-0.2.16/dsptoolbox/filterbanks/__init__.py
@@ -0,0 +1,43 @@
+"""
+Filter Banks
+------------
+This is a collection of useful filter banks. They use primarily the
+`FilterBank` class or some derivation from it.
+
+Perfect magnitude reconstruction:
+
+- `linkwitz_riley_crossovers()`
+
+Perfect reconstruction:
+
+- `reconstructing_fractional_octave_bands()`
+
+Psychoacoustics:
+
+- `auditory_filters_gammatone()`
+
+Other:
+
+- `qmf_crossover()`: Quadrature mirror filters crossover.
+- `fractional_octave_bands()`: Butterworth bandpass filter bank with signal
+  energy conservation.
+- `weightning_filter()`: A- or C-Weightning filter.
+
+"""
+from .filterbanks import (
+    linkwitz_riley_crossovers,
+    reconstructing_fractional_octave_bands,
+    auditory_filters_gammatone,
+    fractional_octave_bands,
+    qmf_crossover,
+    weightning_filter,
+)
+
+__all__ = [
+    "linkwitz_riley_crossovers",
+    "reconstructing_fractional_octave_bands",
+    "fractional_octave_bands",
+    "auditory_filters_gammatone",
+    "qmf_crossover",
+    "weightning_filter",
+]
