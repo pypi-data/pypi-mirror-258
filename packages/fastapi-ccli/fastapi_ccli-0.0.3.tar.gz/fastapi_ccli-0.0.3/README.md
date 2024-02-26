408f2713d8771953e864b501ac9d5baa97a1f323
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-26 03:08:14 +0000

    Add package fastapi-ccli/0.0.3/fastapi_ccli-0.0.3.tar.gz

---
+++ packages/fastapi-ccli/fastapi_ccli-0.0.3.tar.gz/fastapi_ccli-0.0.3/README.md
@@ -0,0 +1,17 @@
+# FastAPI Project Clone CLI
+
+> 此程序使用 [Typer](https://typer.tiangolo.com/) 创建
+
+## 使用
+
+pip 安装：
+
+```shell
+pip install fastapi-ccli
+```
+
+查看使用帮助
+
+```shell
+fastapi-ccli --help
+```
---
+++ packages/fastapi-ccli/fastapi_ccli-0.0.3.tar.gz/fastapi_ccli-0.0.3/fastapi_ccli/utils/get_country.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+from typing import Union
+
+from fastapi_ccli.utils import request
+
+
+def get_current_country(ip: Union[str, None]) -> str:
+    """
+    Get the current country by ip
+
+    :param ip:
+    :return:
+    """
+    proxy = {'http': None, 'https': None}  # extra
+    try:
+        if ip:
+            rp = request.get(f'https://ip.useragentinfo.com/json?ip={ip}', proxies=proxy).json()['short_name']
+        else:
+            rp = request.get('https://ip.useragentinfo.com/json?ip=', proxies=proxy).json()['short_name']
+    except Exception:  # noqa
+        rp = 'Unknown'
+
+    return rp
