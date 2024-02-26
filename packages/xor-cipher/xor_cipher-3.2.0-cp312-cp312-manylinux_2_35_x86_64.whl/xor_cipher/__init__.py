7565392525f4b3441a5276f34ce1e2593eacab27
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-26 03:08:14 +0000

    Add package xor-cipher/3.2.0/xor_cipher-3.2.0-cp312-cp312-manylinux_2_35_x86_64.whl

---
+++ packages/xor-cipher/xor_cipher-3.2.0-cp312-cp312-manylinux_2_35_x86_64.whl/xor_cipher/__init__.py
@@ -0,0 +1,51 @@
+"""Simple, reusable and optimized XOR ciphers in Python."""
+
+__description__ = "Simple, reusable and optimized XOR ciphers in Python."
+__url__ = "https://github.com/xor-cipher/xor-cipher"
+
+__title__ = "xor_cipher"
+__author__ = "nekitdev, RealistikDash"
+__license__ = "MIT"
+__version__ = "3.2.0"
+
+from xor_cipher.core import (
+    DEFAULT_ENCODING,
+    DEFAULT_ERRORS,
+    cyclic_xor,
+    cyclic_xor_in_place,
+    cyclic_xor_in_place_unsafe,
+    cyclic_xor_string,
+    cyclic_xor_string_unsafe,
+    cyclic_xor_unsafe,
+    xor,
+    xor_in_place,
+    xor_string,
+)
+
+__all__ = (
+    "DEFAULT_ENCODING",
+    "DEFAULT_ERRORS",
+    "cyclic_xor",
+    "cyclic_xor_in_place",
+    "cyclic_xor_in_place_unsafe",
+    "cyclic_xor_unsafe",
+    "cyclic_xor_string",
+    "cyclic_xor_string_unsafe",
+    "xor",
+    "xor_in_place",
+    "xor_string",
+)
+
+
+try:
+    from xor_cipher.extension import (
+        cyclic_xor,
+        cyclic_xor_in_place,
+        cyclic_xor_in_place_unsafe,
+        cyclic_xor_unsafe,
+        xor,
+        xor_in_place,
+    )
+
+except ImportError:
+    pass
