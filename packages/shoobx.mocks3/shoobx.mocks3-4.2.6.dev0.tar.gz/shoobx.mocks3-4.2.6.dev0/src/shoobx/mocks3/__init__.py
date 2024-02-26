5b3c69600008ae342f6c500a508aac810efcebf8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-26 03:07:28 +0000

    Add package shoobx.mocks3/4.2.6.dev0/shoobx.mocks3-4.2.6.dev0.tar.gz

---
+++ packages/shoobx.mocks3/shoobx.mocks3-4.2.6.dev0.tar.gz/shoobx.mocks3-4.2.6.dev0/src/shoobx/mocks3/__init__.py
@@ -0,0 +1,34 @@
+###############################################################################
+#
+# Copyright 2016 by Shoobx, Inc.
+#
+###############################################################################
+"""Shoobx Mock S3
+"""
+from pkg_resources import get_distribution
+
+__version__ = get_distribution("shoobx.mocks3").version
+
+import importlib
+
+import moto.backends
+
+# Insert our custom backend into moto.
+from moto.backends import BACKENDS
+
+from .models import s3_backends  # noqa
+
+
+def _import_backend(module_name, backends_name):
+    if module_name.startswith("shoobx."):
+        module = importlib.import_module(module_name)
+    else:
+        module = importlib.import_module("moto." + module_name)
+    return getattr(module, backends_name)
+
+
+# Monkey patch _import_backend to escape "moto." pkg prefixing
+# for shoobx backends
+moto.backends._import_backend = _import_backend
+
+BACKENDS["s3-sbx"] = ("shoobx.mocks3", "s3_backends")
