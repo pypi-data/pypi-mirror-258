62a74fd2c2dd6069b24c4b9a37d37ac716ee8780
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-26 16:05:28 +0000

    Add package pih-auto/0.1/pih-auto-0.1.tar.gz

---
+++ packages/pih-auto/pih-auto-0.1.tar.gz/pih-auto-0.1/auto_build.py
@@ -0,0 +1,9 @@
+import ipih
+
+from pih import A
+from build_tools import build
+from AutomationService.const import SD
+
+build(
+    SD, requires_packages=(A.PTH_FCD_DIST.NAME(A.CT_SR.MOBILE_HELPER.standalone_name),) # type: ignore
+)
