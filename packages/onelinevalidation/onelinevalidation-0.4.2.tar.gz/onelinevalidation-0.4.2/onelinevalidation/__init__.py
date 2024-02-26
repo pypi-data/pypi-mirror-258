70af45074ba3ab5b11dda68fa14c638a818d7464
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-26 04:06:20 +0000

    Add package onelinevalidation/0.4.2/onelinevalidation-0.4.2.tar.gz

---
+++ packages/onelinevalidation/onelinevalidation-0.4.2.tar.gz/onelinevalidation-0.4.2/onelinevalidation/__init__.py
@@ -0,0 +1,6 @@
+
+__all__ = ['onelinevalidation']
+
+from onelinevalidation.onelinevalidation import custom_validate
+from onelinevalidation.onelinevalidation import sanitize_input
+from onelinevalidation.onelinevalidation import validate_form
