65688170a2c227b109214daf2add6fbc2446f40d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-26 04:06:39 +0000

    Add package easy-cloud/0.1.4/easy_cloud-0.1.4-py3-none-any.whl

---
+++ packages/easy-cloud/easy_cloud-0.1.4-py3-none-any.whl/easy_cloud/easy_cloud.py
@@ -0,0 +1,17 @@
+class EasyCloud:
+    def __init__(self):
+        '''
+        First python package
+        '''
+
+    def add(self, numberA, numberB):
+        '''
+        Add two numbers
+        '''
+        return numberA + numberB
+
+    def subtract(self, numberA, numberB):
+        '''
+        Subtract two numbers
+        '''
+        return numberA - numberB
