7c3de4000bec1af5ce6fcb493228f8edbfea02f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-26 04:06:08 +0000

    Add package inka2/2.4.0/inka2-2.4.0.tar.gz

---
+++ packages/inka2/inka2-2.4.0.tar.gz/inka2-2.4.0/tests/test_cli.py
@@ -0,0 +1,20 @@
+import pytest
+from click.testing import CliRunner
+
+from inka2.cli import ROOT_DIR, cli, get_notes_from_file
+
+
+@pytest.mark.integration
+def test_hello():
+    runner = CliRunner()
+    result = runner.invoke(
+        cli, ["collect", f"{ROOT_DIR}/tests/resources/test_inka_data.md"]
+    )
+    assert result.exit_code == 0
+    print(result.output)
+    # assert 'Hello, Test!' in result.output
+
+
+def test_get_notes_from_file():
+    notes = get_notes_from_file(f"{ROOT_DIR}/./tests/resources/test_inka_data.md")  # noqa
+    _ = None
