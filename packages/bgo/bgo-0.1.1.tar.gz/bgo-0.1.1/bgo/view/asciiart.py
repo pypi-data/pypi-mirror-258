bf41742bf456b9a66b2cd6eac07851a329bca16f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-26 03:07:27 +0000

    Add package bgo/0.1.1/bgo-0.1.1.tar.gz

---
+++ packages/bgo/bgo-0.1.1.tar.gz/bgo-0.1.1/bgo/view/asciiart.py
@@ -0,0 +1,19 @@
+clear_sunny = " \\ | / \n" " - O -  \n" " / | \\ "
+
+clear_night = "  .   * \n" "*    . O\n" ". . *  ."
+partial_clouds = " \\ /(  )\n" "- O(    )\n" " / (  )  "
+
+clouds = " ( )()_ \n" "(      )\n" " (  )() "
+
+drizzle = "'  '    '\n" " '   ' ' \n" "'    '   '"
+
+rain = "' '' ' '\n" "'' ' ' '\n" "' ' '' '"
+
+
+snow = "* '* ' *\n" "'* ' * '\n" "*' * ' *"
+
+fog = "-- _ --\n" "-__-- -\n" "- _--__"
+
+thunderstorm = "c__ -- _\n" "-- _-c__\n" "c-- c_ -"
+
+everything_else = "c__ ''' '\n" "' '' c___\n" "c__ ' 'c_"
