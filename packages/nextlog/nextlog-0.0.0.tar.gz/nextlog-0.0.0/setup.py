2539e65e03cb3891f6c37801523baacae1399f0d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-26 04:06:37 +0000

    Add package nextlog/0.0.0/nextlog-0.0.0.tar.gz

---
+++ packages/nextlog/nextlog-0.0.0.tar.gz/nextlog-0.0.0/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+description = 'Project in development phase.'
+long_description = 'Documentation coming soon.'
+
+setup(
+    name='nextlog',
+    version='0.0.0',
+    description=description,
+    long_description=long_description,
+    author='Sourav',
+    author_email='imsrv2k@gmail.com',
+    packages=find_packages(),
+    install_requires=[
+        # list of dependencies
+    ],
+)
