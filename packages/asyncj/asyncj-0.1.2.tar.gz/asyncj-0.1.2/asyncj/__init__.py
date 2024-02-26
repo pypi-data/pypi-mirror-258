1740c52247c002bae4c9f2da8075bba2e9c1be21
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-26 14:06:23 +0000

    Add package asyncj/0.1.2/asyncj-0.1.2.tar.gz

---
+++ packages/asyncj/asyncj-0.1.2.tar.gz/asyncj-0.1.2/asyncj/__init__.py
@@ -0,0 +1,11 @@
+"""
+:authors: VengDevs
+:licence: Apache License, Version 2.0
+
+:copyright: (c) 2024 VengDevs
+"""
+
+from .asyncj import AsyncJson
+
+__author__ = 'VengDevs'
+__version__ = '0.1.0'
---
+++ packages/asyncj/asyncj-0.1.2.tar.gz/asyncj-0.1.2/setup.py
@@ -0,0 +1,35 @@
+from io import open
+from setuptools import setup
+
+"""
+:authors: VengDevs
+:licence: Apache License, Version 2.0
+:copyright: (c) 2024 VengDevs
+"""
+
+version = '0.1.2'
+
+setup(
+    name="asyncj",
+    version=version,
+    author='VengDevs',
+    description='Python module for fast asynchronous work with JSON files',
+    url='https://github.com/Vengryyy/AsyncJ',
+    license='Apache License, Version 2.0',
+    packages=['asyncj'],
+    install_requires=['aiofiles', 'ujson'],
+    python_requires='>=3.8',
+    classifiers=[
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: OS Independent',
+        'Intended Audience :: End Users/Desktop',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12'
+    ]
+)
