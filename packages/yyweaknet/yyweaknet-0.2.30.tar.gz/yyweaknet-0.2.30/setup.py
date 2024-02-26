e063684a4cb359973926ccfacd8d25a87ad789ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-26 15:05:53 +0000

    Add package yyweaknet/0.2.30/yyweaknet-0.2.30.tar.gz

---
+++ packages/yyweaknet/yyweaknet-0.2.30.tar.gz/yyweaknet-0.2.30/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="yyweaknet",  # Replace with your own username
+    version="0.2.30",
+    author="lizhidong",
+    author_email="mblzds@gmail.com",
+    description="weaknetwork test tools",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="",
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.7',
+)
