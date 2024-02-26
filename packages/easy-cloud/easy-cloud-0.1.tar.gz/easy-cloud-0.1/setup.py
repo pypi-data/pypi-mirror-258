886d729fc1ad11dc893868f0d6bc8d9fc880fcc7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-26 04:08:09 +0000

    Add package easy-cloud/0.1/easy-cloud-0.1.tar.gz

---
+++ packages/easy-cloud/easy-cloud-0.1.tar.gz/easy-cloud-0.1/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name='easy-cloud',
+    version='0.1',
+    packages=find_packages(),
+    url='https://github.com/sanchit3110/easy-cloud',
+    author='Sanchit Lodha',
+    author_email='sanchit.lodha@gmail.com',
+    description='Python package to make cloud operations easy',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        # Add your dependencies here
+    ],
+)
