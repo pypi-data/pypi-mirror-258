d229408b1d2e6bdaba46c7e4ab65cc83d4e568ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-26 03:08:06 +0000

    Add package logzio-python-handler/4.1.2/logzio-python-handler-4.1.2.tar.gz

---
+++ packages/logzio-python-handler/logzio-python-handler-4.1.2.tar.gz/logzio-python-handler-4.1.2/setup.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+
+from setuptools import find_packages, setup
+
+setup(
+    name="logzio-python-handler",
+    version='4.1.2',
+    description="Logging handler to send logs to your Logz.io account with bulk SSL",
+    keywords="logging handler logz.io bulk https",
+    author="roiravhon",
+    maintainer="tamir-michaeli",
+    mail="tamir.michaeli@logz.io",
+    url="https://github.com/logzio/logzio-python-handler/",
+    license="Apache License 2",
+    packages=find_packages(),
+    install_requires=[
+        "requests>=2.27.0",
+        "protobuf>=3.20.2"
+    ],
+    extras_require={
+        "opentelemetry-logging": ["opentelemetry-instrumentation-logging==0.39b0"]
+    },
+    test_requires=[
+        "future"
+    ],
+    include_package_data=True,
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python :: 3.9'
+    ]
+)
