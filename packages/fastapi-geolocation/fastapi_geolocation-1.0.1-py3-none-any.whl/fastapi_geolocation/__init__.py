245fd39eccd73d0473c6eedb2eeb7a7d4ce489ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-26 14:06:20 +0000

    Add package fastapi-geolocation/1.0.1/fastapi_geolocation-1.0.1-py3-none-any.whl

---
+++ packages/fastapi-geolocation/fastapi_geolocation-1.0.1-py3-none-any.whl/fastapi_geolocation/__init__.py
@@ -0,0 +1,5 @@
+"""A middleware package that provides geolocation features by integrating IP-based location lookup."""
+
+__version__ = "1.0.1"
+
+from .middleware import GeoIPMiddleware as GeoIPMiddleware
