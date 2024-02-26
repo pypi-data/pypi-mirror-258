361d50a2f5372add64e70b9ddb06ca8be240996e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-26 14:06:54 +0000

    Add package pyonwater/0.3.6/pyonwater-0.3.6.tar.gz

---
+++ packages/pyonwater/pyonwater-0.3.6.tar.gz/pyonwater-0.3.6/pyonwater/models/units.py
@@ -0,0 +1,28 @@
+"""Supported EOW units."""
+
+from __future__ import annotations
+
+from enum import Enum
+
+
+class EOWUnits(str, Enum):
+    """Enum of supported EOW units."""
+
+    UNIT_GAL = "GAL"
+    UNIT_100_GAL = "100 GAL"
+    UNIT_10_GAL = "10 GAL"
+    UNIT_CF = "CF"
+    UNIT_CUBIC_FEET = "CUBIC_FEET"
+    UNIT_CCF = "CCF"
+    UNIT_KGAL = "KGAL"
+    UNIT_CM = "CM"
+    UNIT_CUBIC_METER = "CUBIC_METER"
+    UNIT_LITER = "LITER"
+
+
+class NativeUnits(str, Enum):
+    """Enum of supported native units."""
+
+    GAL = "gal"
+    CF = "cf"
+    CM = "cm"
