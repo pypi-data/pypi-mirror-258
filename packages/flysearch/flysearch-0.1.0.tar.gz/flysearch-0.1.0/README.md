acf17626d75e92a9502bfe1482aa4e72d8ebcac7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-26 03:08:05 +0000

    Add package flysearch/0.1.0/flysearch-0.1.0.tar.gz

---
+++ packages/flysearch/flysearch-0.1.0.tar.gz/flysearch-0.1.0/README.md
@@ -0,0 +1,10 @@
+# Flight Search API Wrapper
+
+This Python project provides a convenient wrapper around the Flight Search API, enabling users to search for flight offers based on various criteria such as departure and arrival locations, date ranges, passenger birth dates, and more.
+
+## Features
+
+- Search flights by departure and arrival IATA codes.
+- Filter flights based on one-way trips, departure and arrival dates.
+- Sort flight offers by price, name ascending, and name descending.
+- Support for specifying passenger birth dates for age-specific searches.
---
+++ packages/flysearch/flysearch-0.1.0.tar.gz/flysearch-0.1.0/tests/test_flight_offer.py
@@ -0,0 +1,26 @@
+import unittest
+from datetime import date
+
+from flysearch.flight_offer import FlightOffer
+
+
+class TestFlightOffer(unittest.TestCase):
+    def test_from_dict(self):
+        test_data = {
+            "Panstwo": "TestCountry",
+            "Nazwa": "TestName",
+            "Klucz": "TestKey",
+            "TerminWyjazdu": "2024-05-01T00:00:00Z",
+            "Cena": 1000,
+            "DataLayerV4": {"currency": "USD"},
+        }
+        expected_result = FlightOffer(
+            country="TestCountry",
+            name="TestName",
+            key="TestKey",
+            departure_date=date(2024, 5, 1),
+            price=1000,
+            currency="USD",
+        )
+        result = FlightOffer.from_dict(test_data)
+        self.assertEqual(result, expected_result)
