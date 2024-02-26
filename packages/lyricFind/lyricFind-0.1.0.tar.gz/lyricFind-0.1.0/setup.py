fa6210d44a7edf230ae127e1bd993fb5ec6286b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-26 14:06:20 +0000

    Add package lyricFind/0.1.0/lyricFind-0.1.0.tar.gz

---
+++ packages/lyricFind/lyricFind-0.1.0.tar.gz/lyricFind-0.1.0/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+setup(
+name='lyricFind',
+version='0.1.0',
+author='Reza',
+author_email='neobyteid@gmail.com',
+description='Simplest find a lyric of the song just type part of the lyric that make your life easier',
+packages=find_packages(),
+classifiers=[
+'Programming Language :: Python :: 3',
+'License :: OSI Approved :: MIT License',
+'Operating System :: OS Independent',
+],
+python_requires='>=3.6',
+)
