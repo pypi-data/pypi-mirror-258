814ba3193119342ead85f44b1e5690dab67f413f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-26 14:06:14 +0000

    Add package cwordtm/0.5.2/cwordtm-0.5.2.tar.gz

---
+++ packages/cwordtm/cwordtm-0.5.2.tar.gz/cwordtm-0.5.2/cwordtm/images/__init__.py
@@ -0,0 +1,14 @@
+# __init__.py
+#
+# Folder 'images'
+#
+# Copyright (c) 2022-2024 CWordTM Project 
+# Author: Johnny Cheng <drjohnnycheng@gmail.com>
+#
+# Updated: 17 February 2024
+#
+# URL: https://github.com/drjohnnycheng/cwordtm.git
+# For license information, see LICENSE.TXT
+
+"""The folder stores an image mask ('heart.jpg') for plotting a wordcloud.
+"""
