dea450fb1277820cacdfc06bfe5a8f24f47ce63d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-26 14:06:11 +0000

    Add package kucoin-python/1.0.17/kucoin_python-1.0.17-py3-none-any.whl

---
+++ packages/kucoin-python/kucoin_python-1.0.17-py3-none-any.whl/kucoin/example_locale.py
@@ -0,0 +1,6 @@
+from kucoin.client import Margin
+
+client = Margin(key='65dbf24d9ab88600016b269d', secret='1394913f-3b11-4a89-bf10-bd03ce9dc07e', passphrase='Aa12345678')
+
+res=client.get_interest_rates("BTC")
+print(res)
