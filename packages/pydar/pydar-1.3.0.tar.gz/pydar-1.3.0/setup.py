71b358bac3875844003c6ca7aef3e412a7a4e28d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-26 14:06:19 +0000

    Add package pydar/1.3.0/pydar-1.3.0.tar.gz

---
+++ packages/pydar/pydar-1.3.0.tar.gz/pydar-1.3.0/setup.py
@@ -0,0 +1,54 @@
+# -*- coding: utf-8 -*-
+
+# Python Package Setup
+from setuptools import setup, find_namespace_packages
+
+VERSION="1.3.0"
+DESCRIPTION="A Python package to access, download, view, and manipulate Cassini RADAR images"
+
+with open("README.md", "r") as f:
+	long_description_readme = f.read()
+
+setup(
+	name="pydar",
+	version=VERSION,
+	description=DESCRIPTION,
+	long_description=long_description_readme,
+	long_description_content_type='text/markdown',
+	url="https://github.com/unaschneck/pydar",
+	download_url="https://github.com/unaschneck/pydar/archive/refs/tags/v{0}.tar.gz".format(VERSION),
+	author="Una Schneck (unaschneck), Cora Schneck (cyschneck)",
+	keywords=[],
+	license="MIT",
+	classifiers=[
+		"Development Status :: 4 - Beta",
+		"Intended Audience :: Developers",
+		"Intended Audience :: Education",
+		"Intended Audience :: Science/Research",
+		"License :: OSI Approved :: MIT License",
+		"Programming Language :: Python",
+		"Programming Language :: Python :: 3.9",
+		"Programming Language :: Python :: 3.10",
+		"Programming Language :: Python :: 3.11",
+		"Programming Language :: Python :: 3.12",
+		"Intended Audience :: Education",
+		"Intended Audience :: Science/Research",
+		"Topic :: Scientific/Engineering :: Physics",
+		"Topic :: Scientific/Engineering :: Image Processing",
+		"Topic :: Scientific/Engineering :: Visualization",
+		"Topic :: Scientific/Engineering :: Astronomy"
+	],
+	packages=find_namespace_packages(include=['pydar', 'pydar.*']),
+	include_package_data=True,
+	install_requires=[
+			"beautifulsoup4>=4.11.1",
+			"matplotlib>=3.1.0",
+			"pandas>=1.5.2",
+			"pdr>=0.7.3",
+			"pyproj>=3.4.1",
+			"pytest>=7.2.2",
+			"rasterio>=1.3.6",
+			"urllib3>=1.26.18"
+			],
+	python_requires='>=3.9'
+)
