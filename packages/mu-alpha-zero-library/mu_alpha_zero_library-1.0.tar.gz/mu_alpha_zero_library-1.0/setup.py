d1741e5f13dd99d34b26e9f989dceffbdddc60c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-26 04:06:20 +0000

    Add package mu-alpha-zero-library/1.0/mu_alpha_zero_library-1.0.tar.gz

---
+++ packages/mu-alpha-zero-library/mu_alpha_zero_library-1.0.tar.gz/mu_alpha_zero_library-1.0/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+from setuptools.command.install import install
+import subprocess
+
+
+def get_requirements():
+    with open("requirements.txt", "r") as file:
+        return file.read().strip().split("\n")
+
+
+
+
+
+setup(
+    name="mu_alpha_zero_library",
+    version="1.0",
+    description="Library for running and training MuZero and AlphaZero models.",
+    author="Skyr",
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    package_data={
+        "mu_alpha_zero": ["*.txt", "*.root"]
+    }
+
+)
