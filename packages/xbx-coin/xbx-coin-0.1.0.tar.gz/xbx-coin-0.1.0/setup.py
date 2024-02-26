0e1476dd9e80b1f46f4f3b804b7a3a95dcc5d3d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-26 16:06:01 +0000

    Add package xbx-coin/0.1.0/xbx-coin-0.1.0.tar.gz

---
+++ packages/xbx-coin/xbx-coin-0.1.0.tar.gz/xbx-coin-0.1.0/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+setup(
+    name='xbx-coin',
+    version='0.1.0',
+    description='xbx-coin',
+    author='xbx',
+    install_requires=[
+        'ccxt==2.2.40',
+        'matplotlib==3.5.1',
+        'dataframe_image==0.1.5',
+        'DrissionPage==3.2.34',
+        'joblib==1.1.0',
+        'numpy==1.22.4',
+        'pandas==1.5.3',
+        'requests==2.27.1',
+        'lxml==4.8.0',
+        'tqdm==4.65.0',
+        'ntplib==0.4.0',
+        'httpx==0.26.0',
+        'bs4==0.0.2'
+    ],
+)
