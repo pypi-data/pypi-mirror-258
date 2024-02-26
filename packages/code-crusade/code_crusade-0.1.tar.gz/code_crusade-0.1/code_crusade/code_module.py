3773a3333ee57ae279ed043a4957583aa650a1b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-26 04:06:20 +0000

    Add package code-crusade/0.1/code_crusade-0.1.tar.gz

---
+++ packages/code-crusade/code_crusade-0.1.tar.gz/code_crusade-0.1/code_crusade/code_module.py
@@ -0,0 +1,2 @@
+def submit():
+    print("Hello World!")
---
+++ packages/code-crusade/code_crusade-0.1.tar.gz/code_crusade-0.1/setup.py
@@ -0,0 +1,9 @@
+# code_crusade/setup.py
+
+from setuptools import setup, find_packages
+
+setup(
+    name='code_crusade',
+    version='0.1',
+    packages=find_packages()
+)
