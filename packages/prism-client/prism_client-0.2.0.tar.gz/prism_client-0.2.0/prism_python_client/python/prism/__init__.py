1e09879394b2604c1f27007ceae00fe192e51f64
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-26 15:05:23 +0000

    Add package prism-client/0.2.0/prism_client-0.2.0.tar.gz

---
+++ packages/prism-client/prism_client-0.2.0.tar.gz/prism_client-0.2.0/prism_python_client/python/prism/__init__.py
@@ -0,0 +1,41 @@
+from threading import Thread, Event
+from typing import List
+from prism._prism import Client as RustClient, Wavelet, Photon
+
+DEFAULT_PING_RATE = 50
+
+
+def ping_loop(client, rate, event):
+    while True:
+        event.wait(rate)
+        if event.is_set():
+            break
+        client.ping()
+
+
+class Client:
+    def __init__(self, addr, callable, ping_rate=DEFAULT_PING_RATE):
+        self._client = RustClient(addr, callable)
+        self._event = Event()
+        Thread(target=ping_loop, args=(self._client, ping_rate, self._event))
+
+    def __del__(self):
+        self._event.set()
+
+    def add_beam(self, beam: str):
+        self._client.add_beam(beam)
+
+    def transmissions(self) -> List[str]:
+        self._client.transmissions()
+
+    def subscribe(self, beam: str, index: int | None = None):
+        self._client.subscribe(beam, index)
+
+    def unsubscribe(self, beam: str):
+        self._client.unsubscribe(beam)
+
+    def emit(self, beam: str, payload: bytes):
+        self._client.emit(beam, payload)
+
+
+__all__ = [Client, Wavelet, Photon]
