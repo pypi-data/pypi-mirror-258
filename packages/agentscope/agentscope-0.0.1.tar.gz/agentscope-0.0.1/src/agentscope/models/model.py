18d618017d3fa5101e2334b8f6d499d83fcd8bfc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-26 16:05:39 +0000

    Add package agentscope/0.0.1/agentscope-0.0.1.tar.gz

---
+++ packages/agentscope/agentscope-0.0.1.tar.gz/agentscope-0.0.1/src/agentscope/models/model.py
@@ -0,0 +1,257 @@
+# -*- coding: utf-8 -*-
+"""The configuration file should contain one or a list of model configs,
+and each model config should follow the following format.
+
+.. code-block:: python
+
+    {
+        "config_name": "{config_name}",
+        "model_type": "openai" | "post_api" | ...,
+        ...
+    }
+
+After that, you can specify model by {config_name}.
+
+Note:
+    The parameters for different types of models are different. For OpenAI API,
+    the format is:
+
+        .. code-block:: python
+
+            {
+                "config_name": "{id of your model}",
+                "model_type": "openai",
+                "model_name": "{model_name_for_openai, e.g. gpt-3.5-turbo}",
+                "api_key": "{your_api_key}",
+                "organization": "{your_organization, if needed}",
+                "client_args": {
+                    # ...
+                },
+                "generate_args": {
+                    # ...
+                }
+            }
+
+
+    For Post API, toking huggingface inference API as an example, its format
+    is:
+
+        .. code-block:: python
+
+            {
+                "config_name": "{config_name}",
+                "model_type": "post_api",
+                "api_url": "{api_url}",
+                "headers": {"Authorization": "Bearer {API_TOKEN}"},
+                "max_length": {max_length_of_model},
+                "timeout": {timeout},
+                "max_retries": {max_retries},
+                "generate_args": {
+                    "temperature": 0.5,
+                    # ...
+                }
+            }
+
+"""
+import inspect
+import time
+from abc import ABCMeta
+from functools import wraps
+from typing import Sequence, Any, Callable
+import json
+
+from loguru import logger
+
+from ..file_manager import file_manager
+from ..utils.tools import _get_timestamp
+from ..constants import _DEFAULT_MAX_RETRIES
+from ..constants import _DEFAULT_RETRY_INTERVAL
+
+
+class ModelResponse:
+    """Encapsulation of data returned by the model.
+
+    The main purpose of this class is to align the return formats of different
+    models and act as a bridge between models and agents.
+    """
+
+    def __init__(
+        self,
+        text: str = None,
+        embedding: Sequence = None,
+        image_urls: Sequence[str] = None,
+        raw: dict = None,
+    ) -> None:
+        self._text = text
+        self._embedding = embedding
+        self._image_urls = image_urls
+        self._raw = raw
+
+    @property
+    def text(self) -> str:
+        """Text field."""
+        return self._text
+
+    @property
+    def embedding(self) -> Sequence:
+        """Embedding field."""
+        return self._embedding
+
+    @property
+    def image_urls(self) -> Sequence[str]:
+        """Image URLs field."""
+        return self._image_urls
+
+    @property
+    def raw(self) -> dict:
+        """Raw dictionary field."""
+        return self._raw
+
+    def __str__(self) -> str:
+        serialized_fields = {
+            "text": self.text,
+            "embedding": self.embedding,
+            "image_urls": self.image_urls,
+            "raw": self.raw,
+        }
+        return json.dumps(serialized_fields, indent=4)
+
+
+def _response_parse_decorator(
+    model_call: Callable,
+) -> Callable:
+    """A decorator for parsing the response of model call. It will take
+    `parse_func`, `fault_handler` and `max_retries` as arguments. The
+    detailed process is as follows:
+
+        1. If `parse_func` is provided, then the response will be parsed first.
+
+        2. If the parsing fails (throws an exception), then response generation
+        will be repeated for `max_retries` times and parsed again.
+
+        3. After `max_retries` times, if the parsing still fails, then if
+        `fault_handler` is provided, the response will be processed by
+        `fault_handler`.
+    """
+
+    # check if the decorated `model_call` function uses the default
+    # arguments of this decorator.
+    parameters = inspect.signature(model_call).parameters
+
+    for name in parameters.keys():
+        if name in ["parse_func", "max_retries"]:
+            logger.warning(
+                f"The argument {name} is used by the decorator, "
+                f"which will not be passed to the model call "
+                f"function.",
+            )
+
+    @wraps(model_call)
+    def checking_wrapper(self: Any, *args: Any, **kwargs: Any) -> dict:
+        # Step1: Extract parse_func and fault_handler
+        parse_func = kwargs.pop("parse_func", None)
+        fault_handler = kwargs.pop("fault_handler", None)
+        max_retries = kwargs.pop("max_retries", None) or _DEFAULT_MAX_RETRIES
+
+        # Step2: Call the model and parse the response
+        # Return the response directly if parse_func is not provided
+        if parse_func is None:
+            return model_call(self, *args, **kwargs)
+
+        # Otherwise, try to parse the response
+        response = None
+        for itr in range(1, max_retries + 1):
+            # Call the model
+            response = model_call(self, *args, **kwargs)
+
+            # Parse the response if needed
+            try:
+                return parse_func(response)
+            except Exception as e:
+                logger.warning(
+                    f"Fail to parsing response: "
+                    f"{response}.\n Exception: {e}, "
+                    f"\t Attempt {itr} / {max_retries}",
+                )
+                time.sleep(_DEFAULT_RETRY_INTERVAL * itr)
+
+        if fault_handler is not None and callable(fault_handler):
+            return fault_handler(response)
+        else:
+            raise ValueError(
+                f"fail to parsing response with: "
+                f"{parse_func.__name__}. \n  "
+                f"\t Attempts fails {max_retries} times",
+            )
+
+    return checking_wrapper
+
+
+class _ModelWrapperMeta(ABCMeta):
+    """A meta call to replace the model wrapper's __call__ function with
+    wrapper about error handling."""
+
+    def __new__(mcs, name: Any, bases: Any, attrs: Any) -> Any:
+        if "__call__" in attrs:
+            attrs["__call__"] = _response_parse_decorator(attrs["__call__"])
+        return super().__new__(mcs, name, bases, attrs)
+
+    def __init__(cls, name: Any, bases: Any, attrs: Any) -> None:
+        if not hasattr(cls, "registry"):
+            cls.registry = {}
+            cls.type_registry = {}
+        else:
+            cls.registry[name] = cls
+            if hasattr(cls, "model_type"):
+                cls.type_registry[cls.model_type] = cls
+        super().__init__(name, bases, attrs)
+
+
+class ModelWrapperBase(metaclass=_ModelWrapperMeta):
+    """The base class for model wrapper."""
+
+    def __init__(self, config_name: str, **kwargs: Any) -> None:
+        """Base class for model wrapper.
+
+        All model wrappers should inherit this class and implement the
+        `__call__` function.
+
+        Args:
+            config_name (`str`):
+                The id of the model, which is used to extract configuration
+                from the config file.
+        """
+        self.config_name = config_name
+        logger.info(
+            f"Initialize model [{config_name}] with config:\n"
+            f"{json.dumps(kwargs, indent=2)}",
+        )
+
+    def __call__(self, *args: Any, **kwargs: Any) -> ModelResponse:
+        """Processing input with the model."""
+        raise NotImplementedError(
+            f"Model Wrapper [{type(self).__name__}]"
+            f" is missing the  the required `__call__`"
+            f" method.",
+        )
+
+    def _save_model_invocation(
+        self,
+        arguments: dict,
+        json_response: Any,
+    ) -> None:
+        """Save model invocation."""
+        model_class = self.__class__.__name__
+        timestamp = _get_timestamp("%Y%m%d-%H%M%S")
+
+        invocation_record = {
+            "model_class": model_class,
+            "timestamp": timestamp,
+            "arguments": arguments,
+            "json_response": json_response,
+        }
+
+        file_manager.save_api_invocation(
+            f"model_{model_class}_{timestamp}",
+            invocation_record,
+        )
---
+++ packages/agentscope/agentscope-0.0.1.tar.gz/agentscope-0.0.1/src/agentscope/models/openai_model.py
@@ -0,0 +1,416 @@
+# -*- coding: utf-8 -*-
+"""Model wrapper for OpenAI models"""
+from typing import Union, Any
+
+from loguru import logger
+
+from .model import ModelWrapperBase, ModelResponse
+from ..file_manager import file_manager
+
+try:
+    import openai
+except ImportError:
+    openai = None
+
+from ..utils.monitor import MonitorFactory
+from ..utils.monitor import get_full_name
+from ..utils import QuotaExceededError
+from ..utils.token_utils import get_openai_max_length
+from ..constants import _DEFAULT_API_BUDGET
+
+
+class OpenAIWrapper(ModelWrapperBase):
+    """The model wrapper for OpenAI API."""
+
+    def __init__(
+        self,
+        config_name: str,
+        model_name: str = None,
+        api_key: str = None,
+        organization: str = None,
+        client_args: dict = None,
+        generate_args: dict = None,
+        budget: float = _DEFAULT_API_BUDGET,
+        **kwargs: Any,
+    ) -> None:
+        """Initialize the openai client.
+
+        Args:
+            config_name (`str`):
+                The name of the model config.
+            model_name (`str`, default `None`):
+                The name of the model to use in OpenAI API.
+            api_key (`str`, default `None`):
+                The API key for OpenAI API. If not specified, it will
+                be read from the environment variable `OPENAI_API_KEY`.
+            organization (`str`, default `None`):
+                The organization ID for OpenAI API. If not specified, it will
+                be read from the environment variable `OPENAI_ORGANIZATION`.
+            client_args (`dict`, default `None`):
+                The extra keyword arguments to initialize the OpenAI client.
+            generate_args (`dict`, default `None`):
+                The extra keyword arguments used in openai api generation,
+                e.g. `temperature`, `seed`.
+            budget (`float`, default `None`):
+                The total budget using this model. Set to `None` means no
+                limit.
+        """
+        if model_name is None:
+            model_name = config_name
+        super().__init__(
+            config_name=config_name,
+            model_name=model_name,
+            client_args=client_args,
+            generate_args=generate_args,
+            budget=budget,
+            **kwargs,
+        )
+
+        if openai is None:
+            raise ImportError(
+                "Cannot find openai package in current python environment.",
+            )
+
+        self.model = model_name
+        self.generate_args = generate_args or {}
+
+        self.client = openai.OpenAI(
+            api_key=api_key,
+            organization=organization,
+            **(client_args or {}),
+        )
+
+        # Set the max length of OpenAI model
+        try:
+            self.max_length = get_openai_max_length(self.model)
+        except Exception as e:
+            logger.warning(
+                f"fail to get max_length for {self.model}: " f"{e}",
+            )
+            self.max_length = None
+
+        # Set monitor accordingly
+        self.monitor = None
+        self.budget = budget
+        self._register_budget()
+        self._register_default_metrics()
+
+    def _register_budget(self) -> None:
+        self.monitor = MonitorFactory.get_monitor()
+        self.monitor.register_budget(
+            model_name=self.model,
+            value=self.budget,
+            prefix=self.model,
+        )
+
+    def _register_default_metrics(self) -> None:
+        """Register metrics to the monitor."""
+        raise NotImplementedError(
+            "The _register_default_metrics function is not Implemented.",
+        )
+
+    def _metric(self, metric_name: str) -> str:
+        """Add the class name and model name as prefix to the metric name.
+
+        Args:
+            metric_name (`str`):
+                The metric name.
+
+        Returns:
+            `str`: Metric name of this wrapper.
+        """
+        return get_full_name(name=metric_name, prefix=self.model)
+
+
+class OpenAIChatWrapper(OpenAIWrapper):
+    """The model wrapper for OpenAI's chat API."""
+
+    model_type: str = "openai"
+
+    def _register_default_metrics(self) -> None:
+        # Set monitor accordingly
+        # TODO: set quota to the following metrics
+        self.monitor = MonitorFactory.get_monitor()
+        self.monitor.register(
+            self._metric("prompt_tokens"),
+            metric_unit="token",
+        )
+        self.monitor.register(
+            self._metric("completion_tokens"),
+            metric_unit="token",
+        )
+        self.monitor.register(
+            self._metric("total_tokens"),
+            metric_unit="token",
+        )
+
+    def __call__(
+        self,
+        messages: list,
+        **kwargs: Any,
+    ) -> ModelResponse:
+        """Processes a list of messages to construct a payload for the OpenAI
+        API call. It then makes a request to the OpenAI API and returns the
+        response. This method also updates monitoring metrics based on the
+        API response.
+
+        Each message in the 'messages' list can contain text content and
+        optionally an 'image_urls' key. If 'image_urls' is provided,
+        it is expected to be a list of strings representing URLs to images.
+        These URLs will be transformed to a suitable format for the OpenAI
+        API, which might involve converting local file paths to data URIs.
+
+        Args:
+            messages (`list`):
+                A list of messages to process.
+            **kwargs (`Any`):
+                The keyword arguments to OpenAI chat completions API,
+                e.g. `temperature`, `max_tokens`, `top_p`, etc. Please refer to
+                https://platform.openai.com/docs/api-reference/chat/create
+                for more detailed arguments.
+
+        Returns:
+            `ModelResponse`:
+                The response text in text field, and the raw response in
+                raw field.
+
+        Note:
+            `parse_func`, `fault_handler` and `max_retries` are reserved for
+            `_response_parse_decorator` to parse and check the response
+            generated by model wrapper. Their usages are listed as follows:
+                - `parse_func` is a callable function used to parse and check
+                the response generated by the model, which takes the response
+                as input.
+                - `max_retries` is the maximum number of retries when the
+                `parse_func` raise an exception.
+                - `fault_handler` is a callable function which is called
+                when the response generated by the model is invalid after
+                `max_retries` retries.
+        """
+
+        # step1: prepare keyword arguments
+        kwargs = {**self.generate_args, **kwargs}
+
+        # step2: checking messages
+        if not all("role" in msg and "content" in msg for msg in messages):
+            raise ValueError(
+                "Each message in the 'messages' list must contain a 'role' "
+                "and 'content' key for OpenAI API.",
+            )
+
+        # step3: forward to generate response
+        response = self.client.chat.completions.create(
+            model=self.model,
+            messages=messages,
+            **kwargs,
+        )
+
+        # step4: record the api invocation if needed
+        self._save_model_invocation(
+            arguments={
+                "model": self.model,
+                "messages": messages,
+                **kwargs,
+            },
+            json_response=response.model_dump(),
+        )
+
+        # step5: update monitor accordingly
+        try:
+            self.monitor.update(
+                response.usage.model_dump(),
+                prefix=self.model,
+            )
+        except QuotaExceededError as e:
+            # TODO: optimize quota exceeded error handling process
+            logger.error(e.message)
+
+        # step6: return response
+        return ModelResponse(
+            text=response.choices[0].message.content,
+            raw=response.model_dump(),
+        )
+
+
+class OpenAIDALLEWrapper(OpenAIWrapper):
+    """The model wrapper for OpenAI's DALL·E API."""
+
+    model_type: str = "openai_dall_e"
+
+    _resolutions: list = [
+        "1792*1024",
+        "1024*1792",
+        "1024*1024",
+        "512*512",
+        "256*256",
+    ]
+
+    def _register_default_metrics(self) -> None:
+        # Set monitor accordingly
+        # TODO: set quota to the following metrics
+        self.monitor = MonitorFactory.get_monitor()
+        for resolution in self._resolutions:
+            self.monitor.register(
+                self._metric(resolution),
+                metric_unit="image",
+            )
+
+    def __call__(
+        self,
+        prompt: str,
+        save_local: bool = False,
+        **kwargs: Any,
+    ) -> ModelResponse:
+        """
+        Args:
+            prompt (`str`):
+                The prompt string to generate images from.
+            save_local: (`bool`, default `False`):
+                Whether to save the generated images locally, and replace
+                the returned image url with the local path.
+            **kwargs (`Any`):
+                The keyword arguments to OpenAI image generation API, e.g.
+                `n`, `quality`, `response_format`, `size`, etc. Please refer to
+                https://platform.openai.com/docs/api-reference/images/create
+                for more detailed arguments.
+
+        Returns:
+            `ModelResponse`:
+                A list of image urls in image_urls field and the
+                raw response in raw field.
+
+        Note:
+            `parse_func`, `fault_handler` and `max_retries` are reserved for
+            `_response_parse_decorator` to parse and check the response
+            generated by model wrapper. Their usages are listed as follows:
+                - `parse_func` is a callable function used to parse and check
+                the response generated by the model, which takes the response
+                as input.
+                - `max_retries` is the maximum number of retries when the
+                `parse_func` raise an exception.
+                - `fault_handler` is a callable function which is called
+                when the response generated by the model is invalid after
+                `max_retries` retries.
+        """
+        # step1: prepare keyword arguments
+        kwargs = {**self.generate_args, **kwargs}
+
+        # step2: forward to generate response
+        try:
+            response = self.client.images.generate(
+                model=self.model,
+                prompt=prompt,
+                **kwargs,
+            )
+        except Exception as e:
+            logger.error(
+                f"Failed to generate images for prompt '{prompt}': {e}",
+            )
+            raise e
+
+        # step3: record the model api invocation if needed
+        self._save_model_invocation(
+            arguments={
+                "model": self.model,
+                "prompt": prompt,
+                **kwargs,
+            },
+            json_response=response.model_dump(),
+        )
+
+        # step4: return response
+        raw_response = response.model_dump()
+        images = raw_response["data"]
+        # Get image urls as a list
+        urls = [_["url"] for _ in images]
+
+        if save_local:
+            # Return local url if save_local is True
+            urls = [file_manager.save_image(_) for _ in urls]
+        return ModelResponse(image_urls=urls, raw=raw_response)
+
+
+class OpenAIEmbeddingWrapper(OpenAIWrapper):
+    """The model wrapper for OpenAI embedding API."""
+
+    model_type: str = "openai_embedding"
+
+    def _register_default_metrics(self) -> None:
+        # Set monitor accordingly
+        # TODO: set quota to the following metrics
+        self.monitor = MonitorFactory.get_monitor()
+        self.monitor.register(
+            self._metric("prompt_tokens"),
+            metric_unit="token",
+        )
+        self.monitor.register(
+            self._metric("total_tokens"),
+            metric_unit="token",
+        )
+
+    def __call__(
+        self,
+        texts: Union[list[str], str],
+        **kwargs: Any,
+    ) -> ModelResponse:
+        """Embed the messages with OpenAI embedding API.
+
+        Args:
+            texts (`list[str]` or `str`):
+                The messages used to embed.
+            **kwargs (`Any`):
+                The keyword arguments to OpenAI embedding API,
+                e.g. `encoding_format`, `user`. Please refer to
+                https://platform.openai.com/docs/api-reference/embeddings
+                for more detailed arguments.
+
+        Returns:
+            `ModelResponse`:
+                A list of embeddings in embedding field and the
+                raw response in raw field.
+
+        Note:
+            `parse_func`, `fault_handler` and `max_retries` are reserved for
+            `_response_parse_decorator` to parse and check the response
+            generated by model wrapper. Their usages are listed as follows:
+                - `parse_func` is a callable function used to parse and check
+                the response generated by the model, which takes the response
+                as input.
+                - `max_retries` is the maximum number of retries when the
+                `parse_func` raise an exception.
+                - `fault_handler` is a callable function which is called
+                when the response generated by the model is invalid after
+                `max_retries` retries.
+        """
+        # step1: prepare keyword arguments
+        kwargs = {**self.generate_args, **kwargs}
+
+        # step2: forward to generate response
+        response = self.client.embeddings.create(
+            input=texts,
+            model=self.model,
+            **kwargs,
+        )
+
+        # step3: record the model api invocation if needed
+        self._save_model_invocation(
+            arguments={
+                "model": self.model,
+                "input": texts,
+                **kwargs,
+            },
+            json_response=response.model_dump(),
+        )
+
+        # step4: return response
+        response_json = response.model_dump()
+        if len(response_json["data"]) == 0:
+            return ModelResponse(
+                embedding=response_json["data"]["embedding"][0],
+                raw=response_json,
+            )
+        else:
+            return ModelResponse(
+                embedding=[_["embedding"] for _ in response_json["data"]],
+                raw=response_json,
+            )
