a988e4e92678d031b86705372d43ced656ff8037
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-26 03:07:16 +0000

    Add package fyodorov-utils/0.2.75/fyodorov_utils-0.2.75-py3-none-any.whl

---
+++ packages/fyodorov-utils/fyodorov_utils-0.2.75-py3-none-any.whl/fyodorov_utils/templates/agents.py
@@ -0,0 +1,11 @@
+from fyodorov_llm_agents.tools.tool import Tool as ToolModel
+import yaml
+
+def parse_yaml(yaml_str: str) -> ToolModel:
+    if not yaml_str:
+        raise ValueError('YAML string is required')
+    print(f"Parsing YAML: {yaml_str}")
+    yaml_str = yaml_str.strip()
+    yaml_dict = yaml.safe_load(yaml_str)
+    # tool = ToolModel.from_yaml(yaml_str)
+    return tool
