25a9ae814f73d915e684bf3de1b669e25d449b9e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-26 14:06:39 +0000

    Add package hnt-nf-jira-library/0.0.8/hnt_nf_jira_library-0.0.8.tar.gz

---
+++ packages/hnt-nf-jira-library/hnt_nf_jira_library-0.0.8.tar.gz/hnt_nf_jira_library-0.0.8/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='hnt_nf_jira_library',
+    version='0.0.8',
+    license='MIT License',
+    author='Guillerme Rezende Manhaes',
+    keywords='nota_fiscal',
+    description=u'Lib to access nf from Jira',
+    packages=['nf_jira'])
