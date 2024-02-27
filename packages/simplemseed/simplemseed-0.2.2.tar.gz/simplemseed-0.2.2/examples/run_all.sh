2f225c35afae4e4b0297067f26a9fe307d31e947
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-27 06:05:05 +0000

    Add package simplemseed/0.2.2/simplemseed-0.2.2.tar.gz

---
+++ packages/simplemseed/simplemseed-0.2.2.tar.gz/simplemseed-0.2.2/examples/run_all.sh
@@ -0,0 +1,7 @@
+python fdsnws_mseed3.py && \
+python ginormous_ms3.py && \
+python minimal_rw_mseed3.py && \
+python miniseed2to3.py && \
+python read_miniseed2.py && \
+python readwrite_miniseed3.py && \
+python sourceid.py
