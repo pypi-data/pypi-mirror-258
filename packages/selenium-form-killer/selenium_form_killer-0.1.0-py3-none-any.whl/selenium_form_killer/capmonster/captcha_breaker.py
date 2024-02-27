9accf70f58b798cb6e9d699876500fa1ce01ffad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-27 04:05:35 +0000

    Add package selenium-form-killer/0.1.0/selenium_form_killer-0.1.0-py3-none-any.whl

---
+++ packages/selenium-form-killer/selenium_form_killer-0.1.0-py3-none-any.whl/selenium_form_killer/capmonster/captcha_breaker.py
@@ -0,0 +1,20 @@
+import os
+from capmonstercloudclient import CapMonsterClient, ClientOptions
+
+from capmonstercloudclient.requests.baseRequest import BaseRequest
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+API_KEY = os.getenv("API_KEY")
+
+
+async def captcha_token(request_captcha: BaseRequest) -> str:
+    client_options = ClientOptions(api_key=API_KEY, client_timeout=30)
+    cap_monster_client = CapMonsterClient(options=client_options)
+    print("Solving captcha...")
+    responses = await cap_monster_client.solve_captcha(request_captcha)
+    print("Captcha solved!")
+
+    return responses["gRecaptchaResponse"]
