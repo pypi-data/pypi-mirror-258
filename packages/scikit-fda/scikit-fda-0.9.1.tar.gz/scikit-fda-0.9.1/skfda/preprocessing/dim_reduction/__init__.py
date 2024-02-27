38350fffc8582088e47225593595582f1f4cb074
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-27 03:04:43 +0000

    Add package scikit-fda/0.9.1/scikit-fda-0.9.1.tar.gz

---
+++ packages/scikit-fda/scikit-fda-0.9.1.tar.gz/scikit-fda-0.9.1/skfda/preprocessing/dim_reduction/__init__.py
@@ -0,0 +1,32 @@
+"""Dim reduction."""
+from __future__ import annotations
+
+import importlib
+from typing import TYPE_CHECKING, Any
+
+import lazy_loader as lazy
+
+_normal_getattr, __dir__, __all__ = lazy.attach(
+    __name__,
+    submodules=[
+        "variable_selection",
+    ],
+    submod_attrs={
+        "_fpca": ["FPCA"],
+        "_fpls": ["FPLS"],
+        "_neighbor_transforms": ["KNeighborsTransformer"],
+    },
+)
+
+if TYPE_CHECKING:
+    from ._fpca import FPCA as FPCA
+    from ._fpls import FPLS as FPLS
+    from ._neighbor_transforms import (
+        KNeighborsTransformer as KNeighborsTransformer,
+    )
+
+
+def __getattr__(name: str) -> Any:
+    if name in {"projection", "feature_extraction"}:
+        return importlib.import_module(f".{name}", __name__)
+    return _normal_getattr(name)
