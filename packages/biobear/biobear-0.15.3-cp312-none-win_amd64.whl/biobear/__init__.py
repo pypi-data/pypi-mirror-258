cffa09441318b6c4b5a5745c95bb276c5be9139a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-27 06:05:24 +0000

    Add package biobear/0.15.3/biobear-0.15.3-cp312-none-win_amd64.whl

---
+++ packages/biobear/biobear-0.15.3-cp312-none-win_amd64.whl/biobear/__init__.py
@@ -0,0 +1,53 @@
+# Copyright 2023 WHERE TRUE Technologies.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Main biobear package."""
+
+from biobear.fasta_reader import FastaReader
+from biobear.fastq_reader import FastqReader
+from biobear.vcf_reader import VCFReader, VCFIndexedReader
+from biobear.bam_reader import BamReader, BamIndexedReader
+from biobear.gtf_reader import GTFReader
+from biobear.gff_reader import GFFReader
+from biobear.mzml_reader import MzMLReader
+from biobear.genbank_reader import GenbankReader
+from biobear.bcf_reader import BCFReader, BCFIndexedReader
+from biobear.session import connect
+
+from biobear import compression
+from biobear.compression import Compression
+
+from .biobear import __runtime
+
+__version__ = "0.15.3"
+
+__all__ = [
+    "FastaReader",
+    "FastqReader",
+    "VCFReader",
+    "VCFIndexedReader",
+    "BamReader",
+    "BamIndexedReader",
+    "BCFReader",
+    "BCFIndexedReader",
+    "GFFReader",
+    "GTFReader",
+    "GenbankReader",
+    "MzMLReader",
+    "compression",
+    "Compression",
+    "__version__",
+    "connect",
+    "__runtime",
+]
