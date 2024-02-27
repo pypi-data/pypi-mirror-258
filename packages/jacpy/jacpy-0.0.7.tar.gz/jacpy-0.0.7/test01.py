1d24814be582a98fb811e8ff63f85fa2a19c6350
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-27 14:05:12 +0000

    Add package jacpy/0.0.7/jacpy-0.0.7.tar.gz

---
+++ packages/jacpy/jacpy-0.0.7.tar.gz/jacpy-0.0.7/test01.py
@@ -0,0 +1,15 @@
+from src.jacpy.io import ioUtils
+from src.jacpy.geometry import geoUtils
+
+items = ioUtils.dirItems('C:/Users/alber/Desktop/BACKUP ABRIL 2022', ioUtils.DirItemPolicy.AllAlphabetic, ioUtils.DirItemOutputForm.Name, "*.txt", 0, 2)
+
+print(items)
+
+
+
+partitions = geoUtils.partition1DGeometry(50, 50, 0.5, 1.5)
+print(partitions)
+
+
+partitions = geoUtils.partition2DGeometry(100, 51, 50, 50, 0.5, 1.5)
+print(partitions)
