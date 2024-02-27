37121f1a35a916af10d219a8584e9f5b36bcb66b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-27 03:05:11 +0000

    Add package aiminify/0.0.2/aiminify-0.0.2.tar.gz

---
+++ packages/aiminify/aiminify-0.0.2.tar.gz/aiminify-0.0.2/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='aiminify',
+    version='0.0.2',
+    packages=find_packages(),
+    license='MIT',
+    description='Just create the package',
+    author='Pablo Perez de Ciriza',
+    author_email='pabloperezdeciriza@stapesit.com',
+    url='https://github.com/',
+)
