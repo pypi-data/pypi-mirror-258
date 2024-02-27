3b053269d6a8c4d5964c5051be3627ff07e8acba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-27 04:05:06 +0000

    Add package luanon/1.0.1.dev0/luanon-1.0.1.dev0-py3-none-any.whl

---
+++ packages/luanon/luanon-1.0.1.dev0-py3-none-any.whl/luanon/__init__.py
@@ -0,0 +1,7 @@
+"""
+    Tác giả: GnU
+    Ngày tạo: 11/09/2023
+    ©2023 LuaNonTeam
+"""
+
+__version__ = "1.0.0-dev"
---
+++ packages/luanon/luanon-1.0.1.dev0-py3-none-any.whl/luanon/cloudscraper/__init__.py
@@ -0,0 +1,11 @@
+"""
+    Tác giả: GnU
+    Ngày tạo: 11/09/2023
+    ©2023 LuaNonTeam
+"""
+
+from .cf_main import CloudflareScraper
+
+__all__ = [
+    "CloudflareScraper"
+]
