a55ed14ad77ce42a3b7fea5c2ddccdee821f1a1d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-27 05:05:37 +0000

    Add package cobra-meta/1.2.3/cobra-meta-1.2.3.tar.gz

---
+++ packages/cobra-meta/cobra-meta-1.2.3.tar.gz/cobra-meta-1.2.3/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+setup(
+    name='cobra-meta',
+    version='1.2.3',
+    py_modules=['cobra'],
+    install_requires=[
+        'biopython',
+        'pysam'
+    ],
+    author='LINXING CHEN',
+    description='COBRA (Contig Overlap Based Re-Assembly) is a bioinformatics tool to get higher quality viral genomes assembled from metagenomes of short paired-end reads. COBRA was written in Python. COBRA has so far only been tested on assembled contigs from metaSPAdes, IDBA_UD, and MEGAHIT.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/linxingchen/cobra',
+    entry_points={
+        'console_scripts': [
+            'cobra-meta=cobra:main',
+        ],
+    },
+)
