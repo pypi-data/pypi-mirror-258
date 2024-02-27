ecc7d6bcbcfa4abcb232dc774a4115bfc9f8d3f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-27 04:05:33 +0000

    Add package seissolxdmfwriter/0.2.1/seissolxdmfwriter-0.2.1.tar.gz

---
+++ packages/seissolxdmfwriter/seissolxdmfwriter-0.2.1.tar.gz/seissolxdmfwriter-0.2.1/seissolxdmfwriter/a12py.py
@@ -0,0 +1,25 @@
+import seissolxdmfwriter as sxw
+import seissolxdmf as sx
+import numpy as np
+
+fn = "test-fault.xdmf"
+# Read data from input file using seissolxdmf
+sx = sx.seissolxdmf(fn)
+geom = sx.ReadGeometry()
+connect = sx.ReadConnect()
+dt = sx.ReadTimeStep()
+SRs = sx.ReadData("SRs")
+SRd = sx.ReadData("SRs")
+SR = np.sqrt(SRs**2 + SRd**2)
+
+# Write the 0,4 and 8th times steps of array SRs and SR in SRtest-fault.xdmf/SRtest-fault.h5
+dictTime = {dt * i: i for i in [0, 4, 8]}
+sxw.write(
+    "test-fault",
+    geom,
+    connect,
+    {"SRs": SRs, "SR": SR},
+    dictTime,
+    reduce_precision=True,
+    to_hdf5=True,
+)
