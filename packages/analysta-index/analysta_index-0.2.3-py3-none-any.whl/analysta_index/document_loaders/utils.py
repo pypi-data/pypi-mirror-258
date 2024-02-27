0ddae964c5cced9a86e4f486ba8a52fd4365f1c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-27 04:05:33 +0000

    Add package analysta-index/0.2.3/analysta_index-0.2.3-py3-none-any.whl

---
+++ packages/analysta-index/analysta_index-0.2.3-py3-none-any.whl/analysta_index/document_loaders/utils.py
@@ -0,0 +1,32 @@
+import re
+import string
+
+from gensim.parsing import remove_stopwords
+
+def cleanse_data(document: str) -> str:
+    # remove numbers
+    document = re.sub(r"\d+", " ", document)
+
+    # print("\n",document)
+    # remove single characters
+    document = " ".join([w for w in document.split() if len(w) > 1])
+
+    # remove punctuations and convert characters to lower case
+    document = "".join([
+        char.lower()
+        for char in document
+        if char not in string.punctuation
+    ])
+
+    # Remove remove all non-alphanumeric characaters
+    document = re.sub(r"\W+", " ", document)
+
+    # Remove 'out of the box' stopwords
+    document = remove_stopwords(document)
+    # print("--- rem ",document)
+
+    # Remove custom keywords
+    # for kw in custom_kw:
+    #     document = document.replace(kw, "")
+
+    return document
