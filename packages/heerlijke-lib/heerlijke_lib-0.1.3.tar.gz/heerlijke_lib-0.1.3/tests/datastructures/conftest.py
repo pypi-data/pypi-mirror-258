61672956f3e934f47cd388e3b90b710428fa2373
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-27 03:04:55 +0000

    Add package heerlijke-lib/0.1.3/heerlijke_lib-0.1.3.tar.gz

---
+++ packages/heerlijke-lib/heerlijke_lib-0.1.3.tar.gz/heerlijke_lib-0.1.3/tests/datastructures/conftest.py
@@ -0,0 +1,25 @@
+from enum import auto
+
+from heerlijke_lib.datastructures import CaseInsensitiveStrEnum
+
+
+class EnumAuto(CaseInsensitiveStrEnum):
+    FIRST = auto()
+    SECOND = auto()
+    THIRD = auto()
+
+
+class EnumManual(CaseInsensitiveStrEnum):
+    FIRST = "FIRST"
+    SECOND = "SECOND"
+    THIRD = "THIRD"
+
+
+def pytest_generate_tests(metafunc):
+    if 'input_enum' in metafunc.fixturenames:
+        # Generate test cases based on the test_data list
+        metafunc.parametrize(
+            "input_enum",
+            (EnumAuto, EnumManual),
+            ids=("Auto", "Manual")
+        )
