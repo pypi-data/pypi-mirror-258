4ea3263d51a01d71518833d1c70453492a87663a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-27 05:04:24 +0000

    Add package daqview/0.6.2/daqview-0.6.2-py3-none-any.whl

---
+++ packages/daqview/daqview-0.6.2-py3-none-any.whl/daqview/views/channel_list.py
@@ -0,0 +1,31 @@
+from PyQt5.QtWidgets import (QDialog, QVBoxLayout, QApplication, QTableView,
+                             QPushButton, QHBoxLayout)
+from PyQt5.QtCore import Qt
+
+
+class ChannelListDialog(QDialog):
+    def __init__(self):
+        super().__init__()
+        self.app = QApplication.instance()
+        self.setWindowTitle("DAQview Server Channel List")
+        self.init_ui()
+
+    def init_ui(self):
+        layout = QVBoxLayout(self)
+        table = QTableView(self)
+        table.setModel(self.app.server.channels_table)
+        table.setAlternatingRowColors(True)
+        table.setSelectionMode(0)
+        table.setHorizontalScrollBarPolicy(Qt.ScrollBarAlwaysOff)
+        table.resizeRowsToContents()
+        table.resizeColumnsToContents()
+        layout.addWidget(table)
+        bottom_buttons = QHBoxLayout()
+        close = QPushButton("Close", self)
+        close.clicked.connect(self.close)
+        bottom_buttons.addStretch()
+        bottom_buttons.addWidget(close)
+        layout.addLayout(bottom_buttons)
+        width = table.horizontalHeader().length() + 24
+        height = table.verticalHeader().length() + 24
+        self.resize(width, height)
