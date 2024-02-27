a29b7878d1c8555e3e7b95e81040a0c4bfe2a1f2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-27 05:05:57 +0000

    Add package deebot-client/6.0.2/deebot-client-6.0.2.tar.gz

---
+++ packages/deebot-client/deebot-client-6.0.2.tar.gz/deebot-client-6.0.2/deebot_client/events/fan_speed.py
@@ -0,0 +1,25 @@
+"""Fan speed event module."""
+from __future__ import annotations
+
+from dataclasses import dataclass
+
+from deebot_client.util import DisplayNameIntEnum
+
+from .base import Event
+
+
+class FanSpeedLevel(DisplayNameIntEnum):
+    """Enum class for all possible fan speed levels."""
+
+    # Values should be sort from low to high on their meanings
+    QUIET = 1000
+    NORMAL = 0
+    MAX = 1
+    MAX_PLUS = 2
+
+
+@dataclass(frozen=True)
+class FanSpeedEvent(Event):
+    """Fan speed event representation."""
+
+    speed: FanSpeedLevel
