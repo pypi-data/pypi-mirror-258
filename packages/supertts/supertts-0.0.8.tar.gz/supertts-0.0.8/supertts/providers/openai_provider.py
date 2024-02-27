a8db873de7064af72f0887e8f4d51f5e4b136682
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-27 04:05:20 +0000

    Add package supertts/0.0.8/supertts-0.0.8.tar.gz

---
+++ packages/supertts/supertts-0.0.8.tar.gz/supertts-0.0.8/supertts/providers/openai_provider.py
@@ -0,0 +1,58 @@
+from openai import OpenAI
+import os
+
+def get_openai_api_key():
+    """Retrieve the OPENAI_API_KEY environment variable."""
+    api_key = os.getenv('OPENAI_API_KEY')
+    if not api_key:
+        raise EnvironmentError('The environment variable OPENAI_API_KEY is not set. Please set it to your OpenAI API key.')
+    return api_key
+
+def speak(
+    text,
+    model="tts-1",
+    voice="alloy"
+):
+    voices = available_voices()
+    if model not in ["tts-1", "tts-1-hd"]:
+        raise ValueError(f"Invalid model: {model}")
+    if voice not in [voice["name"] for voice in voices]:
+        raise ValueError(f"Invalid voice: {voice}")
+    client = OpenAI(
+        api_key=get_openai_api_key()
+    )
+    response = client.audio.speech.create(
+        model=model,
+        voice=voice,
+        input=text
+    )
+    return response
+
+def available_voices():
+    voices = [
+        {
+            "name": "alloy",
+            "gender": "male"
+        },
+        {
+            "name": "echo",
+            "gender": "male"
+        },
+        {
+            "name": "fable",
+            "gender": "male"
+        },
+        {
+            "name": "onyx",
+            "gender": "male"
+        },
+        {
+            "name": "nova",
+            "gender": "female"
+        },
+        {
+            "name": "shimmer",
+            "gender": "male"
+        },
+    ]
+    return voices
