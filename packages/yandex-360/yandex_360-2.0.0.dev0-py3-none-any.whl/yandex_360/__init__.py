80b3168e8acbcc48db9933e30b6b28cc8c680ce5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-27 05:04:53 +0000

    Add package yandex-360/2.0.0.dev0/yandex_360-2.0.0.dev0-py3-none-any.whl

---
+++ packages/yandex-360/yandex_360-2.0.0.dev0-py3-none-any.whl/yandex_360/__init__.py
@@ -0,0 +1,12 @@
+"""Библиотека Yandex 360
+
+.. moduleauthor:: Igor Kuptsov <ya360@uh.net.ru>
+
+"""
+
+try:
+    from ._version import __version__
+except:
+    __version__ = 'dev'
+
+__author__ = 'Igor Kuptsov'
