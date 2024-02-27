278a668a94a923c9b62113daa9289439805905ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-27 14:04:54 +0000

    Add package python-to-cloudwatch/0.2/python_to_cloudwatch-0.2.tar.gz

---
+++ packages/python-to-cloudwatch/python_to_cloudwatch-0.2.tar.gz/python_to_cloudwatch-0.2/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='python_to_cloudwatch',
+    version='0.2',
+    packages=['python_to_cloudwatch'],
+    install_requires=[
+        'boto3',
+    ],
+)
