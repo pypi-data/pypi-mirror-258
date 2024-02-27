31eea9110c30105aa7501f852f3e81ed9a7dfa2b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-27 03:05:19 +0000

    Add package cms-test/1.0/cms-test-1.0.tar.gz

---
+++ packages/cms-test/cms-test-1.0.tar.gz/cms-test-1.0/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='cms-test',
+    version='1.0',
+    author='HCMP DE',
+    author_email='Rishabh2.Mehta@ril.com',
+    description='CMS Helper library',
+    url='https://github.com/test/cms',
+    packages=find_packages(),
+    install_requires=[
+        'requests',
+        'loguru',
+        'pycryptodome',
+        'setuptools'
+    ]
+)
---
+++ packages/cms-test/cms-test-1.0.tar.gz/cms-test-1.0/tests/test_http_utils.py
@@ -0,0 +1,5 @@
+from cms import http_utils
+
+uuid = 'ae899373-8d60-4360-a2d6-045027fdb5ef'
+
+decrypted_cred = http_utils.get_cred_from_uuid(uuid)
