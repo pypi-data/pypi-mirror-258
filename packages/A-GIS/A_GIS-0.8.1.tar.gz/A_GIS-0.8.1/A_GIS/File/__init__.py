be422579340b49682ccd559c41679bcabf1cbfc4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-27 15:05:38 +0000

    Add package A-GIS/0.8.1/A_GIS-0.8.1.tar.gz

---
+++ packages/A-GIS/A_GIS-0.8.1.tar.gz/A_GIS-0.8.1/A_GIS/File/__init__.py
@@ -0,0 +1,6 @@
+# Functions
+from .download import download
+from .read import read
+from .write import write
+# Packages
+from . import Directory
---
+++ packages/A-GIS/A_GIS-0.8.1.tar.gz/A_GIS-0.8.1/A_GIS/__init__.py
@@ -0,0 +1,7 @@
+# Packages
+from . import Code
+from . import Data
+from . import File
+from . import Image
+from . import Math
+from . import Text
