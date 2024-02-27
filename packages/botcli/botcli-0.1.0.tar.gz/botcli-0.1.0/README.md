02c512964090fa5f390f29fcc089dcdfa809800a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-27 14:05:14 +0000

    Add package botcli/0.1.0/botcli-0.1.0.tar.gz

---
+++ packages/botcli/botcli-0.1.0.tar.gz/botcli-0.1.0/README.md
@@ -0,0 +1,31 @@
+![HeaderImage](./images/header.png)
+# BotCli
+![Discord](https://img.shields.io/discord/1146327551334174780) ![PyPI - Version](https://img.shields.io/pypi/v/botcli)
+Bot Cli is a [discord.py](https://github.com/Rapptz/discord.py) project generator for a structure that I prefer. 
+
+# Prerequisites
+- [Python 3.8+](https://www.python.org/downloads/)
+- [Docker](https://www.docker.com/) (Optional)
+- [A Brain](https://en.wikipedia.org/wiki/Brain) (Optional)
+  
+# Installation
+1. Install the package with [pip](https://pip.pypa.io/en/stable) 
+```bash
+pip install botcli
+```
+
+# Usage
+Simply just run 
+```
+makebot
+```
+and follow the prompts provided
+
+# Support
+Join the [TND Discord Server](https://discord.gg/GqdWT74Qwx)
+
+# Contributing
+Pull requests are welcome. For major changes, please open an issue first to discuss what you would like to change.
+
+# License
+This project is under the [MIT License](https://opensource.org/license/MIT)
---
+++ packages/botcli/botcli-0.1.0.tar.gz/botcli-0.1.0/src/botcli/generate.py
@@ -0,0 +1,131 @@
+from __future__ import annotations
+
+import os
+from string import Template
+from typing import Dict, Final, List
+
+import requests
+
+
+class ProjectTemplate(Template):
+    delimiter = "%"
+
+
+"""Holds the template files names for the project"""
+TEMPLATE_FILES: Final[List[str, str]] = (
+    ".gitignore",
+    ".pre-commit-config.yaml",
+    "AUTHORS.md",
+    "CHANGELOG.md",
+    "CODE_OF_CONDUCT.md",
+    "CONTRIBUTING.md",
+    "Dockerfile",
+    "launcher.py",
+    "README.md",
+    "requirements.txt",
+    "tox.ini",
+    "scripts/docker.py",
+    "src/base/config.py",
+    "src/base/database.py",
+    "src/base/router.json",
+    "src/base/keys/token.key",
+    "src/base/keys/cert.key",
+    "src/base/keys/uri.key",
+    "src/bot/bot.py",
+    "src/bot/__init__.py",
+    "src/bot/cogs/example.py",
+    "tests/test-db.py",
+)
+
+
+class Generator:
+    """Generator class for generating the project."""
+
+    def __init__(self):
+        pass
+
+    def find_license(self, name: str) -> str:
+        """Returns the license file name
+
+        Arguments
+        ---------
+        name (str): The name of the license
+
+        Returns
+        -------
+        str: License file name
+
+        """
+        license_mapping: Dict[str, str] = {
+            "MIT": "LICENSE-mit",
+            "Apache-2.0": "LICENSE-ap2",
+            "GPL-3.0": "LICENSE-gpl",
+            "BSD-3-Clause": "LICENSE-bsd3",
+            "BSD-2-Clause": "LICENSE-bsd2",
+            "Creative Commons Zero v1.0 Universal": "LICENSE-ccL",
+            "Affero GPL v3": "LICENSE-agpl",
+            "Eclipse Public License v2.0": "LICENSE-epl",
+            "ISC": "LICENSE-isc",
+            "LGPL-3.0": "LICENSE-lgpl",
+            "Mozilla Public License 2.0": "LICENSE-mpl",
+        }
+
+        return license_mapping.get(name, "LICENSE-MIT")
+
+    def find_license_url(self, name: str) -> str:
+        """Returns the license file url
+
+        Arguments
+        ---------
+        name (str): The name of the license
+
+        Returns
+        -------
+        str: License file url
+
+        """
+        license_mapping: Dict[str, str] = {
+            "MIT": "https://opensource.org/license/mit",
+            "Apache-2.0": "https://opensource.org/license/apache-2-0",
+            "GPL-3.0": "https://opensource.org/license/gpl-3-0",
+            "BSD-3-Clause": "Lhttps://opensource.org/license/bsd-3-clause",
+            "BSD-2-Clause": "https://opensource.org/license/bsd-2-clause",
+            "Creative Commons Zero v1.0 Universal": "https://creativecommons.org/licenses/by/3.0/",
+            "Affero GPL v3": "https://opensource.org/license/agpl-v3",
+            "Eclipse Public License v2.0": "https://opensource.org/license/epl-2-0",
+            "ISC": "https://opensource.org/license/isc-license-txt",
+            "LGPL-3.0": "https://opensource.org/license/lgpl-3-0",
+            "Mozilla Public License 2.0": "https://opensource.org/license/mpl-2-0",
+        }
+
+        return license_mapping[name]
+
+    def generate(self, dst: str, project_data: Dict[str, str]):
+        """Generates the project
+
+        Arguments
+        ---------
+        dst (str): The destination path
+        project_data (Dict[str, str]): The project data
+
+        """
+        for template_file in TEMPLATE_FILES:
+            template_url = "https://raw.githubusercontent.com/combogangreal/BotCli/main/src/botcli/templates"
+            response = requests.get(f"{template_url}/{template_file}.template")
+            template_str = response.text
+
+            template = ProjectTemplate(template_str)
+            processed_content = template.substitute(project_data)
+
+            output_file = template_file.replace(".template", "")
+            output_dir = os.path.dirname(output_file)
+            os.makedirs(f"{dst}/{output_dir}", exist_ok=True)
+
+            with open(
+                f"{dst}/{output_file}", "w", encoding="utf-8"
+            ) as f:  # Corrected 'w+' to 'w'
+                f.write(processed_content)
+
+
+if __name__ == "__main__":
+    generator = Generator()
