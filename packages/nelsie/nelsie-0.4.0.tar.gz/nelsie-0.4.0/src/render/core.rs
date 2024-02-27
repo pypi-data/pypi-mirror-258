f5f9f3b88b25b5901e875fd25676d29f99108f33
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-27 03:05:15 +0000

    Add package nelsie/0.4.0/nelsie-0.4.0.tar.gz

---
+++ packages/nelsie/nelsie-0.4.0.tar.gz/nelsie-0.4.0/src/render/core.rs
@@ -0,0 +1,76 @@
+use crate::model::{FontData, Resources, Slide, Step};
+use crate::render::counters::CountersMap;
+use crate::render::rendering::render_to_svg_tree;
+use crate::render::OutputFormat;
+use crate::NelsieError;
+use crate::Result;
+use resvg::tiny_skia;
+use std::path::Path;
+use std::sync::Arc;
+use usvg::{TreeTextToPath, TreeWriting, XmlOptions};
+
+pub(crate) struct RenderConfig<'a> {
+    pub resources: &'a Resources,
+    pub slide: &'a Slide,
+    pub slide_idx: usize,
+    pub step: Step,
+    pub default_font: &'a Arc<FontData>,
+    pub output_format: OutputFormat,
+    pub output_path: Option<&'a Path>,
+    pub counter_values: &'a CountersMap<'a>,
+}
+
+pub(crate) enum RenderingResult {
+    None,
+    Tree(usvg::Tree),
+    BytesData(Vec<u8>),
+}
+
+pub(crate) fn render_slide_step(render_cfg: &RenderConfig) -> Result<RenderingResult> {
+    log::debug!("Rendering step {}", render_cfg.step);
+    let mut tree = render_to_svg_tree(render_cfg);
+    tree.convert_text(&render_cfg.resources.font_db);
+
+    let result = match render_cfg.output_format {
+        OutputFormat::Pdf => return Ok(RenderingResult::Tree(tree)),
+        OutputFormat::Svg => {
+            let svg = tree.to_string(&XmlOptions::default());
+            RenderingResult::BytesData(svg.into_bytes())
+        }
+        OutputFormat::Png => {
+            let zoom = 1.0;
+            let rtree = resvg::Tree::from_usvg(&tree);
+            let pixmap_size = rtree.size.to_int_size().scale_by(zoom).unwrap();
+            let mut pixmap =
+                tiny_skia::Pixmap::new(pixmap_size.width(), pixmap_size.height()).unwrap();
+            let render_ts = tiny_skia::Transform::from_scale(zoom, zoom);
+            rtree.render(render_ts, &mut pixmap.as_mut());
+            RenderingResult::BytesData(
+                pixmap
+                    .encode_png()
+                    .map_err(|e| NelsieError::Generic(e.to_string()))?,
+            )
+        }
+    };
+
+    if let Some(path) = render_cfg.output_path {
+        if let RenderingResult::BytesData(data) = result {
+            let final_path = path.join(format!(
+                "{}-{}.{}",
+                render_cfg.slide_idx,
+                render_cfg.step,
+                render_cfg.output_format.extension()
+            ));
+            std::fs::write(&final_path, data).map_err(|e| {
+                NelsieError::Generic(format!(
+                    "Cannot write output file: {}: {}",
+                    final_path.display(),
+                    e
+                ))
+            })?;
+        }
+        Ok(RenderingResult::None)
+    } else {
+        Ok(result)
+    }
+}
---
+++ packages/nelsie/nelsie-0.4.0.tar.gz/nelsie-0.4.0/src/render/image.rs
@@ -0,0 +1,126 @@
+use crate::model::{LoadedImageData, NodeContentImage, OraImageData, Step, SvgImageData};
+
+use crate::render::layout::Rectangle;
+
+use usvg::TreeParsing;
+use usvg::{fontdb, TreeTextToPath};
+use usvg_tree::{ImageKind, ImageRendering, NodeKind, ViewBox, Visibility};
+
+fn prepare_svg_tree_for_step(
+    step: Step,
+    image: &NodeContentImage,
+    svg_data: &SvgImageData,
+    font_db: &fontdb::Database,
+) -> usvg::Tree {
+    let mut tree = usvg::Tree::from_data(&svg_data.data, &usvg::Options::default())
+        .expect("SVG Tree build failed");
+
+    if !image.enable_steps || svg_data.id_visibility.is_empty() || step <= image.shift_steps {
+        tree.convert_text(font_db);
+        return tree;
+    }
+    for (id, visibility) in &svg_data.id_visibility {
+        if !visibility.at_step(step - image.shift_steps) {
+            if let Some(node) = tree.node_by_id(id) {
+                node.detach();
+            }
+        }
+    }
+    tree.convert_text(font_db);
+    tree
+}
+
+fn create_image_node(svg_node: &usvg::Node, rect: &Rectangle, kind: ImageKind) {
+    if rect.width > 0.00001 && rect.height > 0.00001 {
+        let svg_image = usvg::Image {
+            id: String::new(),
+            transform: Default::default(),
+            visibility: Visibility::Visible,
+            view_box: ViewBox {
+                rect: usvg::Size::from_wh(rect.width, rect.height)
+                    .unwrap()
+                    .to_non_zero_rect(rect.x, rect.y),
+                aspect: Default::default(),
+            },
+            rendering_mode: ImageRendering::OptimizeQuality,
+            kind,
+        };
+        svg_node.append(usvg::Node::new(NodeKind::Image(svg_image)))
+    }
+}
+
+fn render_ora(
+    step: Step,
+    image: &NodeContentImage,
+    ora_data: &OraImageData,
+    svg_node: &usvg::Node,
+    rect: &Rectangle,
+    width: f32,
+    height: f32,
+) {
+    if rect.width <= 0.00001 || rect.height <= 0.00001 {
+        return;
+    }
+    if step <= image.shift_steps {
+        return;
+    }
+    let scale = (rect.width / width).min(rect.height / height);
+    for layer in &ora_data.layers {
+        if !image.enable_steps
+            || layer
+                .visibility
+                .as_ref()
+                .map(|v| *v.at_step(step - image.shift_steps))
+                .unwrap_or(true)
+        {
+            create_image_node(
+                svg_node,
+                &Rectangle {
+                    x: layer.x * scale + rect.x,
+                    y: layer.y * scale + rect.y,
+                    width: layer.width * scale,
+                    height: layer.height * scale,
+                },
+                ImageKind::PNG(layer.data.clone()),
+            )
+        }
+    }
+}
+
+pub(crate) fn render_image(
+    step: Step,
+    image: &NodeContentImage,
+    rect: &Rectangle,
+    svg_node: &usvg::Node,
+    font_db: &fontdb::Database,
+) {
+    if step <= image.shift_steps {
+        return;
+    }
+
+    match &image.loaded_image.data {
+        LoadedImageData::Png(data) => {
+            create_image_node(svg_node, rect, ImageKind::PNG(data.clone()))
+        }
+        LoadedImageData::Gif(data) => {
+            create_image_node(svg_node, rect, ImageKind::GIF(data.clone()))
+        }
+        LoadedImageData::Jpeg(data) => {
+            create_image_node(svg_node, rect, ImageKind::JPEG(data.clone()))
+        }
+        LoadedImageData::Svg(data) => create_image_node(
+            svg_node,
+            rect,
+            ImageKind::SVG(prepare_svg_tree_for_step(step, image, data, font_db)),
+        ),
+        LoadedImageData::Ora(data) => render_ora(
+            step,
+            image,
+            data,
+            svg_node,
+            rect,
+            image.loaded_image.width,
+            image.loaded_image.height,
+        ),
+    }
+}
