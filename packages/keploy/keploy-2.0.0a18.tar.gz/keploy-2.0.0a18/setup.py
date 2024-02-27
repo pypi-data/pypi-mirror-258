38d0f217d1efab9340c983c17aa5c8275235904a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-27 03:04:55 +0000

    Add package keploy/2.0.0a18/keploy-2.0.0a18.tar.gz

---
+++ packages/keploy/keploy-2.0.0a18.tar.gz/keploy-2.0.0a18/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+VERSION = '2.0.0-alpha18'
+DESCRIPTION = 'Keploy'
+LONG_DESCRIPTION = 'Keploy Python SDK'
+
+# Setting up
+setup(
+        name="keploy",
+        version=VERSION,
+        author="Keploy Inc.",
+        author_email="hello@keploy.io",
+        description="Run your unit tests with Keploy",
+        long_description="This module allows you to run your unit tests along with pytest and get coverage for the same using Keploy",
+        py_modules=['Keploy'],
+        install_requires=[], # add any additional packages that needs to be installed along with your package.
+
+        keywords=['keploy', 'python', 'sdk'],
+        classifiers= [
+            "Development Status :: 3 - Alpha",
+            "Intended Audience :: Education",
+            "Programming Language :: Python :: 2",
+            "Programming Language :: Python :: 3",
+            "Operating System :: MacOS :: MacOS X",
+            "Operating System :: Microsoft :: Windows",
+            "Operating System :: POSIX :: Linux",
+        ]
+)
