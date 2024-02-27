3ea53ca919a40845250bce03699eb3950e842389
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-27 15:05:28 +0000

    Add package adelie/1.1.12/adelie-1.1.12.tar.gz

---
+++ packages/adelie/adelie-1.1.12.tar.gz/adelie-1.1.12/adelie/__init__.py
@@ -0,0 +1,15 @@
+from . import adelie_core
+from . import bcd
+from . import data
+from . import diagnostic
+from . import glm
+from . import io
+from . import matrix
+from . import optimization
+from . import research
+from . import state
+from . import solver
+from .solver import (
+    gaussian_cov, 
+    grpnet,
+)
