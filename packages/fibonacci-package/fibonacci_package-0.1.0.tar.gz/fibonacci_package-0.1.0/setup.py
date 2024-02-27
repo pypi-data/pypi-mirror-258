c73f3f4f9d39047b53d0b4a4d0c74a1106d78ebc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-27 04:04:55 +0000

    Add package fibonacci-package/0.1.0/fibonacci_package-0.1.0.tar.gz

---
+++ packages/fibonacci-package/fibonacci_package-0.1.0.tar.gz/fibonacci_package-0.1.0/setup.py
@@ -0,0 +1,10 @@
+# fibonacci_package/setup.py
+
+from setuptools import setup
+
+setup(
+    name='fibonacci_package',
+    version='0.1.0',
+    packages=['fibonacci_package'],
+    install_requires=[],
+)
