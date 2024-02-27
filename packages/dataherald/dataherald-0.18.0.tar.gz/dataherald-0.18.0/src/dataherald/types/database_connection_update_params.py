10f073a2cfe1c5b443f6a5e383e41462769d5185
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-27 05:04:34 +0000

    Add package dataherald/0.18.0/dataherald-0.18.0.tar.gz

---
+++ packages/dataherald/dataherald-0.18.0.tar.gz/dataherald-0.18.0/src/dataherald/types/database_connection_update_params.py
@@ -0,0 +1,30 @@
+# File generated from our OpenAPI spec by Stainless.
+
+from __future__ import annotations
+
+from typing import Union
+from typing_extensions import Required, TypedDict
+
+__all__ = ["DatabaseConnectionUpdateParams", "SSHSettings"]
+
+
+class DatabaseConnectionUpdateParams(TypedDict, total=False):
+    alias: Required[str]
+
+    connection_uri: Required[str]
+
+    bigquery_credential_file_content: Union[object, str]
+
+    metadata: object
+
+    ssh_settings: SSHSettings
+
+    use_ssh: bool
+
+
+class SSHSettings(TypedDict, total=False):
+    host: str
+
+    password: str
+
+    username: str
