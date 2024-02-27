467f5a86cc5e0f0058c6245d56c65a70d75b083e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-27 06:05:16 +0000

    Add package recurtools/0.4.0/recurtools-0.4.0-py3-none-any.whl

---
+++ packages/recurtools/recurtools-0.4.0-py3-none-any.whl/recurtools/__init__.py
@@ -0,0 +1,12 @@
+from .utils import (  # noqa: D104
+    chainanything,
+    countrecursive,
+    flatten,
+    indexrecursive,
+    inrecursive,
+    lenrecursive,
+    nested,
+    sumrecursive,
+)
+
+__all__ = ["flatten", "lenrecursive", "sumrecursive", "countrecursive", "inrecursive", "indexrecursive", "chainanything", "nested"]  # noqa: E501
