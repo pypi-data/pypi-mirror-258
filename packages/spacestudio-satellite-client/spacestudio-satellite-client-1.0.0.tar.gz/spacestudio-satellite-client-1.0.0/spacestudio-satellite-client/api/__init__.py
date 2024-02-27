33389adc1e3416dd09b054a11a61bba4f0637f84
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-27 03:06:01 +0000

    Add package spacestudio-satellite-client/1.0.0/spacestudio-satellite-client-1.0.0.tar.gz

---
+++ packages/spacestudio-satellite-client/spacestudio-satellite-client-1.0.0.tar.gz/spacestudio-satellite-client-1.0.0/spacestudio-satellite-client/api/__init__.py
@@ -0,0 +1,13 @@
+# flake8: noqa
+
+# import apis into api package
+from spacestudio-satellite-client.api.announcement_api import AnnouncementApi
+from spacestudio-satellite-client.api.computations_api import ComputationsApi
+from spacestudio-satellite-client.api.notification_api import NotificationApi
+from spacestudio-satellite-client.api.orbits_api import OrbitsApi
+from spacestudio-satellite-client.api.platforms_api import PlatformsApi
+from spacestudio-satellite-client.api.propulsion_systems_api import PropulsionSystemsApi
+from spacestudio-satellite-client.api.scenarios_api import ScenariosApi
+from spacestudio-satellite-client.api.spacecraft_geometries_api import SpacecraftGeometriesApi
+from spacestudio-satellite-client.api.studies_api import StudiesApi
+
