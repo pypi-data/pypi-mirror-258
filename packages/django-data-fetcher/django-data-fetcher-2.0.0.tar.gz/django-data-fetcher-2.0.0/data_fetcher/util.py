334094be89228af6153ece8494cf8bbabc40ab52
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-27 14:05:17 +0000

    Add package django-data-fetcher/2.0.0/django-data-fetcher-2.0.0.tar.gz

---
+++ packages/django-data-fetcher/django-data-fetcher-2.0.0.tar.gz/django-data-fetcher-2.0.0/data_fetcher/util.py
@@ -0,0 +1,31 @@
+# from data_fetcher.middleware import get_request
+from .global_request_context import GlobalRequest, get_request
+
+
+class MissingRequestContextException(Exception):
+    pass
+
+
+def get_datafetcher_request_cache():
+    request = get_request()
+    if not request:
+        raise MissingRequestContextException(
+            "No request is available, don't use datafetchers outside of a request context"
+        )
+
+    if not hasattr(request, "datafetcher_cache"):
+        request.datafetcher_cache = {}
+
+    return request.datafetcher_cache
+
+
+def clear_datafetchers():
+    """
+    Clears all cached values for datafetchers
+
+    Only necessary when a request wants data it has modified
+    """
+    request = get_request()
+    if request and hasattr(request, "datafetcher_cache"):
+        # reset the cache to an empty dict
+        request.datafetcher_cache = {}
