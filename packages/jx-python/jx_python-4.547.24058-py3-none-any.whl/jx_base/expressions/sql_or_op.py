9923cbe4e91f30d49847664f6db4fdec0939d2ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-27 15:05:39 +0000

    Add package jx-python/4.547.24058/jx_python-4.547.24058-py3-none-any.whl

---
+++ packages/jx-python/jx_python-4.547.24058-py3-none-any.whl/jx_base/expressions/sql_or_op.py
@@ -0,0 +1,34 @@
+# encoding: utf-8
+#
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this file,
+# You can obtain one at http:# mozilla.org/MPL/2.0/.
+#
+# Contact: Kyle Lahnakoski (kyle@lahnakoski.com)
+#
+from jx_base.expressions.sql_is_null_op import SqlIsNullOp
+from jx_base.expressions.expression import Expression
+from jx_base.language import is_op
+from mo_json.types import JX_BOOLEAN
+
+
+class SqlOrOp(Expression):
+    _data_type = JX_BOOLEAN
+
+    def __init__(self, *terms):
+        Expression.__init__(self, *terms)
+        self.terms = terms
+
+    def __data__(self):
+        return {"sql.or": [t.__data__() for t in self.terms]}
+
+    def missing(self, lang):
+        return SqlIsNullOp(self)
+
+    def __eq__(self, other):
+        if not is_op(other, SqlOrOp):
+            return False
+        if len(self.terms) != len(other.terms):
+            return False
+        return all(t == o for t, o in zip(self.terms, other.terms))
