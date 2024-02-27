0a4b2585b3871b5845edba481713a3ab4432d69d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-27 05:04:27 +0000

    Add package lenvert-python/0.1.0/lenvert_python-0.1.0.tar.gz

---
+++ packages/lenvert-python/lenvert_python-0.1.0.tar.gz/lenvert_python-0.1.0/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    author= "Elias Dzobo",
+    description= "A package for converting imperial lengths and weights",
+    name= "lenvert_python",
+    version="0.1.0",
+    url = "https://github.com/elias-dzobo/lenvert", 
+    packages= find_packages(include=["lenvert", "lenvert.*"]),
+    install_requires=[
+        "numpy>=1.10",
+        "pandas"
+    ],
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+)
