d5bba03ea081516083e2f800a5034a5898085f52
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-27 15:05:27 +0000

    Add package futcli/0.1.2/futcli-0.1.2-py3-none-any.whl

---
+++ packages/futcli/futcli-0.1.2-py3-none-any.whl/futcli/__init__.py
@@ -0,0 +1,8 @@
+# futcli/__init__.py
+
+__title__ = "futcli"
+__version__ = "0.1.2"
+__author__ = "Edwin Amilasan Jr."
+__author_email__ = "ejamilasan@gmail.com"
+__license__ = "GNU GPL v3"
+__copyright__ = "Copyright 2024 Edwin Amilasan Jr."
