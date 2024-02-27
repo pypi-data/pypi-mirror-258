a4f5a2af88f0a3e546cec30a976c9d293cd0e683
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-27 06:04:49 +0000

    Add package damo/2.2.3/damo-2.2.3-py3-none-any.whl

---
+++ packages/damo/damo-2.2.3-py3-none-any.whl/damo/damo_profile.py
@@ -0,0 +1,11 @@
+# SPDX-License-Identifier: GPL-2.0
+
+'''
+Record data access pattern and stacktrace, using DAMON and ftrace (or, just
+perf).
+
+Provide a good visualization of the results, in PDF or on terminal.
+
+In other words, implement what we manually did for
+https://sjp38.github.io/post/damon_profile_callstack_example/
+'''
