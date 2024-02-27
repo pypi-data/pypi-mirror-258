839010a6aef4a61f7fa09fa009311985f59f8b1f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-27 05:04:39 +0000

    Add package citier/1.4.5/citier-1.4.5-py3-none-any.whl

---
+++ packages/citier/citier-1.4.5-py3-none-any.whl/citier/cities.py
@@ -0,0 +1,48 @@
+def city(country, state, city):
+    if country:
+        pass
+    elif state:
+        pass
+    elif city:
+        pass
+    return ''
+
+def state(country, state, city):
+    if country:
+        pass
+    elif state:
+        pass
+    elif city:
+        pass
+    return ''
+
+def country(country, state, city):
+    if country:
+        pass
+    elif state:
+        pass
+    elif city:
+        pass
+    return ''
+
+data = {
+  "pakistan": {
+    "punjab": {
+      "lahore": "lahore",
+      "islamabad": "islamabad",
+      "multan": "multan"
+    },
+    "sindh": {
+      "lahdore": "lahdore",
+      "isldamabad": "isldamabad",
+      "mdultan": "mdultan"
+    }
+  },
+  "india": {
+    "punjab": {
+      "lore": "lore",
+      "islabad": "islabad",
+      "mltan": "mltan"
+    }
+  }
+}
