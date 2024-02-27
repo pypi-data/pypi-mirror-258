07eed05efcb0c7f3ef65897d5c545d401e83849a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-27 14:05:15 +0000

    Add package pyggle/0.0.4.3/pyggle-0.0.4.3.tar.gz

---
+++ packages/pyggle/pyggle-0.0.4.3.tar.gz/pyggle-0.0.4.3/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+from os import path
+
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, "README.md"), "r", encoding="utf-8") as f:
+    long_description = f.read()
+
+setup(
+    name='pyggle',
+    version='0.0.4.3',
+    author='Andre Arcaina',
+    author_email='dtandre331@gmail.com',
+    description='Python-based package that allows you to solve any N x M Boggle board.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/andrearcaina/pyggle',
+    py_modules=['boggle'],
+    python_requires='>=3.6',
+    license='MIT',
+    packages=find_packages(),
+)
