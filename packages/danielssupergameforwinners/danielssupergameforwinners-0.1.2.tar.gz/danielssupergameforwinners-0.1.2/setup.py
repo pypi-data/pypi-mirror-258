1b454bebe8f1ee26058ef122a9f71377928d68bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-27 15:05:38 +0000

    Add package danielssupergameforwinners/0.1.2/danielssupergameforwinners-0.1.2.tar.gz

---
+++ packages/danielssupergameforwinners/danielssupergameforwinners-0.1.2.tar.gz/danielssupergameforwinners-0.1.2/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+setup(
+name='danielssupergameforwinners',
+version='0.1.2',
+author='Daniel Hutchings',
+author_email='thewakemaster@gmail.com',
+description='The bestest game in the whole world.',
+packages=find_packages(),
+classifiers=[
+'Programming Language :: Python :: 3',
+'License :: OSI Approved :: MIT License',
+'Operating System :: OS Independent',
+],
+python_requires='>=3.6',
+)
