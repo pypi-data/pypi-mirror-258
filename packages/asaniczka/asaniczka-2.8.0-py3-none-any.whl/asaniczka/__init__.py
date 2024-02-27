86bfdd381aff6b8005873d2d6d6ee82553123076
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-27 03:06:06 +0000

    Add package asaniczka/2.8.0/asaniczka-2.8.0-py3-none-any.whl

---
+++ packages/asaniczka/asaniczka-2.8.0-py3-none-any.whl/asaniczka/__init__.py
@@ -0,0 +1,16 @@
+"""Asaniczka module provides quick functions to get up and running with a scraper."""
+
+from asaniczka.main import (
+    ProjectSetup,
+    Timer,
+    sanitize_filename,
+    setup_logger,
+    save_file,
+    format_error,
+    get_request,
+    async_get_request,
+    post_request,
+    async_post_request,
+    save_ndjson,
+    create_dir,
+)
