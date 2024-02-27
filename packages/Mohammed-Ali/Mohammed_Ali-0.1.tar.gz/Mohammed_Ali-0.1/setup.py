d7300aa9ae72499c3d6a04657e2f0abbddcaff9e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-27 04:05:25 +0000

    Add package Mohammed-Ali/0.1/Mohammed_Ali-0.1.tar.gz

---
+++ packages/Mohammed-Ali/Mohammed_Ali-0.1.tar.gz/Mohammed_Ali-0.1/setup.py
@@ -0,0 +1,13 @@
+import setuptools
+setuptools.setup(
+    name = 'Mohammed_Ali',
+    version = '0.1',
+    author = 'Mohammed_Ali',
+    description = 'simple Python library for dragging files ',
+    packages = setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+        "License :: OSI Approved :: MIT License"
+    ]
+)
