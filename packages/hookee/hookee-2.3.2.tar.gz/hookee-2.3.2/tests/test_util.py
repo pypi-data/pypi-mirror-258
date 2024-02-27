f9b17f7a9d8c40fe1286607c7366eead3a3216c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-27 05:04:56 +0000

    Add package hookee/2.3.2/hookee-2.3.2.tar.gz

---
+++ packages/hookee/hookee-2.3.2.tar.gz/hookee-2.3.2/tests/test_util.py
@@ -0,0 +1,31 @@
+from hookee import HookeeManager
+from hookee import util
+from hookee.pluginmanager import PluginManager
+from tests.testcase import HookeeTestCase
+
+__author__ = "Alex Laird"
+__copyright__ = "Copyright 2020, Alex Laird"
+__version__ = "1.2.0"
+
+
+class TestUtil(HookeeTestCase):
+    def setUp(self):
+        super(TestUtil, self).setUp()
+
+        self.hookee_manager = HookeeManager()
+        self.plugin_manager = PluginManager(self.hookee_manager)
+        self.plugin = self.plugin_manager.get_plugin("request_url_info")
+
+    def test_get_functions(self):
+        # WHEN
+        funcs = util.get_functions(self.plugin.module)
+
+        # THEN
+        self.assertEqual(funcs, ["run", "setup"])
+
+    def get_args(self):
+        # WHEN
+        args = util.get_args(self.plugin.module.run)
+
+        # THEN
+        self.assertEqual(args, ["request"])
