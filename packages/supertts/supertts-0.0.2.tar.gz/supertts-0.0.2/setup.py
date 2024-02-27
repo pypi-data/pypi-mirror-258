6cded87b5f21cc0f8ec7749c8971df9799958a58
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-27 04:05:20 +0000

    Add package supertts/0.0.2/supertts-0.0.2.tar.gz

---
+++ packages/supertts/supertts-0.0.2.tar.gz/supertts-0.0.2/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='supertts',
+    version='0.0.2',
+    author='Aman Kumar',
+    author_email='2000.aman.sinha@gmail.com',
+    packages=find_packages(),
+    install_requires=[
+        'requests>=2.20',
+        'openai'
+    ],
+    description='A super TTS package supporting multiple providers.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+)
