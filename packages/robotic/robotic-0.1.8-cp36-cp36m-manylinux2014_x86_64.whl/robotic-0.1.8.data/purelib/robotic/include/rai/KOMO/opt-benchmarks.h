2211d2a276d524f48d90f2957391eb2d097ed927
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-27 14:05:20 +0000

    Add package robotic/0.1.8/robotic-0.1.8-cp36-cp36m-manylinux2014_x86_64.whl

---
+++ packages/robotic/robotic-0.1.8-cp36-cp36m-manylinux2014_x86_64.whl/robotic-0.1.8.data/purelib/robotic/include/rai/KOMO/opt-benchmarks.h
@@ -0,0 +1,46 @@
+#pragma once
+
+#include "komo.h"
+#include "skeleton.h"
+
+#include "../Optim/NLP.h"
+
+struct OptBench_InvKin_Simple {
+  OptBench_InvKin_Simple();
+  shared_ptr<KOMO> komo;
+  shared_ptr<NLP> nlp;
+};
+
+struct OptBench_InvKin_Endeff {
+  OptBench_InvKin_Endeff(const char* modelFile, bool unconstrained);
+  shared_ptr<NLP> get(){  return nlp;  }
+
+private:
+  unique_ptr<KOMO> komo;
+  shared_ptr<NLP> nlp;
+};
+
+struct OptBench_Skeleton {
+  void create(const char* modelFile, const rai::Skeleton& S, rai::ArgWord sequenceOrPath);
+  shared_ptr<NLP> get(){  CHECK(nlp, "need to create first"); return nlp;  }
+
+//private:
+  unique_ptr<KOMO> komo;
+  shared_ptr<NLP> nlp;
+};
+
+struct OptBench_Skeleton_Pick : OptBench_Skeleton {
+  rai::Skeleton S;
+  OptBench_Skeleton_Pick(rai::ArgWord sequenceOrPath);
+};
+
+struct OptBench_Skeleton_Handover : OptBench_Skeleton {
+  rai::Skeleton S;
+  OptBench_Skeleton_Handover(rai::ArgWord sequenceOrPath);
+};
+
+struct OptBench_Skeleton_StackAndBalance : OptBench_Skeleton {
+  rai::Skeleton S;
+  OptBench_Skeleton_StackAndBalance(rai::ArgWord sequenceOrPath);
+};
+
