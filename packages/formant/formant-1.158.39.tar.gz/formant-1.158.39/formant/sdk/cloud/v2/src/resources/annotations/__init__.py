0ef151fa95d757560ccc8576bf6d363f77860dac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-27 15:05:36 +0000

    Add package formant/1.158.39/formant-1.158.39.tar.gz

---
+++ packages/formant/formant-1.158.39.tar.gz/formant-1.158.39/formant/sdk/cloud/v2/src/resources/annotations/__init__.py
@@ -0,0 +1,5 @@
+
+from formant.sdk.cloud.v2.formant_admin_api_client.models import AnnotationFieldType, AnnotationMetadata, Annotation, ScopeFilter, UserParametersRolesItem, AnnotationFieldValuesRequestTags, AnnotationFieldValuesResponse, AnnotationField, AnnotationFieldValue, AnnotationType, ScopeFilterTypesItem, AnnotationFieldValuesRequest, AnnotationTemplateTags, SheetParameters, AnnotationTemplateListResponse, AnnotationTemplate, UserParameters, TagParameters, AnnotationTags, AnnotationStreamType, AnnotationFieldValueTags
+from formant.sdk.cloud.v2.formant_admin_api_client.api.annotation import annotation_controller_post
+from formant.sdk.cloud.v2.formant_admin_api_client.api.annotation_template import annotation_template_controller_list, annotation_template_controller_get_one
+from formant.sdk.cloud.v2.formant_admin_api_client.types import *
---
+++ packages/formant/formant-1.158.39.tar.gz/formant-1.158.39/formant/sdk/cloud/v2/src/resources/commands/__init__.py
@@ -0,0 +1,4 @@
+
+from formant.sdk.cloud.v2.formant_admin_api_client.models import CommandTags, CommandParameterMeta, CommandDeliveryEventStreamType, CommandQuery, ScopeFilter, CommandResponseTags, Command, CommandDeliveryEventType, CommandDeliveryEventTags, CommandRequest, CommandListResponse, ScopeFilterTypesItem, CommandParameter, CommandTemplate, CommandTemplateTags, CommandResponseStreamType, CommandResponse, CommandRequestListResponse, CommandTemplateParameterMeta, FileInfo, CommandProgress, CommandStreamType, CommandTemplateListResponse, CommandDeliveryEventMetadata, CommandDeliveryEvent
+from formant.sdk.cloud.v2.formant_admin_api_client.api.command import command_controller_post, command_controller_query
+from formant.sdk.cloud.v2.formant_admin_api_client.types import *
