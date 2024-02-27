1f7d59e8e0e702b6a516a990f6ffbf7a5a30893b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-27 15:05:26 +0000

    Add package onnxruntime-genai-cuda/0.0.0/onnxruntime-genai-cuda-0.0.0.tar.gz

---
+++ packages/onnxruntime-genai-cuda/onnxruntime-genai-cuda-0.0.0.tar.gz/onnxruntime-genai-cuda-0.0.0/README.md
@@ -0,0 +1,12 @@
+# Placeholder Python package
+
+## Instructions
+
+Install build and twine
+
+```
+python -m build
+twine upload dist/*
+```
+## Reference
+
---
+++ packages/onnxruntime-genai-cuda/onnxruntime-genai-cuda-0.0.0.tar.gz/onnxruntime-genai-cuda-0.0.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='onnxruntime-genai-cuda',
+      version='0.0.0',
+      description='Placeholder',
+      url='http://github.com/natke/pypi-package',
+      author='natke',
+      author_email='nakersha@microsoft.com',
+      license='MIT',
+      packages=['onnxruntime_genai_cuda'],
+      zip_safe=False)
