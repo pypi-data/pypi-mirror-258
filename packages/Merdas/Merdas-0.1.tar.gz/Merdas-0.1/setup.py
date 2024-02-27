3f6675db4170aa8993c788ed1cb80731fa4fe18c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-27 04:05:04 +0000

    Add package Merdas/0.1/Merdas-0.1.tar.gz

---
+++ packages/Merdas/Merdas-0.1.tar.gz/Merdas-0.1/setup.py
@@ -0,0 +1,13 @@
+import setuptools
+setuptools.setup(
+    name = 'Merdas',
+    version = '0.1',
+    author = 'Merdas',
+    description = 'Simple Python code to solve common law problems',
+    packages = setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+        "License :: OSI Approved :: MIT License"
+    ]
+)
