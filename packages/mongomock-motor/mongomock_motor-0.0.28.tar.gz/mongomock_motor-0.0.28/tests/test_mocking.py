70ff934e05f4a13c0753021dc27fac3b617e8649
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-27 03:05:09 +0000

    Add package mongomock-motor/0.0.28/mongomock_motor-0.0.28.tar.gz

---
+++ packages/mongomock-motor/mongomock_motor-0.0.28.tar.gz/mongomock_motor-0.0.28/tests/test_mocking.py
@@ -0,0 +1,49 @@
+from unittest.mock import patch
+
+import pytest
+from bson import ObjectId
+from pymongo.results import UpdateResult
+
+from mongomock_motor import AsyncMongoMockClient
+
+
+@pytest.mark.anyio
+async def sample_function(collection):
+    result = await collection.update_one(
+        filter={'_id': ObjectId()},
+        update={'$set': {'field': 'value'}},
+        upsert=True,
+    )
+
+    if result.acknowledged is False:
+        raise RuntimeError()
+
+
+def async_wrapper(value):
+    async def wrapper(*args, **kwargs):
+        return value
+
+    return wrapper
+
+
+@pytest.mark.anyio
+async def test_patch():
+    collection = AsyncMongoMockClient()['test']['test']
+
+    with patch(
+        'mongomock_motor.AsyncMongoMockCollection.update_one',
+        new=async_wrapper(UpdateResult({}, False)),
+    ):
+        with pytest.raises(RuntimeError):
+            await sample_function(collection)
+
+
+@pytest.mark.anyio
+async def test_patch_object():
+    collection = AsyncMongoMockClient()['test']['test']
+
+    with patch.object(
+        collection, 'update_one', new=async_wrapper(UpdateResult({}, False))
+    ):
+        with pytest.raises(RuntimeError):
+            await sample_function(collection)
