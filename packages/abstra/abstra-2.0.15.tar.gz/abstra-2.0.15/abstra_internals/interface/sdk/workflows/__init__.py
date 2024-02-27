c1534f960fa8f04ff879cb82d71c709638d346d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-27 05:04:41 +0000

    Add package abstra/2.0.15/abstra-2.0.15.tar.gz

---
+++ packages/abstra/abstra-2.0.15.tar.gz/abstra-2.0.15/abstra_internals/interface/sdk/workflows/__init__.py
@@ -0,0 +1,33 @@
+import json
+from typing import Optional
+from ....utils.json import to_json_serializable
+from ....execution.execution import get_current_execution_throwable
+from ....execution.stage_run_manager import UnsetStageRun
+
+
+def is_json_serializable(value):
+    try:
+        json.dumps(value)
+        return True
+    except TypeError:
+        return False
+
+
+def get_data(key: Optional[str] = None):
+    execution = get_current_execution_throwable()
+    stage_run = execution.stage_run
+    if not stage_run:
+        raise UnsetStageRun()
+    return stage_run.data.get(key) if key else stage_run.data
+
+
+def set_data(key, value):
+    value = to_json_serializable(value)
+    if not is_json_serializable(value):
+        raise TypeError(f"{key} is not JSON serializable")
+    execution = get_current_execution_throwable()
+    stage_run = execution.stage_run
+    if not stage_run:
+        raise UnsetStageRun()
+    stage_run.data[key] = value
+    execution.stage_run_manager.update_data(stage_run.id, stage_run.data)
