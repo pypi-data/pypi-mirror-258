9417557434c6a021ab36dc31edc21b390239306e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-27 15:05:18 +0000

    Add package weatherflow4py/0.2.2/weatherflow4py-0.2.2-py3-none-any.whl

---
+++ packages/weatherflow4py/weatherflow4py-0.2.2-py3-none-any.whl/weatherflow4py/models/unified.py
@@ -0,0 +1,14 @@
+from dataclasses import dataclass
+
+from weatherflow4py.models.forecast import WeatherData
+from weatherflow4py.models.observation import StationObservation
+from weatherflow4py.models.station import Station
+
+
+@dataclass
+class WeatherFlowData:
+    """Unified Weather Data Object.."""
+
+    weather: WeatherData
+    station: Station
+    observation: StationObservation
