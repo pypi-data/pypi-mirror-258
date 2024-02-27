971c82af5c3d9d7313f4ed69e160e5cb02130161
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-27 06:05:25 +0000

    Add package BabelViscoFDTD/1.0.5/BabelViscoFDTD-1.0.5.tar.gz

---
+++ packages/BabelViscoFDTD/BabelViscoFDTD-1.0.5.tar.gz/BabelViscoFDTD-1.0.5/pi_ocl/build/CMakeFiles/pi_ocl.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "C:/Users/SPichardo/Documents/GitHub/BabelViscoFDTD/pi_ocl/pi_ocl.c" "C:/Users/SPichardo/Documents/GitHub/BabelViscoFDTD/pi_ocl/build/CMakeFiles/pi_ocl.dir/pi_ocl.c.obj"
+  )
+set(CMAKE_C_COMPILER_ID "MSVC")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "C:/Program Files/NVIDIA GPU Computing Toolkit/CUDA/v11.2/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ packages/BabelViscoFDTD/BabelViscoFDTD-1.0.5.tar.gz/BabelViscoFDTD-1.0.5/pi_ocl/build/CMakeFiles/pi_ocl.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/pi_ocl.dir/pi_ocl.c.obj"
+  "pi_ocl.exe"
+  "pi_ocl.exe.manifest"
+  "pi_ocl.lib"
+  "pi_ocl.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/pi_ocl.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
